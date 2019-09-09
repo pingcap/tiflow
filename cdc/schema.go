a4307a5ad1afdaafd513d4cdd1b52d2b2e29c728
leoppro <zhaoyilin@pingcap.com>
2019-09-09 16:43:40 +0800

    copy schema from drainer (#5)

---
+++ cdc/schema.go
@@ -0,0 +1,461 @@
+// Copyright 2019 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cdc
+
+import (
+	"encoding/json"
+
+	"github.com/pingcap/errors"
+	"github.com/pingcap/log"
+	"github.com/pingcap/parser/model"
+	"github.com/pingcap/parser/mysql"
+	"go.uber.org/zap"
+)
+
+const implicitColName = "_tidb_rowid"
+const implicitColID = -1
+
+// Schema stores the source TiDB all schema infomations
+// schema infomations could be changed by drainer init and ddls appear
+type Schema struct {
+	tableIDToName  map[int64]TableName
+	schemaNameToID map[string]int64
+
+	schemas map[int64]*model.DBInfo
+	tables  map[int64]*model.TableInfo
+
+	truncateTableID map[int64]struct{}
+
+	schemaMetaVersion int64
+
+	hasImplicitCol bool
+
+	jobs                []*model.Job
+	version2SchemaTable map[int64]TableName
+	currentVersion      int64
+}
+
+// TableName specify a Schema name and Table name
+type TableName struct {
+	Schema string `toml:"db-name" json:"db-name"`
+	Table  string `toml:"tbl-name" json:"tbl-name"`
+}
+
+// NewSchema returns the Schema object
+func NewSchema(jobs []*model.Job, hasImplicitCol bool) (*Schema, error) {
+	s := &Schema{
+		hasImplicitCol:      hasImplicitCol,
+		version2SchemaTable: make(map[int64]TableName),
+		truncateTableID:     make(map[int64]struct{}),
+		jobs:                jobs,
+	}
+
+	s.tableIDToName = make(map[int64]TableName)
+	s.schemas = make(map[int64]*model.DBInfo)
+	s.schemaNameToID = make(map[string]int64)
+	s.tables = make(map[int64]*model.TableInfo)
+
+	return s, nil
+}
+
+func (s *Schema) String() string {
+	mp := map[string]interface{}{
+		"tableIDToName":  s.tableIDToName,
+		"schemaNameToID": s.schemaNameToID,
+		// "schemas":           s.schemas,
+		// "tables":            s.tables,
+		"schemaMetaVersion": s.schemaMetaVersion,
+		"hasImplicitCol":    s.hasImplicitCol,
+	}
+
+	data, _ := json.MarshalIndent(mp, "\t", "\t")
+
+	return string(data)
+}
+
+// SchemaMetaVersion returns the current schemaversion in drainer
+func (s *Schema) SchemaMetaVersion() int64 {
+	return s.schemaMetaVersion
+}
+
+// SchemaAndTableName returns the tableName by table id
+func (s *Schema) SchemaAndTableName(id int64) (string, string, bool) {
+	tn, ok := s.tableIDToName[id]
+	if !ok {
+		return "", "", false
+	}
+
+	return tn.Schema, tn.Table, true
+}
+
+// SchemaByID returns the DBInfo by schema id
+func (s *Schema) SchemaByID(id int64) (val *model.DBInfo, ok bool) {
+	val, ok = s.schemas[id]
+	return
+}
+
+// SchemaByTableID returns the schema ID by table ID
+func (s *Schema) SchemaByTableID(tableID int64) (*model.DBInfo, bool) {
+	tn, ok := s.tableIDToName[tableID]
+	if !ok {
+		return nil, false
+	}
+	schemaID, ok := s.schemaNameToID[tn.Schema]
+	if !ok {
+		return nil, false
+	}
+	return s.SchemaByID(schemaID)
+}
+
+// TableByID returns the TableInfo by table id
+func (s *Schema) TableByID(id int64) (val *model.TableInfo, ok bool) {
+	val, ok = s.tables[id]
+	return
+}
+
+// DropSchema deletes the given DBInfo
+func (s *Schema) DropSchema(id int64) (string, error) {
+	schema, ok := s.schemas[id]
+	if !ok {
+		return "", errors.NotFoundf("schema %d", id)
+	}
+
+	for _, table := range schema.Tables {
+		delete(s.tables, table.ID)
+		delete(s.tableIDToName, table.ID)
+	}
+
+	delete(s.schemas, id)
+	delete(s.schemaNameToID, schema.Name.O)
+
+	return schema.Name.O, nil
+}
+
+// CreateSchema adds new DBInfo
+func (s *Schema) CreateSchema(db *model.DBInfo) error {
+	if _, ok := s.schemas[db.ID]; ok {
+		return errors.AlreadyExistsf("schema %s(%d)", db.Name, db.ID)
+	}
+
+	s.schemas[db.ID] = db
+	s.schemaNameToID[db.Name.O] = db.ID
+
+	log.Debug("create schema failed, schema id", zap.String("name", db.Name.O), zap.Int64("id", db.ID))
+	return nil
+}
+
+// DropTable deletes the given TableInfo
+func (s *Schema) DropTable(id int64) (string, error) {
+	table, ok := s.tables[id]
+	if !ok {
+		return "", errors.NotFoundf("table %d", id)
+	}
+	err := s.removeTable(id)
+	if err != nil {
+		return "", errors.Trace(err)
+	}
+
+	delete(s.tables, id)
+	delete(s.tableIDToName, id)
+
+	log.Debug("drop table success", zap.String("name", table.Name.O), zap.Int64("id", id))
+	return table.Name.O, nil
+}
+
+// CreateTable creates new TableInfo
+func (s *Schema) CreateTable(schema *model.DBInfo, table *model.TableInfo) error {
+	_, ok := s.tables[table.ID]
+	if ok {
+		return errors.AlreadyExistsf("table %s.%s", schema.Name, table.Name)
+	}
+
+	if s.hasImplicitCol && !table.PKIsHandle {
+		addImplicitColumn(table)
+	}
+
+	schema.Tables = append(schema.Tables, table)
+	s.tables[table.ID] = table
+	s.tableIDToName[table.ID] = TableName{Schema: schema.Name.O, Table: table.Name.O}
+
+	log.Debug("create table success", zap.String("name", schema.Name.O+"."+table.Name.O), zap.Int64("id", table.ID))
+	return nil
+}
+
+// ReplaceTable replace the table by new tableInfo
+func (s *Schema) ReplaceTable(table *model.TableInfo) error {
+	_, ok := s.tables[table.ID]
+	if !ok {
+		return errors.NotFoundf("table %s(%d)", table.Name, table.ID)
+	}
+
+	if s.hasImplicitCol && !table.PKIsHandle {
+		addImplicitColumn(table)
+	}
+
+	s.tables[table.ID] = table
+
+	return nil
+}
+
+func (s *Schema) removeTable(tableID int64) error {
+	schema, ok := s.SchemaByTableID(tableID)
+	if !ok {
+		return errors.NotFoundf("table(%d)'s schema", tableID)
+	}
+
+	for i := range schema.Tables {
+		if schema.Tables[i].ID == tableID {
+			copy(schema.Tables[i:], schema.Tables[i+1:])
+			schema.Tables = schema.Tables[:len(schema.Tables)-1]
+			return nil
+		}
+	}
+	return nil
+}
+
+func (s *Schema) addJob(job *model.Job) {
+	if len(s.jobs) == 0 || s.jobs[len(s.jobs)-1].BinlogInfo.SchemaVersion < job.BinlogInfo.SchemaVersion {
+		s.jobs = append(s.jobs, job)
+	}
+}
+
+func (s *Schema) handlePreviousDDLJobIfNeed(version int64) error {
+	var i int
+	for i = 0; i < len(s.jobs); i++ {
+		if skipJob(s.jobs[i]) {
+			log.Debug("skip ddl job", zap.Stringer("job", s.jobs[i]))
+			continue
+		}
+
+		if s.jobs[i].BinlogInfo.SchemaVersion <= version {
+			if s.jobs[i].BinlogInfo.SchemaVersion <= s.currentVersion {
+				log.Warn("ddl job schema version is less than current version, skip this ddl job",
+					zap.Stringer("job", s.jobs[i]),
+					zap.Int64("currentVersion", s.currentVersion))
+				continue
+			}
+
+			_, _, _, err := s.handleDDL(s.jobs[i])
+			if err != nil {
+				return errors.Annotatef(err, "handle ddl job %v failed, the schema info: %s", s.jobs[i], s)
+			}
+		} else {
+			break
+		}
+	}
+
+	s.jobs = s.jobs[i:]
+
+	return nil
+}
+
+// handleDDL has four return values,
+// the first value[string]: the schema name
+// the second value[string]: the table name
+// the third value[string]: the sql that is corresponding to the job
+// the fourth value[error]: the handleDDL execution's err
+func (s *Schema) handleDDL(job *model.Job) (schemaName string, tableName string, sql string, err error) {
+	log.Debug("handle job: ", zap.Stringer("job", job))
+
+	if skipJob(job) {
+		return "", "", "", nil
+	}
+
+	sql = job.Query
+	if sql == "" {
+		return "", "", "", errors.Errorf("[ddl job sql miss]%+v", job)
+	}
+
+	switch job.Type {
+	case model.ActionCreateSchema:
+		// get the DBInfo from job rawArgs
+		schema := job.BinlogInfo.DBInfo
+
+		err := s.CreateSchema(schema)
+		if err != nil {
+			return "", "", "", errors.Trace(err)
+		}
+
+		s.version2SchemaTable[job.BinlogInfo.SchemaVersion] = TableName{Schema: schema.Name.O, Table: ""}
+		s.currentVersion = job.BinlogInfo.SchemaVersion
+		schemaName = schema.Name.O
+
+	case model.ActionDropSchema:
+		schemaName, err = s.DropSchema(job.SchemaID)
+		if err != nil {
+			return "", "", "", errors.Trace(err)
+		}
+
+		s.version2SchemaTable[job.BinlogInfo.SchemaVersion] = TableName{Schema: schemaName, Table: ""}
+		s.currentVersion = job.BinlogInfo.SchemaVersion
+
+	case model.ActionRenameTable:
+		// ignore schema doesn't support reanme ddl
+		_, ok := s.SchemaByTableID(job.TableID)
+		if !ok {
+			return "", "", "", errors.NotFoundf("table(%d) or it's schema", job.TableID)
+		}
+		// first drop the table
+		_, err := s.DropTable(job.TableID)
+		if err != nil {
+			return "", "", "", errors.Trace(err)
+		}
+		// create table
+		table := job.BinlogInfo.TableInfo
+		schema, ok := s.SchemaByID(job.SchemaID)
+		if !ok {
+			return "", "", "", errors.NotFoundf("schema %d", job.SchemaID)
+		}
+
+		err = s.CreateTable(schema, table)
+		if err != nil {
+			return "", "", "", errors.Trace(err)
+		}
+
+		s.version2SchemaTable[job.BinlogInfo.SchemaVersion] = TableName{Schema: schema.Name.O, Table: table.Name.O}
+		s.currentVersion = job.BinlogInfo.SchemaVersion
+		schemaName = schema.Name.O
+		tableName = table.Name.O
+
+	case model.ActionCreateTable, model.ActionCreateView, model.ActionRecoverTable:
+		table := job.BinlogInfo.TableInfo
+		if table == nil {
+			return "", "", "", errors.NotFoundf("table %d", job.TableID)
+		}
+
+		schema, ok := s.SchemaByID(job.SchemaID)
+		if !ok {
+			return "", "", "", errors.NotFoundf("schema %d", job.SchemaID)
+		}
+
+		err := s.CreateTable(schema, table)
+		if err != nil {
+			return "", "", "", errors.Trace(err)
+		}
+
+		s.version2SchemaTable[job.BinlogInfo.SchemaVersion] = TableName{Schema: schema.Name.O, Table: table.Name.O}
+		s.currentVersion = job.BinlogInfo.SchemaVersion
+		schemaName = schema.Name.O
+		tableName = table.Name.O
+
+	case model.ActionDropTable, model.ActionDropView:
+		schema, ok := s.SchemaByID(job.SchemaID)
+		if !ok {
+			return "", "", "", errors.NotFoundf("schema %d", job.SchemaID)
+		}
+
+		tableName, err = s.DropTable(job.TableID)
+		if err != nil {
+			return "", "", "", errors.Trace(err)
+		}
+
+		s.version2SchemaTable[job.BinlogInfo.SchemaVersion] = TableName{Schema: schema.Name.O, Table: tableName}
+		s.currentVersion = job.BinlogInfo.SchemaVersion
+		schemaName = schema.Name.O
+
+	case model.ActionTruncateTable:
+		schema, ok := s.SchemaByID(job.SchemaID)
+		if !ok {
+			return "", "", "", errors.NotFoundf("schema %d", job.SchemaID)
+		}
+
+		// job.TableID is the old table id, different from table.ID
+		_, err := s.DropTable(job.TableID)
+		if err != nil {
+			return "", "", "", errors.Trace(err)
+		}
+
+		table := job.BinlogInfo.TableInfo
+		if table == nil {
+			return "", "", "", errors.NotFoundf("table %d", job.TableID)
+		}
+
+		err = s.CreateTable(schema, table)
+		if err != nil {
+			return "", "", "", errors.Trace(err)
+		}
+
+		s.version2SchemaTable[job.BinlogInfo.SchemaVersion] = TableName{Schema: schema.Name.O, Table: table.Name.O}
+		s.currentVersion = job.BinlogInfo.SchemaVersion
+		schemaName = schema.Name.O
+		tableName = table.Name.O
+		s.truncateTableID[job.TableID] = struct{}{}
+
+	default:
+		binlogInfo := job.BinlogInfo
+		if binlogInfo == nil {
+			return "", "", "", errors.NotFoundf("table %d", job.TableID)
+		}
+		tbInfo := binlogInfo.TableInfo
+		if tbInfo == nil {
+			return "", "", "", errors.NotFoundf("table %d", job.TableID)
+		}
+
+		schema, ok := s.SchemaByID(job.SchemaID)
+		if !ok {
+			return "", "", "", errors.NotFoundf("schema %d", job.SchemaID)
+		}
+
+		err := s.ReplaceTable(tbInfo)
+		if err != nil {
+			return "", "", "", errors.Trace(err)
+		}
+
+		s.version2SchemaTable[job.BinlogInfo.SchemaVersion] = TableName{Schema: schema.Name.O, Table: tbInfo.Name.O}
+		s.currentVersion = job.BinlogInfo.SchemaVersion
+		schemaName = schema.Name.O
+		tableName = tbInfo.Name.O
+	}
+
+	return
+}
+
+// IsTruncateTableID returns true if the table id have been truncated by truncate table DDL
+func (s *Schema) IsTruncateTableID(id int64) bool {
+	_, ok := s.truncateTableID[id]
+	return ok
+}
+
+func (s *Schema) getSchemaTableAndDelete(version int64) (string, string, error) {
+	schemaTable, ok := s.version2SchemaTable[version]
+	if !ok {
+		return "", "", errors.NotFoundf("version: %d", version)
+	}
+	delete(s.version2SchemaTable, version)
+
+	return schemaTable.Schema, schemaTable.Table, nil
+}
+
+func addImplicitColumn(table *model.TableInfo) {
+	newColumn := &model.ColumnInfo{
+		ID:   implicitColID,
+		Name: model.NewCIStr(implicitColName),
+	}
+	newColumn.Tp = mysql.TypeInt24
+	table.Columns = append(table.Columns, newColumn)
+
+	newIndex := &model.IndexInfo{
+		Primary: true,
+		Columns: []*model.IndexColumn{{Name: model.NewCIStr(implicitColName)}},
+	}
+	table.Indices = []*model.IndexInfo{newIndex}
+}
+
+// TiDB write DDL Binlog for every DDL Job, we must ignore jobs that are cancelled or rollback
+// For older version TiDB, it write DDL Binlog in the txn that the state of job is changed to *synced*
+// Now, it write DDL Binlog in the txn that the state of job is changed to *done* (before change to *synced*)
+// At state *done*, it will be always and only changed to *synced*.
+func skipJob(job *model.Job) bool {
+	return !job.IsSynced() && !job.IsDone()
+}
---
+++ cdc/schema_test.go
@@ -0,0 +1,409 @@
+// Copyright 2019 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cdc
+
+import (
+	"fmt"
+	"testing"
+
+	. "github.com/pingcap/check"
+	"github.com/pingcap/errors"
+	"github.com/pingcap/parser/model"
+	"github.com/pingcap/parser/mysql"
+	"github.com/pingcap/tidb/types"
+)
+
+func Test(t *testing.T) { TestingT(t) }
+
+type schemaSuite struct{}
+
+var _ = Suite(&schemaSuite{})
+
+func (t *schemaSuite) TestSchema(c *C) {
+	var jobs []*model.Job
+	dbName := model.NewCIStr("Test")
+	// db and ignoreDB info
+	dbInfo := &model.DBInfo{
+		ID:    1,
+		Name:  dbName,
+		State: model.StatePublic,
+	}
+	// `createSchema` job
+	job := &model.Job{
+		ID:         3,
+		State:      model.JobStateSynced,
+		SchemaID:   1,
+		Type:       model.ActionCreateSchema,
+		BinlogInfo: &model.HistoryInfo{SchemaVersion: 1, DBInfo: dbInfo, FinishedTS: 123},
+		Query:      "create database test",
+	}
+	jobDup := &model.Job{
+		ID:         3,
+		State:      model.JobStateSynced,
+		SchemaID:   1,
+		Type:       model.ActionCreateSchema,
+		BinlogInfo: &model.HistoryInfo{SchemaVersion: 2, DBInfo: dbInfo, FinishedTS: 123},
+		Query:      "create database test",
+	}
+	jobs = append(jobs, job)
+
+	// construct a rollbackdone job
+	jobs = append(jobs, &model.Job{ID: 5, State: model.JobStateRollbackDone})
+
+	// reconstruct the local schema
+	schema, err := NewSchema(jobs, false)
+	c.Assert(err, IsNil)
+	err = schema.handlePreviousDDLJobIfNeed(2)
+	c.Assert(err, IsNil)
+
+	// test drop schema
+	jobs = append(
+		jobs,
+		&model.Job{
+			ID:         6,
+			State:      model.JobStateSynced,
+			SchemaID:   1,
+			Type:       model.ActionDropSchema,
+			BinlogInfo: &model.HistoryInfo{SchemaVersion: 3, FinishedTS: 123},
+			Query:      "drop database test",
+		},
+	)
+	schema, err = NewSchema(jobs, false)
+	c.Assert(err, IsNil)
+	err = schema.handlePreviousDDLJobIfNeed(3)
+	c.Assert(err, IsNil)
+
+	// test create schema already exist error
+	jobs = jobs[:0]
+	jobs = append(jobs, job)
+	jobs = append(jobs, jobDup)
+	schema, err = NewSchema(jobs, false)
+	c.Assert(err, IsNil)
+	err = schema.handlePreviousDDLJobIfNeed(2)
+	c.Log(err)
+	c.Assert(errors.IsAlreadyExists(err), IsTrue)
+
+	// test schema drop schema error
+	jobs = jobs[:0]
+	jobs = append(
+		jobs,
+		&model.Job{
+			ID:         9,
+			State:      model.JobStateSynced,
+			SchemaID:   1,
+			Type:       model.ActionDropSchema,
+			BinlogInfo: &model.HistoryInfo{SchemaVersion: 1, FinishedTS: 123},
+			Query:      "drop database test",
+		},
+	)
+	schema, err = NewSchema(jobs, false)
+	c.Assert(err, IsNil)
+	err = schema.handlePreviousDDLJobIfNeed(1)
+	c.Assert(errors.IsNotFound(err), IsTrue)
+}
+
+func (*schemaSuite) TestTable(c *C) {
+	var jobs []*model.Job
+	dbName := model.NewCIStr("Test")
+	tbName := model.NewCIStr("T")
+	colName := model.NewCIStr("A")
+	idxName := model.NewCIStr("idx")
+	// column info
+	colInfo := &model.ColumnInfo{
+		ID:        1,
+		Name:      colName,
+		Offset:    0,
+		FieldType: *types.NewFieldType(mysql.TypeLonglong),
+		State:     model.StatePublic,
+	}
+	// index info
+	idxInfo := &model.IndexInfo{
+		Name:  idxName,
+		Table: tbName,
+		Columns: []*model.IndexColumn{
+			{
+				Name:   colName,
+				Offset: 0,
+				Length: 10,
+			},
+		},
+		Unique:  true,
+		Primary: true,
+		State:   model.StatePublic,
+	}
+	// table info
+	tblInfo := &model.TableInfo{
+		ID:    2,
+		Name:  tbName,
+		State: model.StatePublic,
+	}
+	// db info
+	dbInfo := &model.DBInfo{
+		ID:    3,
+		Name:  dbName,
+		State: model.StatePublic,
+	}
+
+	// `createSchema` job
+	job := &model.Job{
+		ID:         5,
+		State:      model.JobStateSynced,
+		SchemaID:   3,
+		Type:       model.ActionCreateSchema,
+		BinlogInfo: &model.HistoryInfo{SchemaVersion: 1, DBInfo: dbInfo, FinishedTS: 123},
+		Query:      "create database " + dbName.O,
+	}
+	jobs = append(jobs, job)
+
+	// `createTable` job
+	job = &model.Job{
+		ID:         6,
+		State:      model.JobStateSynced,
+		SchemaID:   3,
+		TableID:    2,
+		Type:       model.ActionCreateTable,
+		BinlogInfo: &model.HistoryInfo{SchemaVersion: 2, TableInfo: tblInfo, FinishedTS: 123},
+		Query:      "create table " + tbName.O,
+	}
+	jobs = append(jobs, job)
+
+	// `addColumn` job
+	tblInfo.Columns = []*model.ColumnInfo{colInfo}
+	job = &model.Job{
+		ID:         7,
+		State:      model.JobStateSynced,
+		SchemaID:   3,
+		TableID:    2,
+		Type:       model.ActionAddColumn,
+		BinlogInfo: &model.HistoryInfo{SchemaVersion: 3, TableInfo: tblInfo, FinishedTS: 123},
+		Query:      "alter table " + tbName.O + " add column " + colName.O,
+	}
+	jobs = append(jobs, job)
+
+	// construct a historical `addIndex` job
+	tblInfo.Indices = []*model.IndexInfo{idxInfo}
+	job = &model.Job{
+		ID:         8,
+		State:      model.JobStateSynced,
+		SchemaID:   3,
+		TableID:    2,
+		Type:       model.ActionAddIndex,
+		BinlogInfo: &model.HistoryInfo{SchemaVersion: 4, TableInfo: tblInfo, FinishedTS: 123},
+		Query:      fmt.Sprintf("alter table %s add index %s(%s)", tbName, idxName, colName),
+	}
+	jobs = append(jobs, job)
+
+	// reconstruct the local schema
+	schema, err := NewSchema(jobs, false)
+	c.Assert(err, IsNil)
+	err = schema.handlePreviousDDLJobIfNeed(4)
+	c.Assert(err, IsNil)
+
+	// check the historical db that constructed above whether in the schema list of local schema
+	_, ok := schema.SchemaByID(dbInfo.ID)
+	c.Assert(ok, IsTrue)
+	// check the historical table that constructed above whether in the table list of local schema
+	table, ok := schema.TableByID(tblInfo.ID)
+	c.Assert(ok, IsTrue)
+	c.Assert(table.Columns, HasLen, 1)
+	c.Assert(table.Indices, HasLen, 1)
+	// check truncate table
+	tblInfo1 := &model.TableInfo{
+		ID:    9,
+		Name:  tbName,
+		State: model.StatePublic,
+	}
+	jobs = append(
+		jobs,
+		&model.Job{
+			ID:         9,
+			State:      model.JobStateSynced,
+			SchemaID:   3,
+			TableID:    2,
+			Type:       model.ActionTruncateTable,
+			BinlogInfo: &model.HistoryInfo{SchemaVersion: 5, TableInfo: tblInfo1, FinishedTS: 123},
+			Query:      "truncate table " + tbName.O,
+		},
+	)
+	schema1, err := NewSchema(jobs, false)
+	c.Assert(err, IsNil)
+	err = schema1.handlePreviousDDLJobIfNeed(5)
+	c.Assert(err, IsNil)
+	_, ok = schema1.TableByID(tblInfo1.ID)
+	c.Assert(ok, IsTrue)
+
+	_, ok = schema1.TableByID(2)
+	c.Assert(ok, IsFalse)
+	// check drop table
+	jobs = append(
+		jobs,
+		&model.Job{
+			ID:         9,
+			State:      model.JobStateSynced,
+			SchemaID:   3,
+			TableID:    9,
+			Type:       model.ActionDropTable,
+			BinlogInfo: &model.HistoryInfo{SchemaVersion: 6, FinishedTS: 123},
+			Query:      "drop table " + tbName.O,
+		},
+	)
+	schema2, err := NewSchema(jobs, false)
+	c.Assert(err, IsNil)
+	err = schema2.handlePreviousDDLJobIfNeed(6)
+	c.Assert(err, IsNil)
+
+	_, ok = schema2.TableByID(tblInfo.ID)
+	c.Assert(ok, IsFalse)
+	// test schemaAndTableName
+	_, _, ok = schema1.SchemaAndTableName(9)
+	c.Assert(ok, IsTrue)
+	// drop schema
+	_, err = schema1.DropSchema(3)
+	c.Assert(err, IsNil)
+	// test schema version
+	c.Assert(schema.SchemaMetaVersion(), Equals, int64(0))
+}
+
+func (t *schemaSuite) TestHandleDDL(c *C) {
+	schema, err := NewSchema(nil, false)
+	c.Assert(err, IsNil)
+	dbName := model.NewCIStr("Test")
+	colName := model.NewCIStr("A")
+	tbName := model.NewCIStr("T")
+	newTbName := model.NewCIStr("RT")
+
+	// check rollback done job
+	job := &model.Job{ID: 1, State: model.JobStateRollbackDone}
+	_, _, sql, err := schema.handleDDL(job)
+	c.Assert(err, IsNil)
+	c.Assert(sql, Equals, "")
+
+	// check job.Query is empty
+	job = &model.Job{ID: 1, State: model.JobStateDone}
+	_, _, sql, err = schema.handleDDL(job)
+	c.Assert(sql, Equals, "")
+	c.Assert(err, NotNil, Commentf("should return not found job.Query"))
+
+	// db info
+	dbInfo := &model.DBInfo{
+		ID:    2,
+		Name:  dbName,
+		State: model.StatePublic,
+	}
+	// table Info
+	tblInfo := &model.TableInfo{
+		ID:    6,
+		Name:  tbName,
+		State: model.StatePublic,
+	}
+	// column info
+	colInfo := &model.ColumnInfo{
+		ID:        8,
+		Name:      colName,
+		Offset:    0,
+		FieldType: *types.NewFieldType(mysql.TypeLonglong),
+		State:     model.StatePublic,
+	}
+	tblInfo.Columns = []*model.ColumnInfo{colInfo}
+
+	testCases := []struct {
+		name        string
+		jobID       int64
+		schemaID    int64
+		tableID     int64
+		jobType     model.ActionType
+		binlogInfo  *model.HistoryInfo
+		query       string
+		resultQuery string
+		schemaName  string
+		tableName   string
+	}{
+		{name: "createSchema", jobID: 3, schemaID: 2, tableID: 0, jobType: model.ActionCreateSchema, binlogInfo: &model.HistoryInfo{SchemaVersion: 1, DBInfo: dbInfo, TableInfo: nil, FinishedTS: 123}, query: "create database Test", resultQuery: "create database Test", schemaName: dbInfo.Name.O, tableName: ""},
+		{name: "createTable", jobID: 7, schemaID: 2, tableID: 6, jobType: model.ActionCreateTable, binlogInfo: &model.HistoryInfo{SchemaVersion: 3, DBInfo: nil, TableInfo: tblInfo, FinishedTS: 123}, query: "create table T(id int);", resultQuery: "create table T(id int);", schemaName: dbInfo.Name.O, tableName: tblInfo.Name.O},
+		{name: "addColumn", jobID: 9, schemaID: 2, tableID: 6, jobType: model.ActionAddColumn, binlogInfo: &model.HistoryInfo{SchemaVersion: 4, DBInfo: nil, TableInfo: tblInfo, FinishedTS: 123}, query: "alter table T add a varchar(45);", resultQuery: "alter table T add a varchar(45);", schemaName: dbInfo.Name.O, tableName: tblInfo.Name.O},
+		{name: "truncateTable", jobID: 10, schemaID: 2, tableID: 6, jobType: model.ActionTruncateTable, binlogInfo: &model.HistoryInfo{SchemaVersion: 5, DBInfo: nil, TableInfo: tblInfo, FinishedTS: 123}, query: "truncate table T;", resultQuery: "truncate table T;", schemaName: dbInfo.Name.O, tableName: tblInfo.Name.O},
+		{name: "renameTable", jobID: 11, schemaID: 2, tableID: 10, jobType: model.ActionRenameTable, binlogInfo: &model.HistoryInfo{SchemaVersion: 6, DBInfo: nil, TableInfo: tblInfo, FinishedTS: 123}, query: "rename table T to RT;", resultQuery: "rename table T to RT;", schemaName: dbInfo.Name.O, tableName: newTbName.O},
+		{name: "dropTable", jobID: 12, schemaID: 2, tableID: 12, jobType: model.ActionDropTable, binlogInfo: &model.HistoryInfo{SchemaVersion: 7, DBInfo: nil, TableInfo: nil, FinishedTS: 123}, query: "drop table RT;", resultQuery: "drop table RT;", schemaName: dbInfo.Name.O, tableName: newTbName.O},
+		{name: "dropSchema", jobID: 13, schemaID: 2, tableID: 0, jobType: model.ActionDropSchema, binlogInfo: &model.HistoryInfo{SchemaVersion: 8, DBInfo: nil, TableInfo: nil, FinishedTS: 123}, query: "drop database test;", resultQuery: "drop database test;", schemaName: dbInfo.Name.O, tableName: ""},
+	}
+
+	for _, testCase := range testCases {
+		// prepare for ddl
+		switch testCase.name {
+		case "addColumn":
+			tblInfo.Columns = []*model.ColumnInfo{colInfo}
+		case "truncateTable":
+			tblInfo.ID = 10
+		case "renameTable":
+			tblInfo.ID = 12
+			tblInfo.Name = newTbName
+		}
+
+		job = &model.Job{
+			ID:         testCase.jobID,
+			State:      model.JobStateDone,
+			SchemaID:   testCase.schemaID,
+			TableID:    testCase.tableID,
+			Type:       testCase.jobType,
+			BinlogInfo: testCase.binlogInfo,
+			Query:      testCase.query,
+		}
+		testDoDDLAndCheck(c, schema, job, false, testCase.resultQuery, testCase.schemaName, testCase.tableName)
+
+		// custom check after ddl
+		switch testCase.name {
+		case "createSchema":
+			_, ok := schema.SchemaByID(dbInfo.ID)
+			c.Assert(ok, IsTrue)
+		case "createTable":
+			_, ok := schema.TableByID(tblInfo.ID)
+			c.Assert(ok, IsTrue)
+		case "renameTable":
+			tb, ok := schema.TableByID(tblInfo.ID)
+			c.Assert(ok, IsTrue)
+			c.Assert(tblInfo.Name, Equals, tb.Name)
+		case "addColumn", "truncateTable":
+			tb, ok := schema.TableByID(tblInfo.ID)
+			c.Assert(ok, IsTrue)
+			c.Assert(tb.Columns, HasLen, 1)
+		case "dropTable":
+			_, ok := schema.TableByID(tblInfo.ID)
+			c.Assert(ok, IsFalse)
+		case "dropSchema":
+			_, ok := schema.SchemaByID(job.SchemaID)
+			c.Assert(ok, IsFalse)
+		}
+	}
+}
+
+func (t *schemaSuite) TestAddImplicitColumn(c *C) {
+	tbl := model.TableInfo{}
+
+	addImplicitColumn(&tbl)
+
+	c.Assert(tbl.Columns, HasLen, 1)
+	c.Assert(tbl.Columns[0].ID, Equals, int64(implicitColID))
+	c.Assert(tbl.Indices, HasLen, 1)
+	c.Assert(tbl.Indices[0].Primary, IsTrue)
+}
+
+func testDoDDLAndCheck(c *C, schema *Schema, job *model.Job, isErr bool, sql string, expectedSchema string, expectedTable string) {
+	schemaName, tableName, resSQL, err := schema.handleDDL(job)
+	c.Logf("handle: %s", job.Query)
+	c.Logf("result: %s, %s, %s, %v", schemaName, tableName, resSQL, err)
+	c.Assert(err != nil, Equals, isErr)
+	c.Assert(sql, Equals, resSQL)
+	c.Assert(schemaName, Equals, expectedSchema)
+	c.Assert(tableName, Equals, expectedTable)
+}
