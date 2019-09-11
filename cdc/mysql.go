fd921810a264ccd5f6ed1bcd976f4b3da7bc1a5e
satoru <satorulogic@gmail.com>
2019-09-11 15:37:47 +0800

    Add support for DML (#11)
    Add basic support for DML

--- cdc/mysql.go
+++ cdc/mysql.go
@@ -16,6 +16,15 @@ package cdc
 import (
 	"context"
 	"database/sql"
+	"fmt"
+	"strings"
+
+	"github.com/pingcap/tidb/table"
+
+	"github.com/pingcap/parser/mysql"
+	"github.com/pingcap/tidb/types"
+
+	"github.com/pingcap/errors"
 
 	"github.com/pingcap/parser/model"
 
@@ -34,9 +43,16 @@ type tableInspector interface {
 	Refresh(schema, table string)
 }
 
+// TableInfoGetter is used to get table info by table id of TiDB
+type TableInfoGetter interface {
+	TableByID(id int64) (info *model.TableInfo, ok bool)
+	GetTableIDByName(schema, table string) (int64, bool)
+}
+
 type mysqlSink struct {
 	db           *sql.DB
 	tblInspector tableInspector
+	infoGetter   TableInfoGetter
 }
 
 var _ Sink = &mysqlSink{}
@@ -49,8 +65,8 @@ func (s *mysqlSink) Emit(ctx context.Context, txn Txn) error {
 		}
 		return err
 	}
-	// TODO: Handle DML
-	return nil
+	// TODO: Add retry
+	return s.execDMLs(ctx, txn.DMLs)
 }
 
 func (s *mysqlSink) EmitResolvedTimestamp(ctx context.Context, encoder Encoder, resolved uint64) error {
@@ -104,4 +120,143 @@ func (s *mysqlSink) execDDL(ctx context.Context, ddl *DDL) error {
 
 	log.Info("Exec DDL succeeded", zap.String("sql", ddl.SQL))
 	return nil
+}
+
+func (s *mysqlSink) execDMLs(ctx context.Context, dmls []*DML) error {
+	tx, err := s.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	for _, dml := range dmls {
+		query, args, err := s.parseDML(dml)
+		if err != nil {
+			tx.Rollback()
+			return err
+		}
+		if _, err := tx.ExecContext(ctx, query, args...); err != nil {
+			tx.Rollback()
+			return err
+		}
+	}
+
+	if err = tx.Commit(); err != nil {
+		return err
+	}
+
+	log.Info("Exec DML succeeded", zap.Int("num of DMLs", len(dmls)))
+	return nil
+}
+
+func (s *mysqlSink) parseDML(dml *DML) (string, []interface{}, error) {
+	tblID, ok := s.infoGetter.GetTableIDByName(dml.Database, dml.Table)
+	if !ok {
+		return "", nil, fmt.Errorf("table not found: %s.%s", dml.Database, dml.Table)
+	}
+	tableInfo, ok := s.infoGetter.TableByID(tblID)
+	if !ok {
+		return "", nil, fmt.Errorf("no info found for table: %d", tblID)
+	}
+
+	info, err := s.tblInspector.Get(dml.Database, dml.Table)
+	if err != nil {
+		return "", nil, err
+	}
+	var builder strings.Builder
+	cols := "(" + buildColumnList(info.columns) + ")"
+	tblName := quoteSchema(dml.Database, dml.Table)
+	builder.WriteString("REPLACE INTO " + tblName + cols + " VALUES ")
+	builder.WriteString("(" + holderString(len(info.columns)) + ");")
+
+	formattedVals, err := formatColumnValues(tableInfo, dml.Values)
+	if err != nil {
+		return "", nil, err
+	}
+	args := make([]interface{}, 0, len(info.columns))
+	for _, name := range info.columns {
+		val, ok := formattedVals[name]
+		if !ok {
+			return "", nil, fmt.Errorf("missing value for column: %s", name)
+		}
+		args = append(args, val)
+	}
+
+	return builder.String(), args, nil
+}
+
+func formatColumnValues(table *model.TableInfo, colVals map[string]types.Datum) (map[string]interface{}, error) {
+	columns := writableColumns(table)
+
+	formatted := make(map[string]interface{}, len(columns))
+	for _, col := range columns {
+		val, ok := colVals[col.Name.O]
+		if !ok {
+			val = getDefaultOrZeroValue(col)
+		}
+
+		value, err := formatColVal(val, col.FieldType)
+		if err != nil {
+			return nil, errors.Trace(err)
+		}
+		formatted[col.Name.O] = value.GetValue()
+	}
+
+	return formatted, nil
+}
+
+// writableColumns returns all columns which can be written. This excludes
+// generated and non-public columns.
+func writableColumns(table *model.TableInfo) []*model.ColumnInfo {
+	cols := make([]*model.ColumnInfo, 0, len(table.Columns))
+	for _, col := range table.Columns {
+		if col.State == model.StatePublic && !col.IsGenerated() {
+			cols = append(cols, col)
+		}
+	}
+	return cols
+}
+
+func formatColVal(datum types.Datum, ft types.FieldType) (types.Datum, error) {
+	if datum.GetValue() == nil {
+		return datum, nil
+	}
+
+	switch ft.Tp {
+	case mysql.TypeDate, mysql.TypeDatetime, mysql.TypeNewDate, mysql.TypeTimestamp, mysql.TypeDuration, mysql.TypeDecimal, mysql.TypeNewDecimal, mysql.TypeJSON:
+		datum = types.NewDatum(fmt.Sprintf("%v", datum.GetValue()))
+	case mysql.TypeEnum:
+		datum = types.NewDatum(datum.GetMysqlEnum().Value)
+	case mysql.TypeSet:
+		datum = types.NewDatum(datum.GetMysqlSet().Value)
+	case mysql.TypeBit:
+		// Encode bits as integers to avoid pingcap/tidb#10988 (which also affects MySQL itself)
+		val, err := datum.GetBinaryLiteral().ToInt(nil)
+		if err != nil {
+			return types.Datum{}, err
+		}
+		datum = types.NewUintDatum(val)
+	}
+
+	return datum, nil
+}
+
+func getDefaultOrZeroValue(col *model.ColumnInfo) types.Datum {
+	// see https://github.com/pingcap/tidb/issues/9304
+	// must use null if TiDB not write the column value when default value is null
+	// and the value is null
+	if !mysql.HasNotNullFlag(col.Flag) {
+		return types.NewDatum(nil)
+	}
+
+	if col.GetDefaultValue() != nil {
+		return types.NewDatum(col.GetDefaultValue())
+	}
+
+	if col.Tp == mysql.TypeEnum {
+		// For enum type, if no default value and not null is set,
+		// the default value is the first element of the enum list
+		return types.NewDatum(col.FieldType.Elems[0])
+	}
+
+	return table.GetZeroValue(col)
 }
--- cdc/mysql_test.go
+++ cdc/mysql_test.go
@@ -16,18 +16,22 @@ package cdc
 import (
 	"context"
 
+	"github.com/pingcap/parser/mysql"
+
+	"github.com/pingcap/parser/model"
+	"github.com/pingcap/parser/types"
+
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/pingcap/check"
+	dbtypes "github.com/pingcap/tidb/types"
 )
 
 type EmitSuite struct{}
 
 var _ = check.Suite(&EmitSuite{})
 
-type dummyInspector struct{}
-
-func (dummyInspector) Get(schema, table string) (*tableInfo, error) {
-	return nil, nil
+type dummyInspector struct {
+	tableInspector
 }
 
 func (dummyInspector) Refresh(schema, table string) {
@@ -55,6 +59,86 @@ func (s EmitSuite) TestShouldExecDDL(c *check.C) {
 	mock.ExpectBegin()
 	mock.ExpectExec("USE `test`;").WillReturnResult(sqlmock.NewResult(1, 1))
 	mock.ExpectExec(txn.DDL.SQL).WillReturnResult(sqlmock.NewResult(1, 1))
+	mock.ExpectCommit()
+
+	// Execute
+	err = sink.Emit(context.Background(), txn)
+
+	// Validate
+	c.Assert(err, check.IsNil)
+}
+
+type tableHelper struct {
+	tableInspector
+	TableInfoGetter
+}
+
+func (h *tableHelper) Get(schema, table string) (*tableInfo, error) {
+	return &tableInfo{
+		columns: []string{"id", "name"},
+	}, nil
+}
+
+func (h *tableHelper) TableByID(id int64) (info *model.TableInfo, ok bool) {
+	return &model.TableInfo{
+		Columns: []*model.ColumnInfo{
+			{
+				Name:  model.CIStr{O: "id"},
+				State: model.StatePublic,
+				FieldType: types.FieldType{
+					Tp:      mysql.TypeLong,
+					Flen:    types.UnspecifiedLength,
+					Decimal: types.UnspecifiedLength,
+				},
+			},
+			{
+				Name:  model.CIStr{O: "name"},
+				State: model.StatePublic,
+				FieldType: types.FieldType{
+					Tp:      mysql.TypeString,
+					Flen:    types.UnspecifiedLength,
+					Decimal: types.UnspecifiedLength,
+				},
+			},
+		},
+	}, true
+}
+
+func (h *tableHelper) GetTableIDByName(schema, table string) (int64, bool) {
+	return 42, true
+}
+
+func (s EmitSuite) TestShouldExecDMLs(c *check.C) {
+	// Set up
+	db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+	c.Assert(err, check.IsNil)
+	defer db.Close()
+
+	helper := tableHelper{}
+	sink := mysqlSink{
+		db:           db,
+		tblInspector: &helper,
+		infoGetter:   &helper,
+	}
+
+	txn := Txn{
+		DMLs: []*DML{
+			{
+				Database: "test",
+				Table:    "user",
+				Tp:       InsertDMLType,
+				Values: map[string]dbtypes.Datum{
+					"id":   dbtypes.NewDatum(42),
+					"name": dbtypes.NewDatum("tester1"),
+				},
+			},
+		},
+	}
+
+	mock.ExpectBegin()
+	mock.ExpectExec("REPLACE INTO `test`.`user`(`id`,`name`) VALUES (?,?);").
+		WithArgs(42, "tester1").
+		WillReturnResult(sqlmock.NewResult(1, 1))
 	mock.ExpectCommit()
 
 	// Execute
