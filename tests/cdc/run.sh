a1d72dcb5d138d9bc89f191e8a7b7f5731d9b38c
leoppro <zhaoyilin@pingcap.com>
2019-09-25 09:54:34 +0800

    fix tests

--- tests/cdc/run.sh
+++ tests/cdc/run.sh
@@ -4,10 +4,6 @@ set -e
 
 cd "$(dirname "$0")"
 
-echo "Run binlog test"
+echo "Run cdc test"
 
-GO111MODULE=on go build -o out
-
-./out -config ./config.toml > ${OUT_DIR-/tmp}/$TEST_NAME.out 2>&1
-
-
+GO111MODULE=on go run cdc.go -config ./config.toml > ${OUT_DIR-/tmp}/$TEST_NAME.out 2>&1
