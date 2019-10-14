f89ffd36ab945586c7bdc3ff1f859a1acc3a6a43
satoru <satorulogic@gmail.com>
2019-10-14 14:40:35 +0800

    Make buffer an alias of `chan BufferEntry`

--- cdc/buffer.go
+++ cdc/buffer.go
@@ -2,6 +2,7 @@ package cdc
 
 import (
 	"context"
+
 	"github.com/pingcap/tidb-cdc/cdc/kv"
 	"github.com/pingcap/tidb-cdc/cdc/util"
 )
@@ -22,39 +23,35 @@ func (e *BufferEntry) GetValue() interface{} {
 	}
 }
 
-// Buffer buffer kv entry
-type Buffer struct {
-	entriesCh chan BufferEntry
+// Buffer buffers kv entries
+type Buffer chan BufferEntry
+
+func MakeBuffer() Buffer {
+	return make(Buffer)
 }
 
-func MakeBuffer() *Buffer {
-	return &Buffer{
-		entriesCh: make(chan BufferEntry),
-	}
-}
-
-func (b *Buffer) AddEntry(ctx context.Context, entry BufferEntry) error {
+func (b Buffer) AddEntry(ctx context.Context, entry BufferEntry) error {
 	select {
 	case <-ctx.Done():
 		return ctx.Err()
-	case b.entriesCh <- entry:
+	case b <- entry:
 		return nil
 	}
 }
 
-func (b *Buffer) AddKVEntry(ctx context.Context, kv *kv.RawKVEntry) error {
+func (b Buffer) AddKVEntry(ctx context.Context, kv *kv.RawKVEntry) error {
 	return b.AddEntry(ctx, BufferEntry{KV: kv})
 }
 
-func (b *Buffer) AddResolved(ctx context.Context, span util.Span, ts uint64) error {
+func (b Buffer) AddResolved(ctx context.Context, span util.Span, ts uint64) error {
 	return b.AddEntry(ctx, BufferEntry{Resolved: &ResolvedSpan{Span: span, Timestamp: ts}})
 }
 
-func (b *Buffer) Get(ctx context.Context) (BufferEntry, error) {
+func (b Buffer) Get(ctx context.Context) (BufferEntry, error) {
 	select {
 	case <-ctx.Done():
 		return BufferEntry{}, ctx.Err()
-	case e := <-b.entriesCh:
+	case e := <-b:
 		return e, nil
 	}
 }
