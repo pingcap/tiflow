032a8cef09034bc41b2e00e8eb5c545791755c6d
Jiahao Huang <july2993@gmail.com>
2019-10-21 14:41:12 +0800

    tests/: Add a simple runnable integration test.

--- tests/run.sh
+++ tests/run.sh
@@ -15,10 +15,7 @@ export PATH=$PATH:$(dirname $pwd)/bin
 
 
 clean_data() {
-    rm -rf $OUT_DIR/pd || true
-    rm -rf $OUT_DIR/down_pd || true
-    rm -rf $OUT_DIR/tikv || true
-    rm -rf $OUT_DIR/down_tikv || true
+    rm -rf $OUT_DIR/* || true
 }
 
 stop_services() {
@@ -56,6 +53,7 @@ start_services() {
         sleep 1
     done
 
+# TODO set it split region normally.
     # Tries to limit the max number of open files under the system limit
     cat - > "$OUT_DIR/tikv-config.toml" <<EOF
 [rocksdb]
@@ -65,6 +63,36 @@ max-open-files = 4096
 [raftstore]
 # true (default value) for high reliability, this can prevent data loss when power failure.
 sync-log = false
+
+# make tikv don't split now
+[coprocessor]
+## When it is set to `true`, TiKV will try to split a Region with table prefix if that Region
+## crosses tables.
+## It is recommended to turn off this option if there will be a large number of tables created.
+split-region-on-table = false
+
+## One split check produces several split keys in batch. This config limits the number of produced
+## split keys in one batch.
+# batch-split-limit = 10
+
+## When Region [a,e) size exceeds `region_max_size`, it will be split into several Regions [a,b),
+## [b,c), [c,d), [d,e) and the size of [a,b), [b,c), [c,d) will be `region_split_size` (or a
+## little larger).
+region-max-size = "100000MB"
+region-split-size = "100000MB"
+
+## When the number of keys in Region [a,e) exceeds the `region_max_keys`, it will be split into
+## several Regions [a,b), [b,c), [c,d), [d,e) and the number of keys in [a,b), [b,c), [c,d) will be
+## `region_split_keys`.
+region-max-keys = 100000000
+region-split-keys = 100000000
+EOF
+
+# tidb server config file
+    cat - > "$OUT_DIR/tidb-config.toml" <<EOF
+# When create table, split a separated region for it. It is recommended to
+# turn off this option if there will be a large number of tables created.
+split-table = false
 EOF
 
     echo "Starting TiKV..."
@@ -88,6 +116,7 @@ EOF
     echo "Starting TiDB..."
     tidb-server \
         -P 4000 \
+        -config "$OUT_DIR/tidb-config.toml" \
         --store tikv \
         --path 127.0.0.1:2379 \
         --log-file "$OUT_DIR/tidb.log" &
@@ -106,6 +135,7 @@ EOF
     echo "Starting Downstream TiDB..."
     tidb-server \
         -P 3306 \
+        -config "$OUT_DIR/tidb-config.toml" \
         --store tikv \
         --path 127.0.0.1:2381 \
         --status=20080 \
@@ -123,7 +153,7 @@ EOF
     done
 
     echo "Starting CDC..."
-    cdc &
+    cdc --log-file "$OUT_DIR/cdc.log" &
     sleep 1
 }
 
