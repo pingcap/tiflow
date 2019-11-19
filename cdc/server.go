1b2f0efbde4f26ee189ac1422ffc8567ccbca2b0
amyangfei <amyangfei@gmail.com>
2019-11-19 12:07:01 +0800

    *: register tikv store once in cdc server

--- cdc/server.go
+++ cdc/server.go
@@ -5,7 +5,10 @@ import (
 	"net/http"
 	"strings"
 
+	"github.com/pingcap/errors"
 	"github.com/pingcap/log"
+	"github.com/pingcap/tidb/store"
+	"github.com/pingcap/tidb/store/tikv"
 	"go.uber.org/zap"
 )
 
@@ -66,6 +69,10 @@ func NewServer(opt ...ServerOption) (*Server, error) {
 	capture, err := NewCapture(strings.Split(opts.pdEndpoints, ","))
 	if err != nil {
 		return nil, err
+	}
+	err = store.Register("tikv", tikv.Driver{})
+	if err != nil {
+		return nil, errors.Trace(err)
 	}
 
 	s := &Server{
