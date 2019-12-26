32484dda595c5c539d125f878d2554cdb16af914
leoppro <zhaoyilin@pingcap.com>
2019-12-26 19:09:23 +0800

    fix ci

--- tests/cdc/run.sh
+++ tests/cdc/run.sh
@@ -24,9 +24,10 @@ function prepare() {
 }
 
 trap stop_tidb_cluster EXIT
-prepare $*
+# prepare $*
 
 cd "$(dirname "$0")"
+set -o pipefail
 GO111MODULE=on go run cdc.go -config ./config.toml 2>&1 | tee $WORK_DIR/tester.log
 cleanup_process $CDC_BINARY
 echo "[$(date)] <<<<<< run test case $TEST_NAME success! >>>>>>"
