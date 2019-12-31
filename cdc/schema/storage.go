88acf62a21b786b9185c052a81574ad00ca63268
satoru <satorulogic@gmail.com>
2019-12-31 10:25:15 +0800

    Make sure the primary key is placed first

--- cdc/schema/storage.go
+++ cdc/schema/storage.go
@@ -117,14 +117,21 @@ func (ti *TableInfo) WritableColumns() []*model.ColumnInfo {
 // GetUniqueKeys returns all unique keys of the table as a slice of column names
 func (ti *TableInfo) GetUniqueKeys() [][]string {
 	var uniqueKeys [][]string
-	for _, idx := range ti.Indices {
+	pkPos := -1
+	for i, idx := range ti.Indices {
 		if idx.Primary || idx.Unique {
 			colNames := make([]string, 0, len(idx.Columns))
 			for _, col := range idx.Columns {
 				colNames = append(colNames, col.Name.O)
 			}
+			if idx.Primary {
+				pkPos = i
+			}
 			uniqueKeys = append(uniqueKeys, colNames)
 		}
+	}
+	if pkPos > 0 && len(uniqueKeys) > 1 {
+		uniqueKeys[0], uniqueKeys[pkPos] = uniqueKeys[pkPos], uniqueKeys[0]
 	}
 	if ti.PKIsHandle {
 		for _, col := range ti.Columns {
