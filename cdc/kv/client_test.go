3e755d9f45ef68204dc6222e9397e16af2a5e708
leoppro <zhaoyilin@pingcap.com>
2020-01-09 16:20:57 +0800

    kvclient: fix data race about checkpoint ts (#233)

--- cdc/kv/client_test.go
+++ cdc/kv/client_test.go
@@ -1,6 +1,8 @@
 package kv
 
 import (
+	"math/rand"
+	"sync"
 	"testing"
 
 	"github.com/pingcap/check"
@@ -23,4 +25,37 @@ func (s *clientSuite) TestNewClose(c *check.C) {
 
 	err = cli.Close()
 	c.Assert(err, check.IsNil)
+}
+
+func (s *clientSuite) TestUpdateCheckpointTS(c *check.C) {
+	var checkpointTS uint64
+	var g sync.WaitGroup
+	maxValueCh := make(chan uint64, 64)
+	update := func() uint64 {
+		var maxValue uint64
+		for i := 0; i < 1024; i++ {
+			value := rand.Uint64()
+			if value > maxValue {
+				maxValue = value
+			}
+			updateCheckpointTS(&checkpointTS, value)
+		}
+		return maxValue
+	}
+	for i := 0; i < 64; i++ {
+		g.Add(1)
+		go func() {
+			maxValueCh <- update()
+			g.Done()
+		}()
+	}
+	g.Wait()
+	close(maxValueCh)
+	var maxValue uint64
+	for v := range maxValueCh {
+		if maxValue < v {
+			maxValue = v
+		}
+	}
+	c.Assert(checkpointTS, check.Equals, maxValue)
 }
