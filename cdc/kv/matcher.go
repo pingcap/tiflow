db2a895e7d7925d148016dd2107dff3674547d8e
leoppro <zhaoyilin@pingcap.com>
2020-01-10 17:04:00 +0800

    fix

---
+++ cdc/kv/matcher.go
@@ -0,0 +1,44 @@
+package kv
+
+import (
+	"github.com/pingcap/errors"
+	"github.com/pingcap/kvproto/pkg/cdcpb"
+)
+
+type matcher struct {
+	// TODO : clear the single prewrite
+	unmatchedValue map[matchKey][]byte
+}
+
+type matchKey struct {
+	startTs uint64
+	key     string
+}
+
+func newMatchKey(row *cdcpb.Event_Row) matchKey {
+	return matchKey{startTs: row.GetStartTs(), key: string(row.GetKey())}
+}
+
+func newMatcher() *matcher {
+	return &matcher{
+		unmatchedValue: make(map[matchKey][]byte),
+	}
+}
+
+func (m *matcher) putPrewriteRow(row *cdcpb.Event_Row) {
+	m.unmatchedValue[newMatchKey(row)] = row.GetValue()
+}
+
+func (m *matcher) matchRow(row *cdcpb.Event_Row) error {
+	if value, exist := m.unmatchedValue[newMatchKey(row)]; exist {
+		row.Value = value
+		delete(m.unmatchedValue, newMatchKey(row))
+		return nil
+	}
+	return errors.NotFoundf("prewrite row, startTs:%d", row.GetStartTs())
+
+}
+
+func (m *matcher) rollbackRow(row *cdcpb.Event_Row) {
+	delete(m.unmatchedValue, newMatchKey(row))
+}
