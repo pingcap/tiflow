08d9deb2f1e9674f6f9114605d21df4da7d5c85f
leoppro <zhaoyilin@pingcap.com>
2020-02-06 14:40:45 +0800

    bugfix: filter the job which is not synced or done (#248)

--- cdc/processor.go
+++ cdc/processor.go
@@ -707,15 +707,20 @@ func createSchemaStore(pdEndpoints []string) (*schema.Storage, error) {
 	if err != nil {
 		return nil, err
 	}
-	jobs, err := kv.LoadHistoryDDLJobs(kvStore)
+	originalJobs, err := kv.LoadHistoryDDLJobs(kvStore)
+	jobs := make([]*timodel.Job, 0, len(originalJobs))
 	if err != nil {
 		return nil, err
 	}
-	for _, job := range jobs {
+	for _, job := range originalJobs {
+		if job.State != timodel.JobStateSynced && job.State != timodel.JobStateDone {
+			continue
+		}
 		err := resetFinishedTs(kvStore.(tikv.Storage), job)
 		if err != nil {
 			return nil, errors.Trace(err)
 		}
+		jobs = append(jobs, job)
 	}
 	schemaStorage, err := schema.NewStorage(jobs)
 	if err != nil {
