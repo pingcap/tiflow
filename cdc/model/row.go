c867149fd15406163745d74eac568327ef865117
leoppro <zhaoyilin@pingcap.com>
2020-02-24 09:38:47 +0800

    update

---
+++ cdc/model/row.go
@@ -0,0 +1,27 @@
+package model
+
+type RowChangedEvent struct {
+	Ts     uint64
+	Schema string
+	Table  string
+	Update map[string]Column
+	Delete map[string]Column
+}
+
+type Column struct {
+	Name   string
+	Type   byte
+	Unique bool
+	Value  interface{}
+}
+
+type DDLEvent struct {
+	Ts     uint64
+	Schema string
+	Table  string
+	Query  string
+}
+
+type ResolvedEvent struct {
+	Ts uint64
+}
