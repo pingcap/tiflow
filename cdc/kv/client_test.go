262ee673fc9ed0d6d8be71e9b0dcc90b29b770b7
amyangfei <amyangfei@gmail.com>
2020-03-03 14:25:48 +0800

    kv-client: add gRPC conn array (#289)

--- cdc/kv/client_test.go
+++ cdc/kv/client_test.go
@@ -1,6 +1,7 @@
 package kv
 
 import (
+	"context"
 	"math/rand"
 	"sync"
 	"testing"
@@ -58,4 +59,23 @@ func (s *clientSuite) TestUpdateCheckpointTS(c *check.C) {
 		}
 	}
 	c.Assert(checkpointTS, check.Equals, maxValue)
+}
+
+// Use etcdSuite for some special reasons, the embed etcd uses zap as the only candidate
+// logger and in the logger initializtion it also initializes the grpclog/loggerv2, which
+// is not a thread-safe operation and it must be called before any gRPC functions
+// ref: https://github.com/grpc/grpc-go/blob/master/grpclog/loggerv2.go#L67-L72
+func (s *etcdSuite) TestConnArray(c *check.C) {
+	addr := "127.0.0.1:2379"
+	ca, err := newConnArray(context.TODO(), 2, addr)
+	c.Assert(err, check.IsNil)
+
+	conn1 := ca.Get()
+	conn2 := ca.Get()
+	c.Assert(conn1, check.Not(check.Equals), conn2)
+
+	conn3 := ca.Get()
+	c.Assert(conn1, check.Equals, conn3)
+
+	ca.Close()
 }
