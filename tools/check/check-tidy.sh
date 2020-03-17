0a447d5e8aba0d11dabbd9d06bed2bcb068c2bb6
leoppro <zhaoyilin@pingcap.com>
2020-03-17 18:41:50 +0800

    update

--- tools/check/check-tidy.sh
+++ tools/check/check-tidy.sh
@@ -5,8 +5,6 @@ GO111MODULE=on go mod tidy
 
 if [ `git --no-pager diff | wc -c` -ne 0 ]; then
   echo "Please run \`go mod tidy\` to clean up"
-  git diff-index HEAD --
-  git status
   git --no-pager diff
   exit 1
 fi
