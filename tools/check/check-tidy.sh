abe5d85ff21032f466184d5934e0054dbcaf207f
leoppro <zhaoyilin@pingcap.com>
2020-03-17 18:41:05 +0800

    update

--- tools/check/check-tidy.sh
+++ tools/check/check-tidy.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 GO111MODULE=on go mod tidy
 
-if ! git diff-index --quiet HEAD --; then
+if [ `git --no-pager diff | wc -c` -ne 0 ]; then
   echo "Please run \`go mod tidy\` to clean up"
   git diff-index HEAD --
   git status
