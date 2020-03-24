6b5a38a6ce645b4c92e55e4befb8169576f8ee79
leoppro <zhaoyilin@pingcap.com>
2020-03-24 15:19:30 +0800

    add debug code

--- cdc/owner_operator.go
+++ cdc/owner_operator.go
@@ -17,6 +17,9 @@ import (
 	"context"
 	"sync"
 
+	"github.com/pingcap/log"
+	"go.uber.org/zap"
+
 	"github.com/pingcap/errors"
 	timodel "github.com/pingcap/parser/model"
 	pd "github.com/pingcap/pd/client"
@@ -85,6 +88,7 @@ func (h *ddlHandler) receiveDDL(rawDDL *model.RawKVEntry) error {
 	if err != nil {
 		return errors.Trace(err)
 	}
+	log.Warn("owner listen origin ddl", zap.Reflect("job", job))
 	if job == nil || entry.SkipJob(job) {
 		return nil
 	}
