13dcc252451920e7457045228b312ae10f449c3a
leoppro <zhaoyilin@pingcap.com>
2020-04-10 09:22:13 +0800

    update case

--- tests/ddl_sequence/data/prepare.sql
+++ tests/ddl_sequence/data/prepare.sql
@@ -6,7 +6,7 @@ CREATE TABLE many_cols1 (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	val INT DEFAULT 0,
 	col0 INT NOT NULL
-)
+);
 ALTER TABLE many_cols1 DROP COLUMN col0;
 INSERT INTO many_cols1 (val) VALUES (1);
 DROP TABLE many_cols1;
@@ -15,7 +15,7 @@ CREATE TABLE many_cols1 (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	val INT DEFAULT 0,
 	col0 INT NOT NULL
-)
+);
 ALTER TABLE many_cols1 DROP COLUMN col0;
 INSERT INTO many_cols1 (val) VALUES (1);
 DROP TABLE many_cols1;
@@ -24,7 +24,7 @@ CREATE TABLE many_cols1 (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	val INT DEFAULT 0,
 	col0 INT NOT NULL
-)
+);
 ALTER TABLE many_cols1 DROP COLUMN col0;
 INSERT INTO many_cols1 (val) VALUES (1);
 DROP TABLE many_cols1;
@@ -33,7 +33,7 @@ CREATE TABLE many_cols1 (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	val INT DEFAULT 0,
 	col0 INT NOT NULL
-)
+);
 ALTER TABLE many_cols1 DROP COLUMN col0;
 INSERT INTO many_cols1 (val) VALUES (1);
 DROP TABLE many_cols1;
@@ -42,7 +42,7 @@ CREATE TABLE many_cols1 (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	val INT DEFAULT 0,
 	col0 INT NOT NULL
-)
+);
 ALTER TABLE many_cols1 DROP COLUMN col0;
 INSERT INTO many_cols1 (val) VALUES (1);
 DROP TABLE many_cols1;
