948879d08a821255bff4798e84f3b2faa1fde2b7
leoppro <zhaoyilin@pingcap.com>
2020-04-10 13:03:41 +0800

    kv client: ignore prewrite event if value is empty (#446)

--- cdc/kv/matcher.go
+++ cdc/kv/matcher.go
@@ -26,7 +26,14 @@ func newMatcher() *matcher {
 }
 
 func (m *matcher) putPrewriteRow(row *cdcpb.Event_Row) {
-	m.unmatchedValue[newMatchKey(row)] = row.GetValue()
+	key := newMatchKey(row)
+	value := row.GetValue()
+	// tikv may send a prewrite event with empty value
+	// here we need to avoid the invalid prewrite event overwrite the value
+	if _, exist := m.unmatchedValue[key]; exist && len(value) == 0 {
+		return
+	}
+	m.unmatchedValue[key] = value
 }
 
 func (m *matcher) matchRow(row *cdcpb.Event_Row) ([]byte, bool) {
