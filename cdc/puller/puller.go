d642c2adb14134dcdf54a9cd2c96d7670428fc54
amyangfei <amyangfei@gmail.com>
2020-04-13 14:34:07 +0800

    update grafane template

--- cdc/puller/puller.go
+++ cdc/puller/puller.go
@@ -126,7 +126,10 @@ func (p *pullerImpl) Run(ctx context.Context) error {
 				metricEventChanSize.Set(float64(len(eventCh)))
 				metricMemBufferSize.Set(float64(p.buffer.Size()))
 				metricOutputChanSize.Set(float64(len(p.outputCh)))
-				metricPullerResolvedTs.Set(float64(oracle.ExtractPhysical(atomic.LoadUint64(&p.resolvedTs))))
+				resolvedTs := atomic.LoadUint64(&p.resolvedTs)
+				if resolvedTs > 0 {
+					metricPullerResolvedTs.Set(float64(oracle.ExtractPhysical(resolvedTs)))
+				}
 			}
 		}
 	})
