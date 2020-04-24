87d55dbe9624db531893b7aca721c1d3309a9511
leoppro <zhaoyilin@pingcap.com>
2020-04-24 11:15:03 +0800

    update test

---
+++ tests/collation/data/prepare.sql
@@ -0,0 +1,8 @@
+drop database if exists `collation_test`;
+create database `collation_test`;
+use `collation_test`;
+
+CREATE TABLE `t` (`a` varchar(20) COLLATE utf8mb4_general_ci NOT NULL, PRIMARY KEY (`a`)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin
+
+insert into `t` (`a`) value('A');
+insert into `t` (`a`) value('a');
