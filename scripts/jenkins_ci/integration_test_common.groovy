a4419e6cc302ade8b728355d432e33d792705b71
leoppro <zhaoyilin@pingcap.com>
2020-04-27 20:16:22 +0800

    add test

--- scripts/jenkins_ci/integration_test_common.groovy
+++ scripts/jenkins_ci/integration_test_common.groovy
@@ -1,6 +1,6 @@
 test_case_names = ["simple", "cdc", "multi_capture", "split_region", "row_format",
 "tiflash", "availability", "ddl_sequence", "sink_retry", "resolve_lock", "drop_many_tables",
-"file_sort"]
+"file_sort", "batch_add_table"]
 
 def prepare_binaries() {
     stage('Prepare Binaries') {
