2b6b91ca7addcde7aa938961ff41864e96fde9f2
leoppro <zhaoyilin@pingcap.com>
2020-04-27 20:14:24 +0800

    add test

---
+++ tests/batch_add_table/data/test.sql
@@ -0,0 +1,12 @@
+
+use `batch_add_table`;
+
+
+insert into a1 values (2);
+insert into a2 values (2);
+insert into a3 values (2);
+insert into a4 values (2);
+insert into a5 values (2);
+
+
+create table finish_mark (id int primary key);
