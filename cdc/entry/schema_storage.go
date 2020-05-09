a269b7ffb0ccb9d2a274e4a5265a6276e9306e29
crazycs520 <crazycs520@gmail.com>
2020-05-09 16:11:25 +0800

    init
    Signed-off-by: crazycs520 <crazycs520@gmail.com>

--- cdc/entry/schema_storage.go
+++ cdc/entry/schema_storage.go
@@ -531,6 +531,54 @@ func (s *schemaSnapshot) dropTable(id int64) error {
 	return nil
 }
 
+func (s *schemaSnapshot) truncatePartition(tbl *timodel.TableInfo) error {
+	id := tbl.ID
+	table, ok := s.tables[id]
+	if !ok {
+		return errors.NotFoundf("table %d", id)
+	}
+	schema, ok := s.SchemaByTableID(id)
+	if !ok {
+		return errors.NotFoundf("table(%d)'s schema", id)
+	}
+
+	pi := table.GetPartitionInfo()
+	if pi == nil {
+		return errors.NotFoundf("table %d is not a partition table, truncate partition failed", id)
+	}
+	oldIDs := make(map[int64]struct{}, len(pi.Definitions))
+	for _, p := range pi.Definitions {
+		oldIDs[p.ID] = struct{}{}
+	}
+
+	pi = tbl.GetPartitionInfo()
+	if pi == nil {
+		return errors.NotFoundf("table %d is not a partition table, truncate partition failed", id)
+	}
+
+	table = WrapTableInfo(schema.ID, schema.Name.O, tbl.Clone())
+	s.tables[id] = table
+	for _, partition := range pi.Definitions {
+		// update table info.
+		s.partitionTable[partition.ID] = table
+		if !table.ExistTableUniqueColumn() {
+			s.ineligibleTableID[partition.ID] = struct{}{}
+		}
+		delete(oldIDs, partition.ID)
+	}
+
+	// drop truncated partition.
+	for pid := range oldIDs {
+		s.truncateTableID[pid] = struct{}{}
+		delete(s.partitionTable, pid)
+		delete(s.ineligibleTableID, pid)
+		fmt.Printf("snap truncate partition  %v --------\n", pid)
+	}
+
+	log.Debug("truncate table partition success", zap.String("name", table.Name.O), zap.Int64("tid", id), zap.Reflect("truncated id", oldIDs))
+	return nil
+}
+
 func (s *schemaSnapshot) createTable(schemaID int64, tbl *timodel.TableInfo) error {
 	schema, ok := s.schemas[schemaID]
 	if !ok {
@@ -641,6 +689,11 @@ func (s *schemaSnapshot) handleDDL(job *timodel.Job) error {
 		}
 
 		s.truncateTableID[job.TableID] = struct{}{}
+	case timodel.ActionTruncateTablePartition:
+		err := s.truncatePartition(job.BinlogInfo.TableInfo)
+		if err != nil {
+			return errors.Trace(err)
+		}
 	default:
 		binlogInfo := job.BinlogInfo
 		if binlogInfo == nil {
