bc5a45374f6d1fe3eb7cc210c1ce91c0b0de171c
leoppro <zhaoyilin@pingcap.com>
2020-05-18 23:10:57 +0800

    makefile: add check copyright script (#584)

--- go.mod
+++ go.mod
@@ -6,7 +6,6 @@ require (
 	github.com/BurntSushi/toml v0.3.1
 	github.com/DATA-DOG/go-sqlmock v1.3.3 // indirect
 	github.com/Shopify/sarama v1.26.1
-	github.com/biogo/store v0.0.0-20190426020002-884f370e325d
 	github.com/cenkalti/backoff v2.2.1+incompatible
 	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e
 	github.com/coreos/go-semver v0.3.0
---
+++ scripts/check-copyright.sh
@@ -0,0 +1,14 @@
+copyright="// Copyright $(date '+%Y') PingCAP, Inc."
+
+result=$(find ./ -name "*.go" ! -path ".//vendor/*" | while read file_path; do
+    head=`cat "${file_path}" | head -n 1`
+    if [ "$head" != "$copyright" ];then
+        echo "${file_path}"
+    fi
+done)
+
+if [ -n "$result" ]; then
+    echo "The copyright information of following files is incorrect:"
+    echo "$result"
+    exit 1
+fi
