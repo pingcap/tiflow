6150c47f8db00cd0ee042f23f54eb45b87f336a2
leoppro <zhaoyilin@pingcap.com>
2020-05-21 14:50:01 +0800

    update test check time

--- tests/multi_source/run.sh
+++ tests/multi_source/run.sh
@@ -39,7 +39,7 @@ prepare $*
 cd "$(dirname "$0")"
 set -o pipefail
 GO111MODULE=on go run main.go -config ./config.toml 2>&1 | tee $WORK_DIR/tester.log
-check_table_exists test.finish_mark ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT} 180
+check_table_exists test.finish_mark ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT} 300
 check_sync_diff $WORK_DIR $CUR/diff_config.toml
 cleanup_process $CDC_BINARY
 echo "[$(date)] <<<<<< run test case $TEST_NAME success! >>>>>>"
