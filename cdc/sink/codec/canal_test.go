c6079f5a8826846fd9c6330eda302b80b432c905
oscar-zyh <zhangyihao.zyh@gmail.com>
2020-06-15 23:17:57 +0800

    sink: fix imported proto package

--- cdc/sink/codec/canal_test.go
+++ cdc/sink/codec/canal_test.go
@@ -14,7 +14,7 @@
 package codec
 
 import (
-	"github.com/gogo/protobuf/proto"
+	"github.com/golang/protobuf/proto"
 	"github.com/pingcap/check"
 	mm "github.com/pingcap/parser/model"
 	"github.com/pingcap/parser/mysql"
