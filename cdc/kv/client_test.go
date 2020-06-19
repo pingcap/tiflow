e1724a0c1108f31f6f49fe391a65006158ecc96f
leoppro <zhaoyilin@pingcap.com>
2020-06-19 11:27:36 +0800

    support to decrease ts for frontier

--- cdc/kv/client_test.go
+++ cdc/kv/client_test.go
@@ -16,7 +16,6 @@ package kv
 import (
 	"context"
 	"fmt"
-	"math/rand"
 	"net"
 	"sync"
 	"testing"
@@ -50,39 +49,6 @@ func (s *clientSuite) TestNewClose(c *check.C) {
 
 	err = cli.Close()
 	c.Assert(err, check.IsNil)
-}
-
-func (s *clientSuite) TestUpdateCheckpointTS(c *check.C) {
-	var checkpointTS uint64
-	var g sync.WaitGroup
-	maxValueCh := make(chan uint64, 64)
-	update := func() uint64 {
-		var maxValue uint64
-		for i := 0; i < 1024; i++ {
-			value := rand.Uint64()
-			if value > maxValue {
-				maxValue = value
-			}
-			updateCheckpointTS(&checkpointTS, value)
-		}
-		return maxValue
-	}
-	for i := 0; i < 64; i++ {
-		g.Add(1)
-		go func() {
-			maxValueCh <- update()
-			g.Done()
-		}()
-	}
-	g.Wait()
-	close(maxValueCh)
-	var maxValue uint64
-	for v := range maxValueCh {
-		if maxValue < v {
-			maxValue = v
-		}
-	}
-	c.Assert(checkpointTS, check.Equals, maxValue)
 }
 
 type mockChangeDataService struct {
