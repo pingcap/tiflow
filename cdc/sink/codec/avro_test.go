6a62c20292765317b7c97fd85f90d54902d1f9cb
Neil Shen <overvenus@gmail.com>
2020-07-21 18:49:49 +0800

    cdc/sink: support Kafka SSL connection (#764)
    Signed-off-by: Neil Shen <overvenus@gmail.com>

--- cdc/sink/codec/avro_test.go
+++ cdc/sink/codec/avro_test.go
@@ -26,6 +26,7 @@ import (
 	"github.com/pingcap/ticdc/cdc/model"
 	"github.com/pingcap/ticdc/cdc/puller"
 	"github.com/pingcap/ticdc/pkg/regionspan"
+	"github.com/pingcap/ticdc/pkg/security"
 	"github.com/pingcap/tidb/types"
 	"go.uber.org/zap"
 )
@@ -39,7 +40,7 @@ var _ = check.Suite(&avroBatchEncoderSuite{})
 func (s *avroBatchEncoderSuite) SetUpSuite(c *check.C) {
 	startHTTPInterceptForTestingRegistry(c)
 
-	manager, err := NewAvroSchemaManager(context.Background(), "http://127.0.0.1:8081", "-value")
+	manager, err := NewAvroSchemaManager(context.Background(), &security.Credential{}, "http://127.0.0.1:8081", "-value")
 	c.Assert(err, check.IsNil)
 
 	s.encoder = &AvroEventBatchEncoder{
