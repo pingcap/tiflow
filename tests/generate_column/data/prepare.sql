67975bbd69cb508911d54bde4cd8a22cbd541273
leoppro <zhaoyilin@pingcap.com>
2020-07-24 14:59:15 +0800

    fix test

--- tests/generate_column/data/prepare.sql
+++ tests/generate_column/data/prepare.sql
@@ -9,7 +9,7 @@ update t set a = 11 where b = 3;
 delete from t where b=4;
 delete from t where a=4;
 
-create table t1 (a int, b int as (a + 1) virtual not null, c int not null, unique index idx(b), unique index idx(c));
+create table t1 (a int, b int as (a + 1) virtual not null, c int not null, unique index idx1(b), unique index idx2(c));
 insert into t1 (a, c) values (1, 2),(2, 3), (3, 4),(4, 5),(5, 6),(6, 7),(7, 8);
 update t1 set a = 10 where a = 1;
 update t1 set a = 11 where b = 3;
