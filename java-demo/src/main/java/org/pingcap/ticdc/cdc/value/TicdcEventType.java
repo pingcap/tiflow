7b2175e20e1b8673ad48b6a1252c714db42beb3f
dixingxing0 <dixingxing@yeah.net>
2020-08-04 18:20:01 +0800

    Delete java-demo for moved to demo/java

--- java-demo/src/main/java/org/pingcap/ticdc/cdc/value/TicdcEventType.java
+++
@@ -1,8 +0,0 @@
-package org.pingcap.ticdc.cdc.value;
-
-/**
- * https://docs.pingcap.com/zh/tidb/v4.0/ticdc-open-protocol#message-%E6%A0%BC%E5%BC%8F%E5%AE%9A%E4%B9%89
- */
-public enum TicdcEventType {
-    rowChange, ddl, resolved
-}
