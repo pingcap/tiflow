1f11cbe6bd037979ebd432334debf7c6d59b8357
leoppro <zhaoyilin@pingcap.com>
2020-08-04 11:34:11 +0800

    fix check

--- cdc/puller/rectifier.go
+++ cdc/puller/rectifier.go
@@ -1,3 +1,16 @@
+// Copyright 2020 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package puller
 
 import (
@@ -9,6 +22,7 @@ import (
 	"golang.org/x/sync/errgroup"
 )
 
+// Rectifier filters and collates the output stream from the sorter
 type Rectifier struct {
 	EventSorter
 
@@ -19,6 +33,7 @@ type Rectifier struct {
 	outputCh chan *model.PolymorphicEvent
 }
 
+// NewRectifier creates a new Rectifier
 func NewRectifier(s EventSorter, targetTs model.Ts) *Rectifier {
 	return &Rectifier{
 		EventSorter: s,
@@ -27,20 +42,24 @@ func NewRectifier(s EventSorter, targetTs model.Ts) *Rectifier {
 	}
 }
 
+// GetStatus returns the state of the Rectifier
 func (r *Rectifier) GetStatus() model.SorterStatus {
 	return atomic.LoadInt32(&r.status)
 }
 
+// GetMaxResolvedTs returns the maximum resolved ts sent from the Rectifier
 func (r *Rectifier) GetMaxResolvedTs() model.Ts {
 	return atomic.LoadUint64(&r.maxSentResolvedTs)
 }
 
+// SafeStop stops the Rectifier and Sorter safety
 func (r *Rectifier) SafeStop() {
 	atomic.CompareAndSwapInt32(&r.status,
 		model.SorterStatusWorking,
 		model.SorterStatusStopping)
 }
 
+// Run running the Rectifier
 func (r *Rectifier) Run(ctx context.Context) error {
 	output := func(event *model.PolymorphicEvent) {
 		select {
@@ -83,6 +102,8 @@ func (r *Rectifier) Run(ctx context.Context) error {
 	})
 	return errg.Wait()
 }
+
+// Output returns the output streams
 func (r *Rectifier) Output() <-chan *model.PolymorphicEvent {
 	return r.outputCh
 }
