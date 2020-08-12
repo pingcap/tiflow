0d8731283dc43cc45f8ca77c5fdb453a91a1fb1f
leoppro <zhaoyilin@pingcap.com>
2020-08-12 15:09:33 +0800

    remove the indie-mark-col field in row change event

--- cdc/sink/dispatcher/default.go
+++ cdc/sink/dispatcher/default.go
@@ -18,21 +18,37 @@ import (
 )
 
 type defaultDispatcher struct {
-	partitionNum int32
-	tbd          *tableDispatcher
-	ivd          *indexValueDispatcher
+	partitionNum   int32
+	tbd            *tableDispatcher
+	ivd            *indexValueDispatcher
+	enableOldValue bool
 }
 
-func newDefaultDispatcher(partitionNum int32) *defaultDispatcher {
+func newDefaultDispatcher(partitionNum int32, enableOldValue bool) *defaultDispatcher {
 	return &defaultDispatcher{
-		partitionNum: partitionNum,
-		tbd:          newTableDispatcher(partitionNum),
-		ivd:          newIndexValueDispatcher(partitionNum),
+		partitionNum:   partitionNum,
+		tbd:            newTableDispatcher(partitionNum),
+		ivd:            newIndexValueDispatcher(partitionNum),
+		enableOldValue: enableOldValue,
 	}
 }
 
 func (d *defaultDispatcher) Dispatch(row *model.RowChangedEvent) int32 {
-	if len(row.IndieMarkCol) == 0 {
+	if d.enableOldValue {
+		return d.tbd.Dispatch(row)
+	}
+	uniqueKeysNum := 0
+	for _, col := range row.PreColumns {
+		if col.Flag.IsPrimaryKey() || col.Flag.IsUniqueKey() {
+			uniqueKeysNum++
+		}
+	}
+	for _, col := range row.Columns {
+		if col.Flag.IsPrimaryKey() || col.Flag.IsUniqueKey() {
+			uniqueKeysNum++
+		}
+	}
+	if uniqueKeysNum > 1 {
 		return d.tbd.Dispatch(row)
 	}
 	return d.ivd.Dispatch(row)
