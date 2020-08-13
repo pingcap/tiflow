007ae6a5c30004d94009a4491dabd4afcaa9bcdf
luancheng <luancheng@pingcap.com>
2020-08-13 16:54:11 +0800

    update ci

--- tests/cdclog_file/run.sh
+++ tests/cdclog_file/run.sh
@@ -26,35 +26,46 @@ function prepare() {
     run_cdc_cli changefeed create --start-ts=$start_ts --sink-uri="$SINK_URI"
 }
 
-function cdclog_test() {
-  run_sql "drop database if exists $TEST_NAME"
-  run_sql "create database $TEST_NAME"
-  run_sql "create table $TEST_NAME.t1 (c0 int primary key, payload varchar(1024));"
-  run_sql "create table $TEST_NAME.t2 (c0 int primary key, payload varchar(1024));"
-
-  # wait for ddl synced
-  sleep 8
-
-  run_sql "insert into $TEST_NAME.t1 values (1, 'a')"
-  run_sql "insert into $TEST_NAME.t1 values (2, 'b')"
-
-  # wait for log synced
-  sleep 20
-
+function check_cdclog() {
+  # TODO test rotate file
   DATA_DIR="$WORK_DIR/test"
   # retrieve table id by log meta
   table_id=$(cat $DATA_DIR/log.meta | jq | grep t1 | awk -F '"' '{print $2}')
   file_count=$(ls -ahl $DATA_DIR/t_$table_id | grep cdclog | wc -l)
   if [[ ! "$file_count" -eq "1" ]]; then
       echo "$TEST_NAME failed, expect 1 row changed files, obtain $file_count"
-      exit 1
+      return 1
   fi
   ddl_file_count=$(ls -ahl $DATA_DIR/ddls | grep ddl | wc -l)
   if [[ ! "$ddl_file_count" -eq "1" ]]; then
       echo "$TEST_NAME failed, expect 1 ddl file, obtain $ddl_file_count"
-      exit 1
+      return 1
   fi
-  # TODO test rotate file
+  return 0
+}
+
+function cdclog_test() {
+  run_sql "drop database if exists $TEST_NAME"
+  run_sql "create database $TEST_NAME"
+  run_sql "create table $TEST_NAME.t1 (c0 int primary key, payload varchar(1024));"
+  run_sql "create table $TEST_NAME.t2 (c0 int primary key, payload varchar(1024));"
+
+  run_sql "insert into $TEST_NAME.t1 values (1, 'a')"
+  run_sql "insert into $TEST_NAME.t1 values (2, 'b')"
+
+  i=0
+  while [ $i -lt 30 ]
+  do
+    check_cdclog
+    ret=$?
+    if [ "$ret" == 0 ]; then
+        echo "check log successfully"
+        break
+    fi
+    ((i++))
+    echo "check log failed $i-th time, retry later"
+    sleep 2
+  done
   cleanup_process $CDC_BINARY
 }
 
--- tests/cdclog_s3/run.sh
+++ tests/cdclog_s3/run.sh
@@ -55,14 +55,28 @@ function prepare() {
     run_cdc_cli changefeed create --start-ts=$start_ts --sink-uri="$SINK_URI"
 }
 
+function check_cdclog() {
+  DATA_DIR="$WORK_DIR/s3/logbucket/test"
+  # retrieve table id by log meta
+  table_id=$(cat $DATA_DIR/log.meta | jq | grep t1 | awk -F '"' '{print $2}')
+  file_count=$(ls -ahl $DATA_DIR/t_$table_id | grep cdclog | wc -l)
+  if [[ ! "$file_count" -eq "2" ]]; then
+      echo "$TEST_NAME failed, expect 2 row changed files, obtain $file_count"
+      return 1
+  fi
+  ddl_file_count=$(ls -ahl $DATA_DIR/ddls | grep ddl | wc -l)
+  if [[ ! "$ddl_file_count" -eq "1" ]]; then
+      echo "$TEST_NAME failed, expect 1 ddl file, obtain $ddl_file_count"
+      return 1
+  fi
+  reutrn 0
+}
+
 function cdclog_test() {
   run_sql "drop database if exists $TEST_NAME"
   run_sql "create database $TEST_NAME"
   run_sql "create table $TEST_NAME.t1 (c0 int primary key, payload varchar(1024));"
   run_sql "create table $TEST_NAME.t2 (c0 int primary key, payload varchar(1024));"
-
-  # wait for ddl synced
-  sleep 8
 
   run_sql "insert into $TEST_NAME.t1 values (1, 'a')"
   # because flush row changed events interval is 5 second
@@ -70,22 +84,19 @@ function cdclog_test() {
   sleep 10
   run_sql "insert into $TEST_NAME.t1 values (2, 'b')"
 
-  # wait for log synced
-  sleep 8
-
-  DATA_DIR="$WORK_DIR/s3/logbucket/test"
-  # retrieve table id by log meta
-  table_id=$(cat $DATA_DIR/log.meta | jq | grep t1 | awk -F '"' '{print $2}')
-  file_count=$(ls -ahl $DATA_DIR/t_$table_id | grep cdclog | wc -l)
-  if [[ ! "$file_count" -eq "2" ]]; then
-      echo "$TEST_NAME failed, expect 2 row changed files, obtain $file_count"
-      exit 1
-  fi
-  ddl_file_count=$(ls -ahl $DATA_DIR/ddls | grep ddl | wc -l)
-  if [[ ! "$ddl_file_count" -eq "1" ]]; then
-      echo "$TEST_NAME failed, expect 1 ddl file, obtain $ddl_file_count"
-      exit 1
-  fi
+  i=0
+  while [ $i -lt 30 ]
+  do
+    check_cdclog
+    ret=$?
+    if [ "$ret" == 0 ]; then
+        echo "check log successfully"
+        break
+    fi
+    ((i++))
+    echo "check log failed $i-th time, retry later"
+    sleep 2
+  done
   cleanup_process $CDC_BINARY
 }
 
