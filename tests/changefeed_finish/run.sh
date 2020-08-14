4ffe4f4a1c494ff86fec840430e32aeee71b6ff8
amyangfei <amyangfei@gmail.com>
2020-08-14 16:29:43 +0800

    support to deploy multiple PDs in integration test

--- tests/changefeed_finish/run.sh
+++ tests/changefeed_finish/run.sh
@@ -38,7 +38,7 @@ function run() {
     start_tidb_cluster --workdir $WORK_DIR
     cd $WORK_DIR
 
-    pd_addr="http://$UP_PD_HOST:$UP_PD_PORT"
+    pd_addr="http://$UP_PD_HOST_1:$UP_PD_PORT_1"
     TOPIC_NAME="ticdc-changefeed-pause-resume-$RANDOM"
     case $SINK_TYPE in
         kafka) SINK_URI="kafka://127.0.0.1:9092/$TOPIC_NAME?partition-num=4";;
@@ -48,7 +48,7 @@ function run() {
       run_kafka_consumer $WORK_DIR "kafka://127.0.0.1:9092/$TOPIC_NAME?partition-num=4"
     fi
     run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY --addr "127.0.0.1:8300" --pd $pd_addr
-    now=$(cdc cli tso query --pd=http://$UP_PD_HOST:$UP_PD_PORT)
+    now=$(cdc cli tso query --pd=http://$UP_PD_HOST_1:$UP_PD_PORT_1)
     # 90s after now
     target_ts=$(( $now + 90 * 10 ** 3 * 2 ** 18 ))
     changefeed_id=$(cdc cli changefeed create --sink-uri="$SINK_URI" --target-ts=$target_ts 2>&1|tail -n2|head -n1|awk '{print $2}')
