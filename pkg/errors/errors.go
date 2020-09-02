6677e124891ec9f305478916cb7c56b06a8e5a41
amyangfei <amyangfei@gmail.com>
2020-09-02 12:29:42 +0800

    update pingcap/errors to latest

--- pkg/errors/errors.go
+++ pkg/errors/errors.go
@@ -20,35 +20,35 @@ import (
 // errors
 var (
 	// kv related errors
-	ErrWriteTsConflict         = errors.Normalize("write ts conflict").SetErrCodeText("CDC:ErrWriteTsConflict")
-	ErrChangeFeedNotExists     = errors.Normalize("changefeed not exists, key: %s").SetErrCodeText("CDC:ErrChangeFeedNotExists")
-	ErrChangeFeedAlreadyExists = errors.Normalize("changefeed already exists, key: %s").SetErrCodeText("CDC:ErrChangeFeedAlreadyExists")
-	ErrTaskStatusNotExists     = errors.Normalize("task status not exists, key: %s").SetErrCodeText("CDC:ErrTaskStatusNotExists")
-	ErrTaskPositionNotExists   = errors.Normalize("task position not exists, key: %s").SetErrCodeText("CDC:ErrTaskPositionNotExists")
-	ErrCaptureNotExist         = errors.Normalize("capture not exists, key: %s").SetErrCodeText("CDC:ErrCaptureNotExist")
-	ErrGetAllStoresFailed      = errors.Normalize("get stores from pd failed").SetErrCodeText("CDC:ErrGetAllStoresFailed")
+	ErrWriteTsConflict         = errors.Normalize("write ts conflict", errors.RFCCodeText("CDC:ErrWriteTsConflict"))
+	ErrChangeFeedNotExists     = errors.Normalize("changefeed not exists, key: %s", errors.RFCCodeText("CDC:ErrChangeFeedNotExists"))
+	ErrChangeFeedAlreadyExists = errors.Normalize("changefeed already exists, key: %s", errors.RFCCodeText("CDC:ErrChangeFeedAlreadyExists"))
+	ErrTaskStatusNotExists     = errors.Normalize("task status not exists, key: %s", errors.RFCCodeText("CDC:ErrTaskStatusNotExists"))
+	ErrTaskPositionNotExists   = errors.Normalize("task position not exists, key: %s", errors.RFCCodeText("CDC:ErrTaskPositionNotExists"))
+	ErrCaptureNotExist         = errors.Normalize("capture not exists, key: %s", errors.RFCCodeText("CDC:ErrCaptureNotExist"))
+	ErrGetAllStoresFailed      = errors.Normalize("get stores from pd failed", errors.RFCCodeText("CDC:ErrGetAllStoresFailed"))
 
 	// rule related errors
-	ErrEncodeFailed      = errors.Normalize("encode failed: %s").SetErrCodeText("CDC:ErrEncodeFailed")
-	ErrDecodeFailed      = errors.Normalize("decode failed: %s").SetErrCodeText("CDC:ErrDecodeFailed")
-	ErrFilterRuleInvalid = errors.Normalize("filter rule is invalid").SetErrCodeText("CDC:ErrFilterRuleInvalid")
+	ErrEncodeFailed      = errors.Normalize("encode failed: %s", errors.RFCCodeText("CDC:ErrEncodeFailed"))
+	ErrDecodeFailed      = errors.Normalize("decode failed: %s", errors.RFCCodeText("CDC:ErrDecodeFailed"))
+	ErrFilterRuleInvalid = errors.Normalize("filter rule is invalid", errors.RFCCodeText("CDC:ErrFilterRuleInvalid"))
 
 	// internal errors
-	ErrAdminStopProcessor = errors.Normalize("stop processor by admin command").SetErrCodeText("CDC:ErrAdminStopProcessor")
-	ErrUnresolved         = errors.Normalize("unresolved").SetErrCodeText("CDC:ErrUnresolved")
+	ErrAdminStopProcessor = errors.Normalize("stop processor by admin command", errors.RFCCodeText("CDC:ErrAdminStopProcessor"))
+	ErrUnresolved         = errors.Normalize("unresolved", errors.RFCCodeText("CDC:ErrUnresolved"))
 	// ErrVersionIncompatible is an error for running CDC on an incompatible Cluster.
-	ErrVersionIncompatible   = errors.Normalize("version is incompatible: %s").SetErrCodeText("CDC:ErrVersionIncompatible")
-	ErrCreateMarkTableFailed = errors.Normalize("create mark table failed").SetErrCodeText("CDC:ErrCreateMarkTableFailed")
+	ErrVersionIncompatible   = errors.Normalize("version is incompatible: %s", errors.RFCCodeText("CDC:ErrVersionIncompatible"))
+	ErrCreateMarkTableFailed = errors.Normalize("create mark table failed", errors.RFCCodeText("CDC:ErrCreateMarkTableFailed"))
 
 	// sink related errors
-	ErrExecDDLFailed     = errors.Normalize("exec DDL failed").SetErrCodeText("CDC:ErrExecDDLFailed")
-	ErrorDDLEventIgnored = errors.Normalize("ddl event is ignored").SetErrCodeText("CDC:ErrorDDLEventIgnored")
+	ErrExecDDLFailed     = errors.Normalize("exec DDL failed", errors.RFCCodeText("CDC:ErrExecDDLFailed"))
+	ErrorDDLEventIgnored = errors.Normalize("ddl event is ignored", errors.RFCCodeText("CDC:ErrorDDLEventIgnored"))
 
 	// utilities related errors
-	ErrToTLSConfigFailed         = errors.Normalize("generate tls config failed").SetErrCodeText("CDC:ErrToTLSConfigFailed")
-	ErrCheckClusterVersionFromPD = errors.Normalize("failed to request PD").SetErrCodeText("CDC:ErrCheckClusterVersionFromPD")
-	ErrNewSemVersion             = errors.Normalize("create sem version").SetErrCodeText("CDC:ErrNewSemVersion")
-	ErrCheckDirWritable          = errors.Normalize("check dir writable failed").SetErrCodeText("CDC:ErrCheckDirWritable")
-	ErrLoadTimezone              = errors.Normalize("load timezone").SetErrCodeText("CDC:ErrLoadTimezone")
-	ErrURLFormatInvalid          = errors.Normalize("url format is invalid").SetErrCodeText("CDC:ErrURLFormatInvalid")
+	ErrToTLSConfigFailed         = errors.Normalize("generate tls config failed", errors.RFCCodeText("CDC:ErrToTLSConfigFailed"))
+	ErrCheckClusterVersionFromPD = errors.Normalize("failed to request PD", errors.RFCCodeText("CDC:ErrCheckClusterVersionFromPD"))
+	ErrNewSemVersion             = errors.Normalize("create sem version", errors.RFCCodeText("CDC:ErrNewSemVersion"))
+	ErrCheckDirWritable          = errors.Normalize("check dir writable failed", errors.RFCCodeText("CDC:ErrCheckDirWritable"))
+	ErrLoadTimezone              = errors.Normalize("load timezone", errors.RFCCodeText("CDC:ErrLoadTimezone"))
+	ErrURLFormatInvalid          = errors.Normalize("url format is invalid", errors.RFCCodeText("CDC:ErrURLFormatInvalid"))
 )
