a9ba13c16c84dd347786f6adea10f583227739c5
Zixiong Liu <liuzixiong@pingcap.com>
2020-09-03 15:20:44 +0800

    add integration test case

--- tests/processor_panic/run.sh
+++ tests/processor_panic/run.sh
@@ -18,15 +18,15 @@ function prepare() {
     # record tso before we create tables to skip the system table DDLs
     start_ts=$(run_cdc_cli tso query --pd=http://$UP_PD_HOST_1:$UP_PD_PORT_1)
 
-    run_sql "CREATE table test.simple1(id int primary key, val int);"
-    run_sql "CREATE table test.simple2(id int primary key, val int);"
+    export GO_FAILPOINTS='github.com/pingcap/ticdc/cdc/ProcessorSyncResolvedPreEmit=1*return(true)'
+    run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY --restart true
 
-    export GO_FAILPOINTS='github.com/pingcap/ticdc/cdc/processorPanic=1*return(true)'
+    export GO_FAILPOINTS=''
     run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY
 
-    TOPIC_NAME="ticdc-simple-test-$RANDOM"
+    TOPIC_NAME="ticdc-processor-panic-test-$RANDOM"
     case $SINK_TYPE in
-        kafka) SINK_URI="kafka://127.0.0.1:9092/$TOPIC_NAME?partition-num=4&kafka-client-id=cdc_test_simple";;
+        kafka) SINK_URI="kafka://127.0.0.1:9092/$TOPIC_NAME?partition-num=4&kafka-client-id=cdc_test_processor_panic";;
         *) SINK_URI="mysql://root@127.0.0.1:3306/";;
     esac
     run_cdc_cli changefeed create --start-ts=$start_ts --sink-uri="$SINK_URI"
@@ -35,72 +35,15 @@ function prepare() {
     fi
 }
 
-function sql_check() {
-    # run check in sequence and short circuit principle, if error hanppens,
-    # the following statement will be not executed
-
-    # check table simple1.
-    run_sql "SELECT id, val FROM test.simple1;" ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT} && \
-    check_contains "id: 1" && \
-    check_contains "val: 1" && \
-    check_contains "id: 2" && \
-    check_contains "val: 22" && \
-    check_not_contains "id: 3" && \
-
-    # check table simple2.
-    run_sql "SELECT id, val FROM test.simple2;" ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT} && \
-    check_contains "id: 1" && \
-    check_contains "val: 1" && \
-    check_contains "id: 2" && \
-    check_contains "val: 22" && \
-    check_not_contains "id: 3"
-}
-
-function sql_test() {
-    # test insert/update/delete for two table in the same way.
-    run_sql "INSERT INTO test.simple1(id, val) VALUES (1, 1);"
-    run_sql "INSERT INTO test.simple1(id, val) VALUES (2, 2);"
-    run_sql "INSERT INTO test.simple1(id, val) VALUES (3, 3);"
-
-    # update id = 2 and delete id = 3
-    run_sql "UPDATE test.simple1 set val = 22 where id = 2;"
-    run_sql "DELETE from test.simple1 where id = 3;"
-
-
-    # same dml for table simple2
-    run_sql "INSERT INTO test.simple2(id, val) VALUES (1, 1);"
-    run_sql "INSERT INTO test.simple2(id, val) VALUES (2, 2);"
-    run_sql "INSERT INTO test.simple2(id, val) VALUES (3, 3);"
-
-    run_sql "UPDATE test.simple2 set val = 22 where id = 2;"
-    run_sql "DELETE from test.simple2 where id = 3;"
-
-    i=0
-    check_time=50
-    set +e
-    while [ $i -lt $check_time ]
-    do
-        sql_check
-        ret=$?
-        if [ "$ret" == 0 ]; then
-            echo "check data successfully"
-            break
-        fi
-        ((i++))
-        echo "check data failed $i-th time, retry later"
-        sleep 2
-    done
-    set -e
-
-    if [ $i -ge $check_time ]; then
-        echo "check data failed at last"
-        exit 1
-    fi
-
-    cleanup_process $CDC_BINARY
-}
 
 trap stop_tidb_cluster EXIT
 prepare $*
-sql_test $*
+
+cd "$(dirname "$0")"
+set -o pipefail
+GO111MODULE=on go run main.go -config ./config.toml 2>&1 | tee $WORK_DIR/tester.log
+check_table_exists test.end_mark_table ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT}
+check_sync_diff $WORK_DIR $CUR/diff_config.toml
+cleanup_process $CDC_BINARY
+
 echo "[$(date)] <<<<<< run test case $TEST_NAME success! >>>>>>"
