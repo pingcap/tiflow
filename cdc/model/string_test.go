e3fb65c7f867ddde9410d23817b2ca4ec57ddf73
amyangfei <amyangfei@gmail.com>
2020-11-16 14:43:29 +0800

    tests: add more unit tests in cdc/model package

--- cdc/model/string_test.go
+++ cdc/model/string_test.go
@@ -21,6 +21,24 @@ type stringSuite struct{}
 
 var _ = check.Suite(&stringSuite{})
 
+func (s *stringSuite) TestHolderString(c *check.C) {
+	testCases := []struct {
+		count    int
+		expected string
+	}{
+		{1, "?"},
+		{2, "?,?"},
+		{10, "?,?,?,?,?,?,?,?,?,?"},
+	}
+	for _, tc := range testCases {
+		s := HolderString(tc.count)
+		c.Assert(s, check.Equals, tc.expected)
+	}
+	// test invalid input
+	c.Assert(func() { HolderString(0) }, check.Panics, "strings.Builder.Grow: negative count")
+	c.Assert(func() { HolderString(-1) }, check.Panics, "strings.Builder.Grow: negative count")
+}
+
 func (s *stringSuite) TestExtractKeySuffix(c *check.C) {
 	testCases := []struct {
 		input  string
