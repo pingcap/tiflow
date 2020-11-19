d2df24cb9d17c2bd9be54fb33c55f81e3d16ee5a
amyangfei <amyangfei@gmail.com>
2020-11-19 17:24:37 +0800

    owner: fix multiple owners exist when owner campaign key is deleted (#1072)

--- cdc/owner_test.go
+++ cdc/owner_test.go
@@ -16,7 +16,9 @@ package cdc
 import (
 	"context"
 	"errors"
+	"fmt"
 	"net/url"
+	"sync"
 	"time"
 
 	"github.com/pingcap/check"
@@ -796,4 +798,54 @@ func (s *ownerSuite) TestChangefeedApplyDDLJob(c *check.C) {
 		c.Assert(cf.tables, check.DeepEquals, expectTables[i])
 	}
 	s.TearDownTest(c)
+}
+
+func (s *ownerSuite) TestWatchCampaignKey(c *check.C) {
+	ctx := context.Background()
+	capture, err := NewCapture(ctx, []string{s.clientURL.String()},
+		&security.Credential{}, "127.0.0.1:12034", &processorOpts{})
+	c.Assert(err, check.IsNil)
+	err = capture.Campaign(ctx)
+	c.Assert(err, check.IsNil)
+
+	cctx, cancel := context.WithCancel(ctx)
+	owner, err := NewOwner(cctx, nil, &security.Credential{}, capture.session,
+		DefaultCDCGCSafePointTTL, time.Millisecond*200)
+	c.Assert(err, check.IsNil)
+
+	// check campaign key deleted can be detected
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		err := owner.watchCampaignKey(cctx)
+		c.Assert(cerror.ErrOwnerCampaignKeyDeleted.Equal(err), check.IsTrue)
+		cancel()
+	}()
+	// ensure the watch loop has started
+	time.Sleep(time.Millisecond * 100)
+	etcdCli := owner.etcdClient.Client.Unwrap()
+	key := fmt.Sprintf("%s/%x", kv.CaptureOwnerKey, owner.session.Lease())
+	_, err = etcdCli.Delete(ctx, key)
+	c.Assert(err, check.IsNil)
+	wg.Wait()
+
+	// check key is deleted before watch loop starts
+	cctx, cancel = context.WithCancel(ctx)
+	err = owner.watchCampaignKey(cctx)
+	c.Assert(cerror.ErrOwnerCampaignKeyDeleted.Equal(err), check.IsTrue)
+
+	// check the watch routine can be canceled
+	err = capture.Campaign(ctx)
+	c.Assert(err, check.IsNil)
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		err := owner.watchCampaignKey(cctx)
+		c.Assert(err, check.IsNil)
+	}()
+	// ensure the watch loop has started
+	time.Sleep(time.Millisecond * 100)
+	cancel()
+	wg.Wait()
 }
