7b4c0d2169bf441782b2a36d72d563cf76fd2783
leoppro <zhaoyilin@pingcap.com>
2020-11-23 17:58:55 +0800

    update checker

--- cdc/sink/simple_mysql_tester.go
+++ cdc/sink/simple_mysql_tester.go
@@ -37,7 +37,7 @@ func init() {
 	failpoint.Inject("SimpleMySQLSinkTester", func() {
 		sinkIniterMap["simple-mysql"] = func(ctx context.Context, changefeedID model.ChangeFeedID, sinkURI *url.URL,
 			filter *filter.Filter, config *config.ReplicaConfig, opts map[string]string, errCh chan error) (Sink, error) {
-			return newSimpleMySQLSink(ctx, sinkURI, config, opts)
+			return newSimpleMySQLSink(ctx, sinkURI, config)
 		}
 	})
 }
@@ -50,7 +50,7 @@ type simpleMySQLSink struct {
 	rowsBufferLock      sync.Mutex
 }
 
-func newSimpleMySQLSink(ctx context.Context, sinkURI *url.URL, config *config.ReplicaConfig, opts map[string]string) (*simpleMySQLSink, error) {
+func newSimpleMySQLSink(ctx context.Context, sinkURI *url.URL, config *config.ReplicaConfig) (*simpleMySQLSink, error) {
 	var db *sql.DB
 
 	// dsn format of the driver:
@@ -97,11 +97,9 @@ func newSimpleMySQLSink(ctx context.Context, sinkURI *url.URL, config *config.Re
 		db:             db,
 		enableOldValue: config.EnableOldValue,
 	}
-	if checkOldValue, ok := opts["check-old-value"]; ok {
-		sink.enableCheckOldValue = strings.ToLower(checkOldValue) == "true"
-		if sink.enableCheckOldValue {
-			log.Info("the old value checker is enabled")
-		}
+	if strings.ToLower(sinkURI.Query().Get("check-old-value")) == "true" {
+		sink.enableCheckOldValue = true
+		log.Info("the old value checker is enabled")
 	}
 	return sink, nil
 }
