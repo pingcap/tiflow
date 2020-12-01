9dc16ef955de9494105f81773d6468c459ee4503
leoppro <zhaoyilin@pingcap.com>
2020-12-01 14:24:45 +0800

    add cases for clustered index

--- tests/clustered_index/data/test.sql
+++ tests/clustered_index/data/test.sql
@@ -52,3 +52,60 @@ INSERT INTO t2 VALUES ('bbbb', 1111, 12.0);
 INSERT INTO t2 VALUES ('cccc', 1111, 13.0);
 INSERT INTO t2 VALUES ('dddd', 1111, 14.0);
 INSERT INTO t2 VALUES ('eeee', 1111, 15.0);
+
+
+
+create table t3(a date primary key, b int);
+
+INSERT INTO t3 VALUES ('2020-02-20', 1);
+INSERT INTO t3 VALUES ('2020-02-21', 2);
+INSERT INTO t3 VALUES ('2020-02-22', 3);
+
+UPDATE t3 SET a = '2020-02-23' WHERE b = 3;
+DELETE FROM t3 WHERE b = 2;
+
+
+create table t4(a time primary key, b int);
+
+INSERT INTO t4 VALUES ('11:22:33', 1);
+INSERT INTO t4 VALUES ('11:33:22', 2);
+INSERT INTO t4 VALUES ('11:43:11', 3);
+
+UPDATE t4 SET a = '11:44:55' WHERE b = 3;
+DELETE FROM t4 WHERE b = 2;
+
+create table t5(a datetime primary key, b int);
+
+INSERT INTO t5 VALUES ('2020-02-20 11:22:33', 1);
+INSERT INTO t5 VALUES ('2020-02-21 11:33:22', 2);
+INSERT INTO t5 VALUES ('2020-02-22 11:43:11', 3);
+
+UPDATE t5 SET a = '2020-02-23 11:44:55' WHERE b = 3;
+DELETE FROM t5 WHERE b = 2;
+
+create table t6(a timestamp primary key, b int);
+
+INSERT INTO t6 VALUES ('2020-02-20 11:22:33', 1);
+INSERT INTO t6 VALUES ('2020-02-21 11:33:22', 2);
+INSERT INTO t6 VALUES ('2020-02-22 11:43:11', 3);
+
+UPDATE t6 SET a = '2020-02-23 11:44:55' WHERE b = 3;
+DELETE FROM t6 WHERE b = 2;
+
+create table t7(a year primary key, b int);
+
+INSERT INTO t7 VALUES ('2020', 1);
+INSERT INTO t7 VALUES ('2021', 2);
+INSERT INTO t7 VALUES ('2022', 3);
+
+UPDATE t7 SET a = '2023' WHERE b = 3;
+DELETE FROM t7 WHERE b = 2;
+
+create table t8(a int, b varchar(255) as (concat(a, "test")) stored, primary key(b));
+
+INSERT INTO t8(a) VALUES (2020);
+INSERT INTO t8(a) VALUES (2021);
+INSERT INTO t8(a) VALUES (2022);
+
+UPDATE t8 SET a = 2023 WHERE a = 2022;
+DELETE FROM t8 WHERE a = 2021;
