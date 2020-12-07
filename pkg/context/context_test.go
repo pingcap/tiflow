0b259f7251dc28a680a766d2bbf13eed5d8754fd
leoppro <zhaoyilin@pingcap.com>
2020-12-07 11:51:23 +0800

    add test for context

--- pkg/context/context_test.go
+++ pkg/context/context_test.go
@@ -13,11 +13,55 @@
 
 package context
 
-import "github.com/pingcap/check"
+import (
+	"context"
+	"testing"
+
+	"github.com/pingcap/check"
+	"github.com/pingcap/errors"
+	"github.com/pingcap/ticdc/pkg/config"
+)
+
+func TestSuite(t *testing.T) { check.TestingT(t) }
 
 type contextSuite struct{}
 
 var _ = check.Suite(&contextSuite{})
 
 func (s *contextSuite) TestVars(c *check.C) {
+	stdCtx := context.Background()
+	conf := config.GetDefaultReplicaConfig()
+	conf.Filter.Rules = []string{"hello.world"}
+	ctx, _ := NewContext(stdCtx, &Vars{
+		Config: conf,
+	})
+	c.Assert(ctx.Vars().Config, check.DeepEquals, conf)
+}
+
+func (s *contextSuite) TestStdCancel(c *check.C) {
+	stdCtx := context.Background()
+	stdCtx, cancel := context.WithCancel(stdCtx)
+	ctx, _ := NewContext(stdCtx, &Vars{})
+	cancel()
+	<-ctx.StdContext().Done()
+	<-ctx.Done()
+}
+
+func (s *contextSuite) TestCancel(c *check.C) {
+	stdCtx := context.Background()
+	ctx, cancel := NewContext(stdCtx, &Vars{})
+	cancel()
+	<-ctx.StdContext().Done()
+	<-ctx.Done()
+}
+
+func (s *contextSuite) TestThrow(c *check.C) {
+	stdCtx := context.Background()
+	ctx, cancel := NewContext(stdCtx, &Vars{})
+	ctx = WatchThrow(ctx, func(err error) {
+		c.Assert(err.Error(), check.Equals, "mock error")
+		cancel()
+	})
+	ctx.Throw(errors.New("mock error"))
+	<-ctx.Done()
 }
