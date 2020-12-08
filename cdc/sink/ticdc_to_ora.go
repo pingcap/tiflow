1bd984e1cc83af4c5167344b709d2f4bb02c15cc
liukehao <1150699372@qq.com>
2020-12-08 13:31:52 +0800

    Submit daliy.

--- cdc/sink/ticdc_to_ora.go
+++ cdc/sink/ticdc_to_ora.go
@@ -255,9 +255,10 @@ func (b *ticdcToOraclSink) Close() error {
 
 func getRowDataByClomns(colFlag int32, colums []*model.Column, rowdataBuilder dsgpb.RowData) dsgpb.RowData {
 
-	var colType string
+	//var colType string
 
 	for _, column := range colums {
+		colType := string("")
 		columnBuilder := &dsgpb.Column{}
 		columnBuilder.ColName = &column.Name
 		columnValue := model.ColumnValueString(column.Value, column.Flag)
