f9d7989d9d027611e02e363c0f78b5e9b36698be
leoppro <zhaoyilin@pingcap.com>
2020-12-09 17:50:22 +0800

    cherry pick #1172 to release-4.0
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>

--- pkg/pipeline/context.go
+++ pkg/pipeline/context.go
@@ -13,81 +13,37 @@
 
 package pipeline
 
-import (
-	"context"
-	"time"
+import "github.com/pingcap/ticdc/pkg/context"
 
-	"github.com/pingcap/ticdc/pkg/config"
-)
+// NodeContext contains some read-only parameters and provide control for the pipeline life cycle
+type NodeContext interface {
+	context.Context
 
-// Context contains some read-only parameters and provide control for the pipeline life cycle
-type Context interface {
-	// ReplicaConfig returns the replica config
-	ReplicaConfig() *config.ReplicaConfig
 	// Message returns the message sent by the previous node
 	Message() *Message
 	// SendToNextNode sends the message to the next node
 	SendToNextNode(msg *Message)
-
-	// StdContext returns a std context
-	StdContext() context.Context
-	// Cancel cancels the context
-	Cancel()
-	// Done returns a channel like context.Context.Done
-	Done() <-chan struct{}
 }
 
-type baseContext struct {
-	parent Context
+type nodeContext struct {
+	context.Context
+	msg      *Message
+	outputCh chan *Message
 }
 
-func (ctx *baseContext) ReplicaConfig() *config.ReplicaConfig {
-	return ctx.parent.ReplicaConfig()
-}
-
-func (ctx *baseContext) Message() *Message {
-	return ctx.parent.Message()
-}
-
-func (ctx *baseContext) SendToNextNode(msg *Message) {
-	ctx.parent.SendToNextNode(msg)
-}
-
-func (ctx *baseContext) Done() <-chan struct{} {
-	return ctx.parent.Done()
-}
-
-func (ctx *baseContext) StdContext() context.Context {
-	return ctx.parent.StdContext()
-}
-
-func (ctx *baseContext) Cancel() {
-	ctx.parent.Cancel()
-}
-
-type rootContext struct {
-	baseContext
-	config  *config.ReplicaConfig
-	closeCh chan struct{}
-}
-
-// NewRootContext returns a new root context
-func NewRootContext(config *config.ReplicaConfig) Context {
-	return &rootContext{
-		baseContext: baseContext{},
-		config:      config,
-		closeCh:     make(chan struct{}),
+func newNodeContext(ctx context.Context, msg *Message, outputCh chan *Message) NodeContext {
+	return &nodeContext{
+		Context:  ctx,
+		msg:      msg,
+		outputCh: outputCh,
 	}
 }
 
-func (ctx *rootContext) Message() *Message {
-	panic("unreachable")
+func (ctx *nodeContext) Message() *Message {
+	return ctx.msg
 }
 
-func (ctx *rootContext) SendToNextNode(msg *Message) {
-	panic("unreachable")
-}
-
+<<<<<<< HEAD
 func (ctx *rootContext) ReplicaConfig() *config.ReplicaConfig {
 	return ctx.config
 }
@@ -107,61 +63,25 @@ func (ctx *rootContext) Cancel() {
 		recover() //nolint:errcheck
 	}()
 	close(ctx.closeCh)
+=======
+func (ctx *nodeContext) SendToNextNode(msg *Message) {
+	// The header channel should never be blocked
+	ctx.outputCh <- msg
+>>>>>>> 9e77a0c... pipeline: refine pipeline, refactor processor part one (#1172)
 }
 
 type messageContext struct {
-	baseContext
+	NodeContext
 	message *Message
 }
 
-func withMessage(ctx Context, msg *Message) Context {
-	return &messageContext{
-		baseContext: baseContext{
-			parent: ctx,
-		},
-		message: msg,
+func withMessage(ctx NodeContext, msg *Message) NodeContext {
+	return messageContext{
+		NodeContext: ctx,
+		message:     msg,
 	}
 }
 
-func (ctx *messageContext) Message() *Message {
+func (ctx messageContext) Message() *Message {
 	return ctx.message
-}
-
-type outputChContext struct {
-	baseContext
-	outputCh chan *Message
-}
-
-func (ctx *outputChContext) SendToNextNode(msg *Message) {
-	// The header channel should never be blocked
-	ctx.outputCh <- msg
-}
-
-func withOutputCh(ctx Context, outputCh chan *Message) Context {
-	return &outputChContext{
-		baseContext: baseContext{
-			parent: ctx,
-		},
-		outputCh: outputCh,
-	}
-}
-
-type cancelStdContext struct {
-	closeCh chan struct{}
-}
-
-func (ctx cancelStdContext) Deadline() (deadline time.Time, ok bool) {
-	return time.Now(), false
-}
-
-func (ctx cancelStdContext) Done() <-chan struct{} {
-	return ctx.closeCh
-}
-
-func (ctx cancelStdContext) Err() error {
-	return nil
-}
-
-func (ctx cancelStdContext) Value(key interface{}) interface{} {
-	return nil
 }
