fdd9fb053acf74566345c63dc11c3aea46c9a7c6
amyangfei <amyangfei@gmail.com>
2021-01-07 17:00:29 +0800

    owner: add more observations for owner operation (#1249)

--- cdc/server_test.go
+++ cdc/server_test.go
@@ -14,16 +14,50 @@
 package cdc
 
 import (
+	"context"
+	"net/url"
+	"strings"
+	"time"
+
 	"github.com/pingcap/check"
+	"github.com/pingcap/ticdc/pkg/etcd"
+	"github.com/pingcap/ticdc/pkg/util"
 	"github.com/pingcap/ticdc/pkg/util/testleak"
+	"go.etcd.io/etcd/embed"
+	"golang.org/x/sync/errgroup"
 )
 
-type serverOptionSuite struct{}
+type serverSuite struct {
+	e         *embed.Etcd
+	clientURL *url.URL
+	ctx       context.Context
+	cancel    context.CancelFunc
+	errg      *errgroup.Group
+}
 
-var _ = check.Suite(&serverOptionSuite{})
+func (s *serverSuite) SetUpTest(c *check.C) {
+	dir := c.MkDir()
+	var err error
+	s.clientURL, s.e, err = etcd.SetupEmbedEtcd(dir)
+	c.Assert(err, check.IsNil)
+	s.ctx, s.cancel = context.WithCancel(context.Background())
+	s.errg = util.HandleErrWithErrGroup(s.ctx, s.e.Err(), func(e error) { c.Log(e) })
+}
 
-func (s *serverOptionSuite) TestNewServer(c *check.C) {
+func (s *serverSuite) TearDownTest(c *check.C) {
+	s.e.Close()
+	s.cancel()
+	err := s.errg.Wait()
+	if err != nil {
+		c.Errorf("Error group error: %s", err)
+	}
+}
+
+var _ = check.Suite(&serverSuite{})
+
+func (s *serverSuite) TestNewServer(c *check.C) {
 	defer testleak.AfterTest(c)()
+	defer s.TearDownTest(c)
 	svr, err := NewServer()
 	c.Assert(err, check.ErrorMatches, ".*empty PD address")
 	c.Assert(svr, check.IsNil)
@@ -68,4 +102,30 @@ func (s *serverOptionSuite) TestNewServer(c *check.C) {
 		AdvertiseAddress("advertise"))
 	c.Assert(err, check.ErrorMatches, ".*does not contain a port")
 	c.Assert(svr, check.IsNil)
+}
+
+func (s *serverSuite) TestEtcdHealthChecker(c *check.C) {
+	defer testleak.AfterTest(c)()
+	defer s.TearDownTest(c)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	pdEndpoints := strings.Join([]string{
+		"http://" + s.clientURL.Host,
+		"http://invalid-pd-host:2379",
+	}, ",")
+	server, err := NewServer(
+		PDEndpoints(pdEndpoints), Address("0.0.0.0:1234"), GCTTL(DefaultCDCGCSafePointTTL),
+		AdvertiseAddress("127.0.0.1:1234"))
+	c.Assert(err, check.IsNil)
+	c.Assert(server, check.NotNil)
+
+	s.errg.Go(func() error {
+		server.pdEndpoints = strings.Split(server.opts.pdEndpoints, ",")
+		err := server.etcdHealthChecker(ctx)
+		c.Assert(err, check.Equals, context.Canceled)
+		return nil
+	})
+	// longer than one check tick 3s
+	time.Sleep(time.Second * 4)
+	cancel()
 }
