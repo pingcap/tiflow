2b4e337b0553b8cb55b2c273a455d128befb2310
leoppro <zhaoyilin@pingcap.com>
2021-01-08 16:45:13 +0800

    update test script

--- pkg/processor/state.go
+++ pkg/processor/state.go
@@ -1,11 +1,33 @@
 package processor
 
-import "github.com/pingcap/ticdc/cdc/model"
+import (
+	"github.com/pingcap/ticdc/cdc/model"
+	"github.com/pingcap/ticdc/pkg/orchestrator"
+	"github.com/pingcap/ticdc/pkg/orchestrator/util"
+)
 
 type processorState struct {
-	Owner              model.CaptureID
-	Captures           map[model.CaptureID]*model.CaptureInfo
+	ID                 model.CaptureID
 	ChangefeedStatuses map[model.ChangeFeedID]*model.ChangeFeedStatus
-	TaskPositions      map[model.ChangeFeedID]map[model.CaptureID]*model.TaskPosition
-	TaskStatuses       map[model.ChangeFeedID]map[model.CaptureID]*model.TaskStatus
+	TaskPositions      map[model.ChangeFeedID]*model.TaskPosition
+	TaskStatuses       map[model.ChangeFeedID]*model.TaskStatus
+	Workloads          map[model.ChangeFeedID]*model.WorkloadInfo
+}
+
+func newProcessorState(captureID model.CaptureID) *processorState {
+	return &processorState{
+		ID:                 captureID,
+		ChangefeedStatuses: make(map[model.ChangeFeedID]*model.ChangeFeedStatus),
+		TaskPositions:      make(map[model.ChangeFeedID]*model.TaskPosition),
+		TaskStatuses:       make(map[model.ChangeFeedID]*model.TaskStatus),
+		Workloads:          make(map[model.ChangeFeedID]*model.WorkloadInfo),
+	}
+}
+
+func (p *processorState) Update(key util.EtcdKey, value []byte) error {
+	panic("implement me")
+}
+
+func (p *processorState) GetPatches() []*orchestrator.DataPatch {
+	panic("implement me")
 }
