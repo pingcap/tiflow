cef3548a051cc3b69a2143ce2964b3724700da9c
leoppro <zhaoyilin@pingcap.com>
2021-01-22 11:14:42 +0800

    Apply suggestions from code review

--- tests/multi_changefeed/run.sh
+++ tests/multi_changefeed/run.sh
@@ -51,14 +51,16 @@ function run() {
     run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY --addr "127.0.0.1:8300" --pd $pd_addr
     cdc cli changefeed create --pd=$pd_addr --sink-uri="$SINK_URI" -c "old-value-cf" --config="$CUR/conf/changefeed1.toml"
     cdc cli changefeed create --pd=$pd_addr --sink-uri="$SINK_URI" -c "no-old-value-cf" --config="$CUR/conf/changefeed2.toml"
-
+    echo "c"
     run_sql "CREATE DATABASE multi_changefeed;" ${UP_TIDB_HOST} ${UP_TIDB_PORT}
     run_sql "CREATE table multi_changefeed.t1 (id int primary key, val int);" ${UP_TIDB_HOST} ${UP_TIDB_PORT}
     run_sql "INSERT INTO multi_changefeed.t1 VALUES (1,1);" ${UP_TIDB_HOST} ${UP_TIDB_PORT}
     run_sql "UPDATE multi_changefeed.t1 SET val = 2;" ${UP_TIDB_HOST} ${UP_TIDB_PORT}
     run_sql "DELETE FROM multi_changefeed.t1;" ${UP_TIDB_HOST} ${UP_TIDB_PORT}
 
-    grep "BlockHoleSink: FlushRowChangedEvents" $WORK_DIR/cdc.log
+    echo "a"
+    grep "BlockHoleSink: FlushRowChangedEvents" $WORK_DIR/cdc.log > $WORK_DIR/output.log
+    echo "b"
     cat aabbccdd
 
     cleanup_process $CDC_BINARY
