11ca030c3a8b20a6d5e8a0becb31fb53691be634
ti-srebot <66930949+ti-srebot@users.noreply.github.com>
2021-01-28 02:01:31 +0800

    test: fix unstable multi changefeed integration test cases (#1356) (#1361)
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>

--- tests/multi_changefeed/run.sh
+++ tests/multi_changefeed/run.sh
@@ -11,13 +11,36 @@ SINK_TYPE=$1
 MAX_RETRIES=10
 
 function check_old_value_enabled() {
-    old_value_count=$(grep "EmitRowChangedEvents" "$1/cdc.log" | grep  'pre\-columns\\\":null' | wc -l || true)
     row_logs=$(grep "EmitRowChangedEvents" "$1/cdc.log" || true)
     echo $row_logs
-    # 3 rows include `pre-column` is expected, one for a delete event in the disable-old-value changefeed
-    # two for a delete event and a update event in the enable-old-value changefeed
-    if [[ ! "$old_value_count" -eq 3 ]]; then
-       echo "count: $old_value_count expected: 3"
+
+    # check update rows
+    # check if exist a row include `column` and `pre-column`
+    update_with_old_value_count=$(grep "EmitRowChangedEvents" "$1/cdc.log" | grep 'pre\-columns\\\":\[' | grep '\"columns\\\":\[' | wc -l || true)
+    if [[ "$update_with_old_value_count" -eq 0 ]]; then
+       echo "can't found update row with old value"
+       exit 1
+    fi
+
+    # check if exist a update row without `pre-column`
+    update_without_old_value_count=$(grep "EmitRowChangedEvents" "$1/cdc.log" | grep -v 'pre\-columns\\\":\[' | grep 'value\\\":2' | wc -l || true)
+    if [[ "$update_without_old_value_count" -eq 0 ]]; then
+       echo "can't found update row without old value"
+       exit 1
+    fi
+
+    # check delete rows
+    # check if exist a delete row with a complete `pre-column`
+    delete_with_old_value_count=$(grep "EmitRowChangedEvents" "$1/cdc.log" | grep 'pre\-columns\\\":\[' | grep 'columns\\\":null' | grep 'value\\\":2' | wc -l || true)
+    if [[ "$delete_with_old_value_count" -eq 0 ]]; then
+       echo "can't found delete row with old value"
+       exit 1
+    fi
+
+    # check if exist a delete row without a complete `pre-column`
+    delete_without_old_value_count=$(grep "EmitRowChangedEvents" "$1/cdc.log" | grep 'pre\-columns\\\":\[' | grep 'columns\\\":null' | grep 'value\\\":1},null' | wc -l || true)
+    if [[ "$delete_without_old_value_count" -eq 0 ]]; then
+       echo "can't found delete row without old value"
        exit 1
     fi
 }
