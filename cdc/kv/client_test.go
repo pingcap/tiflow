9d9137a474d6add90c1cb87912bb8c851dfc286f
amyangfei <amyangfei@gmail.com>
2021-01-28 10:11:02 +0800

    fix 4.0 api

--- cdc/kv/client_test.go
+++ cdc/kv/client_test.go
@@ -1500,7 +1500,9 @@ func (s *etcdSuite) TestResolveLock(c *check.C) {
 		wg.Wait()
 	}()
 
-	rpcClient, cluster, pdClient, err := mocktikv.NewTiKVAndPDClient("")
+	cluster := mocktikv.NewCluster()
+	mvccStore := mocktikv.MustNewMVCCStore()
+	rpcClient, pdClient, err := mocktikv.NewTiKVAndPDClient(cluster, mvccStore, "")
 	c.Assert(err, check.IsNil)
 	pdClient = &mockPDClient{Client: pdClient, versionGen: defaultVersionGen}
 	tiStore, err := tikv.NewTestTiKVStore(rpcClient, pdClient, nil, nil, 0)
