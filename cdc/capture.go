5ebd0ab92adadd8b358b4b50c161c20bc12b3087
leoppro <i@leop.pro>
2021-02-01 17:49:05 +0800

    update

--- cdc/capture.go
+++ cdc/capture.go
@@ -187,9 +187,7 @@ func (c *Capture) Resign(ctx context.Context) error {
 
 // Close closes the capture by unregistering it from etcd
 func (c *Capture) Close() error {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	c.etcdClient.Client.Revoke(ctx, c.session.Lease())
-	cancel()
+	// TODO close the capture
 	return c.session.Close()
 }
 
