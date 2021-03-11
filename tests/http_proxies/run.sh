0113f7c5366a8caeb430eb11f1c61e4457cbf762
hillium <yujuncen@pingcap.com>
2021-03-11 13:29:25 +0800

    tests: fix some known problems of CI enviorment

--- tests/http_proxies/run.sh
+++ tests/http_proxies/run.sh
@@ -9,34 +9,39 @@ CDC_BINARY=cdc.test
 TEST_HOST_LIST=(UP_TIDB_HOST UP_PD_HOST_{1..3} UP_TIKV_HOST_{1..3})
 
 function start_proxy() {
-    tools/bin/grpc-dump --port=8080 > $OUR_DIR/packets.dump 2>/dev/null &
+    echo "dumpling grpc packet to $WORK_DIR/packets.dump..."
+    WORK_DIR=$WORK_DIR tools/bin/grpc-dump --port=8080 2>$WORK_DIR/grpc-dump.log 1>$WORK_DIR/packets.dump &
     pid=$!
-    export http_proxy=http://127.0.0.1:8080 \
-        https_proxy=$http_proxy
 }
 
 function prepare() {
-    for host in $TEST_HOST_LIST; do
-        check_is_loopback='import ipaddress; import os; exit(0) if ipaddress.ip_address(os.getenv(u"%")).is_loopback else exit(1)'
-        if echo $host | xargs -I% python3 -c "$check_is_loopback" ; then
-            echo "[WARNING] http_proxies: the host of component $host is loopback, hence proxies would be ignored, skipping this test..."
-            exit 0
-        fi
+    for host in ${TEST_HOST_LIST[@]}; do
+        echo "$host $(printenv $host)"
+        case $(printenv $host) in
+            # Should we handle ::1/128 here?
+            127.*.*.* )
+                echo "[WARNING] http_proxies: the host of component $host is loopback, hence proxies would be ignored, skipping this test..."
+                exit 0
+                ;;
+            *)  ;;
+        esac
     done
-
-    # firstly, let's build grpc-dump...
-    make tools/bin/grpc-dump
-    start_proxy
 
     rm -rf "$WORK_DIR"
     mkdir -p "$WORK_DIR"
     stop_tidb_cluster
     start_tidb_cluster --workdir $WORK_DIR
 
+    # firstly, let's build grpc-dump...
+    make tools/bin/grpc-dump
+    start_proxy
+
+
     cd $WORK_DIR
-    # record tso before we create tables to skip the system table DDLs
     start_ts=$(run_cdc_cli tso query --pd=http://$UP_PD_HOST_1:$UP_PD_PORT_1)
 
+    export http_proxy=http://127.0.0.1:8080 \
+        https_proxy=http://127.0.0.1:8080
     run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY
 
     SINK_URI="blackhole:///"
@@ -44,14 +49,20 @@ function prepare() {
 }
 
 function check() {
-    service_type_count=$(cat $OUR_DIR/packets.dump | jq '.service' | sort | uniq | wc -l)
+    services=$(cat $WORK_DIR/packets.dump | jq '.service' | sort | uniq)
+    service_type_count=$(echo $services | awk '{i+=NF}END{print(i)}' )
     # at least two types: 
     #   "pdpb.PD"
     #   "tikvpb.TiKV"
-    [ $service_type_count -ge 2 ]
+    if ! [ $service_type_count -ge 2 ] ; then
+        echo "services didn't match expected, it is:"
+        echo $services
+        echo "[total count]: $service_type_count (expected >= 2)"
+        exit 1
+    fi
 }
 
-trap stop_tidb_cluster EXIT
+trap "stop_tidb_cluster" EXIT
 trap "killall grpc-dump" EXIT
 
 prepare
