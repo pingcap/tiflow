82b0ae5a6651857326b5cf1ecc4acf4495c281e4
yuqi1129 <yuqi4733@gmail.com>
2021-03-16 17:33:31 +0800

    Fix run make test error

--- scripts/fix_lib_zstd.sh
+++ scripts/fix_lib_zstd.sh
@@ -7,4 +7,10 @@ GO111MODULE=on go mod download ${module}
 # In CI environment, the gopath contains multiple dirs, choose the first one
 cd $(echo ${GOPATH}|awk -F':' '{print $1}')/pkg/mod/${module}/pulsar/internal/compression
 sudo rm zstd_cgo.go
-sudo sed -i '/build !cgo/d' zstd.go
+
+if [[ "$(uname)" == "Darwin" ]]
+then
+  sudo sed -i zstd.go '/build !cgo/d' zstd.go
+else
+  sudo sed -i '/build !cgo/d' zstd.go
+fi
