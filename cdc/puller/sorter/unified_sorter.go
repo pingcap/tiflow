19d765e337ceede6cc1a4fe589e36c4e78938d0e
Zixiong Liu <liuzixiong@gmail.com>
2021-03-25 17:47:36 +0800

    fix

--- cdc/puller/sorter/unified_sorter.go
+++ cdc/puller/sorter/unified_sorter.go
@@ -15,7 +15,10 @@ package sorter
 
 import (
 	"context"
+	"os"
 	"sync"
+
+	cerror "github.com/pingcap/ticdc/pkg/errors"
 
 	"github.com/pingcap/errors"
 	"github.com/pingcap/failpoint"
@@ -43,6 +46,34 @@ type metricsInfo struct {
 }
 
 type ctxKey struct {
+}
+
+// UnifiedSorterCheckDir checks whether the directory needed exists and is writable.
+// If it does not exist, we try to create one.
+// parameter: cfSortDir - the directory designated in changefeed's setting,
+// which will be overridden by a non-empty local setting of `sort-dir`.
+// TODO better way to organize this function after we obsolete chanegfeed setting's `sort-dir`
+func UnifiedSorterCheckDir(cfSortDir string) error {
+	dir := cfSortDir
+	sorterConfig := config.GetGlobalServerConfig().Sorter
+	if sorterConfig.SortDir != "" {
+		// Let the local setting override the changefeed setting
+		dir = sorterConfig.SortDir
+	}
+
+	err := util.IsDirAndWritable(dir)
+	if err != nil {
+		if os.IsNotExist(errors.Cause(err)) {
+			err = os.MkdirAll(dir, 0o755)
+			if err != nil {
+				return errors.Annotate(cerror.WrapError(cerror.ErrProcessorSortDir, err), "create dir")
+			}
+		} else {
+			return errors.Annotate(cerror.WrapError(cerror.ErrProcessorSortDir, err), "sort dir check")
+		}
+	}
+
+	return nil
 }
 
 // NewUnifiedSorter creates a new UnifiedSorter
