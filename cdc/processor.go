f842f48e7ac544e2bec31cd742f08fcce4c44e4a
Zixiong Liu <liuzixiong@pingcap.com>
2021-04-27 23:42:53 +0800

    WIP

--- cdc/processor.go
+++ cdc/processor.go
@@ -1268,7 +1268,7 @@ func runProcessor(
 		cancel()
 		processor.wait()
 		cause := errors.Cause(err)
-		if cause != nil && cerror.ErrProcessorTableCanceled.NotEqual(cause) && cerror.ErrAdminStopProcessor.NotEqual(cause) {
+		if cause != nil && cause != context.Canceled && cerror.ErrAdminStopProcessor.NotEqual(cause) {
 			processorErrorCounter.WithLabelValues(changefeedID, captureInfo.AdvertiseAddr).Inc()
 			log.Error("error on running processor",
 				util.ZapFieldCapture(ctx),
