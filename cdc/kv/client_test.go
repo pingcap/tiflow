179df48f5f7e24a399f4c2c8cd6743a9baee67d3
amyangfei <amyangfei@gmail.com>
2021-04-29 21:30:41 +0800

    refine test cases

--- cdc/kv/client_test.go
+++ cdc/kv/client_test.go
@@ -3132,7 +3132,7 @@ func (s *etcdSuite) TestConcurrentProcessRangeRequest(c *check.C) {
 			count++
 			return true
 		})
-		if count == regionNum-1 {
+		if count == regionNum {
 			return nil
 		}
 		return errors.Errorf("region number %d is not as expected %d", count, regionNum)
@@ -3162,7 +3162,7 @@ checkEvent:
 		select {
 		case <-eventCh:
 			resolvedCount++
-			if resolvedCount == (regionNum-1)*2 {
+			if resolvedCount == regionNum*2 {
 				break checkEvent
 			}
 		case <-time.After(time.Second):
