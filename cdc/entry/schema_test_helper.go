d592806b72aa409273c08c27423f84f2083cb6e5
leoppro <i@leop.pro>
2021-05-11 20:14:24 +0800

    update

---
+++ cdc/entry/schema_test_helper.go
@@ -0,0 +1,64 @@
+package entry
+
+import (
+	"github.com/pingcap/check"
+	timodel "github.com/pingcap/parser/model"
+	ticonfig "github.com/pingcap/tidb/config"
+	"github.com/pingcap/tidb/domain"
+	"github.com/pingcap/tidb/kv"
+	timeta "github.com/pingcap/tidb/meta"
+	"github.com/pingcap/tidb/session"
+	"github.com/pingcap/tidb/store/mockstore"
+	"github.com/pingcap/tidb/store/tikv/oracle"
+	"github.com/pingcap/tidb/util/testkit"
+)
+
+type SchemaTestHelper struct {
+	c       *check.C
+	tk      *testkit.TestKit
+	storage kv.Storage
+	domain  *domain.Domain
+}
+
+func NewSchemaTestHelper(c *check.C) *SchemaTestHelper {
+	store, err := mockstore.NewMockStore()
+	c.Assert(err, check.IsNil)
+	ticonfig.UpdateGlobal(func(conf *ticonfig.Config) {
+		conf.AlterPrimaryKey = true
+	})
+	session.SetSchemaLease(0)
+	session.DisableStats4Test()
+	domain, err := session.BootstrapSession(store)
+	c.Assert(err, check.IsNil)
+	domain.SetStatsUpdating(true)
+	tk := testkit.NewTestKit(c, store)
+	return &SchemaTestHelper{
+		c:       c,
+		tk:      tk,
+		storage: store,
+		domain:  domain,
+	}
+}
+
+func (s *SchemaTestHelper) DDL2Job(ddl string) *timodel.Job {
+	s.tk.MustExec(ddl)
+	jobs,err := s.GetCurrentMeta().GetLastNHistoryDDLJobs(1)
+	s.c.Assert(err,check.IsNil)
+	s.c.Assert(jobs, check.HasLen, 1)
+	return jobs[0]
+}
+
+func (s *SchemaTestHelper) Storage() kv.Storage {
+	return s.storage
+}
+
+func (s *SchemaTestHelper) GetCurrentMeta() *timeta.Meta{
+	ver, err := s.storage.CurrentVersion(oracle.GlobalTxnScope)
+	s.c.Assert(err, check.IsNil)
+	return timeta.NewSnapshotMeta(s.storage.GetSnapshot(ver))
+}
+
+func (s *SchemaTestHelper) Close() {
+	s.domain.Close()
+	s.storage.Close() //nolint:errcheck
+}
--- cdc/owner/schema_test.go
+++ cdc/owner/schema_test.go
@@ -15,7 +15,11 @@ package owner
 
 import (
 	"github.com/pingcap/check"
+	"github.com/pingcap/ticdc/cdc/entry"
+	"github.com/pingcap/ticdc/cdc/model"
 	"github.com/pingcap/ticdc/pkg/config"
+	"github.com/pingcap/tidb/store/tikv/oracle"
+	"sort"
 )
 
 var _ = check.Suite(&schemaSuite{})
@@ -24,7 +28,47 @@ type schemaSuite struct {
 }
 
 func (s *schemaSuite) TestAllPhysicalTables(c *check.C) {
-	schema,err:=newSchemaWrap4Owner(nil,0,config.GetDefaultReplicaConfig())
-	c.Assert(err,check.IsNil)
-	c.Assert(schema.AllPhysicalTables(),check.HasLen,0)
+	helper := entry.NewSchemaTestHelper(c)
+	defer helper.Close()
+	ver, err := helper.Storage().CurrentVersion(oracle.GlobalTxnScope)
+	c.Assert(err, check.IsNil)
+	schema, err := newSchemaWrap4Owner(helper.Storage(), ver.Ver, config.GetDefaultReplicaConfig())
+	c.Assert(err, check.IsNil)
+	c.Assert(schema.AllPhysicalTables(), check.HasLen, 0)
+	// add normal table
+	job:=helper.DDL2Job("create table test.t1(id int primary key)")
+	tableIDT1:=job.BinlogInfo.TableInfo.ID
+	c.Assert(schema.HandleDDL(job), check.IsNil)
+	c.Assert(schema.AllPhysicalTables(), check.DeepEquals, []model.TableID{tableIDT1})
+	// add ineligible table
+	c.Assert(schema.HandleDDL(helper.DDL2Job("create table test.t2(id int)")), check.IsNil)
+	c.Assert(schema.AllPhysicalTables(), check.DeepEquals, []model.TableID{tableIDT1})
+	// add partition table
+	job=helper.DDL2Job(`CREATE TABLE test.employees  (
+			id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+			fname VARCHAR(25) NOT NULL,
+			lname VARCHAR(25) NOT NULL,
+			store_id INT NOT NULL,
+			department_id INT NOT NULL
+		)
+
+		PARTITION BY RANGE(id)  (
+			PARTITION p0 VALUES LESS THAN (5),
+			PARTITION p1 VALUES LESS THAN (10),
+			PARTITION p2 VALUES LESS THAN (15),
+			PARTITION p3 VALUES LESS THAN (20)
+		)`)
+	c.Assert(schema.HandleDDL(job), check.IsNil)
+	expectedTableIDs:=[]model.TableID{tableIDT1}
+	for _,p:=range job.BinlogInfo.TableInfo.GetPartitionInfo().Definitions{
+		expectedTableIDs=append(expectedTableIDs,p.ID)
+	}
+	sortTableIDs:= func(tableIDs []model.TableID) {
+		sort.Slice(tableIDs, func(i, j int) bool {
+			return tableIDs[i]<tableIDs[j]
+		})
+	}
+	sortTableIDs(expectedTableIDs)
+	sortTableIDs(schema.AllPhysicalTables())
+	c.Assert(schema.AllPhysicalTables(), check.DeepEquals, expectedTableIDs)
 }
