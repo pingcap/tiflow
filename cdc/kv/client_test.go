a2c51004d1dfb3ae1bea185b9f71020a7d528a95
amyangfei <amyangfei@gmail.com>
2021-05-25 11:15:38 +0800

    fix tidb api change

--- cdc/kv/client_test.go
+++ cdc/kv/client_test.go
@@ -2863,7 +2863,7 @@ func (s *etcdSuite) TestConcurrentProcessRangeRequest(c *check.C) {
 		wg.Wait()
 	}()
 
-	rpcClient, cluster, pdClient, err := mocktikv.NewTiKVAndPDClient("", mockcopr.NewCoprRPCHandler())
+	rpcClient, cluster, pdClient, err := mocktikv.NewTiKVAndPDClient("")
 	c.Assert(err, check.IsNil)
 	pdClient = &mockPDClient{Client: pdClient, versionGen: defaultVersionGen}
 	tiStore, err := tikv.NewTestTiKVStore(rpcClient, pdClient, nil, nil, 0)
