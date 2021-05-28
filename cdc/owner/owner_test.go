adf08992f66f837ba6ce76870f8db60eb7d3e7fe
leoppro <i@leop.pro>
2021-05-28 10:17:18 +0800

    update

--- cdc/owner/owner_test.go
+++ cdc/owner/owner_test.go
@@ -1,1 +1,82 @@
 package owner
+
+import (
+	"context"
+	"time"
+
+	"github.com/pingcap/check"
+	"github.com/pingcap/ticdc/cdc/model"
+	"github.com/pingcap/ticdc/pkg/config"
+	cdcContext "github.com/pingcap/ticdc/pkg/context"
+	"github.com/pingcap/ticdc/pkg/etcd"
+	"github.com/pingcap/ticdc/pkg/orchestrator"
+	"github.com/pingcap/tidb/store/tikv/oracle"
+)
+
+var _ = check.Suite(&ownerSuite{})
+
+type ownerSuite struct {
+}
+
+func createOwner4Test(ctx cdcContext.Context, c *check.C) (*Owner, *model.GlobalReactorState, *orchestrator.ReactorStateTester) {
+	ctx.GlobalVars().PDClient = &mockPDClient{updateServiceGCSafePointFunc: func(ctx context.Context, serviceID string, ttl int64, safePoint uint64) (uint64, error) {
+		return safePoint, nil
+	}}
+	cf := NewOwner4Test(func(ctx cdcContext.Context, startTs uint64) (DDLPuller, error) {
+		return &mockDDLPuller{resolvedTs: startTs - 1}, nil
+	}, func(ctx cdcContext.Context) (AsyncSink, error) {
+		return &mockAsyncSink{}, nil
+	})
+	state := model.NewGlobalState().(*model.GlobalReactorState)
+	tester := orchestrator.NewReactorStateTester(c, state, nil)
+
+	// set captures
+	cdcKey := etcd.CDCKey{
+		Tp:        etcd.CDCKeyTypeCapture,
+		CaptureID: ctx.GlobalVars().CaptureInfo.ID,
+	}
+	captureBytes, err := ctx.GlobalVars().CaptureInfo.Marshal()
+	c.Assert(err, check.IsNil)
+	tester.MustUpdate(cdcKey.String(), captureBytes)
+	return cf, state, tester
+}
+
+func (s *ownerSuite) TestCreateRemoveChangefeed(c *check.C) {
+	ctx := cdcContext.NewBackendContext4Test(false)
+	owner, state, tester := createOwner4Test(ctx, c)
+	changefeedID := "test-changefeed"
+	changefeedInfo := &model.ChangeFeedInfo{
+		StartTs: oracle.GoTimeToTS(time.Now()),
+		Config:  config.GetDefaultReplicaConfig(),
+	}
+	changefeedStr, err := changefeedInfo.Marshal()
+	c.Assert(err, check.IsNil)
+	cdcKey := etcd.CDCKey{
+		Tp:           etcd.CDCKeyTypeChangefeedInfo,
+		ChangefeedID: changefeedID,
+	}
+	tester.MustUpdate(cdcKey.String(), []byte(changefeedStr))
+	_, err = owner.Tick(ctx, state)
+	tester.MustApplyPatches()
+	c.Assert(err, check.IsNil)
+	c.Assert(owner.changefeeds, check.HasKey, changefeedID)
+
+	// remove changefeed forcibly
+	tester.MustUpdate(cdcKey.String(), nil)
+	_, err = owner.Tick(ctx, state)
+	tester.MustApplyPatches()
+	c.Assert(err, check.IsNil)
+	c.Assert(owner.changefeeds, check.Not(check.HasKey), changefeedID)
+}
+
+func (s *ownerSuite) TestCaptureOffline(c *check.C) {
+
+}
+
+func (s *ownerSuite) TestStopChangefeed(c *check.C) {
+
+}
+
+func (s *ownerSuite) TestAdminJob(c *check.C) {
+
+}
