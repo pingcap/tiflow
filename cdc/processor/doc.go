be7243145717dc8d72aed60a83f80f1979bd16f3
ben1009 <liuhe1009@gmail.com>
2021-05-29 10:29:36 +0800

    docs: fix typo in design of new processor (#1834)

--- cdc/processor/doc.go
+++ cdc/processor/doc.go
@@ -14,15 +14,15 @@
 /*
 Package processor implements the processor logic based on ETCD worker(pkg/orchestrator).
 
-There are three mainly module: Manager, Processor and TablePipeline(cdc/processor/pipeline).
-The Manager's main responsibility is to maintain the Processor's life cycle, like create and destory the processor instances.
+There are three main modules: Manager, Processor and TablePipeline(cdc/processor/pipeline).
+The Manager's main responsibility is to maintain the Processor's life cycle, like create and destroy the processor instances.
 The Processor's main responsibility is to maintain the TablePipeline's life cycle according to the state stored by ETCD,
 and calculate the local resolved TS and local checkpoint Ts and put them into ETCD.
-The TablePipeline listen the kv change logs of a specified table(with its mark table if exist), send logs to Sink After sorting and mounting.
+The TablePipeline listens to the kv change logs of a specified table(with its mark table if it exists), and sends logs to Sink After sorting and mounting.
 
 The relationship between the three module is as follows:
 
-One Capture(with processor role)  -> Processr Manager -> Processor(changefeed1) -> TablePipeline(tableA)
+One Capture(with processor role)  -> Processor Manager -> Processor(changefeed1) -> TablePipeline(tableA)
                                                       ╲                         ╲
                                                        ╲                         -> TablePipeline(tableB)
                                                         ╲
