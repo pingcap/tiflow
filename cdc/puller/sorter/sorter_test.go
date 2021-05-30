55387aaec85349b67bdb251358125b825f46364d
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2021-05-30 11:29:36 +0800

    sorter: fix unstable unit tests (#1855) (#1868)

--- cdc/puller/sorter/sorter_test.go
+++ cdc/puller/sorter/sorter_test.go
@@ -354,7 +354,7 @@ func (s *sorterSuite) TestSorterIOError(c *check.C) {
 
 	finishedCh := make(chan struct{})
 	go func() {
-		err := testSorter(ctx, c, sorter, 100000, true)
+		err := testSorter(ctx, c, sorter, 10000, true)
 		c.Assert(err, check.ErrorMatches, ".*injected alloc error.*")
 		close(finishedCh)
 	}()
@@ -376,7 +376,7 @@ func (s *sorterSuite) TestSorterIOError(c *check.C) {
 
 	finishedCh = make(chan struct{})
 	go func() {
-		err := testSorter(ctx, c, sorter, 100000, true)
+		err := testSorter(ctx, c, sorter, 10000, true)
 		c.Assert(err, check.ErrorMatches, ".*injected write error.*")
 		close(finishedCh)
 	}()
@@ -429,7 +429,7 @@ func (s *sorterSuite) TestSorterErrorReportCorrect(c *check.C) {
 
 	finishedCh := make(chan struct{})
 	go func() {
-		err := testSorter(ctx, c, sorter, 100000, true)
+		err := testSorter(ctx, c, sorter, 10000, true)
 		c.Assert(err, check.ErrorMatches, ".*injected alloc error.*")
 		close(finishedCh)
 	}()
