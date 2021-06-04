b724874d57cec9514593a24c51531a2979e6946e
leoppro <zhaoyilin@pingcap.com>
2021-06-04 17:11:27 +0800

    Update cdc/processor/manager.go
    Co-authored-by: Zixiong Liu <zl2683@columbia.edu>

--- cdc/processor/manager.go
+++ cdc/processor/manager.go
@@ -100,7 +100,7 @@ func (m *Manager) Tick(stdCtx context.Context, state orchestrator.ReactorState) 
 			if changefeedState.Status.AdminJobType.IsStopState() || changefeedState.TaskStatuses[captureID].AdminJobType.IsStopState() {
 				continue
 			}
-			// the processor should start after one table added to this processor at least
+			// the processor should start after at least one table has been added to this capture
 			taskStatus := changefeedState.TaskStatuses[captureID]
 			if taskStatus == nil || (len(taskStatus.Tables) == 0 && len(taskStatus.Operation) == 0) {
 				continue
