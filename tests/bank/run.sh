ad39021ac9e154b64c685738731d66be38ac5336
JinLingChristopher <jinl1037@hotmail.com>
2021-06-08 00:43:05 +0800

    only run bank for mysql, ignore kafka.

--- tests/bank/run.sh
+++ tests/bank/run.sh
@@ -9,17 +9,11 @@ CDC_BINARY=cdc.test
 SINK_TYPE=$1
 
 function prepare() {
-    # kafka is not supported yet.
-    if [ "$SINK_TYPE" == "kafka" ]; then
-      return
-    fi
-
     rm -rf $WORK_DIR && mkdir -p $WORK_DIR
 
     start_tidb_cluster --workdir $WORK_DIR
 
     cd $WORK_DIR
-
 
     # create table to upstream.
     run_sql "CREATE DATABASE bank" ${UP_TIDB_HOST} ${UP_TIDB_PORT}
@@ -31,14 +25,21 @@ function prepare() {
 }
 
 trap stop_tidb_cluster EXIT
-prepare $*
+# kafka is not supported yet.
+if [ "$SINK_TYPE" != "kafka" ]; then
+    prepare $*
 
-cd "$(dirname "$0")"
-set -euxo pipefail
+    cd "$(dirname "$0")"
+    set -euxo pipefail
 
-GO111MODULE=on go run bank.go case.go -u "root@tcp(${UP_TIDB_HOST}:${UP_TIDB_PORT})/bank" \
-    -d "root@tcp(${DOWN_TIDB_HOST}:${DOWN_TIDB_PORT})/bank" \
-    --test-round=20
+    GO111MODULE=on go run bank.go case.go -u "root@tcp(${UP_TIDB_HOST}:${UP_TIDB_PORT})/bank" \
+        -d "root@tcp(${DOWN_TIDB_HOST}:${DOWN_TIDB_PORT})/bank" \
+        --test-round=20
 
-cleanup_process $CDC_BINARY
-echo "[$(date)] <<<<<< run test case $TEST_NAME success! >>>>>>"
+    cleanup_process $CDC_BINARY
+    echo "[$(date)] <<<<<< run test case $TEST_NAME success! >>>>>>"
+fi
+
+
+
+
