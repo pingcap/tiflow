d175e2b0721af2457a8374a4511cea1e144f05e4
JinlingChristopher <jinl1037@hotmail.com>
2021-06-15 17:11:28 +0800

    try to enable failpoint in sorter_test.

--- cdc/puller/sorter/sorter_test.go
+++ cdc/puller/sorter/sorter_test.go
@@ -382,8 +382,10 @@ func (s *sorterSuite) TestSorterIOError(c *check.C) {
 	// enable the failpoint to simulate backEnd write error (usually would happen when writing to a file)
 	err = failpoint.Enable("github.com/pingcap/ticdc/cdc/puller/sorter/InjectErrorBackEndWrite", "return(true)")
 	c.Assert(err, check.IsNil)
+	c.Assert(failpoint.Enable("github.com/pingcap/ticdc/pkg/util/InjectCheckDataDirSatisfied", ""), check.IsNil)
 	defer func() {
 		_ = failpoint.Disable("github.com/pingcap/ticdc/cdc/puller/sorter/InjectErrorBackEndWrite")
+		c.Assert(failpoint.Disable("github.com/pingcap/ticdc/pkg/util/InjectCheckDataDirSatisfied"), check.IsNil)
 	}()
 
 	// recreate the sorter
