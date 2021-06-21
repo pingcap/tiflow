873934118699bf3e0275ef4a4d59af4a302d752e
Ling Jin <JinLingChristopher@users.noreply.github.com>
2021-06-21 14:50:54 +0800

    Update tests/unified_sorter_sort_dir_conflict/run.sh
    Co-authored-by: Neil Shen <overvenus@gmail.com>

--- tests/unified_sorter_sort_dir_conflict/run.sh
+++ tests/unified_sorter_sort_dir_conflict/run.sh
@@ -59,7 +59,7 @@ function prepare() {
     run_sql "CREATE table test.simple4(id int primary key, val int);"
 
     sleep 10
-    # starts the first second server instance. It should fail, and bring down the changefeed， due to sort-dir conflict
+    # starts the first second server instance. It should fail, and bring down the changefeed, due to sort-dir conflict
     run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY --addr "127.0.0.1:8301" --logsuffix 1 --data-dir "${WORK_DIR}"/cdc_data1
 
     ensure $MAX_RETRIES check_changefeed_mark_error_regex http://${UP_PD_HOST_1}:${UP_PD_PORT_1} ${changefeedid} ".*ErrConflictingFileLocks.*"
