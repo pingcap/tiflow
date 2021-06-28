441057877738e993cc24c9691656223743d6effc
hi-rustin <rustin.liu@gmail.com>
2021-06-28 20:14:29 +0800

    Add go.mod

--- tools/check/check-tidy.sh
+++ tools/check/check-tidy.sh
@@ -3,8 +3,8 @@ set -euo pipefail
 
 GO111MODULE=on go mod tidy
 
-if [ "$(git --no-pager diff go.sum | wc -c)" -ne 0 ]; then
+if [ "$(git --no-pager diff go.mod go.sum | wc -c)" -ne 0 ]; then
   echo "Please run \`go mod tidy\` to clean up"
-  git --no-pager diff go.sum
+  git --no-pager diff go.mod go.sum
   exit 1
 fi
