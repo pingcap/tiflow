ecbd7d4e0843b48c8edaf65023ccc145667bb642
super <supeng842499467@gmail.com>
2021-07-06 14:36:18 +0800

    config: Add log configuration items to the config file

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -166,8 +166,8 @@ var defaultServerConfig = &ServerConfig{
 			MaxBackups: 0,
 		},
 	},
-	GcTTL:                  24 * 60 * 60, // 24H
-	TZ:                     "System",
+	GcTTL: 24 * 60 * 60, // 24H
+	TZ:    "System",
 	// The default election-timeout in PD is 3s and minimum session TTL is 5s,
 	// which is calculated by `math.Ceil(3 * election-timeout / 2)`, we choose
 	// default capture session ttl to 10s to increase robust to PD jitter,
