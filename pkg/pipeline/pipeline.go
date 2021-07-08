511189fdeeb340704d49c1cbf0ad88ccbc70cc04
Neil Shen <overvenus@gmail.com>
2021-07-09 00:11:30 +0800

    processor, pipeline: reduce channel buffer
    Signed-off-by: Neil Shen <overvenus@gmail.com>

--- pkg/pipeline/pipeline.go
+++ pkg/pipeline/pipeline.go
@@ -24,12 +24,6 @@ import (
 	"go.uber.org/zap"
 )
 
-// TODO: processor output chan size, the accumulated data is determined by
-// the count of sorted data and unmounted data. In current benchmark a single
-// processor can reach 50k-100k QPS, and accumulated data is around
-// 200k-400k in most cases. We need a better chan cache mechanism.
-const defaultOutputChannelSize = 512
-
 // Pipeline represents a pipeline includes a number of nodes
 type Pipeline struct {
 	header    headRunner
@@ -37,16 +31,21 @@ type Pipeline struct {
 	runnersWg sync.WaitGroup
 	closeMu   sync.Mutex
 	isClosed  bool
+
+	outputChSize int
 }
 
 // NewPipeline creates a new pipeline
-func NewPipeline(ctx context.Context, tickDuration time.Duration) *Pipeline {
+func NewPipeline(
+	ctx context.Context, tickDuration time.Duration, runnerSize, outputChSize int,
+) *Pipeline {
 	header := make(headRunner, 4)
-	runners := make([]runner, 0, 16)
+	runners := make([]runner, 0, runnerSize)
 	runners = append(runners, header)
 	p := &Pipeline{
-		header:  header,
-		runners: runners,
+		header:       header,
+		runners:      runners,
+		outputChSize: outputChSize,
 	}
 	go func() {
 		var tickCh <-chan time.Time
@@ -81,7 +80,7 @@ func (p *Pipeline) AppendNode(ctx context.Context, name string, node Node) {
 		return err
 	})
 	lastRunner := p.runners[len(p.runners)-1]
-	runner := newNodeRunner(name, node, lastRunner)
+	runner := newNodeRunner(name, node, lastRunner, p.outputChSize)
 	p.runners = append(p.runners, runner)
 	p.runnersWg.Add(1)
 	go p.driveRunner(ctx, lastRunner, runner)
