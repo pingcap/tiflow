4c7d9a65302ffb61179ef09b6ba33bb94089307d
Ling Jin <jinl1037@hotmail.com>
2021-07-12 16:55:03 +0800

    remove metric monite when handle admin job.

--- cdc/owner.go
+++ cdc/owner.go
@@ -695,7 +695,6 @@ func (o *Owner) loadChangeFeeds(ctx context.Context) error {
 
 		o.changeFeeds[changeFeedID] = newCf
 		changefeedStatusGauge.WithLabelValues(string(model.StateNormal)).Inc()
-
 		if _, ok := o.stoppedFeeds[changeFeedID]; ok {
 			delete(o.stoppedFeeds, changeFeedID)
 			changefeedStatusGauge.WithLabelValues(string(model.StateStopped)).Dec()
@@ -1120,10 +1119,6 @@ func (o *Owner) handleAdminJob(ctx context.Context) error {
 			if err != nil {
 				return errors.Trace(err)
 			}
-
-			changefeedStatusGauge.WithLabelValues(string(feedState)).Dec()
-			changefeedStatusGauge.WithLabelValues(string(model.StateStopped)).Inc()
-
 			err = o.dispatchJob(ctx, job)
 			if err != nil {
 				return errors.Trace(err)
@@ -1179,8 +1174,6 @@ func (o *Owner) handleAdminJob(ctx context.Context) error {
 					return errors.Trace(err)
 				}
 			}
-			changefeedStatusGauge.WithLabelValues(string(feedState)).Dec()
-			changefeedStatusGauge.WithLabelValues(string(model.StateFinished)).Inc()
 		case model.AdminResume:
 			// resume changefeed must read checkpoint from ChangeFeedStatus
 			if cerror.ErrChangeFeedNotExists.Equal(err) {
@@ -1207,9 +1200,6 @@ func (o *Owner) handleAdminJob(ctx context.Context) error {
 			cfInfo.AdminJobType = model.AdminResume
 			// clear last running error
 			cfInfo.State = model.StateNormal
-			changefeedStatusGauge.WithLabelValues(string(feedState)).Dec()
-			changefeedStatusGauge.WithLabelValues(string(model.StateNormal)).Inc()
-
 			cfInfo.Error = nil
 			err = o.etcdClient.LeaseGuardSaveChangeFeedInfo(ctx, cfInfo, job.CfID, o.session.Lease())
 			if err != nil {
