111f132da3867638fcdaf5328a287b93c1bd7f9d
hi-rustin <rustin.liu@gmail.com>
2021-07-12 18:13:54 +0800

    refactor(cli): sort imports and add comments for tso command

--- pkg/cmd/cli/tso/tso_query.go
+++ pkg/cmd/cli/tso/tso_query.go
@@ -1,14 +1,29 @@
+// Copyright 2021 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package tso
 
 import (
+	"os"
+
 	"github.com/pingcap/ticdc/pkg/cmd/context"
 	"github.com/pingcap/ticdc/pkg/cmd/util"
 	"github.com/spf13/cobra"
 	"github.com/tikv/client-go/v2/oracle"
-	"os"
 )
 
-func NewCmdQueryTso(f util.Factory) *cobra.Command {
+// newCmdQueryTso creates the `cli tso query` command.
+func newCmdQueryTso(f util.Factory) *cobra.Command {
 	command := &cobra.Command{
 		Use:   "query",
 		Short: "Get tso from PD",
