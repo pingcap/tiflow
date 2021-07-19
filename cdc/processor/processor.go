d748922e2ca8ea31fc608bdfafd55feaa5658d74
二手掉包工程师 <rustin.liu@gmail.com>
2021-07-19 13:35:35 +0800

    puller: clean up unused parameter (#2309)

--- cdc/processor/processor.go
+++ cdc/processor/processor.go
@@ -460,7 +460,7 @@ func (p *processor) createAndDriveSchemaStorage(ctx cdcContext.Context) (entry.S
 		ctx.GlobalVars().PDClient,
 		conf.Security,
 		ctx.GlobalVars().KVStorage,
-		checkpointTs, ddlspans, p.limitter, false)
+		checkpointTs, ddlspans, false)
 	meta, err := kv.GetSnapshotMeta(kvStorage, checkpointTs)
 	if err != nil {
 		return nil, errors.Trace(err)
