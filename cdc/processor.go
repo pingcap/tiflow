cd7cf4c536b85084faa663f125753605324bc600
ben1009 <liuhe1009@gmail.com>
2021-07-19 12:01:34 +0800

    This is an automated cherry-pick of #1928
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- cdc/processor.go
+++ cdc/processor.go
@@ -61,10 +61,14 @@ const (
 
 	schemaStorageGCLag = time.Minute * 20
 
+<<<<<<< HEAD
 	// for better sink performance under flow control
 	resolvedTsInterpolateInterval = 200 * time.Millisecond
 	flushMemoryMetricsDuration    = time.Second * 5
 	flowControlOutChSize          = 128
+=======
+	maxTries = 3
+>>>>>>> 283fa588 (sink: fix issue, Fail-fast for unrecoverable DML errors (#1928))
 )
 
 type oldProcessor struct {
@@ -324,7 +328,7 @@ func (p *oldProcessor) positionWorker(ctx context.Context) error {
 	lastFlushTime := time.Now()
 	retryFlushTaskStatusAndPosition := func() error {
 		t0Update := time.Now()
-		err := retry.Run(500*time.Millisecond, 3, func() error {
+		err := retry.Do(ctx, func() error {
 			inErr := p.flushTaskStatusAndPosition(ctx)
 			if inErr != nil {
 				if errors.Cause(inErr) != context.Canceled {
@@ -337,11 +341,11 @@ func (p *oldProcessor) positionWorker(ctx context.Context) error {
 					logError("update info failed", util.ZapFieldChangefeed(ctx), errField)
 				}
 				if p.isStopped() || cerror.ErrAdminStopProcessor.Equal(inErr) {
-					return backoff.Permanent(cerror.ErrAdminStopProcessor.FastGenByArgs())
+					return cerror.ErrAdminStopProcessor.FastGenByArgs()
 				}
 			}
 			return inErr
-		})
+		}, retry.WithBackoffBaseDelay(500), retry.WithMaxTries(maxTries), retry.WithIsRetryableErr(isRetryable))
 		updateInfoDuration.
 			WithLabelValues(p.captureInfo.AdvertiseAddr).
 			Observe(time.Since(t0Update).Seconds())
@@ -431,6 +435,10 @@ func (p *oldProcessor) positionWorker(ctx context.Context) error {
 			lastFlushTime = time.Now()
 		}
 	}
+}
+
+func isRetryable(err error) bool {
+	return cerror.IsRetryableError(err) && cerror.ErrAdminStopProcessor.NotEqual(err)
 }
 
 func (p *oldProcessor) ddlPullWorker(ctx context.Context) error {
@@ -769,13 +777,14 @@ func (p *oldProcessor) addTable(ctx context.Context, tableID int64, replicaInfo 
 	defer p.stateMu.Unlock()
 
 	var tableName string
-	err := retry.Run(time.Millisecond*5, 3, func() error {
+
+	err := retry.Do(ctx, func() error {
 		if name, ok := p.schemaStorage.GetLastSnapshot().GetTableNameByID(tableID); ok {
 			tableName = name.QuoteString()
 			return nil
 		}
 		return errors.Errorf("failed to get table name, fallback to use table id: %d", tableID)
-	})
+	}, retry.WithBackoffBaseDelay(5), retry.WithMaxTries(maxTries), retry.WithIsRetryableErr(cerror.IsRetryableError))
 	if err != nil {
 		log.Warn("get table name for metric", util.ZapFieldChangefeed(ctx), zap.String("error", err.Error()))
 		tableName = strconv.Itoa(int(tableID))
