2dd3c924522b9544c2eb2809d69e2fb796a9187e
二手掉包工程师 <rustin.liu@gmail.com>
2021-07-19 13:35:35 +0800

    This is an automated cherry-pick of #2309
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- cdc/processor.go
+++ cdc/processor.go
@@ -174,7 +174,7 @@ func newProcessor(
 		return nil, errors.Trace(err)
 	}
 	ddlspans := []regionspan.Span{regionspan.GetDDLSpan(), regionspan.GetAddIndexDDLSpan()}
-	ddlPuller := puller.NewPuller(ctx, pdCli, credential, kvStorage, checkpointTs, ddlspans, limitter, false)
+	ddlPuller := puller.NewPuller(ctx, pdCli, credential, kvStorage, checkpointTs, ddlspans, false)
 	filter, err := filter.NewFilter(changefeed.Config)
 	if err != nil {
 		return nil, errors.Trace(err)
@@ -833,7 +833,7 @@ func (p *oldProcessor) addTable(ctx context.Context, tableID int64, replicaInfo 
 		// NOTICE: always pull the old value internally
 		// See also: TODO(hi-rustin): add issue link here.
 		plr := puller.NewPuller(ctx, p.pdCli, p.credential, kvStorage,
-			replicaInfo.StartTs, []regionspan.Span{span}, p.limitter,
+			replicaInfo.StartTs, []regionspan.Span{span},
 			true)
 		go func() {
 			err := plr.Run(ctx)
