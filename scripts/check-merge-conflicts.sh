6e0fb889ae9dc1bc5db0e4db5c95e6dc57121ef3
Neil Shen <overvenus@gmail.com>
2021-07-20 00:31:19 +0800

    scripts: check merge conflicts
    Signed-off-by: Neil Shen <overvenus@gmail.com>

---
+++ scripts/check-merge-conflicts.sh
@@ -0,0 +1,11 @@
+result=$(find ./ -type f \( -iname \*.go -o -iname \*.json -o -iname \*.sh \) | while read file_path; do
+    if [[ `grep -E "^<<<<<<< HEAD$" "${file_path}"` ]]; then
+        echo "${file_path}"
+    fi
+done)
+
+if [ -n "$result" ]; then
+    echo "Merge conflicts detected:"
+    echo "$result"
+    exit 1
+fi
