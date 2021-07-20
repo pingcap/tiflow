a9cd394766c6a296e9f058894817abf85dffeef4
hi-rustin <rustin.liu@gmail.com>
2021-07-20 16:16:01 +0800

    refactor(cli): add client construction factory abstraction

---
+++ pkg/cmd/factory/factory.go
@@ -0,0 +1,102 @@
+// Copyright 2021 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package factory
+
+import (
+	"crypto/tls"
+
+	"github.com/pingcap/errors"
+	"github.com/pingcap/ticdc/cdc/kv"
+	"github.com/pingcap/ticdc/pkg/security"
+	"github.com/spf13/cobra"
+	pd "github.com/tikv/pd/client"
+	"google.golang.org/grpc"
+)
+
+// Factory defines the client-side construction factory.
+type Factory interface {
+	ClientGetter
+	EtcdClient() (*kv.CDCEtcdClient, error)
+	PdClient() (pd.Client, error)
+}
+
+// ClientGetter defines the client getter.
+type ClientGetter interface {
+	ToTLSConfig() (*tls.Config, error)
+	ToGRPCDialOption() (grpc.DialOption, error)
+	GetPdAddr() string
+	GetCredential() *security.Credential
+}
+
+// ClientFlags specifies the parameters needed to construct the client.
+type ClientFlags struct {
+	cliPdAddr string
+	caPath    string
+	certPath  string
+	keyPath   string
+}
+
+var _ ClientGetter = &ClientFlags{}
+
+// ToTLSConfig returns the configuration of tls.
+func (c *ClientFlags) ToTLSConfig() (*tls.Config, error) {
+	credential := c.GetCredential()
+	tlsConfig, err := credential.ToTLSConfig()
+	if err != nil {
+		return nil, errors.Annotate(err, "fail to validate TLS settings")
+	}
+	return tlsConfig, nil
+}
+
+// ToGRPCDialOption returns the option of GRPC dial.
+func (c *ClientFlags) ToGRPCDialOption() (grpc.DialOption, error) {
+	credential := c.GetCredential()
+	grpcTLSOption, err := credential.ToGRPCDialOption()
+	if err != nil {
+		return nil, errors.Annotate(err, "fail to validate TLS settings")
+	}
+
+	return grpcTLSOption, nil
+}
+
+// GetPdAddr returns pd address.
+func (c *ClientFlags) GetPdAddr() string {
+	return c.cliPdAddr
+}
+
+// NewClientFlags creates new client flags.
+func NewClientFlags() *ClientFlags {
+	return &ClientFlags{}
+}
+
+// AddFlags receives a *cobra.Command reference and binds
+// flags related to template printing to it.
+func (c *ClientFlags) AddFlags(cmd *cobra.Command) {
+	cmd.PersistentFlags().StringVar(&c.cliPdAddr, "pd", "http://127.0.0.1:2379", "PD address, use ',' to separate multiple PDs")
+	cmd.PersistentFlags().StringVar(&c.caPath, "ca", "", "CA certificate path for TLS connection")
+	cmd.PersistentFlags().StringVar(&c.certPath, "cert", "", "Certificate path for TLS connection")
+	cmd.PersistentFlags().StringVar(&c.keyPath, "key", "", "Private key path for TLS connection")
+}
+
+// GetCredential returns credential.
+func (c *ClientFlags) GetCredential() *security.Credential {
+	var certAllowedCN []string
+
+	return &security.Credential{
+		CAPath:        c.caPath,
+		CertPath:      c.certPath,
+		KeyPath:       c.keyPath,
+		CertAllowedCN: certAllowedCN,
+	}
+}
---
+++ pkg/cmd/factory/factory_impl.go
@@ -0,0 +1,148 @@
+// Copyright 2021 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package factory
+
+import (
+	"crypto/tls"
+	"strings"
+	"time"
+
+	"github.com/pingcap/errors"
+	"github.com/pingcap/ticdc/cdc/kv"
+	cmdconetxt "github.com/pingcap/ticdc/pkg/cmd/context"
+	"github.com/pingcap/ticdc/pkg/security"
+	"github.com/pingcap/ticdc/pkg/version"
+	pd "github.com/tikv/pd/client"
+	"go.etcd.io/etcd/clientv3"
+	etcdlogutil "go.etcd.io/etcd/pkg/logutil"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/backoff"
+)
+
+type factoryImpl struct {
+	clientGetter ClientGetter
+}
+
+// NewFactory creates a client build factory.
+func NewFactory(clientGetter ClientGetter) Factory {
+	if clientGetter == nil {
+		panic("attempt to instantiate factory with nil clientGetter")
+	}
+	f := &factoryImpl{
+		clientGetter: clientGetter,
+	}
+
+	return f
+}
+
+func (f *factoryImpl) ToTLSConfig() (*tls.Config, error) {
+	return f.clientGetter.ToTLSConfig()
+}
+
+func (f *factoryImpl) ToGRPCDialOption() (grpc.DialOption, error) {
+	return f.clientGetter.ToGRPCDialOption()
+}
+
+func (f *factoryImpl) GetPdAddr() string {
+	return f.clientGetter.GetPdAddr()
+}
+
+func (f *factoryImpl) GetCredential() *security.Credential {
+	return f.clientGetter.GetCredential()
+}
+
+func (f *factoryImpl) EtcdClient() (*kv.CDCEtcdClient, error) {
+	ctx := cmdconetxt.GetDefaultContext()
+
+	tlsConfig, err := f.ToTLSConfig()
+	if err != nil {
+		return nil, err
+	}
+	grpcTLSOption, err := f.ToGRPCDialOption()
+	if err != nil {
+		return nil, err
+	}
+
+	logConfig := etcdlogutil.DefaultZapLoggerConfig
+	logConfig.Level = zap.NewAtomicLevelAt(zapcore.ErrorLevel)
+	pdEndpoints := strings.Split(f.GetPdAddr(), ",")
+
+	etcdClient, err := clientv3.New(clientv3.Config{
+		Context:     ctx,
+		Endpoints:   pdEndpoints,
+		TLS:         tlsConfig,
+		LogConfig:   &logConfig,
+		DialTimeout: 30 * time.Second,
+		DialOptions: []grpc.DialOption{
+			grpcTLSOption,
+			grpc.WithBlock(),
+			grpc.WithConnectParams(grpc.ConnectParams{
+				Backoff: backoff.Config{
+					BaseDelay:  time.Second,
+					Multiplier: 1.1,
+					Jitter:     0.1,
+					MaxDelay:   3 * time.Second,
+				},
+				MinConnectTimeout: 3 * time.Second,
+			}),
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	client := kv.NewCDCEtcdClient(ctx, etcdClient)
+	return &client, nil
+}
+
+func (f factoryImpl) PdClient() (pd.Client, error) {
+	ctx := cmdconetxt.GetDefaultContext()
+
+	credential := f.GetCredential()
+	grpcTLSOption, err := f.ToGRPCDialOption()
+	if err != nil {
+		return nil, err
+	}
+
+	pdAddr := f.GetPdAddr()
+	pdEndpoints := strings.Split(pdAddr, ",")
+
+	pdClient, err := pd.NewClientWithContext(
+		ctx, pdEndpoints, credential.PDSecurityOption(),
+		pd.WithGRPCDialOptions(
+			grpcTLSOption,
+			grpc.WithBlock(),
+			grpc.WithConnectParams(grpc.ConnectParams{
+				Backoff: backoff.Config{
+					BaseDelay:  time.Second,
+					Multiplier: 1.1,
+					Jitter:     0.1,
+					MaxDelay:   3 * time.Second,
+				},
+				MinConnectTimeout: 3 * time.Second,
+			}),
+		))
+	if err != nil {
+		return nil, errors.Annotatef(err, "fail to open PD client, pd=\"%s\"", pdAddr)
+	}
+
+	err = version.CheckClusterVersion(ctx, pdClient, pdEndpoints[0], credential, true)
+	if err != nil {
+		return nil, err
+	}
+
+	return pdClient, nil
+}
