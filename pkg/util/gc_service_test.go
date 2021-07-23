27ee305ad064d44bff6b518d07e5908def697178
Ling Jin <jinl1037@hotmail.com>
2021-07-23 15:43:33 +0800

    add test for retry .

--- pkg/util/gc_service_test.go
+++ pkg/util/gc_service_test.go
@@ -18,21 +18,24 @@ import (
 	"math"
 
 	"github.com/pingcap/check"
+	"github.com/pingcap/errors"
 	"github.com/pingcap/ticdc/pkg/util/testleak"
 	pd "github.com/tikv/pd/client"
 )
 
 type gcServiceSuite struct {
-	pdCli mockPdClientForServiceGCSafePoint
+	pdCli *mockPdClientForServiceGCSafePoint
 }
 
 var _ = check.Suite(&gcServiceSuite{
-	mockPdClientForServiceGCSafePoint{serviceSafePoint: make(map[string]uint64)},
+	&mockPdClientForServiceGCSafePoint{serviceSafePoint: make(map[string]uint64)},
 })
 
 func (s *gcServiceSuite) TestCheckSafetyOfStartTs(c *check.C) {
 	defer testleak.AfterTest(c)()
 	ctx := context.Background()
+
+	// assume no pd leader switch
 	s.pdCli.UpdateServiceGCSafePoint(ctx, "service1", 10, 60) //nolint:errcheck
 	err := CheckSafetyOfStartTs(ctx, s.pdCli, "changefeed1", 50)
 	c.Assert(err.Error(), check.Equals, "[CDC:ErrStartTsBeforeGC]fail to create changefeed because start-ts 50 is earlier than GC safepoint at 60")
@@ -46,15 +49,45 @@ func (s *gcServiceSuite) TestCheckSafetyOfStartTs(c *check.C) {
 		"service3":                   70,
 		"ticdc-creating-changefeed2": 65,
 	})
+
+	// simulate pd client leader switch
+	// only can retry one time, and then success
+	s.pdCli.enableLeaderSwitch = true
+
+	s.pdCli.retryThresh = 1
+	s.pdCli.retryCount = 0
+	err = CheckSafetyOfStartTs(ctx, s.pdCli, "changefeed2", 65)
+	c.Assert(err, check.IsNil)
+
+	// try 8 time, then failed due to exceed retry limit
+	s.pdCli.retryThresh = 8
+	s.pdCli.retryCount = 0
+	err = CheckSafetyOfStartTs(ctx, s.pdCli, "changefeed2", 65)
+	c.Assert(err, check.Not(check.IsNil))
+	c.Assert(err.Error(), check.Equals, "[CDC:ErrReachMaxTry]reach maximum try: 8")
+
+	// retry 3 time, failed due to bad start-ts
+	s.pdCli.retryThresh = 3
+	s.pdCli.retryCount = 0
+	err = CheckSafetyOfStartTs(ctx, s.pdCli, "changefeed1", 50)
+	c.Assert(err.Error(), check.Equals, "[CDC:ErrStartTsBeforeGC]fail to create changefeed because start-ts 50 is earlier than GC safepoint at 60")
 }
 
 type mockPdClientForServiceGCSafePoint struct {
 	pd.Client
-	serviceSafePoint map[string]uint64
+	serviceSafePoint   map[string]uint64
+	enableLeaderSwitch bool
+	retryCount         int
+	retryThresh        int
 }
 
-func (m mockPdClientForServiceGCSafePoint) UpdateServiceGCSafePoint(ctx context.Context, serviceID string, ttl int64, safePoint uint64) (uint64, error) {
+func (m *mockPdClientForServiceGCSafePoint) UpdateServiceGCSafePoint(ctx context.Context, serviceID string, ttl int64, safePoint uint64) (uint64, error) {
+	defer func() { m.retryCount++ }()
 	minSafePoint := uint64(math.MaxUint64)
+	if m.enableLeaderSwitch && m.retryCount < m.retryThresh {
+		return minSafePoint, errors.New("not pd leader")
+	}
+
 	for _, safePoint := range m.serviceSafePoint {
 		if minSafePoint > safePoint {
 			minSafePoint = safePoint
