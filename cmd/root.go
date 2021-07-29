287cccc23180d60a5e0a979e364b9ac884be5fbf
hi-rustin <rustin.liu@gmail.com>
2021-07-29 18:47:37 +0800

    server: fmt

--- cmd/root.go
+++ cmd/root.go
@@ -31,7 +31,7 @@ func Execute() {
 	// Outputs cmd.Print to stdout.
 	rootCmd.SetOut(os.Stdout)
 
-	//rootCmd.AddCommand(server.NewCmdServer())
+	// rootCmd.AddCommand(server.NewCmdServer())
 	rootCmd.AddCommand(version.NewCmdVersion())
 
 	if err := rootCmd.Execute(); err != nil {
