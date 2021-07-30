61c94f7db3933f10ac5cb0a652194a589dbfa57a
Neil Shen <overvenus@gmail.com>
2021-07-30 15:35:13 +0800

    workerpool: use task value to reduce GC pressure
    Signed-off-by: Neil Shen <overvenus@gmail.com>

--- pkg/workerpool/pool_impl.go
+++ pkg/workerpool/pool_impl.go
@@ -125,7 +125,7 @@ func (h *defaultEventHandle) AddEvent(ctx context.Context, event interface{}) er
 
 	failpoint.Inject("addEventDelayPoint", func() {})
 
-	task := &task{
+	task := task{
 		handle: h,
 		f: func(ctx1 context.Context) error {
 			// Here we merge the context passed down from WorkerPool.Run,
@@ -246,7 +246,7 @@ type task struct {
 }
 
 type worker struct {
-	taskCh       chan *task
+	taskCh       chan task
 	handles      map[*defaultEventHandle]struct{}
 	handleRWLock sync.RWMutex
 	// A message is passed to handleCancelCh when we need to wait for the
@@ -260,7 +260,7 @@ type worker struct {
 
 func newWorker() *worker {
 	return &worker{
-		taskCh:         make(chan *task, 128),
+		taskCh:         make(chan task, 128),
 		handles:        make(map[*defaultEventHandle]struct{}),
 		handleCancelCh: make(chan struct{}), // this channel must be unbuffered, i.e. blocking
 	}
@@ -280,9 +280,6 @@ func (w *worker) run(ctx context.Context) error {
 		case <-ctx.Done():
 			return errors.Trace(ctx.Err())
 		case task := <-w.taskCh:
-			if task == nil {
-				return cerrors.ErrWorkerPoolEmptyTask.GenWithStackByArgs()
-			}
 			if atomic.LoadInt32(&task.handle.isCancelled) == 1 {
 				// ignored cancelled handle
 				continue
