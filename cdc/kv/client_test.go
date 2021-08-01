8d259ce24c6a6c387b0e79f71de6aea3b71b6ada
amyangfei <amyangfei@gmail.com>
2021-08-01 12:01:20 +0800

    remove error check in mock server

--- cdc/kv/client_test.go
+++ cdc/kv/client_test.go
@@ -245,8 +245,7 @@ loop:
 			if e == nil {
 				break loop
 			}
-			err := server.Send(e)
-			s.c.Assert(err, check.IsNil)
+			server.Send(e) //nolint:errcheck
 		case <-notify.notify:
 			break loop
 		}
