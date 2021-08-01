e94e1f72e4f83e17582fda2982f1ff446e4af523
amyangfei <amyangfei@gmail.com>
2021-08-01 11:30:10 +0800

    fix 5.0 api

--- cdc/kv/client_test.go
+++ cdc/kv/client_test.go
@@ -3241,7 +3241,7 @@ func (s *etcdSuite) TestRegionWorkerExitWhenIsIdle(c *check.C) {
 		}
 	}
 
-	rpcClient, cluster, pdClient, err := mocktikv.NewTiKVAndPDClient("", mockcopr.NewCoprRPCHandler())
+	rpcClient, cluster, pdClient, err := mocktikv.NewTiKVAndPDClient("")
 	c.Assert(err, check.IsNil)
 	pdClient = &mockPDClient{Client: pdClient, versionGen: defaultVersionGen}
 	tiStore, err := tikv.NewTestTiKVStore(rpcClient, pdClient, nil, nil, 0)
