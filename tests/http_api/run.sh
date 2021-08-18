5c4277441cb6924e50a0de762aa14ccf10fcf439
asddongmen <414110582@qq.com>
2021-08-18 11:00:46 +0800

    tests: add integration tests for http api

---
+++ tests/http_api/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+CUR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+source $CUR/../_utils/test_prepare
+WORK_DIR=$OUT_DIR/$TEST_NAME
+CDC_BINARY=cdc.test
+SINK_TYPE=$1
+TLS_DIR=$( cd $CUR/../_certificates && pwd )
+
