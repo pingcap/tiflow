0db5d9ad404c04cf83953c27c005e77e13d8a58b
asddongmen <414110582@qq.com>
2021-08-18 17:29:05 +0800

    tests: fix http api integration test error

--- tests/http_api/run.sh
+++ tests/http_api/run.sh
@@ -20,6 +20,7 @@ function run() {
     pd_addr="http://$UP_PD_HOST_1:$UP_PD_PORT_1"
 
     run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY
+    sleep 1
 
     TOPIC_NAME="ticdc-http-api-test-$RANDOM"
     case $SINK_TYPE in
