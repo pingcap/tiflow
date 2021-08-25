22bbde257ca60c2af6a7e2d92a7e309921742c16
hi-rustin <rustin.liu@gmail.com>
2021-08-25 15:07:21 +0800

    cli(refactor): fix import

--- pkg/cmd/cli/cli_changefeed_update.go
+++ pkg/cmd/cli/cli_changefeed_update.go
@@ -17,11 +17,10 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/pingcap/ticdc/cdc/model"
-
 	"github.com/pingcap/errors"
 	"github.com/pingcap/log"
 	"github.com/pingcap/ticdc/cdc/kv"
+	"github.com/pingcap/ticdc/cdc/model"
 	cmdcontext "github.com/pingcap/ticdc/pkg/cmd/context"
 	"github.com/pingcap/ticdc/pkg/cmd/factory"
 	"github.com/pingcap/ticdc/pkg/security"
