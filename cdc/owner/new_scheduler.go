8c5f731181a7100f30798048d9a7db915ff9ee85
Zixiong Liu <liuzixiong@pingcap.com>
2021-09-08 14:15:16 +0800

    WIP

---
+++ cdc/owner/new_scheduler.go
@@ -0,0 +1,39 @@
+// Copyright 2021 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package owner
+
+import (
+	"github.com/pingcap/ticdc/cdc/model"
+	"github.com/pingcap/ticdc/pkg/p2p"
+)
+
+type schedulerV2 struct {
+	state *model.ChangefeedReactorState
+	messageServer *p2p.MessageServer
+	messageRouter p2p.MessageRouter
+
+	
+}
+
+func (s *schedulerV2) Tick(state *model.ChangefeedReactorState, currentTables []model.TableID, captures map[model.CaptureID]*model.CaptureInfo) {
+
+}
+
+func (s *schedulerV2) MoveTable(tableID model.TableID, target model.CaptureID) {
+
+}
+
+func (s *schedulerV2) Rebalance() {
+
+}
--- go.mod
+++ go.mod
@@ -44,6 +44,7 @@ require (
 	github.com/pingcap/tidb-tools v5.0.3+incompatible
 	github.com/prometheus/client_golang v1.5.1
 	github.com/r3labs/diff v1.1.0
+	github.com/soheilhy/cmux v0.1.4 // indirect
 	github.com/spf13/cobra v1.0.0
 	github.com/spf13/pflag v1.0.5
 	github.com/stretchr/testify v1.7.0
