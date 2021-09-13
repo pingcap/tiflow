807c9f106050f51c7d6e7494ab1a0adf0418c548
二手掉包工程师 <rustin.liu@gmail.com>
2021-09-13 14:06:40 +0800

    This is an automated cherry-pick of #2798
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- tests/availability/owner.sh
+++ tests/availability/owner.sh
@@ -10,6 +10,7 @@ CDC_BINARY=cdc.test
 MAX_RETRIES=10
 
 function test_owner_ha() {
+<<<<<<< HEAD
     test_kill_owner
     test_hang_up_owner
     test_expire_owner
@@ -18,11 +19,20 @@ function test_owner_ha() {
     # test_owner_cleanup_stale_tasks
     test_owner_retryable_error
     test_gap_between_watch_capture
+=======
+	test_kill_owner
+	test_hang_up_owner
+	test_expire_owner
+	test_owner_cleanup_stale_tasks
+	test_owner_retryable_error
+	test_gap_between_watch_capture
+>>>>>>> 257041655 (lint: add shfmt to format sh scripts (#2798))
 }
 # test_kill_owner starts two captures and kill the owner
 # we expect the live capture will be elected as the new
 # owner
 function test_kill_owner() {
+<<<<<<< HEAD
     echo "run test case test_kill_owner"
     # start a capture server
     run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY --logsuffix test_kill_owner.server1
@@ -47,14 +57,41 @@ function test_kill_owner() {
     echo "test_kill_owner: pass"
 
     cleanup_process $CDC_BINARY
+=======
+	echo "run test case test_kill_owner"
+	# start a capture server
+	run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY --logsuffix test_kill_owner.server1
+	# ensure the server become the owner
+	ensure $MAX_RETRIES "$CDC_BINARY cli capture list --disable-version-check 2>&1 | grep '\"is-owner\": true'"
+	owner_pid=$(ps -C $CDC_BINARY -o pid= | awk '{print $1}')
+	owner_id=$($CDC_BINARY cli capture list --disable-version-check 2>&1 | awk -F '"' '/id/{print $4}')
+	echo "owner pid:" $owner_pid
+	echo "owner id" $owner_id
+
+	# run another server
+	run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY --addr "127.0.0.1:8301" --logsuffix test_kill_owner.server2
+	ensure $MAX_RETRIES "$CDC_BINARY cli capture list --disable-version-check 2>&1 | grep -v \"$owner_id\" | grep id"
+	capture_id=$($CDC_BINARY cli capture list --disable-version-check 2>&1 | awk -F '"' '/id/{print $4}' | grep -v "$owner_id")
+	echo "capture_id:" $capture_id
+
+	# kill the server
+	kill $owner_pid
+
+	# check that the new owner is elected
+	ensure $MAX_RETRIES "$CDC_BINARY cli capture list --disable-version-check 2>&1 |grep $capture_id -A1 | grep '\"is-owner\": true'"
+	echo "test_kill_owner: pass"
+
+	cleanup_process $CDC_BINARY
+>>>>>>> 257041655 (lint: add shfmt to format sh scripts (#2798))
 }
 
 # test_hang_up_owner starts two captures and stops the owner
 # by sending a SIGSTOP signal.
 # We expect another capture will be elected as the new owner
 function test_hang_up_owner() {
-    echo "run test case test_hang_up_owner"
+	echo "run test case test_hang_up_owner"
 
+<<<<<<< HEAD
     run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY --logsuffix test_hang_up_owner.server1
     # ensure the server become the owner
     ensure $MAX_RETRIES "$CDC_BINARY cli capture list 2>&1 | grep '\"is-owner\": true'"
@@ -69,18 +106,41 @@ function test_hang_up_owner() {
     ensure $MAX_RETRIES "$CDC_BINARY cli capture list 2>&1 | grep -v \"$owner_id\" | grep id"
     capture_id=$($CDC_BINARY cli capture list 2>&1 | awk -F '"' '/id/{print $4}' | grep -v "$owner_id")
     echo "capture_id:" $capture_id
+=======
+	run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY --logsuffix test_hang_up_owner.server1
+	# ensure the server become the owner
+	ensure $MAX_RETRIES "$CDC_BINARY cli capture list --disable-version-check 2>&1 | grep '\"is-owner\": true'"
 
-    # stop the owner
-    kill -SIGSTOP $owner_pid
+	owner_pid=$(ps -C $CDC_BINARY -o pid= | awk '{print $1}')
+	owner_id=$($CDC_BINARY cli capture list --disable-version-check 2>&1 | awk -F '"' '/id/{print $4}')
+	echo "owner pid:" $owner_pid
+	echo "owner id" $owner_id
 
+	# run another server
+	run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY --addr "127.0.0.1:8301" --logsuffix test_hang_up_owner.server2
+	ensure $MAX_RETRIES "$CDC_BINARY cli capture list --disable-version-check 2>&1 | grep -v \"$owner_id\" | grep id"
+	capture_id=$($CDC_BINARY cli capture list --disable-version-check 2>&1 | awk -F '"' '/id/{print $4}' | grep -v "$owner_id")
+	echo "capture_id:" $capture_id
+>>>>>>> 257041655 (lint: add shfmt to format sh scripts (#2798))
+
+	# stop the owner
+	kill -SIGSTOP $owner_pid
+
+<<<<<<< HEAD
     # check that the new owner is elected
     ensure $MAX_RETRIES "$CDC_BINARY cli capture list 2>&1 |grep $capture_id -A1 | grep '\"is-owner\": true'"
     # resume the original process
     kill -SIGCONT $owner_pid
+=======
+	# check that the new owner is elected
+	ensure $MAX_RETRIES "$CDC_BINARY cli capture list --disable-version-check 2>&1 |grep $capture_id -A1 | grep '\"is-owner\": true'"
+	# resume the original process
+	kill -SIGCONT $owner_pid
+>>>>>>> 257041655 (lint: add shfmt to format sh scripts (#2798))
 
-    echo "test_hang_up_owner: pass"
+	echo "test_hang_up_owner: pass"
 
-    cleanup_process $CDC_BINARY
+	cleanup_process $CDC_BINARY
 }
 
 # test_expire_owner stops the owner by sending
@@ -89,8 +149,9 @@ function test_hang_up_owner() {
 # We expect when the owner process resumes, it suicides
 # itself and recovers from the death.
 function test_expire_owner() {
-    echo "run test case test_expire_owner"
+	echo "run test case test_expire_owner"
 
+<<<<<<< HEAD
     run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY --logsuffix test_expire_owner.server1
     # ensure the server become the owner
     ensure $MAX_RETRIES "$CDC_BINARY cli capture list 2>&1 | grep '\"is-owner\": true'"
@@ -99,11 +160,22 @@ function test_expire_owner() {
     owner_id=$($CDC_BINARY cli capture list 2>&1 | awk -F '"' '/id/{print $4}')
     echo "owner pid:" $owner_pid
     echo "owner id" $owner_id
+=======
+	run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY --logsuffix test_expire_owner.server1
+	# ensure the server become the owner
+	ensure $MAX_RETRIES "$CDC_BINARY cli capture list --disable-version-check 2>&1 | grep '\"is-owner\": true'"
 
-    # stop the owner
-    kill -SIGSTOP $owner_pid
-    echo "process status:" $(ps -h -p $owner_pid -o "s")
+	owner_pid=$(ps -C $CDC_BINARY -o pid= | awk '{print $1}')
+	owner_id=$($CDC_BINARY cli capture list --disable-version-check 2>&1 | awk -F '"' '/id/{print $4}')
+	echo "owner pid:" $owner_pid
+	echo "owner id" $owner_id
+>>>>>>> 257041655 (lint: add shfmt to format sh scripts (#2798))
 
+	# stop the owner
+	kill -SIGSTOP $owner_pid
+	echo "process status:" $(ps -h -p $owner_pid -o "s")
+
+<<<<<<< HEAD
     # ensure the session has expired
     ensure $MAX_RETRIES "$CDC_BINARY cli capture list 2>&1 | grep '\[\]'"
 
@@ -113,11 +185,23 @@ function test_expire_owner() {
     # ensure the owner has recovered
     ensure $MAX_RETRIES "$CDC_BINARY cli capture list 2>&1 | grep '\"is-owner\": true'"
     echo "test_expire_owner pass"
+=======
+	# ensure the session has expired
+	ensure $MAX_RETRIES "$CDC_BINARY cli capture list --disable-version-check 2>&1 | grep '\[\]'"
 
-    cleanup_process $CDC_BINARY
+	# resume the owner
+	kill -SIGCONT $owner_pid
+	echo "process status:" $(ps -h -p $owner_pid -o "s")
+	# ensure the owner has recovered
+	ensure $MAX_RETRIES "$CDC_BINARY cli capture list --disable-version-check 2>&1 | grep '\"is-owner\": true'"
+	echo "test_expire_owner pass"
+>>>>>>> 257041655 (lint: add shfmt to format sh scripts (#2798))
+
+	cleanup_process $CDC_BINARY
 }
 
 function test_owner_cleanup_stale_tasks() {
+<<<<<<< HEAD
     echo "run test case test_owner_cleanup_stale_tasks"
 
     # start a capture server
@@ -155,10 +239,50 @@ function test_owner_cleanup_stale_tasks() {
 
     echo "test_owner_cleanup_stale_tasks pass"
     cleanup_process $CDC_BINARY
+=======
+	echo "run test case test_owner_cleanup_stale_tasks"
+
+	# start a capture server
+	run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY --logsuffix test_owner_cleanup_stale_tasks.server1
+	# ensure the server become the owner
+	ensure $MAX_RETRIES "$CDC_BINARY cli capture list --disable-version-check 2>&1 | grep '\"is-owner\": true'"
+	owner_pid=$(ps -C $CDC_BINARY -o pid= | awk '{print $1}')
+	owner_id=$($CDC_BINARY cli capture list --disable-version-check 2>&1 | awk -F '"' '/id/{print $4}')
+	echo "owner pid:" $owner_pid
+	echo "owner id" $owner_id
+
+	# run another server
+	run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY --addr "127.0.0.1:8301" --logsuffix test_owner_cleanup_stale_tasks.server2
+	ensure $MAX_RETRIES "$CDC_BINARY cli capture list --disable-version-check 2>&1 | grep -v \"$owner_id\" | grep id"
+	capture_pid=$(ps -C $CDC_BINARY -o pid= | awk '{print $1}' | grep -v "$owner_pid")
+	capture_id=$($CDC_BINARY cli capture list --disable-version-check 2>&1 | awk -F '"' '/id/{print $4}' | grep -v "$owner_id")
+	echo "capture_id:" $capture_id
+
+	kill -SIGKILL $owner_pid
+	kill -SIGKILL $capture_pid
+	# wait capture info expires
+	sleep 3
+
+	# simulate task status is deleted but task position stales
+	ETCDCTL_API=3 etcdctl del /tidb/cdc/task/status --prefix
+	run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY --addr "127.0.0.1:8302" --logsuffix test_owner_cleanup_stale_tasks.server3
+	ensure $MAX_RETRIES "$CDC_BINARY cli capture list --disable-version-check 2>&1 | grep '\"is-owner\": true'"
+
+	run_sql "INSERT INTO test.availability1(id, val) VALUES (1, 1);"
+	ensure $MAX_RETRIES nonempty 'select id, val from test.availability1 where id=1 and val=1'
+	run_sql "UPDATE test.availability1 set val = 22 where id = 1;"
+	ensure $MAX_RETRIES nonempty 'select id, val from test.availability1 where id=1 and val=22'
+	run_sql "DELETE from test.availability1 where id=1;"
+	ensure $MAX_RETRIES empty 'select id, val from test.availability1 where id=1'
+
+	echo "test_owner_cleanup_stale_tasks pass"
+	cleanup_process $CDC_BINARY
+>>>>>>> 257041655 (lint: add shfmt to format sh scripts (#2798))
 }
 
 # test some retryable error meeting in the campaign owner loop
 function test_owner_retryable_error() {
+<<<<<<< HEAD
     echo "run test case test_owner_retryable_error"
 
     export GO_FAILPOINTS='github.com/pingcap/ticdc/cdc/capture-campaign-compacted-error=1*return(true)'
@@ -232,4 +356,79 @@ function test_gap_between_watch_capture() {
     export GO_FAILPOINTS=''
     echo "test_gap_between_watch_capture pass"
     cleanup_process $CDC_BINARY
+=======
+	echo "run test case test_owner_retryable_error"
+
+	export GO_FAILPOINTS='github.com/pingcap/ticdc/cdc/capture/capture-campaign-compacted-error=1*return(true)'
+
+	# start a capture server
+	run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY --logsuffix test_owner_retryable_error.server1
+
+	# ensure the server become the owner
+	ensure $MAX_RETRIES "$CDC_BINARY cli capture list --disable-version-check 2>&1 | grep '\"is-owner\": true'"
+	owner_pid=$(ps -C $CDC_BINARY -o pid= | awk '{print $1}')
+	owner_id=$($CDC_BINARY cli capture list --disable-version-check 2>&1 | awk -F '"' '/id/{print $4}')
+	echo "owner pid:" $owner_pid
+	echo "owner id" $owner_id
+
+	export GO_FAILPOINTS='github.com/pingcap/ticdc/cdc/owner/owner-run-with-error=1*return(true);github.com/pingcap/ticdc/cdc/capture/capture-resign-failed=1*return(true)'
+
+	# run another server
+	run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY --logsuffix test_owner_retryable_error.server2 --addr "127.0.0.1:8301"
+	ensure $MAX_RETRIES "$CDC_BINARY cli capture list --disable-version-check 2>&1 | grep -v \"$owner_id\" | grep id"
+	capture_pid=$(ps -C $CDC_BINARY -o pid= | awk '{print $1}' | grep -v "$owner_pid")
+	capture_id=$($CDC_BINARY cli capture list --disable-version-check 2>&1 | awk -F '"' '/id/{print $4}' | grep -v "$owner_id")
+	echo "capture_id:" $capture_id
+
+	# resign the first capture, the second capture campaigns to be owner.
+	# However we have injected two failpoints, the second capture owner runs
+	# with error and before it exits resign owner also failed, so the second
+	# capture will exit and the first capture campaigns to be owner again.
+	curl -X POST http://127.0.0.1:8300/capture/owner/resign
+	ensure $MAX_RETRIES "$CDC_BINARY cli capture list --disable-version-check 2>&1 | grep $owner_id -A1 | grep '\"is-owner\": true'"
+	ensure $MAX_RETRIES "ps -C $CDC_BINARY -o pid= | awk '{print \$1}' | wc -l | grep 1"
+
+	echo "test_owner_retryable_error pass"
+	export GO_FAILPOINTS=''
+	cleanup_process $CDC_BINARY
+}
+
+function test_gap_between_watch_capture() {
+	echo "run test case test_gap_between_watch_capture"
+
+	export GO_FAILPOINTS='github.com/pingcap/ticdc/cdc/owner/sleep-in-owner-tick=1*sleep(6000)'
+
+	# start a capture server
+	run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY --logsuffix test_gap_between_watch_capture.server1
+	# ensure the server become the owner
+	ensure $MAX_RETRIES "$CDC_BINARY cli capture list --disable-version-check 2>&1 | grep '\"is-owner\": true'"
+	owner_pid=$(ps -C $CDC_BINARY -o pid= | awk '{print $1}')
+	owner_id=$($CDC_BINARY cli capture list --disable-version-check 2>&1 | awk -F '"' '/id/{print $4}')
+	echo "owner pid:" $owner_pid
+	echo "owner id" $owner_id
+
+	# run another server
+	run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY --addr "127.0.0.1:8301" --logsuffix test_gap_between_watch_capture.server2
+	ensure $MAX_RETRIES "$CDC_BINARY cli capture list --disable-version-check 2>&1 | grep -v \"$owner_id\" | grep id"
+	capture_pid=$(ps -C $CDC_BINARY -o pid= | awk '{print $1}' | grep -v "$owner_pid")
+	capture_id=$($CDC_BINARY cli capture list --disable-version-check 2>&1 | awk -F '"' '/id/{print $4}' | grep -v "$owner_id")
+	echo "capture_id:" $capture_id
+
+	kill -SIGKILL $capture_pid
+	# wait capture info expires
+	sleep 3
+
+	for i in $(seq 1 3); do
+		run_sql "INSERT INTO test.availability$i(id, val) VALUES (1, 1);"
+		ensure $MAX_RETRIES nonempty "select id, val from test.availability$i where id=1 and val=1"
+		run_sql "UPDATE test.availability$i set val = 22 where id = 1;"
+		ensure $MAX_RETRIES nonempty "select id, val from test.availability$i where id=1 and val=22"
+		run_sql "DELETE from test.availability$i where id=1;"
+		ensure $MAX_RETRIES empty "select id, val from test.availability$i where id=1"
+	done
+
+	export GO_FAILPOINTS=''
+	echo "test_gap_between_watch_capture pass"
+	cleanup_process $CDC_BINARY
+>>>>>>> 257041655 (lint: add shfmt to format sh scripts (#2798))
 }
--- tests/changefeed_error/run.sh
+++ tests/changefeed_error/run.sh
@@ -2,13 +2,14 @@
 
 set -e
 
-CUR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+CUR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 source $CUR/../_utils/test_prepare
 WORK_DIR=$OUT_DIR/$TEST_NAME
 CDC_BINARY=cdc.test
 SINK_TYPE=$1
 MAX_RETRIES=10
 
+<<<<<<< HEAD
 function check_changefeed_mark_failed() {
     endpoints=$1
     changefeedid=$2
@@ -25,76 +26,94 @@ function check_changefeed_mark_failed() {
         echo "error message '$message' is not as expected '$error_msg'"
         exit 1
     fi
+=======
+function check_changefeed_mark_error() {
+	endpoints=$1
+	changefeedid=$2
+	error_msg=$3
+	info=$(cdc cli changefeed query --pd=$endpoints -c $changefeedid -s)
+	echo "$info"
+	state=$(echo $info | jq -r '.state')
+	if [[ ! "$state" == "error" ]]; then
+		echo "changefeed state $state does not equal to error"
+		exit 1
+	fi
+	message=$(echo $info | jq -r '.error.message')
+	if [[ ! "$message" =~ "$error_msg" ]]; then
+		echo "error message '$message' is not as expected '$error_msg'"
+		exit 1
+	fi
+>>>>>>> 257041655 (lint: add shfmt to format sh scripts (#2798))
 }
 
 function check_changefeed_mark_failed_regex() {
-    endpoints=$1
-    changefeedid=$2
-    error_msg=$3
-    info=$(cdc cli changefeed query --pd=$endpoints -c $changefeedid -s)
-    echo "$info"
-    state=$(echo $info|jq -r '.state')
-    if [[ ! "$state" == "failed" ]]; then
-        echo "changefeed state $state does not equal to failed"
-        exit 1
-    fi
-    message=$(echo $info|jq -r '.error.message')
-    if [[ ! "$message" =~ $error_msg ]]; then
-        echo "error message '$message' does not match '$error_msg'"
-        exit 1
-    fi
+	endpoints=$1
+	changefeedid=$2
+	error_msg=$3
+	info=$(cdc cli changefeed query --pd=$endpoints -c $changefeedid -s)
+	echo "$info"
+	state=$(echo $info | jq -r '.state')
+	if [[ ! "$state" == "failed" ]]; then
+		echo "changefeed state $state does not equal to failed"
+		exit 1
+	fi
+	message=$(echo $info | jq -r '.error.message')
+	if [[ ! "$message" =~ $error_msg ]]; then
+		echo "error message '$message' does not match '$error_msg'"
+		exit 1
+	fi
 }
 
 function check_changefeed_mark_stopped_regex() {
-    endpoints=$1
-    changefeedid=$2
-    error_msg=$3
-    info=$(cdc cli changefeed query --pd=$endpoints -c $changefeedid -s)
-    echo "$info"
-    state=$(echo $info|jq -r '.state')
-    if [[ ! "$state" == "stopped" ]]; then
-        echo "changefeed state $state does not equal to stopped"
-        exit 1
-    fi
-    message=$(echo $info|jq -r '.error.message')
-    if [[ ! "$message" =~ $error_msg ]]; then
-        echo "error message '$message' does not match '$error_msg'"
-        exit 1
-    fi
+	endpoints=$1
+	changefeedid=$2
+	error_msg=$3
+	info=$(cdc cli changefeed query --pd=$endpoints -c $changefeedid -s)
+	echo "$info"
+	state=$(echo $info | jq -r '.state')
+	if [[ ! "$state" == "stopped" ]]; then
+		echo "changefeed state $state does not equal to stopped"
+		exit 1
+	fi
+	message=$(echo $info | jq -r '.error.message')
+	if [[ ! "$message" =~ $error_msg ]]; then
+		echo "error message '$message' does not match '$error_msg'"
+		exit 1
+	fi
 }
 
 function check_changefeed_mark_stopped() {
-    endpoints=$1
-    changefeedid=$2
-    error_msg=$3
-    info=$(cdc cli changefeed query --pd=$endpoints -c $changefeedid -s)
-    echo "$info"
-    state=$(echo $info|jq -r '.state')
-    if [[ ! "$state" == "stopped" ]]; then
-        echo "changefeed state $state does not equal to stopped"
-        exit 1
-    fi
-    message=$(echo $info|jq -r '.error.message')
-    if [[ ! "$message" =~ "$error_msg" ]]; then
-        echo "error message '$message' is not as expected '$error_msg'"
-        exit 1
-    fi
+	endpoints=$1
+	changefeedid=$2
+	error_msg=$3
+	info=$(cdc cli changefeed query --pd=$endpoints -c $changefeedid -s)
+	echo "$info"
+	state=$(echo $info | jq -r '.state')
+	if [[ ! "$state" == "stopped" ]]; then
+		echo "changefeed state $state does not equal to stopped"
+		exit 1
+	fi
+	message=$(echo $info | jq -r '.error.message')
+	if [[ ! "$message" =~ "$error_msg" ]]; then
+		echo "error message '$message' is not as expected '$error_msg'"
+		exit 1
+	fi
 }
 
 function check_no_changefeed() {
-    pd=$1
-    count=$(cdc cli changefeed list --pd=$pd 2>&1|jq '.|length')
-    if [[ ! "$count" -eq "0" ]]; then
-        exit 1
-    fi
+	pd=$1
+	count=$(cdc cli changefeed list --pd=$pd 2>&1 | jq '.|length')
+	if [[ ! "$count" -eq "0" ]]; then
+		exit 1
+	fi
 }
 
 function check_no_capture() {
-    pd=$1
-    count=$(cdc cli capture list --pd=$pd 2>&1|jq '.|length')
-    if [[ ! "$count" -eq "0" ]]; then
-        exit 1
-    fi
+	pd=$1
+	count=$(cdc cli capture list --pd=$pd 2>&1 | jq '.|length')
+	if [[ ! "$count" -eq "0" ]]; then
+		exit 1
+	fi
 }
 
 export -f check_changefeed_mark_failed
@@ -105,6 +124,7 @@ export -f check_no_changefeed
 export -f check_no_capture
 
 function run() {
+<<<<<<< HEAD
     rm -rf $WORK_DIR && mkdir -p $WORK_DIR
 
     start_tidb_cluster --workdir $WORK_DIR
@@ -184,6 +204,76 @@ function run() {
     cdc cli changefeed remove -c $changefeedid_2
     export GO_FAILPOINTS=''
     cleanup_process $CDC_BINARY
+=======
+	rm -rf $WORK_DIR && mkdir -p $WORK_DIR
+
+	start_tidb_cluster --workdir $WORK_DIR
+
+	cd $WORK_DIR
+
+	start_ts=$(run_cdc_cli tso query --pd=http://$UP_PD_HOST_1:$UP_PD_PORT_1)
+	run_sql "CREATE DATABASE changefeed_error;" ${UP_TIDB_HOST} ${UP_TIDB_PORT}
+	go-ycsb load mysql -P $CUR/conf/workload -p mysql.host=${UP_TIDB_HOST} -p mysql.port=${UP_TIDB_PORT} -p mysql.user=root -p mysql.db=changefeed_error
+	export GO_FAILPOINTS='github.com/pingcap/ticdc/cdc/owner/NewChangefeedNoRetryError=1*return(true)'
+	run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY
+	capture_pid=$(ps -C $CDC_BINARY -o pid= | awk '{print $1}')
+
+	TOPIC_NAME="ticdc-sink-retry-test-$RANDOM"
+	case $SINK_TYPE in
+	kafka) SINK_URI="kafka://127.0.0.1:9092/$TOPIC_NAME?partition-num=4&kafka-version=${KAFKA_VERSION}" ;;
+	*) SINK_URI="mysql://normal:123456@127.0.0.1:3306/?max-txn-row=1" ;;
+	esac
+	changefeedid="changefeed-error"
+	run_cdc_cli changefeed create --start-ts=$start_ts --sink-uri="$SINK_URI" -c $changefeedid
+	if [ "$SINK_TYPE" == "kafka" ]; then
+		run_kafka_consumer $WORK_DIR "kafka://127.0.0.1:9092/$TOPIC_NAME?partition-num=4&version=${KAFKA_VERSION}"
+	fi
+
+	ensure $MAX_RETRIES check_changefeed_mark_failed_regex http://${UP_PD_HOST_1}:${UP_PD_PORT_1} ${changefeedid} ".*CDC:ErrStartTsBeforeGC.*"
+	run_cdc_cli changefeed resume -c $changefeedid
+
+	check_table_exists "changefeed_error.USERTABLE" ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT}
+	check_sync_diff $WORK_DIR $CUR/conf/diff_config.toml
+
+	go-ycsb load mysql -P $CUR/conf/workload -p mysql.host=${UP_TIDB_HOST} -p mysql.port=${UP_TIDB_PORT} -p mysql.user=root -p mysql.db=changefeed_error
+	check_sync_diff $WORK_DIR $CUR/conf/diff_config.toml
+
+	export GO_FAILPOINTS='github.com/pingcap/ticdc/cdc/owner/NewChangefeedRetryError=return(true)'
+	kill $capture_pid
+	ensure $MAX_RETRIES check_no_capture http://${UP_PD_HOST_1}:${UP_PD_PORT_1}
+	run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY
+	ensure $MAX_RETRIES check_changefeed_mark_error http://${UP_PD_HOST_1}:${UP_PD_PORT_1} ${changefeedid} "failpoint injected retriable error"
+
+	run_cdc_cli changefeed remove -c $changefeedid
+	ensure $MAX_RETRIES check_no_changefeed ${UP_PD_HOST_1}:${UP_PD_PORT_1}
+
+	export GO_FAILPOINTS=''
+	cleanup_process $CDC_BINARY
+
+	# owner DDL error case
+	export GO_FAILPOINTS='github.com/pingcap/ticdc/cdc/owner/InjectChangefeedDDLError=return(true)'
+	run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY
+	changefeedid_1="changefeed-error-1"
+	run_cdc_cli changefeed create --start-ts=$start_ts --sink-uri="$SINK_URI" -c $changefeedid_1
+
+	run_sql "CREATE table changefeed_error.DDLERROR(id int primary key, val int);"
+	ensure $MAX_RETRIES check_changefeed_mark_error http://${UP_PD_HOST_1}:${UP_PD_PORT_1} ${changefeedid_1} "[CDC:ErrExecDDLFailed]exec DDL failed"
+
+	run_cdc_cli changefeed remove -c $changefeedid_1
+	cleanup_process $CDC_BINARY
+
+	# updating GC safepoint failure case
+	export GO_FAILPOINTS='github.com/pingcap/ticdc/cdc/owner/InjectActualGCSafePoint=return(9223372036854775807)'
+	run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY
+
+	changefeedid_2="changefeed-error-2"
+	run_cdc_cli changefeed create --start-ts=$start_ts --sink-uri="$SINK_URI" -c $changefeedid_2
+	ensure $MAX_RETRIES check_changefeed_mark_failed_regex http://${UP_PD_HOST_1}:${UP_PD_PORT_1} ${changefeedid_2} "[CDC:ErrStartTsBeforeGC]"
+
+	run_cdc_cli changefeed remove -c $changefeedid_2
+	export GO_FAILPOINTS=''
+	cleanup_process $CDC_BINARY
+>>>>>>> 257041655 (lint: add shfmt to format sh scripts (#2798))
 }
 
 trap stop_tidb_cluster EXIT
