df33e2967fd936ec0955a914f38f2d5aed7e707b
Neil Shen <overvenus@gmail.com>
2021-09-21 22:27:42 +0800

    server: add go gc metrics
    Signed-off-by: Neil Shen <overvenus@gmail.com>

--- cdc/metrics_server.go
+++ cdc/metrics_server.go
@@ -14,19 +14,42 @@
 package cdc
 
 import (
+	"os"
+	"strconv"
+
 	"github.com/prometheus/client_golang/prometheus"
 )
 
-var etcdHealthCheckDuration = prometheus.NewHistogramVec(
-	prometheus.HistogramOpts{
-		Namespace: "ticdc",
-		Subsystem: "server",
-		Name:      "etcd_health_check_duration",
-		Help:      "Bucketed histogram of processing time (s) of flushing events in processor",
-		Buckets:   prometheus.ExponentialBuckets(0.0001 /* 0.1ms */, 2, 18),
-	}, []string{"capture", "pd"})
+var (
+	etcdHealthCheckDuration = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Namespace: "ticdc",
+			Subsystem: "server",
+			Name:      "etcd_health_check_duration",
+			Help:      "Bucketed histogram of processing time (s) of flushing events in processor",
+			Buckets:   prometheus.ExponentialBuckets(0.0001 /* 0.1ms */, 2, 18),
+		}, []string{"capture", "pd"})
+
+	goGC = prometheus.NewGauge(
+		prometheus.GaugeOpts{
+			Namespace: "ticdc",
+			Subsystem: "server",
+			Name:      "gogc",
+			Help:      "The value of GOGC",
+		})
+)
+
+func init() {
+	// The default GOGC value is 100. See debug.SetGCPercent.
+	gogcValue := 100
+	if val, err := strconv.Atoi(os.Getenv("GOGC")); err == nil {
+		gogcValue = int(val)
+	}
+	goGC.Set(float64(gogcValue))
+}
 
 // initServerMetrics registers all metrics used in processor
 func initServerMetrics(registry *prometheus.Registry) {
 	registry.MustRegister(etcdHealthCheckDuration)
+	registry.MustRegister(goGC)
 }
