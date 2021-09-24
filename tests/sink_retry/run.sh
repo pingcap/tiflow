59c9eaa126d458686cf91ef11bb86e656a66966f
hi-rustin <rustin.liu@gmail.com>
2021-09-24 11:24:00 +0800

    lint: add shfmt to format sh scripts

--- tests/sink_retry/run.sh
+++ tests/sink_retry/run.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-CUR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+CUR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 source $CUR/../_utils/test_prepare
 WORK_DIR=$OUT_DIR/$TEST_NAME
 CDC_BINARY=cdc.test
@@ -12,35 +12,35 @@ CDC_COUNT=3
 DB_COUNT=4
 
 function run() {
-    rm -rf $WORK_DIR && mkdir -p $WORK_DIR
+	rm -rf $WORK_DIR && mkdir -p $WORK_DIR
 
-    start_tidb_cluster --workdir $WORK_DIR
+	start_tidb_cluster --workdir $WORK_DIR
 
-    cd $WORK_DIR
+	cd $WORK_DIR
 
-    start_ts=$(run_cdc_cli tso query --pd=http://$UP_PD_HOST_1:$UP_PD_PORT_1)
-    run_sql "CREATE DATABASE sink_retry;"
-    go-ycsb load mysql -P $CUR/conf/workload -p mysql.host=${UP_TIDB_HOST} -p mysql.port=${UP_TIDB_PORT} -p mysql.user=root -p mysql.db=sink_retry
-    export GO_FAILPOINTS='github.com/pingcap/ticdc/cdc/sink/MySQLSinkTxnRandomError=25%return(true)'
-    run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY
+	start_ts=$(run_cdc_cli tso query --pd=http://$UP_PD_HOST_1:$UP_PD_PORT_1)
+	run_sql "CREATE DATABASE sink_retry;"
+	go-ycsb load mysql -P $CUR/conf/workload -p mysql.host=${UP_TIDB_HOST} -p mysql.port=${UP_TIDB_PORT} -p mysql.user=root -p mysql.db=sink_retry
+	export GO_FAILPOINTS='github.com/pingcap/ticdc/cdc/sink/MySQLSinkTxnRandomError=25%return(true)'
+	run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY
 
-    TOPIC_NAME="ticdc-sink-retry-test-$RANDOM"
-    case $SINK_TYPE in
-        kafka) SINK_URI="kafka://127.0.0.1:9092/$TOPIC_NAME?partition-num=4&kafka-version=${KAFKA_VERSION}";;
-        *) SINK_URI="mysql://normal:123456@127.0.0.1:3306/?max-txn-row=1";;
-    esac
-    run_cdc_cli changefeed create --start-ts=$start_ts --sink-uri="$SINK_URI"
-    if [ "$SINK_TYPE" == "kafka" ]; then
-      run_kafka_consumer $WORK_DIR "kafka://127.0.0.1:9092/$TOPIC_NAME?partition-num=4&version=${KAFKA_VERSION}"
-    fi
+	TOPIC_NAME="ticdc-sink-retry-test-$RANDOM"
+	case $SINK_TYPE in
+	kafka) SINK_URI="kafka://127.0.0.1:9092/$TOPIC_NAME?partition-num=4&kafka-version=${KAFKA_VERSION}" ;;
+	*) SINK_URI="mysql://normal:123456@127.0.0.1:3306/?max-txn-row=1" ;;
+	esac
+	run_cdc_cli changefeed create --start-ts=$start_ts --sink-uri="$SINK_URI"
+	if [ "$SINK_TYPE" == "kafka" ]; then
+		run_kafka_consumer $WORK_DIR "kafka://127.0.0.1:9092/$TOPIC_NAME?partition-num=4&version=${KAFKA_VERSION}"
+	fi
 
-    check_table_exists "sink_retry.USERTABLE" ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT}
-    check_sync_diff $WORK_DIR $CUR/conf/diff_config.toml
+	check_table_exists "sink_retry.USERTABLE" ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT}
+	check_sync_diff $WORK_DIR $CUR/conf/diff_config.toml
 
-    go-ycsb load mysql -P $CUR/conf/workload -p mysql.host=${UP_TIDB_HOST} -p mysql.port=${UP_TIDB_PORT} -p mysql.user=root -p mysql.db=sink_retry
-    check_sync_diff $WORK_DIR $CUR/conf/diff_config.toml
+	go-ycsb load mysql -P $CUR/conf/workload -p mysql.host=${UP_TIDB_HOST} -p mysql.port=${UP_TIDB_PORT} -p mysql.user=root -p mysql.db=sink_retry
+	check_sync_diff $WORK_DIR $CUR/conf/diff_config.toml
 
-    cleanup_process $CDC_BINARY
+	cleanup_process $CDC_BINARY
 }
 
 trap stop_tidb_cluster EXIT
