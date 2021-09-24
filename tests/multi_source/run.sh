746c7d415169e44702dc06f631a46aa4f1f04d5d
asddongmen <414110582@qq.com>
2021-09-24 16:51:35 +0800

    tests: fmt

--- tests/multi_source/run.sh
+++ tests/multi_source/run.sh
@@ -2,34 +2,34 @@
 
 set -e
 
-CUR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+CUR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 source $CUR/../_utils/test_prepare
 WORK_DIR=$OUT_DIR/$TEST_NAME
 CDC_BINARY=cdc.test
 SINK_TYPE=$1
 
 function prepare() {
-    rm -rf $WORK_DIR && mkdir -p $WORK_DIR
-    stop_tidb_cluster
+	rm -rf $WORK_DIR && mkdir -p $WORK_DIR
+	stop_tidb_cluster
 
-    start_tidb_cluster --workdir $WORK_DIR
+	start_tidb_cluster --workdir $WORK_DIR
 
-    cd $WORK_DIR
+	cd $WORK_DIR
 
-    # record tso before we create tables to skip the system table DDLs
-    start_ts=$(run_cdc_cli_tso_query ${UP_PD_HOST_1} ${UP_PD_PORT_1})
+	# record tso before we create tables to skip the system table DDLs
+	start_ts=$(run_cdc_cli_tso_query ${UP_PD_HOST_1} ${UP_PD_PORT_1})
 
-    run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY
+	run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY
 
-    TOPIC_NAME="ticdc-multi-source-test-$RANDOM"
-    case $SINK_TYPE in
-        kafka) SINK_URI="kafka://127.0.0.1:9092/$TOPIC_NAME?partition-num=4&kafka-version=${KAFKA_VERSION}";;
-        *) SINK_URI="mysql://normal:123456@127.0.0.1:3306/";;
-    esac
-    run_cdc_cli changefeed create --start-ts=$start_ts --sink-uri="$SINK_URI"
-    if [ "$SINK_TYPE" == "kafka" ]; then
-        run_kafka_consumer $WORK_DIR "kafka://127.0.0.1:9092/$TOPIC_NAME?partition-num=4&version=${KAFKA_VERSION}"
-    fi
+	TOPIC_NAME="ticdc-multi-source-test-$RANDOM"
+	case $SINK_TYPE in
+	kafka) SINK_URI="kafka://127.0.0.1:9092/$TOPIC_NAME?partition-num=4&kafka-version=${KAFKA_VERSION}" ;;
+	*) SINK_URI="mysql://normal:123456@127.0.0.1:3306/" ;;
+	esac
+	run_cdc_cli changefeed create --start-ts=$start_ts --sink-uri="$SINK_URI"
+	if [ "$SINK_TYPE" == "kafka" ]; then
+		run_kafka_consumer $WORK_DIR "kafka://127.0.0.1:9092/$TOPIC_NAME?partition-num=4&version=${KAFKA_VERSION}"
+	fi
 }
 
 trap stop_tidb_cluster EXIT
