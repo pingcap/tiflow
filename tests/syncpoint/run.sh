93a7bbd2f08d2b3c4c586a21b04d566497fb193f
asddongmen <414110582@qq.com>
2021-09-27 20:39:57 +0800

    tests: update integration tests

--- tests/syncpoint/run.sh
+++ tests/syncpoint/run.sh
@@ -124,9 +124,10 @@ function run() {
 	start_tidb_cluster --workdir $WORK_DIR
 
 	cd $WORK_DIR
+	run_sql "CREATE DATABASE testSync;"
+	run_sql "CREATE DATABASE testSync;" ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT}
 
 	start_ts=$(run_cdc_cli tso query --pd=http://$UP_PD_HOST_1:$UP_PD_PORT_1)
-	run_sql "CREATE DATABASE testSync;"
 	run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY
 
 	SINK_URI="mysql://normal:123456@127.0.0.1:3306/?max-txn-row=1"
