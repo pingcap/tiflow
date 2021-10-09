1e31724a48e008e537f77d43b147643ae833da41
asddongmen <414110582@qq.com>
2021-10-09 12:37:59 +0800

    tests: fmt

--- tests/processor_stop_delay/run.sh
+++ tests/processor_stop_delay/run.sh
@@ -26,7 +26,6 @@ function run() {
 	fi
 	export GO_FAILPOINTS='github.com/pingcap/ticdc/cdc/processor/processorStopDelay=1*sleep(10000)'
 
-
 	run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY --addr "127.0.0.1:8300" --pd $pd_addr
 	changefeed_id=$(cdc cli changefeed create --pd=$pd_addr --sink-uri="$SINK_URI" 2>&1 | tail -n2 | head -n1 | awk '{print $2}')
 	if [ "$SINK_TYPE" == "kafka" ]; then
