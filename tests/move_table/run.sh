e75b38b3413572f4acd8a2f031fb32fd6948d4c8
asddongmen <414110582@qq.com>
2021-10-22 17:26:55 +0800

    tests: skip move_table kafka test

--- tests/move_table/run.sh
+++ tests/move_table/run.sh
@@ -9,6 +9,11 @@ CDC_BINARY=cdc.test
 SINK_TYPE=$1
 
 function run() {
+  # TODO: remove after kafka-consumer/main.go is fixed
+	if [ "$SINK_TYPE" == "kafka" ]; then
+		return
+	fi
+
 	rm -rf $WORK_DIR && mkdir -p $WORK_DIR
 
 	start_tidb_cluster --workdir $WORK_DIR
