6c3acd9241935d5aa89820c38d9b531366377690
amyangfei <amyangfei@gmail.com>
2021-10-30 18:02:56 +0800

    Update cdc/sink/manager_test.go

--- cdc/sink/manager_test.go
+++ cdc/sink/manager_test.go
@@ -193,7 +193,7 @@ func (s *managerSuite) TestManagerAddRemoveTable(c *check.C) {
 	go func() {
 		defer wg.Done()
 		// add three table and then remote one table
-		for i := 0; i < 200; i++ {
+		for i := 0; i < goroutineNum; i++ {
 			if i%4 != 3 {
 				// add table
 				table := manager.CreateTableSink(model.TableID(i), maxResolvedTs, redoManager)
