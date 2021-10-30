dd4f7d471a69b04f4d3da4508fb101140a9f8fda
gmhdbjd <gmhdbjd@gmail.com>
2021-10-29 23:25:07 -0400

    fix test

--- dm/tests/shardddl1/run.sh
+++ dm/tests/shardddl1/run.sh
@@ -606,15 +606,6 @@ function DM_COMPACT() {
 	run_case COMPACT "single-source-no-sharding" \
 		"run_sql_source1 \"create table ${shardddl1}.${tb1} (a int primary key, b int unique, c int);\"" \
 		"clean_table" ""
-
-	ps aux | grep dm-worker | awk '{print $2}' | xargs kill || true
-	check_port_offline $WORKER1_PORT 20
-	check_port_offline $WORKER2_PORT 20
-	export GO_FAILPOINTS=''
-	run_dm_worker $WORK_DIR/worker1 $WORKER1_PORT $cur/conf/dm-worker1.toml
-	run_dm_worker $WORK_DIR/worker2 $WORKER2_PORT $cur/conf/dm-worker2.toml
-	check_rpc_alive $cur/../bin/check_worker_online 127.0.0.1:$WORKER1_PORT
-	check_rpc_alive $cur/../bin/check_worker_online 127.0.0.1:$WORKER2_PORT
 }
 
 function DM_MULTIPLE_ROWS_CASE() {
