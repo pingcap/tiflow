873ea9d50f7f8e22f1aee87cdc5d4325ed3935c8
Ling Jin <7138436+3AceShowHand@users.noreply.github.com>
2021-11-02 17:51:06 +0800

    Update cdc/sink/codec/canal_flat.go
    Co-authored-by: Zixiong Liu <liuzixiong@pingcap.com>

--- cdc/sink/codec/canal_flat.go
+++ cdc/sink/codec/canal_flat.go
@@ -88,7 +88,7 @@ type canalFlatMessage struct {
 	Old  []map[string]interface{} `json:"old"`
 
 	// CheckpointTs is a TiCDC custom field, which is not supported by original Canal protocol
-	// this is useful for message consumer to reform the original transactions.
+	// It is useful if the message consumer needs to restore the original transactions.
 	CheckpointTs uint64 `json:"checkpointTs"`
 	// Used internally by CanalFlatEventBatchEncoder
 	tikvTs uint64
