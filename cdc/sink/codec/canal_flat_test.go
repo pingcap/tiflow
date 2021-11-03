f49a60f449c2c0b156219967c9db4f357c35e137
Ling Jin <jinl1037@hotmail.com>
2021-11-03 18:15:58 +0800

    fix canal_flat_test watermark .

--- cdc/sink/codec/canal_flat_test.go
+++ cdc/sink/codec/canal_flat_test.go
@@ -140,14 +140,14 @@ func (s *canalFlatSuite) TestBatching(c *check.C) {
 
 func (s *canalFlatSuite) TestEncodeCheckpointEvent(c *check.C) {
 	defer testleak.AfterTest(c)()
-	encoder := &CanalFlatEventBatchEncoder{builder: NewCanalEntryBuilder(), waterMark: false}
+	encoder := &CanalFlatEventBatchEncoder{builder: NewCanalEntryBuilder(), watermark: false}
 	c.Assert(encoder, check.NotNil)
 
 	msg, err := encoder.EncodeCheckpointEvent(2333)
 	c.Assert(err, check.IsNil)
 	c.Assert(msg, check.IsNil)
 
-	encoder.waterMark = true
+	encoder.watermark = true
 	msg, err = encoder.EncodeCheckpointEvent(2333)
 	c.Assert(err, check.IsNil)
 	c.Assert(msg, check.NotNil)
