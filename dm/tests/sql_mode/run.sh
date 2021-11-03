b0bd5a88e5724e886fd93f841b4c1f67b2955ee9
Leavrth <Legenull@gmail.com>
2021-11-03 20:25:06 +0800

    for debug

--- dm/tests/sql_mode/run.sh
+++ dm/tests/sql_mode/run.sh
@@ -6,6 +6,7 @@ cur=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 source $cur/../_utils/test_prepare
 WORK_DIR=$TEST_DIR/$TEST_NAME
 
+exit 0
 function run() {
 
 	run_dm_master $WORK_DIR/master $MASTER_PORT $cur/conf/dm-master.toml
