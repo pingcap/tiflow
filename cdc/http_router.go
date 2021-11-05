a2cfdbd77591b5069b6faa66eb3e4623f61230b7
dongmen <20351731+asddongmen@users.noreply.github.com>
2021-11-05 18:53:03 +0800

    This is an automated cherry-pick of #2997
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- cdc/http_router.go
+++ cdc/http_router.go
@@ -21,22 +21,32 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/pingcap/log"
 	"github.com/pingcap/ticdc/cdc/capture"
+	"github.com/pingcap/ticdc/cdc/model"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
+<<<<<<< HEAD
+=======
+	"go.uber.org/zap"
+
+>>>>>>> 28424edb9 (http_*: add log for http api and refine the err handle logic (#2997))
 	// use for OpenAPI online docs
 	_ "github.com/pingcap/ticdc/docs/api"
 )
 
 // newRouter create a router for OpenAPI
+
 func newRouter(captureHandler capture.HTTPHandler) *gin.Engine {
 	// discard gin log output
 	gin.DefaultWriter = ioutil.Discard
 
 	router := gin.New()
 
+	router.Use(logMiddleware())
 	// request will timeout after 10 second
 	router.Use(timeoutMiddleware(time.Second * 10))
+	router.Use(errorHandleMiddleware())
 
 	// OpenAPI online docs
 	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
@@ -95,7 +105,7 @@ func newRouter(captureHandler capture.HTTPHandler) *gin.Engine {
 }
 
 // timeoutMiddleware wraps the request context with a timeout
-func timeoutMiddleware(timeout time.Duration) func(c *gin.Context) {
+func timeoutMiddleware(timeout time.Duration) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		// wrap the request context with a timeout
 		ctx, cancel := context.WithTimeout(c.Request.Context(), timeout)
@@ -116,5 +126,53 @@ func timeoutMiddleware(timeout time.Duration) func(c *gin.Context) {
 		// replace request with context wrapped request
 		c.Request = c.Request.WithContext(ctx)
 		c.Next()
+	}
+}
+
+func logMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		start := time.Now()
+		path := c.Request.URL.Path
+		query := c.Request.URL.RawQuery
+		c.Next()
+
+		cost := time.Since(start)
+
+		err := c.Errors.Last()
+		var stdErr error
+		if err != nil {
+			stdErr = err.Err
+		}
+
+		log.Info(path,
+			zap.Int("status", c.Writer.Status()),
+			zap.String("method", c.Request.Method),
+			zap.String("path", path),
+			zap.String("query", query),
+			zap.String("ip", c.ClientIP()),
+			zap.String("user-agent", c.Request.UserAgent()),
+			zap.Error(stdErr),
+			zap.Duration("cost", cost),
+		)
+	}
+}
+
+func errorHandleMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Next()
+		// because we will return immediately after an error occurs in http_handler
+		// there wil be only one error in c.Errors
+		lastError := c.Errors.Last()
+		if lastError != nil {
+			err := lastError.Err
+			// put the error into response
+			if capture.IsHTTPBadRequestError(err) {
+				c.IndentedJSON(http.StatusBadRequest, model.NewHTTPError(err))
+			} else {
+				c.IndentedJSON(http.StatusInternalServerError, model.NewHTTPError(err))
+			}
+			c.Abort()
+			return
+		}
 	}
 }
