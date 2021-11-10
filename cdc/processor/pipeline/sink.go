0a3f9d4485f838634a106df02df905c10aa457d5
hi-rustin <rustin.liu@gmail.com>
2021-11-10 09:38:27 +0800

    sink: add log when skip the empty rows

--- cdc/processor/pipeline/sink.go
+++ cdc/processor/pipeline/sink.go
@@ -161,6 +161,7 @@ func (n *sinkNode) emitEvent(ctx pipeline.NodeContext, event *model.PolymorphicE
 	// begin; insert into t (id) values (1); delete from t where id=1; commit;
 	// Just ignore these row changed events
 	if colLen == 0 && preColLen == 0 {
+		log.Warn("skip emit empty rows", zap.Any("event", event))
 		return nil
 	}
 
