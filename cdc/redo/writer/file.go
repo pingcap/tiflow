ef61b4f603bb942a1a6ca08ba2b7ac2130d00100
ben1009 <liuhe1009@gmail.com>
2021-11-12 11:27:56 +0800

    add ut&comment

--- cdc/redo/writer/file.go
+++ cdc/redo/writer/file.go
@@ -255,6 +255,7 @@ func (w *Writer) Close() error {
 	}
 	w.Lock()
 	defer w.Unlock()
+	// always set to false when closed, since if having err may not get fixed just by retry
 	defer w.running.Store(false)
 
 	err := w.close()
