70625458cb0015f6b7b1102a40121732b89ebbd7
Zixiong Liu <liuzixiong@pingcap.com>
2021-11-17 16:09:08 +0800

    scheduler_v2: implement balancer

---
+++ cdc/scheduler/balancer.go
@@ -0,0 +1,142 @@
+// Copyright 2021 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package scheduler
+
+import (
+	"math"
+
+	"github.com/pingcap/ticdc/cdc/model"
+	"github.com/pingcap/ticdc/cdc/scheduler/util"
+	"go.uber.org/zap"
+)
+
+// balancer is used to support the rebalance process, in which
+// some victims are chosen and de-scheduled. Later, the victims
+// will be automatically rescheduled, during which the target captures
+// will be chosen so that the workload is the most balanced.
+type balancer interface {
+	// FindVictims returns a set of possible victim tables.
+	// Removing these tables will make the workload more balanced.
+	FindVictims(
+		tables *util.TableSet,
+		captures map[model.CaptureID]*model.CaptureInfo,
+	) (tablesToRemove []model.TableID)
+
+	// FindTarget returns a target capture to add a table to.
+	FindTarget(
+		tables *util.TableSet,
+		captures map[model.CaptureID]*model.CaptureInfo,
+	) (minLoadCapture model.CaptureID, ok bool)
+}
+
+// tableNumberBalancer implements a balance strategy based on the
+// current number of tables replicated by each capture.
+type tableNumberBalancer struct {
+	logger *zap.Logger
+}
+
+func newTableNumberRebalancer(logger *zap.Logger) balancer {
+	return &tableNumberBalancer{
+		logger: logger,
+	}
+}
+
+// FindTarget returns the capture with the smallest workload (in table count).
+func (r *tableNumberBalancer) FindTarget(
+	tables *util.TableSet,
+	captures map[model.CaptureID]*model.CaptureInfo,
+) (minLoadCapture model.CaptureID, ok bool) {
+	if len(captures) == 0 {
+		return "", false
+	}
+
+	captureWorkload := make(map[model.CaptureID]int)
+	for captureID := range captures {
+		captureWorkload[captureID] = 0
+	}
+
+	for captureID, tables := range tables.GetAllTablesGroupedByCaptures() {
+		// We use the number of tables as workload
+		captureWorkload[captureID] = len(tables)
+	}
+
+	candidate := ""
+	minWorkload := math.MaxInt64
+
+	for captureID, workload := range captureWorkload {
+		if workload < minWorkload {
+			minWorkload = workload
+			candidate = captureID
+		}
+	}
+
+	if minWorkload == math.MaxInt64 {
+		r.logger.Panic("unexpected minWorkerload == math.MaxInt64")
+	}
+
+	return candidate, true
+}
+
+// FindVictims returns some victims to remove.
+// Read the comment in the function body on the details of the victim selection.
+func (r *tableNumberBalancer) FindVictims(
+	tables *util.TableSet,
+	captures map[model.CaptureID]*model.CaptureInfo,
+) []model.TableID {
+	// Algorithm overview: We try to remove some tables as the victims so that
+	// no captures are assigned more tables than the average workload measured in table number,
+	// modulo the necessary margin due to the fraction part of the average.
+	//
+	// In formula, we try to maintain the invariant:
+	//
+	// num(tables assigned to any capture) < num(tables) / num(captures) + 1
+
+	totalTableNum := len(tables.GetAllTables())
+	captureNum := len(captures)
+	upperLimitPerCapture := int(math.Ceil(float64(totalTableNum) / float64(captureNum)))
+
+	r.logger.Info("Start rebalancing",
+		zap.Int("table-num", totalTableNum),
+		zap.Int("capture-num", captureNum),
+		zap.Int("target-limit", upperLimitPerCapture))
+
+	var victims []model.TableID
+	for _, tables := range tables.GetAllTablesGroupedByCaptures() {
+		var tableList []model.TableID
+		for tableID := range tables {
+			tableList = append(tableList, tableID)
+		}
+		// We sort the tableIDs here so that the result is deterministic,
+		// which would aid testing and debugging.
+		util.SorterTableIDs(tableList)
+
+		tableNum2Remove := len(tables) - upperLimitPerCapture
+		if tableNum2Remove <= 0 {
+			continue
+		}
+
+		// here we pick `tableNum2Remove` tables to delete,
+		for _, tableID := range tableList {
+			if tableNum2Remove <= 0 {
+				break
+			}
+
+			r.logger.Info("Rebalance: remove table",
+				zap.Int64("table-id", tableID))
+			victims = append(victims, tableID)
+			tableNum2Remove--
+		}
+	}
+	return victims
+}
---
+++ cdc/scheduler/balancer_test.go
@@ -0,0 +1,124 @@
+// Copyright 2021 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package scheduler
+
+import (
+	"testing"
+
+	"github.com/pingcap/ticdc/cdc/model"
+	"github.com/pingcap/ticdc/cdc/scheduler/util"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/zap"
+)
+
+func TestBalancerFindVictims(t *testing.T) {
+	balancer := newTableNumberRebalancer(zap.L())
+	tables := util.NewTableSet()
+
+	tables.AddTableRecord(&util.TableRecord{
+		TableID:   1,
+		CaptureID: "capture-1",
+	})
+	tables.AddTableRecord(&util.TableRecord{
+		TableID:   2,
+		CaptureID: "capture-1",
+	})
+	tables.AddTableRecord(&util.TableRecord{
+		TableID:   3,
+		CaptureID: "capture-1",
+	})
+	tables.AddTableRecord(&util.TableRecord{
+		TableID:   4,
+		CaptureID: "capture-1",
+	})
+	tables.AddTableRecord(&util.TableRecord{
+		TableID:   5,
+		CaptureID: "capture-2",
+	})
+	tables.AddTableRecord(&util.TableRecord{
+		TableID:   6,
+		CaptureID: "capture-2",
+	})
+
+	mockCaptureInfos := map[model.CaptureID]*model.CaptureInfo{
+		"capture-1": {
+			ID: "capture-1",
+		},
+		"capture-2": {
+			ID: "capture-2",
+		},
+		"capture-3": {
+			ID: "capture-2",
+		},
+	}
+
+	victims := balancer.FindVictims(tables, mockCaptureInfos)
+	require.Len(t, victims, 2)
+	require.Contains(t, victims, model.TableID(1))
+	require.Contains(t, victims, model.TableID(2))
+}
+
+func TestBalancerFindTarget(t *testing.T) {
+	balancer := newTableNumberRebalancer(zap.L())
+	tables := util.NewTableSet()
+
+	tables.AddTableRecord(&util.TableRecord{
+		TableID:   1,
+		CaptureID: "capture-1",
+	})
+	tables.AddTableRecord(&util.TableRecord{
+		TableID:   2,
+		CaptureID: "capture-1",
+	})
+	tables.AddTableRecord(&util.TableRecord{
+		TableID:   3,
+		CaptureID: "capture-1",
+	})
+	tables.AddTableRecord(&util.TableRecord{
+		TableID:   4,
+		CaptureID: "capture-2",
+	})
+	tables.AddTableRecord(&util.TableRecord{
+		TableID:   5,
+		CaptureID: "capture-2",
+	})
+	tables.AddTableRecord(&util.TableRecord{
+		TableID:   6,
+		CaptureID: "capture-3",
+	})
+
+	mockCaptureInfos := map[model.CaptureID]*model.CaptureInfo{
+		"capture-1": {
+			ID: "capture-1",
+		},
+		"capture-2": {
+			ID: "capture-2",
+		},
+		"capture-3": {
+			ID: "capture-2",
+		},
+	}
+
+	target, ok := balancer.FindTarget(tables, mockCaptureInfos)
+	require.True(t, ok)
+	require.Equal(t, "capture-3", target)
+}
+
+func TestBalancerNoCaptureAvailable(t *testing.T) {
+	balancer := newTableNumberRebalancer(zap.L())
+	tables := util.NewTableSet()
+
+	_, ok := balancer.FindTarget(tables, map[model.CaptureID]*model.CaptureInfo{})
+	require.False(t, ok)
+}
