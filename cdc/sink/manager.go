b7557d0db0ff5745ef255b2f5a564d563b4a6663
Neil Shen <overvenus@gmail.com>
2021-11-20 13:57:13 +0800

    address lints
    Signed-off-by: Neil Shen <overvenus@gmail.com>

--- cdc/sink/manager.go
+++ cdc/sink/manager.go
@@ -110,11 +110,12 @@ func (m *Manager) flushBackendSink(ctx context.Context) (model.Ts, error) {
 	// NOTICE: Because all table sinks will try to flush backend sink,
 	// which will cause a lot of lock contention and blocking in high concurrency cases.
 	// So here we use flushing as a lightweight lock to improve the lock competition problem.
-	if !atomic.CompareAndSwapInt64(&m.flushing, 0, 1) {
-		// Do not skip flushing for resolving #3503.
-		// TODO uncomment the following return.
-		// return m.getCheckpointTs(), nil
-	}
+	//
+	// Do not skip flushing for resolving #3503.
+	// TODO uncomment the following return.
+	// if !atomic.CompareAndSwapInt64(&m.flushing, 0, 1) {
+	// return m.getCheckpointTs(), nil
+	// }
 	m.flushMu.Lock()
 	defer func() {
 		m.flushMu.Unlock()
