4406b10d34432835dbbd28bb9b039b88910a463f
Ling Jin <7138436+3AceShowHand@users.noreply.github.com>
2021-11-30 23:39:02 +0800

    Update cdc/sink/codec/java.go
    Co-authored-by: Neil Shen <overvenus@gmail.com>

--- cdc/sink/codec/java.go
+++ cdc/sink/codec/java.go
@@ -65,7 +65,7 @@ const (
 )
 
 // MySQLType2JavaType converts the mysql protocol types to java sql types
-// take https://github.com/alibaba/canal/blob/master/dbsync/src/main/java/com/taobao/tddl/dbsync/binlog/event/RowsLogBuffer.java#L132 as reference
+// take https://github.com/alibaba/canal/blob/b54bea5e3/dbsync/src/main/java/com/taobao/tddl/dbsync/binlog/event/RowsLogBuffer.java#L1 as reference
 // for official `meta` related logic, it's not supported.
 func MySQLType2JavaType(mysqlType byte, isBinary bool) JavaSQLType {
 	// see https://github.com/mysql/mysql-connector-j/blob/5.1.49/src/com/mysql/jdbc/MysqlDefs.java
