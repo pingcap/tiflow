5701a598e2ff0d163956a57431ded24b33697183
hi-rustin <rustin.liu@gmail.com>
2021-12-02 14:40:57 +0800

    tests(ticdc): tests ci

--- tests/integration_tests/run.sh
+++ tests/integration_tests/run.sh
@@ -5,7 +5,9 @@ set -eu
 OUT_DIR=/tmp/tidb_cdc_test
 CUR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 export PATH=$PATH:$CUR/_utils:$CUR/../bin
-
+echo $CUR
+ls bin
+ls $CUR/../bin
 mkdir -p $OUT_DIR || true
 
 if [ "${1-}" = '--debug' ]; then
