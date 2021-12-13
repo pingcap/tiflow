1bb7095a10cb735acd93d2c4fa7d7f086a243989
3AceShowHand <jinl1037@hotmail.com>
2021-12-13 21:58:41 +0800

    try to make canal support unsigned test.

--- tests/mq_protocol_tests/main.go
+++ tests/mq_protocol_tests/main.go
@@ -56,7 +56,7 @@ func testCanal() {
 		cases.NewSimpleCase(task),
 		cases.NewDeleteCase(task),
 		cases.NewManyTypesCase(task),
-		// tests.NewUnsignedCase(task), //now canal adapter can not deal with unsigned int greater than int max
+		cases.NewUnsignedCase(task),
 		cases.NewCompositePKeyCase(task),
 		// tests.NewAlterCase(task), // basic implementation can not grantee ddl dml sequence, so can not pass
 	}
