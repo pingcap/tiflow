db245398daa8702b42db9828298ed80ea85492ab
3AceShowHand <jinl1037@hotmail.com>
2021-12-15 19:55:21 +0800

    try fix the unit test.

--- cdc/sink/codec/canal.go
+++ cdc/sink/codec/canal.go
@@ -265,11 +265,11 @@ func getMySQLType(c *model.Column) string {
 	}
 
 	if types.IsTypeBlob(c.Type) {
-		return strings.Replace(mysqlType, "text", "blob", 1)
+		return strings.Replace(mysqlType, "TEXT", "BLOB", 1)
 	}
 
 	if types.IsTypeChar(c.Type) {
-		return strings.Replace(mysqlType, "char", "binary", 1)
+		return strings.Replace(mysqlType, "CHAR", "BINARY", 1)
 	}
 
 	return mysqlType
