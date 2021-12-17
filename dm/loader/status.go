29743770fa9d26426d9e5e47082a9c7437981009
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2021-12-17 11:32:35 +0800

    dm/load: fix concurrent call Loader.Status (#3459) (#3468)

--- dm/loader/status.go
+++ dm/loader/status.go
@@ -44,19 +44,25 @@ func (l *Loader) printStatus() {
 	totalSize := l.totalDataSize.Load()
 	totalFileCount := l.totalFileCount.Load()
 
-	interval := time.Since(l.dbTableDataLastUpdatedTime)
+	interval := time.Since(l.dbTableDataLastUpdatedTime.Load())
+	intervalSecond := interval.Seconds()
+	if intervalSecond == 0 {
+		return
+	}
+
 	for db, tables := range l.dbTableDataFinishedSize {
 		for table, size := range tables {
 			curFinished := size.Load()
-			speed := float64(curFinished-l.dbTableDataLastFinishedSize[db][table]) / interval.Seconds()
-			l.dbTableDataLastFinishedSize[db][table] = curFinished
+			lastFinished := l.dbTableDataFinishedSize[db][table].Load()
+			speed := float64(curFinished-lastFinished) / intervalSecond
+			l.dbTableDataLastFinishedSize[db][table].Store(curFinished)
 			if speed > 0 {
 				remainingSeconds := float64(l.dbTableDataTotalSize[db][table].Load()-curFinished) / speed
 				remainingTimeGauge.WithLabelValues(l.cfg.Name, l.cfg.WorkerName, l.cfg.SourceID, db, table).Set(remainingSeconds)
 			}
 		}
 	}
-	l.dbTableDataLastUpdatedTime = time.Now()
+	l.dbTableDataLastUpdatedTime.Store(time.Now())
 
 	l.logger.Info("progress status of load",
 		zap.Int64("finished_bytes", finishedSize),
