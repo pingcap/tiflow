ed4808f3293d3ce71f6e82a7acc542e859853f82
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2021-12-17 19:04:38 +0800

    pkg,cdc: do not use log package (#3902) (#3942)

--- pkg/context/context_test.go
+++ pkg/context/context_test.go
@@ -136,7 +136,7 @@ func (s *contextSuite) TestThrowPanic(c *check.C) {
 	defer testleak.AfterTest(c)()
 	defer func() {
 		panicMsg := recover()
-		c.Assert(panicMsg, check.Equals, "an error has escaped, please report a bug{error 26 0  mock error}")
+		c.Assert(panicMsg, check.Equals, "an error has escaped, please report a bug")
 	}()
 	stdCtx := context.Background()
 	ctx := NewContext(stdCtx, &GlobalVars{})
