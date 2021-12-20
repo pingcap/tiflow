ac4fbc898465933b0ad4ff6dad1e771e1b4d0442
sdojjy <sdojjy@qq.com>
2021-12-20 14:27:46 +0800

    This is an automated cherry-pick of #3845
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- cdc/owner/changefeed.go
+++ cdc/owner/changefeed.go
@@ -15,13 +15,20 @@ package owner
 
 import (
 	"context"
+	"strings"
 	"sync"
 
 	"github.com/pingcap/errors"
 	"github.com/pingcap/failpoint"
 	"github.com/pingcap/log"
+<<<<<<< HEAD
 	timodel "github.com/pingcap/parser/model"
 	"github.com/pingcap/tidb/sessionctx/binloginfo"
+=======
+	"github.com/pingcap/tidb/parser"
+	"github.com/pingcap/tidb/parser/format"
+	timodel "github.com/pingcap/tidb/parser/model"
+>>>>>>> 7ccaad224 (ticdc/owner: Fix ddl special comment syntax error (#3845))
 	"github.com/pingcap/tiflow/cdc/model"
 	cdcContext "github.com/pingcap/tiflow/pkg/context"
 	cerror "github.com/pingcap/tiflow/pkg/errors"
@@ -422,7 +429,11 @@ func (c *changefeed) asyncExecDDL(ctx cdcContext.Context, job *timodel.Job) (don
 		if err != nil {
 			return false, errors.Trace(err)
 		}
-		ddlEvent.Query = binloginfo.AddSpecialComment(ddlEvent.Query)
+		ddlEvent.Query, err = addSpecialComment(ddlEvent.Query)
+		if err != nil {
+			return false, errors.Trace(err)
+		}
+
 		c.ddlEventCache = ddlEvent
 	}
 	if job.BinlogInfo.TableInfo != nil && c.schema.IsIneligibleTableID(job.BinlogInfo.TableInfo.ID) {
@@ -479,4 +490,28 @@ func (c *changefeed) updateStatus(currentTs int64, barrierTs model.Ts) {
 
 func (c *changefeed) Close() {
 	c.releaseResources()
+}
+
+// addSpecialComment translate tidb feature to comment
+func addSpecialComment(ddlQuery string) (string, error) {
+	stms, _, err := parser.New().ParseSQL(ddlQuery)
+	if err != nil {
+		return "", errors.Trace(err)
+	}
+	if len(stms) != 1 {
+		log.Panic("invalid ddlQuery statement size", zap.String("ddlQuery", ddlQuery))
+	}
+	var sb strings.Builder
+	// translate TiDB feature to special comment
+	restoreFlags := format.RestoreTiDBSpecialComment
+	// escape the keyword
+	restoreFlags |= format.RestoreNameBackQuotes
+	// upper case keyword
+	restoreFlags |= format.RestoreKeyWordUppercase
+	// wrap string with single quote
+	restoreFlags |= format.RestoreStringSingleQuotes
+	if err = stms[0].Restore(format.NewRestoreCtx(restoreFlags, &sb)); err != nil {
+		return "", errors.Trace(err)
+	}
+	return sb.String(), nil
 }
