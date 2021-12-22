3048e682fd0d6ae6f82b7a9efa656d64d0009775
CharlesCheung <61726649+CharlesCheung96@users.noreply.github.com>
2021-12-22 10:50:42 +0800

    handle comment
    Co-authored-by: amyangfei <amyangfei@gmail.com>

--- cdc/owner/scheduler.go
+++ cdc/owner/scheduler.go
@@ -303,7 +303,7 @@ func (s *scheduler) cleanUpFinishedOperations() {
 	for captureID := range s.state.TaskStatuses {
 		s.state.PatchTaskStatus(captureID, func(status *model.TaskStatus) (*model.TaskStatus, bool, error) {
 			if status == nil {
-				log.Warn("task status of the capture is not found, may be the key in etcd was deleted", zap.Any("captureID", captureID), zap.Any("changeFeedID", s.state.ID))
+				log.Warn("task status of the capture is not found, may be the key in etcd was deleted", zap.String("captureID", captureID), zap.String("changeFeedID", s.state.ID))
 				return status, false, nil
 			}
 
