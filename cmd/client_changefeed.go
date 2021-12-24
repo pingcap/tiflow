e7b5a8e1c984701f892242d897df67501c0b00c7
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2021-12-24 15:51:47 +0800

    config(ticdc): Fix old value configuration check for maxwell protocol (#3747) (#3781)

--- cmd/client_changefeed.go
+++ cmd/client_changefeed.go
@@ -43,6 +43,7 @@ import (
 
 var forceEnableOldValueProtocols = []string{
 	"canal",
+	"canal-json",
 	"maxwell",
 }
 
@@ -321,9 +322,12 @@ func verifyChangefeedParameters(
 		}
 
 		protocol := sinkURIParsed.Query().Get("protocol")
+		if protocol != "" {
+			cfg.Sink.Protocol = protocol
+		}
 		for _, fp := range forceEnableOldValueProtocols {
-			if protocol == fp {
-				log.Warn("Attempting to replicate without old value enabled. CDC will enable old value and continue.", zap.String("protocol", protocol))
+			if cfg.Sink.Protocol == fp {
+				log.Warn("Attempting to replicate without old value enabled. CDC will enable old value and continue.", zap.String("protocol", cfg.Sink.Protocol))
 				cfg.EnableOldValue = true
 				break
 			}
