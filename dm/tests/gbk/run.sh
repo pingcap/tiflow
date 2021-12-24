c568c6096cb78e31131288ee2918142a471a0ad7
lance6716 <lance6716@gmail.com>
2021-12-24 17:13:04 +0800

    add test

--- dm/tests/gbk/run.sh
+++ dm/tests/gbk/run.sh
@@ -7,6 +7,31 @@ source $cur/../_utils/test_prepare
 WORK_DIR=$TEST_DIR/$TEST_NAME
 TASK_NAME="gbk"
 
+function test_from_tidb() {
+  cleanup_data_upstream gbk3
+
+  # check table can inherited charset from database
+  run_sql_source1 "use gbk; create table ddl1(b char(20));"
+  run_sql_tidb_with_retry "show create table gbk.ddl1;" "CHARSET=gbk"
+
+  # can't test "create table as select", because it is not supported in GTID mode
+
+  # test create table like
+  run_sql_source1 "use gbk; create table ddl2(a char(20) charset gbk, b char(20) charset utf8mb4);"
+  run_sql_source1 "use gbk; create table ddl2_copy like ddl2; insert into ddl2_copy values('一二三', '一二三');"
+  run_sql_tidb_with_retry "select hex(a) from gbk.ddl2_copy;" "D2BBB6FEC8FD"
+  run_sql_tidb_with_retry "select hex(b) from gbk.ddl2_copy;" "E4B880E4BA8CE4B889"
+
+  # test create partition table
+  run_sql_source1 "use gbk; create table ddl3(id int, a char(20) charset gbk) partition by hash(id) partitions 4;"
+  run_sql_tidb_with_retry "show create table gbk.ddl3;" "CHARSET=gbk"
+  run_sql_tidb_with_retry "show create table gbk.ddl3;" "ARTITION BY HASH (\`id\`) PARTITIONS 4"
+
+  # test alter database charset
+  run_sql_source1 "create database gbk3 charset utf8; alter database gbk3 charset gbk;"
+  run_sql_tidb_with_retry "show create database gbk3;" "CHARACTER SET gbk"
+}
+
 function run() {
 	run_dm_master $WORK_DIR/master $MASTER_PORT $cur/conf/dm-master.toml
 	check_rpc_alive $cur/../bin/check_master_online 127.0.0.1:$MASTER_PORT
@@ -36,9 +61,11 @@ function run() {
 
 	echo "check incremental phase"
 	check_sync_diff $WORK_DIR $cur/conf/diff_config.toml
+
+	test_from_tidb
 }
 
-cleanup_data gbk gbk2
+cleanup_data gbk gbk2 gbk3
 # also cleanup dm processes in case of last run failed
 cleanup_process $*
 run $*
