c26ee41b66de318a3adc8ea15b53218ed6e7b4fa
3AceShowHand <jinl1037@hotmail.com>
2021-12-26 21:17:54 +0800

    fix kafka test

--- cdc/sink/producer/kafka/kafka_test.go
+++ cdc/sink/producer/kafka/kafka_test.go
@@ -152,8 +152,7 @@ func (s *kafkaSuite) TestSaramaProducer(c *check.C) {
 		_ = failpoint.Disable("github.com/pingcap/tiflow/cdc/sink/producer/kafka/SkipTopicAutoCreate")
 	}()
 
-	opts := make(map[string]string)
-	producer, err := NewKafkaSaramaProducer(ctx, topic, config, opts, errCh)
+	producer, err := NewKafkaSaramaProducer(ctx, topic, config, map[string]string{}, errCh)
 	c.Assert(err, check.IsNil)
 	c.Assert(producer.GetPartitionNum(), check.Equals, int32(2))
 	for i := 0; i < 100; i++ {
@@ -403,8 +402,7 @@ func (s *kafkaSuite) TestProducerSendMessageFailed(c *check.C) {
 	}()
 
 	errCh := make(chan error, 1)
-	opts := make(map[string]string)
-	producer, err := NewKafkaSaramaProducer(ctx, topic, config, opts, errCh)
+	producer, err := NewKafkaSaramaProducer(ctx, topic, config, map[string]string{}, errCh)
 	defer func() {
 		_ = failpoint.Disable("github.com/pingcap/tiflow/cdc/sink/producer/kafka/SkipTopicAutoCreate")
 		err := producer.Close()
@@ -469,8 +467,7 @@ func (s *kafkaSuite) TestProducerDoubleClose(c *check.C) {
 	c.Assert(failpoint.Enable("github.com/pingcap/tiflow/cdc/sink/producer/kafka/SkipTopicAutoCreate", "return(true)"), check.IsNil)
 
 	errCh := make(chan error, 1)
-	opts := make(map[string]string)
-	producer, err := NewKafkaSaramaProducer(ctx, topic, config, opts, errCh)
+	producer, err := NewKafkaSaramaProducer(ctx, topic, config, map[string]string{}, errCh)
 	defer func() {
 		err := producer.Close()
 		c.Assert(err, check.IsNil)
