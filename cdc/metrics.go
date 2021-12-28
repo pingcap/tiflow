04f7c6267634817491ec8456929d960ed3659f08
ben1009 <liuhe1009@gmail.com>
2021-12-28 14:53:49 +0800

    fix build

--- cdc/metrics.go
+++ cdc/metrics.go
@@ -20,6 +20,7 @@ import (
 	"github.com/pingcap/tiflow/cdc/processor"
 	tablepipeline "github.com/pingcap/tiflow/cdc/processor/pipeline"
 	"github.com/pingcap/tiflow/cdc/puller"
+	redowriter "github.com/pingcap/tiflow/cdc/redo/writer"
 	"github.com/pingcap/tiflow/cdc/sink"
 	"github.com/pingcap/tiflow/cdc/sorter/memory"
 	"github.com/pingcap/tiflow/cdc/sorter/unified"
