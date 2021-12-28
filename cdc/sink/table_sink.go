106b1106f60cc0d2a991b34a9a6a9d6d325a69f3
dongmen <20351731+asddongmen@users.noreply.github.com>
2021-12-28 12:55:50 +0800

    This is an automated cherry-pick of #4084
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- cdc/sink/table_sink.go
+++ cdc/sink/table_sink.go
@@ -34,10 +34,14 @@ type tableSink struct {
 	redoManager redo.LogManager
 }
 
+<<<<<<< HEAD
 func (t *tableSink) Initialize(ctx context.Context, tableInfo []*model.SimpleTableInfo) error {
 	// do nothing
 	return nil
 }
+=======
+var _ Sink = (*tableSink)(nil)
+>>>>>>> 623143076 (sink (ticdc): fix a deadlock due to checkpointTs fall back in sinkNode (#4084))
 
 func (t *tableSink) EmitRowChangedEvents(ctx context.Context, rows ...*model.RowChangedEvent) error {
 	t.buffer = append(t.buffer, rows...)
@@ -132,7 +136,7 @@ func (t *tableSink) EmitCheckpointTs(ctx context.Context, ts uint64) error {
 	return nil
 }
 
-// Note once the Close is called, no more events can be written to this table sink
+// Close once the method is called, no more events can be written to this table sink
 func (t *tableSink) Close(ctx context.Context) error {
 	return t.manager.destroyTableSink(ctx, t.tableID)
 }
