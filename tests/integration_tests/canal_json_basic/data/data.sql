08a6e57f7a82d44d26f0c07ebebb70d18496d5c8
hi-rustin <rustin.liu@gmail.com>
2022-01-01 01:06:35 +0800

    tests(ticdc): support for canal-json test and add basic case

---
+++ tests/integration_tests/canal_json_basic/data/data.sql
@@ -0,0 +1,33 @@
+USE
+`test`;
+
+CREATE TABLE multi_data_type
+(
+    id          INT AUTO_INCREMENT,
+    t_boolean   BOOLEAN,
+    t_bigint    BIGINT,
+    t_double    DOUBLE,
+    t_decimal   DECIMAL(38, 19),
+    t_datetime  DATETIME,
+    t_timestamp TIMESTAMP NULL,
+    t_time      TIME,
+    t_char      CHAR,
+    t_varchar   VARCHAR(10),
+    t_blob      BLOB,
+    t_text      TEXT,
+    t_enum      ENUM ('enum1', 'enum2', 'enum3'),
+    t_set SET ('a', 'b', 'c'),
+    t_json      JSON,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8
+  COLLATE = utf8_bin;
+
+INSERT INTO multi_data_type( t_boolean, t_bigint, t_double, t_decimal
+                           , t_datetime, t_timestamp, t_time
+                           , t_char, t_varchar, t_blob, t_text, t_enum
+                           , t_set, t_json)
+VALUES ( true, 9223372036854775807, 123.123, 123456789012.123456789012
+       , '9999-12-31 23:59:59', '19731230153000', '23:59:59'
+       , '测', '测试', 'blob', '测试text', 'enum2'
+       , 'a,b', NULL);
