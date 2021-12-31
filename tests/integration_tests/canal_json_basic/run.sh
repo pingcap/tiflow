88131728eaceb612c2f076f6a619658a5e320d04
hi-rustin <rustin.liu@gmail.com>
2022-01-01 01:09:29 +0800

    tests(ticdc): fmt

--- tests/integration_tests/canal_json_basic/run.sh
+++ tests/integration_tests/canal_json_basic/run.sh
@@ -9,32 +9,32 @@ CDC_BINARY=cdc.test
 SINK_TYPE=$1
 
 function run() {
-  if [ "$SINK_TYPE" == "mysql" ]; then
-    return
-  fi
+	if [ "$SINK_TYPE" == "mysql" ]; then
+		return
+	fi
 
-  rm -rf $WORK_DIR && mkdir -p $WORK_DIR
+	rm -rf $WORK_DIR && mkdir -p $WORK_DIR
 
-  start_tidb_cluster --workdir $WORK_DIR
+	start_tidb_cluster --workdir $WORK_DIR
 
-  cd $WORK_DIR
+	cd $WORK_DIR
 
-  # record tso before we create tables to skip the system table DDLs
-  start_ts=$(run_cdc_cli_tso_query ${UP_PD_HOST_1} ${UP_PD_PORT_1})
+	# record tso before we create tables to skip the system table DDLs
+	start_ts=$(run_cdc_cli_tso_query ${UP_PD_HOST_1} ${UP_PD_PORT_1})
 
-  run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY
+	run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY
 
-  SINK_URI="kafka://127.0.0.1:9092/test?protocol=canal-json&kafka-version=${KAFKA_VERSION}&max-message-bytes=10485760"
+	SINK_URI="kafka://127.0.0.1:9092/test?protocol=canal-json&kafka-version=${KAFKA_VERSION}&max-message-bytes=10485760"
 
-  run_cdc_cli changefeed create --start-ts=$start_ts --sink-uri="$SINK_URI"
+	run_cdc_cli changefeed create --start-ts=$start_ts --sink-uri="$SINK_URI"
 
-  run_sql_file $CUR/data/data.sql ${UP_TIDB_HOST} ${UP_TIDB_PORT}
+	run_sql_file $CUR/data/data.sql ${UP_TIDB_HOST} ${UP_TIDB_PORT}
 
-  # sync_diff can't check non-exist table, so we check expected tables are created in downstream first
-  check_table_exists test.multi_data_type ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT}  200
-  check_sync_diff $WORK_DIR $CUR/conf/diff_config.toml
+	# sync_diff can't check non-exist table, so we check expected tables are created in downstream first
+	check_table_exists test.multi_data_type ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT} 200
+	check_sync_diff $WORK_DIR $CUR/conf/diff_config.toml
 
-  cleanup_process $CDC_BINARY
+	cleanup_process $CDC_BINARY
 }
 
 trap stop_tidb_cluster EXIT
