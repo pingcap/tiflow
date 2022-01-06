a2fb8142901742c85c877dac3221453bc68fb569
sdojjy <sdojjy@qq.com>
2022-01-06 16:27:05 +0800

    address comment

--- tests/integration_tests/run.sh
+++ tests/integration_tests/run.sh
@@ -60,7 +60,7 @@ fi
 
 set -eu
 
-if [ "$test_case" == "*" ]; then
+if [ "$test_case" == "*" ] && [ "$start_at" == "" ];; then
 	for script in $CUR/*/run.sh; do
 		test_name="$(basename "$(dirname "$script")")"
 		run_case $test_name $script $sink_type
