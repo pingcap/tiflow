39da31711ec1743ee6ed10e7b3c0dbdb00892fa9
3AceShowHand <jinl1037@hotmail.com>
2022-01-17 17:01:03 +0800

    revert unnecessary changes.

--- cdc/sink/producer/kafka/config.go
+++ cdc/sink/producer/kafka/config.go
@@ -248,6 +248,7 @@ func newSaramaConfig(ctx context.Context, c *Config) (*sarama.Config, error) {
 	config.Producer.Return.Successes = true
 	config.Producer.Return.Errors = true
 	config.Producer.RequiredAcks = sarama.WaitForAll
+	// Time out in five minutes(600 * 500ms).
 	config.Producer.Retry.Max = 600
 	config.Producer.Retry.Backoff = 500 * time.Millisecond
 	switch strings.ToLower(strings.TrimSpace(c.Compression)) {
