309b58b6673e42a162d3f8f7594c16e33c3415e1
glorv <glorvs@163.com>
2022-01-17 20:03:17 +0800

    fix

--- dm/syncer/syncer.go
+++ dm/syncer/syncer.go
@@ -1048,14 +1048,15 @@ func (s *Syncer) addJob(job *job) error {
 
 	// Periodically create checkpoint snapshot and async flush checkpoint snapshot
 	if s.checkpoint.CheckGlobalPoint() && s.checkpoint.CheckLastSnapshotCreationTime() {
+		jobSeq := s.getFlushSeq()
+		s.jobWg.Add(1)
 		if s.cfg.Experimental.AsyncCheckpointFlush {
-			jobSeq := s.getFlushSeq()
 			s.tctx.L().Info("Start to async flush current checkpoint to downstream based on flush interval", zap.Int64("job sequence", jobSeq))
 			j := newAsyncFlushJob(s.cfg.WorkerCount, jobSeq)
-			s.jobWg.Add(1)
 			s.dmlJobCh <- j
 			s.flushCheckPointsAsync(j)
 		} else {
+			s.dmlJobCh <- newFlushJob(s.cfg.WorkerCount, jobSeq)
 			s.jobWg.Wait()
 			return s.flushCheckPoints()
 		}
