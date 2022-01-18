cf0d005e777f092d9593aad71966a6220cbd1949
Neil Shen <overvenus@gmail.com>
2022-01-18 14:12:05 +0800

    fix conflicts
    Signed-off-by: Neil Shen <overvenus@gmail.com>

--- cdc/processor/pipeline/puller.go
+++ cdc/processor/pipeline/puller.go
@@ -57,15 +57,6 @@ func (n *pullerNode) tableSpan(ctx cdcContext.Context) []regionspan.Span {
 }
 
 func (n *pullerNode) Init(ctx pipeline.NodeContext) error {
-<<<<<<< HEAD
-	metricTableResolvedTsGauge := tableResolvedTsGauge.WithLabelValues(ctx.ChangefeedVars().ID, ctx.GlobalVars().CaptureInfo.AdvertiseAddr, n.tableName)
-=======
-	return n.InitWithWaitGroup(ctx, new(errgroup.Group))
-}
-
-func (n *pullerNode) InitWithWaitGroup(ctx pipeline.NodeContext, wg *errgroup.Group) error {
-	n.wg = wg
->>>>>>> 662354ad5 (processor(ticdc): add min resolved/checkpoint table ID metrics (#4198))
 	ctxC, cancel := context.WithCancel(ctx)
 	ctxC = util.PutTableInfoInCtx(ctxC, n.tableID, n.tableName)
 	ctxC = util.PutCaptureAddrInCtx(ctxC, ctx.GlobalVars().CaptureInfo.AdvertiseAddr)
