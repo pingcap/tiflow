6b0b7a8ba0ea088bb3e9412d42800286ad04f01b
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2022-01-20 16:33:46 +0800

    owner(ticdc): Fix a nil pointer access bug when appending DataPatches (#4085) (#4181)
    close pingcap/tiflow#3128

--- cdc/owner/feed_state_manager.go
+++ cdc/owner/feed_state_manager.go
@@ -136,6 +136,9 @@ func (m *feedStateManager) handleAdminJob() (jobsPending bool) {
 		m.patchState(model.StateNormal)
 		// remove error history to make sure the changefeed can running in next tick
 		m.state.PatchInfo(func(info *model.ChangeFeedInfo) (*model.ChangeFeedInfo, bool, error) {
+			if info == nil {
+				return nil, false, nil
+			}
 			if info.Error != nil || len(info.ErrorHis) != 0 {
 				info.Error = nil
 				info.ErrorHis = nil
@@ -199,6 +202,9 @@ func (m *feedStateManager) patchState(feedState model.FeedState) {
 	})
 	m.state.PatchInfo(func(info *model.ChangeFeedInfo) (*model.ChangeFeedInfo, bool, error) {
 		changed := false
+		if info == nil {
+			return nil, changed, nil
+		}
 		if info.State != feedState {
 			info.State = feedState
 			changed = true
@@ -259,6 +265,9 @@ func (m *feedStateManager) errorsReportedByProcessors() []*model.RunningError {
 
 func (m *feedStateManager) HandleError(errs ...*model.RunningError) {
 	m.state.PatchInfo(func(info *model.ChangeFeedInfo) (*model.ChangeFeedInfo, bool, error) {
+		if info == nil {
+			return nil, false, nil
+		}
 		for _, err := range errs {
 			info.Error = err
 			info.ErrorHis = append(info.ErrorHis, time.Now().UnixNano()/1e6)
