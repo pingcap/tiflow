bb6e2a664b1d8792cdfce111cf7ce80b05b6a36e
3AceShowHand <jinl1037@hotmail.com>
2022-02-08 15:00:41 +0800

    add basic definition of sarama metrics.

--- cdc/sink/producer/kafka/metrics.go
+++ cdc/sink/producer/kafka/metrics.go
@@ -18,103 +18,141 @@ import (
 )
 
 var (
-//execBatchHistogram = prometheus.NewHistogramVec(
-//	prometheus.HistogramOpts{
-//		Namespace: "ticdc",
-//		Subsystem: "sink",
-//		Name:      "txn_batch_size",
-//		Help:      "Bucketed histogram of batch size of a txn.",
-//		Buckets:   prometheus.ExponentialBuckets(1, 2, 18),
-//	}, []string{"capture", "changefeed"})
-//execTxnHistogram = prometheus.NewHistogramVec(
-//	prometheus.HistogramOpts{
-//		Namespace: "ticdc",
-//		Subsystem: "sink",
-//		Name:      "txn_exec_duration",
-//		Help:      "Bucketed histogram of processing time (s) of a txn.",
-//		Buckets:   prometheus.ExponentialBuckets(0.002 /* 2 ms */, 2, 18),
-//	}, []string{"capture", "changefeed"})
-//execDDLHistogram = prometheus.NewHistogramVec(
-//	prometheus.HistogramOpts{
-//		Namespace: "ticdc",
-//		Subsystem: "sink",
-//		Name:      "ddl_exec_duration",
-//		Help:      "Bucketed histogram of processing time (s) of a ddl.",
-//		Buckets:   prometheus.ExponentialBuckets(0.01, 2, 18),
-//	}, []string{"capture", "changefeed"})
-//executionErrorCounter = prometheus.NewCounterVec(
-//	prometheus.CounterOpts{
-//		Namespace: "ticdc",
-//		Subsystem: "sink",
-//		Name:      "execution_error",
-//		Help:      "total count of execution errors",
-//	}, []string{"capture", "changefeed"})
-//conflictDetectDurationHis = prometheus.NewHistogramVec(
-//	prometheus.HistogramOpts{
-//		Namespace: "ticdc",
-//		Subsystem: "sink",
-//		Name:      "conflict_detect_duration",
-//		Help:      "Bucketed histogram of conflict detect time (s) for single DML statement",
-//		Buckets:   prometheus.ExponentialBuckets(0.001 /* 1 ms */, 2, 20),
-//	}, []string{"capture", "changefeed"})
-//bucketSizeCounter = prometheus.NewCounterVec(
-//	prometheus.CounterOpts{
-//		Namespace: "ticdc",
-//		Subsystem: "sink",
-//		Name:      "bucket_size",
-//		Help:      "size of the DML bucket",
-//	}, []string{"capture", "changefeed", "bucket"})
-//totalRowsCountGauge = prometheus.NewGaugeVec(
-//	prometheus.GaugeOpts{
-//		Namespace: "ticdc",
-//		Subsystem: "sink",
-//		Name:      "total_rows_count",
-//		Help:      "The total count of rows that are processed by sink",
-//	}, []string{"capture", "changefeed"})
-//totalFlushedRowsCountGauge = prometheus.NewGaugeVec(
-//	prometheus.GaugeOpts{
-//		Namespace: "ticdc",
-//		Subsystem: "sink",
-//		Name:      "total_flushed_rows_count",
-//		Help:      "The total count of rows that are flushed by sink",
-//	}, []string{"capture", "changefeed"})
-//flushRowChangedDuration = prometheus.NewHistogramVec(
-//	prometheus.HistogramOpts{
-//		Namespace: "ticdc",
-//		Subsystem: "sink",
-//		Name:      "flush_event_duration_seconds",
-//		Help:      "Bucketed histogram of processing time (s) of flushing events in processor",
-//		Buckets:   prometheus.ExponentialBuckets(0.002 /* 2ms */, 2, 20),
-//	}, []string{"capture", "changefeed", "type"})
-//
-//tableSinkTotalRowsCountCounter = prometheus.NewCounterVec(
-//	prometheus.CounterOpts{
-//		Namespace: "ticdc",
-//		Subsystem: "sink",
-//		Name:      "table_sink_total_rows_count",
-//		Help:      "The total count of rows that are processed by table sink",
-//	}, []string{"capture", "changefeed"})
-//
-//bufferSinkTotalRowsCountCounter = prometheus.NewCounterVec(
-//	prometheus.CounterOpts{
-//		Namespace: "ticdc",
-//		Subsystem: "sink",
-//		Name:      "buffer_sink_total_rows_count",
-//		Help:      "The total count of rows that are processed by buffer sink",
-//	}, []string{"capture", "changefeed"})
+	// Producer level metrics
+	batchSizeHistogram = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Namespace: "ticdc",
+			Subsystem: "sink",
+			Name:      "kafka_producer_batch_size",
+			Help:      "Distribution of the number of bytes sent per partition per request for all topics",
+			Buckets:   prometheus.ExponentialBuckets(1, 2, 18),
+		}, []string{})
+
+	recordSendRateGauge = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Namespace: "ticdc",
+			Subsystem: "sink",
+			Name:      "kafka_producer_batch_size",
+			Help:      "Distribution of the number of bytes sent per partition per request for all topics",
+		}, []string{})
+
+	recordsPerRequestHistogram = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Namespace: "ticdc",
+		Subsystem: "sink",
+		Name:      "kafka_producer_batch_size",
+		Help:      "Distribution of the number of bytes sent per partition per request for all topics",
+		Buckets:   prometheus.ExponentialBuckets(1, 2, 18),
+	}, []string{})
+
+	compressionRatioHistogram = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Namespace: "ticdc",
+		Subsystem: "sink",
+		Name:      "kafka_producer_batch_size",
+		Help:      "Distribution of the number of bytes sent per partition per request for all topics",
+		Buckets:   prometheus.ExponentialBuckets(1, 2, 18),
+	}, []string{})
+
+	// Broker level metrics
+	incomingBytesRateGauge = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Namespace: "ticdc",
+			Subsystem: "sink",
+			Name:      "kafka_producer_batch_size",
+			Help:      "Distribution of the number of bytes sent per partition per request for all topics",
+		}, []string{})
+
+	outgoingBytesRateGauge = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Namespace: "ticdc",
+			Subsystem: "sink",
+			Name:      "kafka_producer_batch_size",
+			Help:      "Distribution of the number of bytes sent per partition per request for all topics",
+		}, []string{})
+
+	requestRateGauge = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Namespace: "ticdc",
+			Subsystem: "sink",
+			Name:      "kafka_producer_batch_size",
+			Help:      "Distribution of the number of bytes sent per partition per request for all topics",
+		}, []string{})
+
+	requestSizeGauge = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Namespace: "ticdc",
+			Subsystem: "sink",
+			Name:      "kafka_producer_batch_size",
+			Help:      "Distribution of the number of bytes sent per partition per request for all topics",
+		}, []string{})
+
+	requestLatencyInMsHistogram = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Namespace: "ticdc",
+		Subsystem: "sink",
+		Name:      "kafka_producer_batch_size",
+		Help:      "Distribution of the number of bytes sent per partition per request for all topics",
+		Buckets:   prometheus.ExponentialBuckets(1, 2, 18),
+	}, []string{})
+
+	responseRateGauge = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Namespace: "ticdc",
+			Subsystem: "sink",
+			Name:      "kafka_producer_batch_size",
+			Help:      "Distribution of the number of bytes sent per partition per request for all topics",
+		}, []string{})
+
+	responseSizeHistogram = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Namespace: "ticdc",
+		Subsystem: "sink",
+		Name:      "kafka_producer_batch_size",
+		Help:      "Distribution of the number of bytes sent per partition per request for all topics",
+		Buckets:   prometheus.ExponentialBuckets(1, 2, 18),
+	}, []string{})
+
+	requestInFlightCounter = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Namespace: "ticdc",
+		Subsystem: "sink",
+		Name:      "kafka_producer_request_in_flight",
+		Help:      "The current number of in-flight requests awaiting a response for all brokers",
+	}, []string{})
 )
 
 // InitMetrics registers all metrics in this file
 func InitMetrics(registry *prometheus.Registry) {
-	//registry.MustRegister(execBatchHistogram)
-	//registry.MustRegister(execTxnHistogram)
-	//registry.MustRegister(execDDLHistogram)
-	//registry.MustRegister(executionErrorCounter)
-	//registry.MustRegister(conflictDetectDurationHis)
-	//registry.MustRegister(bucketSizeCounter)
-	//registry.MustRegister(totalRowsCountGauge)
-	//registry.MustRegister(totalFlushedRowsCountGauge)
-	//registry.MustRegister(flushRowChangedDuration)
-	//registry.MustRegister(tableSinkTotalRowsCountCounter)
-	//registry.MustRegister(bufferSinkTotalRowsCountCounter)
+	registry.MustRegister(batchSizeHistogram)
+	registry.MustRegister(recordSendRateGauge)
+	registry.MustRegister(recordsPerRequestHistogram)
+	registry.MustRegister(compressionRatioHistogram)
+
+	registry.MustRegister(incomingBytesRateGauge)
+	registry.MustRegister(outgoingBytesRateGauge)
+	registry.MustRegister(requestRateGauge)
+	registry.MustRegister(requestSizeGauge)
+	registry.MustRegister(requestLatencyInMsHistogram)
+	registry.MustRegister(responseRateGauge)
+	registry.MustRegister(responseSizeHistogram)
+	registry.MustRegister(requestInFlightCounter)
 }
+
+//+----------------------------------------------+------------+---------------------------------------------------------------+
+//| Name                                         | Type       | Description                                                   |
+//+----------------------------------------------+------------+---------------------------------------------------------------+
+//| incoming-byte-rate                           | meter      | Bytes/second read off all brokers                             |
+//| incoming-byte-rate-for-broker-<broker-id>    | meter      | Bytes/second read off a given broker                          |
+//| outgoing-byte-rate                           | meter      | Bytes/second written off all brokers                          |
+//| outgoing-byte-rate-for-broker-<broker-id>    | meter      | Bytes/second written off a given broker                       |
+//| request-rate                                 | meter      | Requests/second sent to all brokers                           |
+//| request-rate-for-broker-<broker-id>          | meter      | Requests/second sent to a given broker                        |
+//| request-size                                 | histogram  | Distribution of the request size in bytes for all brokers     |
+//| request-size-for-broker-<broker-id>          | histogram  | Distribution of the request size in bytes for a given broker  |
+//| request-latency-in-ms                        | histogram  | Distribution of the request latency in ms for all brokers     |
+//| request-latency-in-ms-for-broker-<broker-id> | histogram  | Distribution of the request latency in ms for a given broker  |
+//| response-rate                                | meter      | Responses/second received from all brokers                    |
+//| response-rate-for-broker-<broker-id>         | meter      | Responses/second received from a given broker                 |
+//| response-size                                | histogram  | Distribution of the response size in bytes for all brokers    |
+//| response-size-for-broker-<broker-id>         | histogram  | Distribution of the response size in bytes for a given broker |
+//| requests-in-flight                           | counter    | The current number of in-flight requests awaiting a response  |
+//|                                              |            | for all brokers                                               |
+//| requests-in-flight-for-broker-<broker-id>    | counter    | The current number of in-flight requests awaiting a response  |
+//|                                              |            | for a given broker                                            |
+//+----------------------------------------------+------------+---------------------------------------------------------------+
