1e9b167655791f317c31a01b703852621a96b750
3AceShowHand <jinl1037@hotmail.com>
2022-02-08 16:07:05 +0800

    only producer metrics amt.

--- cdc/sink/producer/kafka/metrics.go
+++ cdc/sink/producer/kafka/metrics.go
@@ -20,8 +20,7 @@ import (
 type kafkaComponentType int
 
 const (
-	kafkaBroker kafkaComponentType = iota
-	kafkaProducer
+	kafkaProducer kafkaComponentType = iota
 )
 
 type saramaMetrics struct {
@@ -29,56 +28,6 @@ type saramaMetrics struct {
 	name        string
 	description string
 }
-
-var (
-	incomingByteRate = saramaMetrics{
-		component:   kafkaBroker,
-		name:        "incoming-byte-rate",
-		description: "Bytes/second read off all brokers",
-	}
-
-	outgoingByteRate = saramaMetrics{
-		component:   kafkaBroker,
-		name:        "outgoing-byte-rate",
-		description: "Bytes/second written off all brokers",
-	}
-
-	requestRate = saramaMetrics{
-		component:   kafkaBroker,
-		name:        "request-rate",
-		description: "Requests/second sent to all brokers",
-	}
-
-	requestSize = saramaMetrics{
-		component:   kafkaBroker,
-		name:        "request-size",
-		description: "Distribution of the request size in bytes for all brokers",
-	}
-
-	requestLatencyInMs = saramaMetrics{
-		component:   kafkaBroker,
-		name:        "request-latency-in-ms",
-		description: "Distribution of the request latency in ms for all brokers",
-	}
-
-	responseRate = saramaMetrics{
-		component:   kafkaBroker,
-		name:        "response-rate",
-		description: "Responses/second received from all brokers",
-	}
-
-	responseSize = saramaMetrics{
-		component:   kafkaBroker,
-		name:        "response-size",
-		description: "Distribution of the response size in bytes for all brokers",
-	}
-
-	requestInFlight = saramaMetrics{
-		component:   kafkaBroker,
-		name:        "requests-in-flight",
-		description: "The current number of in-flight requests awaiting a response for all brokers",
-	}
-)
 
 var (
 	batchSize = saramaMetrics{
@@ -140,70 +89,6 @@ var (
 		Help:      "Distribution of the number of bytes sent per partition per request for all topics",
 		Buckets:   prometheus.ExponentialBuckets(1, 2, 18),
 	}, []string{})
-
-	// Broker level metrics
-	incomingBytesRateGauge = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Namespace: "ticdc",
-			Subsystem: "sink",
-			Name:      "kafka_producer_batch_size",
-			Help:      "Distribution of the number of bytes sent per partition per request for all topics",
-		}, []string{})
-
-	outgoingBytesRateGauge = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Namespace: "ticdc",
-			Subsystem: "sink",
-			Name:      "kafka_producer_batch_size",
-			Help:      "Distribution of the number of bytes sent per partition per request for all topics",
-		}, []string{})
-
-	requestRateGauge = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Namespace: "ticdc",
-			Subsystem: "sink",
-			Name:      "kafka_producer_batch_size",
-			Help:      "Distribution of the number of bytes sent per partition per request for all topics",
-		}, []string{})
-
-	requestSizeGauge = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Namespace: "ticdc",
-			Subsystem: "sink",
-			Name:      "kafka_producer_batch_size",
-			Help:      "Distribution of the number of bytes sent per partition per request for all topics",
-		}, []string{})
-
-	requestLatencyInMsHistogram = prometheus.NewHistogramVec(prometheus.HistogramOpts{
-		Namespace: "ticdc",
-		Subsystem: "sink",
-		Name:      "kafka_producer_batch_size",
-		Help:      "Distribution of the number of bytes sent per partition per request for all topics",
-		Buckets:   prometheus.ExponentialBuckets(1, 2, 18),
-	}, []string{})
-
-	responseRateGauge = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Namespace: "ticdc",
-			Subsystem: "sink",
-			Name:      "kafka_producer_batch_size",
-			Help:      "Distribution of the number of bytes sent per partition per request for all topics",
-		}, []string{})
-
-	responseSizeHistogram = prometheus.NewHistogramVec(prometheus.HistogramOpts{
-		Namespace: "ticdc",
-		Subsystem: "sink",
-		Name:      "kafka_producer_batch_size",
-		Help:      "Distribution of the number of bytes sent per partition per request for all topics",
-		Buckets:   prometheus.ExponentialBuckets(1, 2, 18),
-	}, []string{})
-
-	requestInFlightCounter = prometheus.NewCounterVec(prometheus.CounterOpts{
-		Namespace: "ticdc",
-		Subsystem: "sink",
-		Name:      "kafka_producer_request_in_flight",
-		Help:      "The current number of in-flight requests awaiting a response for all brokers",
-	}, []string{})
 )
 
 // InitMetrics registers all metrics in this file
@@ -212,13 +97,4 @@ func InitMetrics(registry *prometheus.Registry) {
 	registry.MustRegister(recordSendRateGauge)
 	registry.MustRegister(recordsPerRequestHistogram)
 	registry.MustRegister(compressionRatioHistogram)
-
-	registry.MustRegister(incomingBytesRateGauge)
-	registry.MustRegister(outgoingBytesRateGauge)
-	registry.MustRegister(requestRateGauge)
-	registry.MustRegister(requestSizeGauge)
-	registry.MustRegister(requestLatencyInMsHistogram)
-	registry.MustRegister(responseRateGauge)
-	registry.MustRegister(responseSizeHistogram)
-	registry.MustRegister(requestInFlightCounter)
 }
