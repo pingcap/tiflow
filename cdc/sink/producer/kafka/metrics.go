2c090cba4d37e1f7f11518a82e96983cba46669f
3AceShowHand <jinl1037@hotmail.com>
2022-02-08 17:47:45 +0800

    add more metrics.

--- cdc/sink/producer/kafka/metrics.go
+++ cdc/sink/producer/kafka/metrics.go
@@ -17,90 +17,107 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/pingcap/log"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/rcrowley/go-metrics"
+	"go.uber.org/zap"
 )
 
-type kafkaComponentType int
-
-const (
-	kafkaProducer kafkaComponentType = iota
-)
-
+// saramaMetrics wrap prometheus metrics, update value from sarama metrics.
 type saramaMetrics struct {
-	component   kafkaComponentType
-	name        string
-	description string
+	// metricsName is used to fetch sarama metrics
+	metricsName string
+	collector   prometheus.Collector
 }
+
+//type kafkaMetrics struct {
+//	metrics []*saramaMetrics
+//}
+//
 
 var (
 	batchSize = saramaMetrics{
-		component:   kafkaProducer,
-		name:        "batch-size",
-		description: "Distribution of the number of bytes sent per partition per request for all topics",
+		metricsName: "batch-size",
+		collector: prometheus.NewHistogramVec(
+			prometheus.HistogramOpts{
+				Namespace: "ticdc",
+				Subsystem: "sink",
+				Name:      "kafka_producer_batch_size",
+				Help:      "Distribution of the number of bytes sent per partition per request for all topics",
+				Buckets:   prometheus.ExponentialBuckets(1, 2, 18),
+			}, []string{}),
 	}
 
 	recordSendRate = saramaMetrics{
-		component:   kafkaProducer,
-		name:        "record-send-rate",
-		description: "Records/second sent to all topics",
+		metricsName: "record-send-rate",
+		collector: prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Namespace: "ticdc",
+				Subsystem: "sink",
+				Name:      "kafka_producer_record_send_rate",
+				Help:      "Records/second sent to all topics",
+			}, []string{}),
 	}
 
 	recordsPerRequest = saramaMetrics{
-		component:   kafkaProducer,
-		name:        "records-per-request",
-		description: "Distribution of the number of records sent per request for all topics",
+		metricsName: "records-per-request",
+		collector: prometheus.NewHistogramVec(prometheus.HistogramOpts{
+			Namespace: "ticdc",
+			Subsystem: "sink",
+			Name:      "kafka_producer_records_per_request",
+			Help:      "Distribution of the number of records sent per request for all topics",
+			Buckets:   prometheus.ExponentialBuckets(1, 2, 18),
+		}, []string{}),
 	}
 
 	compressionRatio = saramaMetrics{
-		component:   kafkaProducer,
-		name:        "compression-ratio",
-		description: "Distribution of the compression ratio times 100 of record batches for all topics",
+		metricsName: "compression-ratio",
+		collector: prometheus.NewHistogramVec(prometheus.HistogramOpts{
+			Namespace: "ticdc",
+			Subsystem: "sink",
+			Name:      "kafka_producer_compression_ratio",
+			Help:      "Distribution of the compression ratio times 100 of record batches for all topics",
+			Buckets:   prometheus.ExponentialBuckets(1, 2, 18),
+		}, []string{}),
 	}
 )
 
-var (
-	// Producer level metrics
-	batchSizeHistogram = prometheus.NewHistogramVec(
-		prometheus.HistogramOpts{
-			Namespace: "ticdc",
-			Subsystem: "sink",
-			Name:      "kafka_producer_batch_size",
-			Help:      "Distribution of the number of bytes sent per partition per request for all topics",
-			Buckets:   prometheus.ExponentialBuckets(1, 2, 18),
-		}, []string{})
+func (m saramaMetrics) Update(registry metrics.Registry) {
+	// fetch sarama metrics
+	rawMetric := registry.Get(m.metricsName)
+	if rawMetric == nil {
+		log.Warn("sarama metrics not found", zap.String("metricName", m.metricsName))
+		return
+	}
 
-	recordSendRateGauge = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Namespace: "ticdc",
-			Subsystem: "sink",
-			Name:      "kafka_producer_record_send_rate",
-			Help:      "Records/second sent to all topics",
-		}, []string{})
+	var value float64
+	switch tp := rawMetric.(type) {
+	case metrics.Meter:
+		snapshot := tp.Snapshot()
+		value = snapshot.RateMean()
+		//rate1 := snapshot.Rate1()
+		//rate5 := snapshot.Rate5()
+		//rate15 := snapshot.Rate15()
+		//rateMean := snapshot.RateMean()
+	case metrics.Histogram:
+		snapshot := tp.Snapshot()
+		value = snapshot.Mean()
+		snapshot.Mean()
+	case metrics.Counter:
+		snapshot := tp.Snapshot()
+		value = float64(snapshot.Count())
+	case metrics.Gauge:
+		snapshot := tp.Snapshot()
+		value = float64(snapshot.Value())
+	}
 
-	recordsPerRequestHistogram = prometheus.NewHistogramVec(prometheus.HistogramOpts{
-		Namespace: "ticdc",
-		Subsystem: "sink",
-		Name:      "kafka_producer_records_per_request",
-		Help:      "Distribution of the number of records sent per request for all topics",
-		Buckets:   prometheus.ExponentialBuckets(1, 2, 18),
-	}, []string{})
-
-	compressionRatioHistogram = prometheus.NewHistogramVec(prometheus.HistogramOpts{
-		Namespace: "ticdc",
-		Subsystem: "sink",
-		Name:      "kafka_producer_compression_ratio",
-		Help:      "Distribution of the compression ratio times 100 of record batches for all topics",
-		Buckets:   prometheus.ExponentialBuckets(1, 2, 18),
-	}, []string{})
-)
-
-// InitMetrics registers all metrics in this file
-func InitMetrics(registry *prometheus.Registry) {
-	registry.MustRegister(batchSizeHistogram)
-	registry.MustRegister(recordSendRateGauge)
-	registry.MustRegister(recordsPerRequestHistogram)
-	registry.MustRegister(compressionRatioHistogram)
+	// update prometheus metrics
+	switch tp := m.collector.(type) {
+	case prometheus.Histogram:
+		tp.Observe(value)
+	default:
+		log.Panic("not support prometheus collector type")
+	}
 }
 
 func printMetrics(w io.Writer, r metrics.Registry) {
@@ -134,4 +151,12 @@ func printMetrics(w io.Writer, r metrics.Registry) {
 		requestLatencyPercentiles[4],
 		requestsInFlight,
 	)
+}
+
+// InitMetrics registers all metrics in this file
+func InitMetrics(registry *prometheus.Registry) {
+	registry.MustRegister(batchSize.collector)
+	//registry.MustRegister(recordSendRateGauge)
+	//registry.MustRegister(recordsPerRequestHistogram)
+	//registry.MustRegister(compressionRatioHistogram)
 }
