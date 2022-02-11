58e0fec7d560dcba431acedfc4ac4e5c608c0c11
ehco1996 <zh19960202@gmail.com>
2022-02-11 18:27:56 +0800

    revert change

--- dm/dm/master/openapi_test.go
+++ dm/dm/master/openapi_test.go
@@ -155,7 +155,7 @@ func (t *openAPISuite) TestOpenAPIWillNotStartInDefaultConfig(c *check.C) {
 	}), check.IsTrue)
 	c.Assert(s1.echo, check.IsNil)
 	defer s1.Close()
-	defer cancel() // this cancel must call before s.Close() to avoid deadlock
+	cancel()
 }
 
 func (t *openAPISuite) TestSourceAPI(c *check.C) {
