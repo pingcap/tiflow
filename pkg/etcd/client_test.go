aa9d2663e0516c796d8b64f9e7eb4f7f6876ab1a
dongmen <20351731+asddongmen@users.noreply.github.com>
2022-02-13 23:59:37 +0800

    This is an automated cherry-pick of #4551
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- pkg/etcd/client_test.go
+++ pkg/etcd/client_test.go
@@ -15,6 +15,13 @@ package etcd
 
 import (
 	"context"
+<<<<<<< HEAD
+=======
+	"io/ioutil"
+	"os"
+	"sync/atomic"
+	"testing"
+>>>>>>> fc3b5ad53 (etcd (ticdc): fix a data race in unit test (#4551))
 	"time"
 
 	"github.com/benbjohnson/clock"
@@ -49,17 +56,32 @@ func (m *mockClient) Put(ctx context.Context, key, val string, opts ...clientv3.
 type mockWatcher struct {
 	clientv3.Watcher
 	watchCh      chan clientv3.WatchResponse
+<<<<<<< HEAD
 	resetCount   *int
 	requestCount *int
 }
 
 func (m mockWatcher) Watch(ctx context.Context, key string, opts ...clientv3.OpOption) clientv3.WatchChan {
 	*m.resetCount++
+=======
+	resetCount   *int32
+	requestCount *int32
+	rev          *int64
+}
+
+func (m mockWatcher) Watch(ctx context.Context, key string, opts ...clientv3.OpOption) clientv3.WatchChan {
+	atomic.AddInt32(m.resetCount, 1)
+	op := &clientv3.Op{}
+	for _, opt := range opts {
+		opt(op)
+	}
+	atomic.StoreInt64(m.rev, op.Rev())
+>>>>>>> fc3b5ad53 (etcd (ticdc): fix a data race in unit test (#4551))
 	return m.watchCh
 }
 
 func (m mockWatcher) RequestProgress(ctx context.Context) error {
-	*m.requestCount++
+	atomic.AddInt32(m.requestCount, 1)
 	return nil
 }
 
@@ -115,8 +137,14 @@ func (s *etcdSuite) TestWatchChBlocked(c *check.C) {
 	defer testleak.AfterTest(c)()
 	defer s.TearDownTest(c)
 	cli := clientv3.NewCtxClient(context.TODO())
+<<<<<<< HEAD
 	resetCount := 0
 	requestCount := 0
+=======
+	resetCount := int32(0)
+	requestCount := int32(0)
+	rev := int64(0)
+>>>>>>> fc3b5ad53 (etcd (ticdc): fix a data race in unit test (#4551))
 	watchCh := make(chan clientv3.WatchResponse, 1)
 	watcher := mockWatcher{watchCh: watchCh, resetCount: &resetCount, requestCount: &requestCount}
 	cli.Watcher = watcher
@@ -162,9 +190,15 @@ func (s *etcdSuite) TestWatchChBlocked(c *check.C) {
 
 	c.Check(sentRes, check.DeepEquals, receivedRes)
 	// make sure watchCh has been reset since timeout
+<<<<<<< HEAD
 	c.Assert(*watcher.resetCount > 1, check.IsTrue)
 	// make sure RequestProgress has been call since timeout
 	c.Assert(*watcher.requestCount > 1, check.IsTrue)
+=======
+	require.True(t, atomic.LoadInt32(watcher.resetCount) > 1)
+	// make sure RequestProgress has been call since timeout
+	require.True(t, atomic.LoadInt32(watcher.requestCount) > 1)
+>>>>>>> fc3b5ad53 (etcd (ticdc): fix a data race in unit test (#4551))
 	// make sure etcdRequestProgressDuration is less than etcdWatchChTimeoutDuration
 	c.Assert(etcdRequestProgressDuration, check.Less, etcdWatchChTimeoutDuration)
 }
@@ -175,8 +209,14 @@ func (s *etcdSuite) TestOutChBlocked(c *check.C) {
 	defer s.TearDownTest(c)
 
 	cli := clientv3.NewCtxClient(context.TODO())
+<<<<<<< HEAD
 	resetCount := 0
 	requestCount := 0
+=======
+	resetCount := int32(0)
+	requestCount := int32(0)
+	rev := int64(0)
+>>>>>>> fc3b5ad53 (etcd (ticdc): fix a data race in unit test (#4551))
 	watchCh := make(chan clientv3.WatchResponse, 1)
 	watcher := mockWatcher{watchCh: watchCh, resetCount: &resetCount, requestCount: &requestCount}
 	cli.Watcher = watcher
@@ -215,7 +255,102 @@ func (s *etcdSuite) TestOutChBlocked(c *check.C) {
 
 	for r := range outCh {
 		receivedRes = append(receivedRes, r)
+<<<<<<< HEAD
 	}
 
 	c.Check(sentRes, check.DeepEquals, receivedRes)
+=======
+		if len(receivedRes) == len(sentRes) {
+			cancel()
+		}
+	}
+
+	require.Equal(t, sentRes, receivedRes)
+}
+
+func TestRevisionNotFallBack(t *testing.T) {
+	cli := clientv3.NewCtxClient(context.TODO())
+
+	resetCount := int32(0)
+	requestCount := int32(0)
+	rev := int64(0)
+	watchCh := make(chan clientv3.WatchResponse, 1)
+	watcher := mockWatcher{watchCh: watchCh, resetCount: &resetCount, requestCount: &requestCount, rev: &rev}
+	cli.Watcher = watcher
+	mockClock := clock.NewMock()
+	watchCli := Wrap(cli, nil)
+	watchCli.clock = mockClock
+
+	key := "testRevisionNotFallBack"
+	outCh := make(chan clientv3.WatchResponse, 1)
+	// watch from revision = 2
+	revision := int64(2)
+
+	sentRes := []clientv3.WatchResponse{
+		{CompactRevision: 1},
+	}
+
+	go func() {
+		for _, r := range sentRes {
+			watchCh <- r
+		}
+	}()
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*2)
+	defer cancel()
+	go func() {
+		watchCli.WatchWithChan(ctx, outCh, key, "", clientv3.WithPrefix(), clientv3.WithRev(revision))
+	}()
+	// wait for WatchWithChan set up
+	<-outCh
+	// move time forward
+	mockClock.Add(time.Second * 30)
+	// make sure watchCh has been reset since timeout
+	require.True(t, atomic.LoadInt32(watcher.resetCount) > 1)
+	// make sure revision in WatchWitchChan does not fall back
+	// even if there has not any response been received from WatchCh
+	// while WatchCh was reset
+	require.Equal(t, atomic.LoadInt64(watcher.rev), revision)
+}
+
+type mockTxn struct {
+	ctx  context.Context
+	mode int
+}
+
+func (txn *mockTxn) If(cs ...clientv3.Cmp) clientv3.Txn {
+	if cs != nil {
+		txn.mode += 1
+	}
+	return txn
+}
+
+func (txn *mockTxn) Then(ops ...clientv3.Op) clientv3.Txn {
+	if ops != nil {
+		txn.mode += 1 << 1
+	}
+	return txn
+}
+
+func (txn *mockTxn) Else(ops ...clientv3.Op) clientv3.Txn {
+	if ops != nil {
+		txn.mode += 1 << 2
+	}
+	return txn
+}
+
+func (txn *mockTxn) Commit() (*clientv3.TxnResponse, error) {
+	switch txn.mode {
+	case 0:
+		return &clientv3.TxnResponse{}, nil
+	case 1:
+		return nil, rpctypes.ErrNoSpace
+	case 2:
+		return nil, rpctypes.ErrTimeoutDueToLeaderFail
+	case 3:
+		return nil, context.DeadlineExceeded
+	default:
+		return nil, errors.New("mock error")
+	}
+>>>>>>> fc3b5ad53 (etcd (ticdc): fix a data race in unit test (#4551))
 }
