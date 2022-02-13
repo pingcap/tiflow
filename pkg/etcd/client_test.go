7110fc1bec95522807e205cd4b87093fe55e706b
dongmen <20351731+asddongmen@users.noreply.github.com>
2022-02-13 23:59:37 +0800

    This is an automated cherry-pick of #4551
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- pkg/etcd/client_test.go
+++ pkg/etcd/client_test.go
@@ -15,12 +15,15 @@ package etcd
 
 import (
 	"context"
+	"sync/atomic"
+	"testing"
 	"time"
 
 	"github.com/benbjohnson/clock"
 	"github.com/pingcap/check"
 	"github.com/pingcap/errors"
 	"github.com/pingcap/tiflow/pkg/util/testleak"
+	"github.com/stretchr/testify/require"
 	"go.etcd.io/etcd/clientv3"
 )
 
@@ -48,23 +51,23 @@ func (m *mockClient) Put(ctx context.Context, key, val string, opts ...clientv3.
 type mockWatcher struct {
 	clientv3.Watcher
 	watchCh      chan clientv3.WatchResponse
-	resetCount   *int
-	requestCount *int
+	resetCount   *int32
+	requestCount *int32
 	rev          *int64
 }
 
 func (m mockWatcher) Watch(ctx context.Context, key string, opts ...clientv3.OpOption) clientv3.WatchChan {
-	*m.resetCount++
+	atomic.AddInt32(m.resetCount, 1)
 	op := &clientv3.Op{}
 	for _, opt := range opts {
 		opt(op)
 	}
-	*m.rev = op.Rev()
+	atomic.StoreInt64(m.rev, op.Rev())
 	return m.watchCh
 }
 
 func (m mockWatcher) RequestProgress(ctx context.Context) error {
-	*m.requestCount++
+	atomic.AddInt32(m.requestCount, 1)
 	return nil
 }
 
@@ -116,11 +119,10 @@ func (s *etcdSuite) TestDelegateLease(c *check.C) {
 }
 
 // test no data lost when WatchCh blocked
-func (s *clientSuite) TestWatchChBlocked(c *check.C) {
-	defer testleak.AfterTest(c)()
+func TestWatchChBlocked(t *testing.T) {
 	cli := clientv3.NewCtxClient(context.TODO())
-	resetCount := 0
-	requestCount := 0
+	resetCount := int32(0)
+	requestCount := int32(0)
 	rev := int64(0)
 	watchCh := make(chan clientv3.WatchResponse, 1)
 	watcher := mockWatcher{watchCh: watchCh, resetCount: &resetCount, requestCount: &requestCount, rev: &rev}
@@ -168,22 +170,20 @@ func (s *clientSuite) TestWatchChBlocked(c *check.C) {
 		}
 	}
 
-	c.Check(sentRes, check.DeepEquals, receivedRes)
+	require.Equal(t, sentRes, receivedRes)
 	// make sure watchCh has been reset since timeout
-	c.Assert(*watcher.resetCount > 1, check.IsTrue)
+	require.True(t, atomic.LoadInt32(watcher.resetCount) > 1)
 	// make sure RequestProgress has been call since timeout
-	c.Assert(*watcher.requestCount > 1, check.IsTrue)
+	require.True(t, atomic.LoadInt32(watcher.requestCount) > 1)
 	// make sure etcdRequestProgressDuration is less than etcdWatchChTimeoutDuration
-	c.Assert(etcdRequestProgressDuration, check.Less, etcdWatchChTimeoutDuration)
+	require.Less(t, etcdRequestProgressDuration, etcdWatchChTimeoutDuration)
 }
 
 // test no data lost when OutCh blocked
-func (s *clientSuite) TestOutChBlocked(c *check.C) {
-	defer testleak.AfterTest(c)()
-
+func TestOutChBlocked(t *testing.T) {
 	cli := clientv3.NewCtxClient(context.TODO())
-	resetCount := 0
-	requestCount := 0
+	resetCount := int32(0)
+	requestCount := int32(0)
 	rev := int64(0)
 	watchCh := make(chan clientv3.WatchResponse, 1)
 	watcher := mockWatcher{watchCh: watchCh, resetCount: &resetCount, requestCount: &requestCount, rev: &rev}
@@ -228,15 +228,14 @@ func (s *clientSuite) TestOutChBlocked(c *check.C) {
 		}
 	}
 
-	c.Check(sentRes, check.DeepEquals, receivedRes)
+	require.Equal(t, sentRes, receivedRes)
 }
 
-func (s *clientSuite) TestRevisionNotFallBack(c *check.C) {
-	defer testleak.AfterTest(c)()
+func TestRevisionNotFallBack(t *testing.T) {
 	cli := clientv3.NewCtxClient(context.TODO())
 
-	resetCount := 0
-	requestCount := 0
+	resetCount := int32(0)
+	requestCount := int32(0)
 	rev := int64(0)
 	watchCh := make(chan clientv3.WatchResponse, 1)
 	watcher := mockWatcher{watchCh: watchCh, resetCount: &resetCount, requestCount: &requestCount, rev: &rev}
@@ -270,9 +269,9 @@ func (s *clientSuite) TestRevisionNotFallBack(c *check.C) {
 	// move time forward
 	mockClock.Add(time.Second * 30)
 	// make sure watchCh has been reset since timeout
-	c.Assert(*watcher.resetCount > 1, check.IsTrue)
-	// make suer revision in WatchWitchChan does not fall back
+	require.True(t, atomic.LoadInt32(watcher.resetCount) > 1)
+	// make sure revision in WatchWitchChan does not fall back
 	// even if there has not any response been received from WatchCh
 	// while WatchCh was reset
-	c.Assert(*watcher.rev, check.Equals, revision)
+	require.Equal(t, atomic.LoadInt64(watcher.rev), revision)
 }
