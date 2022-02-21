759b3ecfd28034e297dd40a51990c2b0ae6990ae
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2022-02-21 19:21:42 +0800

    etcd/client (ticdc): Prevent revision in WatchWitchChan fallback. (#3851) (#3927)

--- pkg/etcd/client.go
+++ pkg/etcd/client.go
@@ -188,7 +188,9 @@ func (c *Client) WatchWithChan(ctx context.Context, outCh chan<- clientv3.WatchR
 		close(outCh)
 		log.Info("WatchWithChan exited", zap.String("role", role))
 	}()
-	var lastRevision int64
+	// get initial revision from opts to avoid revision fall back
+	lastRevision := getRevisionFromWatchOpts(opts...)
+
 	watchCtx, cancel := context.WithCancel(ctx)
 	defer cancel()
 	watchCh := c.cli.Watch(watchCtx, key, opts...)
@@ -238,7 +240,7 @@ func (c *Client) WatchWithChan(ctx context.Context, outCh chan<- clientv3.WatchR
 					zap.String("role", role))
 				cancel()
 				watchCtx, cancel = context.WithCancel(ctx)
-				watchCh = c.cli.Watch(watchCtx, key, clientv3.WithPrefix(), clientv3.WithRev(lastRevision+1))
+				watchCh = c.cli.Watch(watchCtx, key, clientv3.WithPrefix(), clientv3.WithRev(lastRevision))
 				// we need to reset lastReceivedResponseTime after reset Watch
 				lastReceivedResponseTime = c.clock.Now()
 			}
