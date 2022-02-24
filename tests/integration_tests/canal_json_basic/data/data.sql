c019e9a89a79ea53b071a7c7a34cfec0578ee527
工业废水 <rustin.liu@gmail.com>
2022-02-24 13:37:42 +0800

    test(ticdc): revert canal json basic test changes (#4662)
    close pingcap/tiflow#4661

--- tests/integration_tests/canal_json_basic/data/data.sql
+++ tests/integration_tests/canal_json_basic/data/data.sql
@@ -53,7 +53,7 @@ INSERT INTO multi_data_type( t_tinyint, t_tinyint_unsigned, t_smallint, t_smalli
                            , t_set, t_json)
 VALUES ( -1, 1, -129, 129, -65536, 65536, -16777216, 16777216, -2147483649, 2147483649
        , true, 123.456, 123.123, 123456789012.123456789012
-       , '测', '测试', '89504E470D0A1A0A', '89504E470D0A1A0A', '测试tinytext', '测试text', '测试mediumtext', '测试longtext'
+       , '测', '测试', x'89504E470D0A1A0A', x'89504E470D0A1A0A', '测试tinytext', '测试text', '测试mediumtext', '测试longtext'
        , 'tinyblob', 'blob', 'mediumblob', 'longblob'
        , '1977-01-01', '9999-12-31 23:59:59', '19731230153000', '23:59:59'
        , 'enum2'
@@ -69,7 +69,7 @@ INSERT INTO multi_data_type( t_tinyint, t_tinyint_unsigned, t_smallint, t_smalli
                            , t_set, t_json)
 VALUES ( -2, 2, -130, 130, -65537, 65537, -16777217, 16777217, -2147483650, 2147483650
        , false, 123.4567, 123.1237, 123456789012.1234567890127
-       , '2', '测试2', '89504E470D0A1A0B', '89504E470D0A1A0B', '测试2tinytext', '测试2text', '测试2mediumtext', '测试longtext'
+       , '2', '测试2', x'89504E470D0A1A0B', x'89504E470D0A1A0B', '测试2tinytext', '测试2text', '测试2mediumtext', '测试longtext'
        , 'tinyblob2', 'blob2', 'mediumblob2', 'longblob2'
        , '2021-01-01', '2021-12-31 23:59:59', '19731230153000', '22:59:59'
        , 'enum1'
