9713d42c2ee213f4fe0f762c5aa68b7136b757bb
3AceShowHand <jinl1037@hotmail.com>
2022-02-25 15:01:09 +0800

    Revert "cdc/sink: kafka sink integrate broker level metrics (#4517) (#4594)"
    This reverts commit 334329d9c3f56492fe04a15647e0b6070252ed8e.

--- cdc/sink/producer/kafka/kafka.go
+++ cdc/sink/producer/kafka/kafka.go
@@ -218,11 +218,9 @@ type kafkaSaramaProducer struct {
 	// Since we don't close these two clients (which have an input chan) from the
 	// sender routine, data race or send on closed chan could happen.
 	clientLock    sync.RWMutex
-	admin         kafka.ClusterAdminClient
 	client        sarama.Client
 	asyncProducer sarama.AsyncProducer
 	syncProducer  sarama.SyncProducer
-
 	// producersReleased records whether asyncProducer and syncProducer have been closed properly
 	producersReleased bool
 	topic             string
@@ -428,18 +426,7 @@ func (k *kafkaSaramaProducer) Close() error {
 			zap.String("changefeed", k.id), zap.Any("role", k.role))
 	}
 
-	k.metricsMonitor.cleanup()
-
-	start = time.Now()
-	if err := k.admin.Close(); err != nil {
-		log.Warn("close kafka cluster admin with error", zap.Error(err),
-			zap.Duration("duration", time.Since(start)),
-			zap.String("changefeed", k.id), zap.Any("role", k.role))
-	} else {
-		log.Info("kafka cluster admin closed", zap.Duration("duration", time.Since(start)),
-			zap.String("changefeed", k.id), zap.Any("role", k.role))
-	}
-
+	k.metricsMonitor.Cleanup()
 	return nil
 }
 
@@ -506,6 +493,11 @@ func NewKafkaSaramaProducer(ctx context.Context, topic string, config *Config, o
 	if err != nil {
 		return nil, cerror.WrapError(cerror.ErrKafkaNewSaramaProducer, err)
 	}
+	defer func() {
+		if err := admin.Close(); err != nil {
+			log.Warn("close kafka cluster admin failed", zap.Error(err))
+		}
+	}()
 
 	if err := validateAndCreateTopic(admin, topic, config, cfg); err != nil {
 		return nil, cerror.WrapError(cerror.ErrKafkaNewSaramaProducer, err)
@@ -533,7 +525,6 @@ func NewKafkaSaramaProducer(ctx context.Context, topic string, config *Config, o
 		return nil, err
 	}
 	k := &kafkaSaramaProducer{
-		admin:         admin,
 		client:        client,
 		asyncProducer: asyncProducer,
 		syncProducer:  syncProducer,
@@ -552,8 +543,8 @@ func NewKafkaSaramaProducer(ctx context.Context, topic string, config *Config, o
 		id:   changefeedID,
 		role: role,
 
-		metricsMonitor: newSaramaMetricsMonitor(cfg.MetricRegistry,
-			util.CaptureAddrFromCtx(ctx), changefeedID, admin),
+		metricsMonitor: NewSaramaMetricsMonitor(cfg.MetricRegistry,
+			util.CaptureAddrFromCtx(ctx), changefeedID),
 	}
 	go func() {
 		if err := k.run(ctx); err != nil && errors.Cause(err) != context.Canceled {
