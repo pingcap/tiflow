9adcc5fb2df80ff490bdc635f92d80f8cd5839bf
maxshuang <huangguohao@pingcap.com>
2022-03-01 23:55:04 +0800

    test(integration): add convert script for tidb_mysql_test

--- tests/integration_tests/tidb_mysql_test/run.sh
+++ tests/integration_tests/tidb_mysql_test/run.sh
@@ -9,19 +9,19 @@ CDC_BINARY=cdc.test
 SINK_TYPE=$1
 
 function prepare() {
-	rm -rf $WORK_DIR && mkdir -p $WORK_DIR
 	stop_tidb_cluster
+	rm -rf $WORK_DIR && mkdir -p $WORK_DIR
 
 	start_tidb_cluster --workdir $WORK_DIR
-
-	cd $WORK_DIR
+	
+    cd $WORK_DIR
 
 	# record tso before we create tables to skip the system table DDLs
 	start_ts=$(run_cdc_cli_tso_query ${UP_PD_HOST_1} ${UP_PD_PORT_1})
 
 	run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY
 
-	TOPIC_NAME="ticdc-default-value-test-$RANDOM"
+	TOPIC_NAME="ticdc-tidb-mysql-test-$RANDOM"
 	case $SINK_TYPE in
 	kafka) SINK_URI="kafka://127.0.0.1:9092/$TOPIC_NAME?protocol=open-protocol&partition-num=4&kafka-version=${KAFKA_VERSION}&max-message-bytes=10485760" ;;
 	*) SINK_URI="mysql://normal:123456@127.0.0.1:3306/" ;;
@@ -43,14 +43,39 @@ if [ "$SINK_TYPE" = "kafka" ]; then
 fi
 
 # mysql test may suffer from duplicate DML for no pk/uk
-# [TODO] need add pk or move from integration test
 trap stop_tidb_cluster EXIT
+
+# setup cluster
+echo -e "start upstream and downstream cluster"
 prepare $*
-cd "$(dirname "$0")"
+
+MYSQL_TEST_PATH=$CUR/tidb_test/mysql_test
+
+cp -f $CUR/converter2.sh $MYSQL_TEST_PATH
+cp -f $CUR/build.sh $MYSQL_TEST_PATH
+cd $MYSQL_TEST_PATH
+
+# convert test case
+cases="bigint composite_index date_formats datetime_insert \
+    datetime_update drop concurrent_ddl gcol_alter_table \
+    partition_bug18198 update_stmt\
+    partition_list partition_range single_delete_update time \
+    timestamp_insert timestamp_update type_decimal \
+    type_time type_timestamp type_uint update"
+
+./converter2.sh "$cases"
 # run mysql-test cases
-./test.sh
+echo -e "mysql_test start\n"
+TEST_BIN_PATH=./mysql_test
+./build.sh
+echo "run mysql test cases:${cases}"
+"$TEST_BIN_PATH" --host=${UP_TIDB_HOST} --port=${UP_TIDB_PORT} --log-level=error --reserve-schema=true ${cases}
+echo "mysqltest end"
+
+cd $CUR
 mysql -h${UP_TIDB_HOST} -P${UP_TIDB_PORT} -uroot -e "create table test.finish_mark(id int primary key)"
-check_table_exists test.finish_mark ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT} 300
+# all tests will take too much time, how to optimize it?
+check_table_exists test.finish_mark ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT} 2000
 check_sync_diff $WORK_DIR $CUR/diff_config.toml
 cleanup_process $CDC_BINARY
 check_logs $WORK_DIR
