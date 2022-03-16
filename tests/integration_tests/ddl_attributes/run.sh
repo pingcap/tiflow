bbc94be43c20a8ad4e6707709c65b46ba81d5a2f
Weizhen Wang <wangweizhen@pingcap.com>
2022-03-16 12:23:30 +0800

    fix

--- tests/integration_tests/ddl_attributes/run.sh
+++ tests/integration_tests/ddl_attributes/run.sh
@@ -39,10 +39,10 @@ function run() {
 	cleanup_process $CDC_BINARY
 
 	run_sql "show create table t1;" ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT} &&
-	  check_contains "CREATE TABLE t1 (id BIGINT NOT NULL PRIMARY KEY auto_increment, b varchar(255)) PLACEMENT POLICY=placement1;"
+		check_contains "CREATE TABLE t1 (id BIGINT NOT NULL PRIMARY KEY auto_increment, b varchar(255)) PLACEMENT POLICY=placement1;"
 
 	run_sql "show create table t2;" ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT} &&
-	  check_contains "CREATE TABLE t2 (a int(11) DEFAULT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin /*T![placement] PLACEMENT POLICY=p2 */;"
+		check_contains "CREATE TABLE t2 (a int(11) DEFAULT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin /*T![placement] PLACEMENT POLICY=p2 */;"
 }
 
 trap stop_tidb_cluster EXIT
