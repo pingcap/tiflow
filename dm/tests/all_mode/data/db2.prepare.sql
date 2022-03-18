2abda699c6a9d36297f2b4de050349255fb8b877
lance6716 <lance6716@gmail.com>
2022-03-18 18:41:46 +0800

    *(dm): support MySQL 8.0 collations
    Signed-off-by: lance6716 <lance6716@gmail.com>

--- dm/tests/all_mode/data/db2.prepare.sql
+++ dm/tests/all_mode/data/db2.prepare.sql
@@ -3,9 +3,9 @@ create database `all_mode`;
 use `all_mode`;
 create table t2 (
     id int NOT NULL AUTO_INCREMENT,
-    name varchar(20),
+    name varchar(20) COLLATE utf8mb4_0900_as_cs,
     ts timestamp,
-    PRIMARY KEY (id));;
+    PRIMARY KEY (id));
 insert into t2 (name, ts) values ('Arya', now()), ('Bran', '2021-05-11 10:01:05'), ('Sansa', NULL);
 
 -- test block-allow-list
