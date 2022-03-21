f5d60a36ae1feea6bcbb4eb5f4aef0d80c52b20d
lance6716 <lance6716@gmail.com>
2022-03-21 13:17:23 +0800

    add some IT, need update sync-diff

--- dm/tests/all_mode/data/db2.prepare.sql
+++ dm/tests/all_mode/data/db2.prepare.sql
@@ -3,7 +3,7 @@ create database `all_mode`;
 use `all_mode`;
 create table t2 (
     id int NOT NULL AUTO_INCREMENT,
-    name varchar(20) COLLATE utf8mb4_0900_as_cs,
+    name varchar(20),
     ts timestamp,
     PRIMARY KEY (id));
 insert into t2 (name, ts) values ('Arya', now()), ('Bran', '2021-05-11 10:01:05'), ('Sansa', NULL);
