d16f5cef6cc637c60f3a2dc3c265c67217721ee7
amyangfei <amyangfei@gmail.com>
2022-03-23 10:56:24 +0800

    ci: add e2e test in github action (#251)
    * ci: add e2e test in github action
    
    * empty commit

--- sample/prepare.sh
+++ sample/prepare.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker build -f ../Dockerfile -t dataflow:test ..
+current_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
+docker build -f $current_dir/../Dockerfile -t dataflow:test $current_dir/..
