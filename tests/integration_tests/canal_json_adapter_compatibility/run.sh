63e06c907e6b554eaeea7c3a998785adcb562826
zhaoxinyu <zhaoxinyu512@gmail.com>
2022-03-30 20:12:30 +0800

    sink(cdc): Add GBK charset tests for canal-json protocol (#4992)
    ref pingcap/tiflow#4806

--- tests/integration_tests/canal_json_adapter_compatibility/run.sh
+++ tests/integration_tests/canal_json_adapter_compatibility/run.sh
@@ -36,6 +36,10 @@ function run() {
 	check_table_exists test.binary_columns ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT} 200
 	check_sync_diff $WORK_DIR $CUR/conf/diff_config.toml
 
+	run_sql_file $CUR/data/data_gbk.sql ${UP_TIDB_HOST} ${UP_TIDB_PORT}
+	check_table_exists test.binary_columns ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT} 200
+	check_sync_diff $WORK_DIR $CUR/conf/diff_config.toml
+
 	cleanup_process $CDC_BINARY
 }
 
