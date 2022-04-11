5014d8e76255491bc066479dfb6da8e33922c3c1
3AceShowHand <jinl1037@hotmail.com>
2022-04-11 23:35:38 +0800

    refact flow controller, use syncMap to handle concurrent access to map of tables.

--- cdc/processor/flow_controller.go
+++ cdc/processor/flow_controller.go
@@ -3,7 +3,6 @@ package processor
 import (
 	"math"
 	"sync"
-	"sync/atomic"
 
 	"github.com/edwingeng/deque"
 	"github.com/pingcap/errors"
@@ -18,44 +17,26 @@ type FlowController struct {
 
 	memoryQuota *common.TableMemoryQuota
 
-	mu   sync.Mutex
-	memo map[model.TableID]*tableQuotaTracker
+	memo syncMap
 }
 
 func NewFlowController(changefeedID model.ChangeFeedID, quota uint64) *FlowController {
 	return &FlowController{
 		changefeedID: changefeedID,
-		mu:           sync.Mutex{},
 		memoryQuota:  common.NewTableMemoryQuota(quota),
-		memo:         make(map[model.TableID]*tableQuotaTracker),
+		memo:         newSyncMap(changefeedID),
 	}
 }
 
 func (f *FlowController) AddTable(tableID model.TableID) {
-	if _, ok := f.memo[tableID]; ok {
-		log.Panic("table already exist",
-			zap.Any("changefeed", f.changefeedID),
-			zap.Any("tableID", tableID))
-	}
-	f.memo[tableID] = NewTableQuotaTracker(tableID)
-	log.Info("add table to the processor's flow controller",
-		zap.Any("changefeed", f.changefeedID),
-		zap.Any("tableID", tableID))
+	f.memo.addTable(tableID)
 }
 
 // RemoveTable remove the table from the controller
 // should be called after the whole table pipeline closed, memory released.
 func (f *FlowController) RemoveTable(tableID model.TableID) {
-	tracker, ok := f.memo[tableID]
-	if !ok {
-		log.Panic("drop the table not found",
-			zap.Any("changefeed", f.changefeedID),
-			zap.Any("tableID", tableID))
-	}
-
-	bytes := tracker.resolve(math.MaxUint64)
+	bytes := f.memo.removeTable(tableID)
 	f.memoryQuota.Release(bytes)
-	delete(f.memo, tableID)
 	log.Info("table removed from the processor's flow controller",
 		zap.Any("changefeed", f.changefeedID),
 		zap.Any("tableID", tableID),
@@ -65,15 +46,7 @@ func (f *FlowController) RemoveTable(tableID model.TableID) {
 //Consume is called when an event has arrived for being processed by the sink.
 //It will handle transaction boundaries automatically, and will not block intra-transaction.
 func (f *FlowController) Consume(tableID model.TableID, commitTs uint64, size uint64, blockCallBack func() error) error {
-	f.mu.Lock()
-	defer f.mu.Unlock()
-	tracker, ok := f.memo[tableID]
-	if !ok {
-		log.Panic("FlowController.Consume: table not found",
-			zap.Any("changefeed", f.changefeedID),
-			zap.Any("tableID", tableID))
-	}
-	lastCommitTs := atomic.LoadUint64(&tracker.lastCommitTs)
+	lastCommitTs := f.memo.getLastCommitTsByTableID(tableID)
 	if commitTs < lastCommitTs {
 		log.Panic("commitTs regressed, report a bug",
 			zap.Any("changefeed", f.changefeedID),
@@ -81,40 +54,29 @@ func (f *FlowController) Consume(tableID model.TableID, commitTs uint64, size ui
 			zap.Uint64("commitTs", commitTs),
 			zap.Uint64("lastCommitTs", lastCommitTs))
 	}
+
 	if commitTs == lastCommitTs {
 		// Here commitTs == lastCommitTs, which means that we are not crossing
 		// a transaction boundary. In this situation, we use `ForceConsume` because
 		// blocking the event stream mid-transaction is highly likely to cause
 		// a deadlock.
 		// TODO fix this in the future, after we figure out how to elegantly support large txns.
-		err := f.memoryQuota.ForceConsume(size)
-		if err != nil {
+		if err := f.memoryQuota.ForceConsume(size); err != nil {
 			return errors.Trace(err)
 		}
 	} else {
-		atomic.StoreUint64(&tracker.lastCommitTs, commitTs)
-		err := f.memoryQuota.ConsumeWithBlocking(size, blockCallBack)
-		if err != nil {
+		if err := f.memoryQuota.ConsumeWithBlocking(size, blockCallBack); err != nil {
 			return errors.Trace(err)
 		}
 	}
 
-	tracker.add(commitTs, size)
-
+	f.memo.addEntryByTableID(tableID, commitTs, size)
 	return nil
 }
 
 // Release is called when all events committed before resolvedTs has been freed from memory.
 func (f *FlowController) Release(tableID model.TableID, resolvedTs uint64) {
-	f.mu.Lock()
-	defer f.mu.Unlock()
-	tracker, ok := f.memo[tableID]
-	if !ok {
-		log.Panic("FlowController.Release: table not found",
-			zap.Any("changefeed", f.changefeedID),
-			zap.Any("tableID", tableID))
-	}
-	bytes := tracker.resolve(resolvedTs)
+	bytes := f.memo.resolve(tableID, resolvedTs)
 	f.memoryQuota.Release(bytes)
 	log.Debug("FlowController.Release",
 		zap.Any("changefeed", f.changefeedID),
@@ -173,4 +135,84 @@ func (t *tableQuotaTracker) resolve(resolvedTs uint64) uint64 {
 		t.queue.PopFront()
 	}
 	return result
+}
+
+type syncMap struct {
+	changefeedID model.ChangeFeedID
+	sync.Mutex
+	memo map[model.TableID]*tableQuotaTracker
+}
+
+func newSyncMap(changefeedID model.ChangeFeedID) syncMap {
+	return syncMap{
+		changefeedID: changefeedID,
+		memo:         make(map[model.TableID]*tableQuotaTracker),
+	}
+}
+
+func (m syncMap) addTable(tableID model.TableID) {
+	m.Lock()
+	defer m.Unlock()
+	if _, ok := m.memo[tableID]; ok {
+		log.Panic("table already exist",
+			zap.Any("changefeed", m.changefeedID),
+			zap.Any("tableID", tableID))
+	}
+	m.memo[tableID] = NewTableQuotaTracker(tableID)
+	log.Info("add table to the processor's flow controller",
+		zap.Any("changefeed", m.changefeedID),
+		zap.Any("tableID", tableID))
+}
+
+func (m syncMap) removeTable(tableID model.TableID) uint64 {
+	m.Lock()
+	defer m.Unlock()
+	tracker, ok := m.memo[tableID]
+	if !ok {
+		log.Panic("drop the table not found",
+			zap.Any("changefeed", m.changefeedID),
+			zap.Any("tableID", tableID))
+	}
+
+	bytes := tracker.resolve(math.MaxUint64)
+	delete(m.memo, tableID)
+	return bytes
+}
+
+func (m syncMap) getLastCommitTsByTableID(tableID model.TableID) uint64 {
+	m.Lock()
+	defer m.Unlock()
+	tracker, ok := m.memo[tableID]
+	if !ok {
+		log.Panic("FlowController.syncMap.getLastCommitTsByTableID: table not found",
+			zap.Any("changefeed", m.changefeedID),
+			zap.Any("tableID", tableID))
+	}
+	return tracker.lastCommitTs
+}
+
+func (m syncMap) addEntryByTableID(tableID model.TableID, commitTs, size uint64) {
+	m.Lock()
+	defer m.Unlock()
+	tracker, ok := m.memo[tableID]
+	if !ok {
+		log.Panic("FlowController.syncMap.addEntryByTableID: table not found",
+			zap.Any("changefeed", m.changefeedID),
+			zap.Any("tableID", tableID))
+	}
+	tracker.add(commitTs, size)
+}
+
+func (m syncMap) resolve(tableID model.TableID, resolvedTs uint64) uint64 {
+	m.Lock()
+	defer m.Unlock()
+	tracker, ok := m.memo[tableID]
+	if !ok {
+		log.Panic("FlowController.syncMap.resolve: table not found",
+			zap.Any("changefeedID", m.changefeedID),
+			zap.Any("tableID", tableID))
+	}
+
+	bytes := tracker.resolve(resolvedTs)
+	return bytes
 }
