9c8f4a55ede30da3051e95b5ee514e61b2f9d67b
3AceShowHand <jinl1037@hotmail.com>
2022-04-27 17:14:51 +0800

    add replication-factor as an idiom

--- scripts/check-log-style.sh
+++ scripts/check-log-style.sh
@@ -15,7 +15,7 @@
 grep -RnE "zap.[A-Z][a-z]+\(\"[0-9A-Za-z]*[-_ ][0-9A-Za-z]*\"(,|\))" cdc tests pkg |
 	grep -vE "user-agent" |
 	grep -vE "https_proxy|http_proxy|no_proxy" |
-	grep -vE "max-message-bytes|max-message-size" |
+	grep -vE "max-message-bytes|max-message-size|replication-factor" |
 	grep -vE "release-version|git-hash|git-branch|go-version" |
 	grep -vE "failpoint-build|utc-build-time" |
 	awk '{ print  } END { if (NR > 0) { exit 1  }  }'
