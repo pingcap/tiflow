40c823f4a57312f0710edc584f5e385286fa2854
lance6716 <lance6716@gmail.com>
2022-05-06 09:34:31 +0800

    test(dm): try fix upstream switch CI

--- dm/tests/upstream_switch/lib.sh
+++ dm/tests/upstream_switch/lib.sh
@@ -31,9 +31,9 @@ function exec_tidb() {
 }
 
 function install_sync_diff() {
-	curl https://download.pingcap.org/tidb-enterprise-tools-nightly-linux-amd64.tar.gz | tar xz
+	curl https://download.pingcap.org/tidb-community-toolkit-v5.3.0-linux-amd64.tar.gz | tar xz
 	mkdir -p bin
-	mv tidb-enterprise-tools-nightly-linux-amd64/bin/sync_diff_inspector bin/
+	mv tidb-community-toolkit-v5.3.0-linux-amd64/bin/sync_diff_inspector bin/
 }
 
 function prepare_more_binlogs() {
