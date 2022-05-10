9bc0616a2b3b0cde938760e1b45850ed9786c171
二手掉包工程师 <rustin.liu@gmail.com>
2022-05-11 00:24:34 +0800

    This is an automated cherry-pick of #5372
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- cdc/sink/mysql.go
+++ cdc/sink/mysql.go
@@ -715,6 +715,7 @@ type mysqlSinkWorker struct {
 	closedCh         chan struct{}
 }
 
+<<<<<<< HEAD:cdc/sink/mysql.go
 func newMySQLSinkWorker(
 	maxTxnRow int,
 	bucket int,
@@ -849,6 +850,25 @@ func (w *mysqlSinkWorker) cleanup() {
 			return
 		}
 	}
+=======
+func (s *mysqlSink) cleanTableResource(tableID model.TableID) {
+	// We need to clean up the old values of the table,
+	// otherwise when the table is dispatched back again,
+	// it may read the old values.
+	// See: https://github.com/pingcap/tiflow/issues/4464#issuecomment-1085385382.
+	if resolvedTs, loaded := s.tableMaxResolvedTs.LoadAndDelete(tableID); loaded {
+		log.Info("clean up table max resolved ts in MySQL sink",
+			zap.Int64("tableID", tableID),
+			zap.Uint64("resolvedTs", resolvedTs.(uint64)))
+	}
+	if checkpointTs, loaded := s.tableCheckpointTs.LoadAndDelete(tableID); loaded {
+		log.Info("clean up table checkpoint ts in MySQL sink",
+			zap.Int64("tableID", tableID),
+			zap.Uint64("checkpointTs", checkpointTs.(uint64)))
+	}
+	// try to remove table txn cache
+	s.txnCache.RemoveTableTxn(tableID)
+>>>>>>> 544aadb0f (sink(ticdc): clean up table checkpoint ts in buffer and MQ sink (#5372)):cdc/sink/mysql/mysql.go
 }
 
 func (s *mysqlSink) Close(ctx context.Context) error {
