f3bf091a656301a59f2c98fff0ed5e49355d49e8
lance6716 <lance6716@gmail.com>
2022-05-13 10:46:35 +0800

    tracker(dm): close and recreate tracker when pause and resume (#5350)
    close pingcap/tiflow#5344

--- dm/syncer/dbconn/db.go
+++ dm/syncer/dbconn/db.go
@@ -18,7 +18,10 @@ import (
 	"strings"
 	"time"
 
+	"github.com/go-sql-driver/mysql"
+	"github.com/pingcap/errors"
 	"github.com/pingcap/failpoint"
+	"github.com/pingcap/tidb/errno"
 	"github.com/pingcap/tidb/util/dbutil"
 	"go.uber.org/zap"
 
@@ -197,7 +200,8 @@ func (conn *DBConn) ExecuteSQLWithIgnore(tctx *tcontext.Context, ignoreError fun
 				metrics.SQLRetriesTotal.WithLabelValues("stmt_exec", conn.cfg.Name).Add(1)
 				return true
 			}
-			return false
+			// TODO: move it to above IsRetryableError
+			return isRetryableError(err)
 		},
 	}
 
@@ -232,6 +236,16 @@ func (conn *DBConn) ExecuteSQLWithIgnore(tctx *tcontext.Context, ignoreError fun
 		return ret.(int), err
 	}
 	return ret.(int), nil
+}
+
+func isRetryableError(err error) bool {
+	err = errors.Cause(err) // check the original error
+	mysqlErr, ok := err.(*mysql.MySQLError)
+	if !ok {
+		return false
+	}
+
+	return mysqlErr.Number == errno.ErrKeyColumnDoesNotExits
 }
 
 // ExecuteSQL does some SQL executions.
