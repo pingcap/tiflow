ece90ec41016d59f200c6de10b755981a89860d9
zhangyangyu <angwerzx@126.com>
2022-05-17 00:16:27 +0800

    fix missing

--- cdc/sink/mq/mq_flush_worker.go
+++ cdc/sink/mq/mq_flush_worker.go
@@ -154,7 +154,7 @@ func (w *flushWorker) asyncSend(
 ) error {
 	for key, events := range partitionedRows {
 		for _, event := range events {
-			err := w.encoder.AppendRowChangedEvent(ctx, event, key.topic)
+			err := w.encoder.AppendRowChangedEvent(ctx, key.topic, event)
 			if err != nil {
 				return err
 			}
