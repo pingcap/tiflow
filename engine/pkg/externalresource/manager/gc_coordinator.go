0544968e838dcc4ff9876243ad1a0f4aec5f1378
Zixiong Liu <liuzixiong@pingcap.com>
2022-05-24 12:25:07 +0800

    externalresources(engine): implement GCCoordinator & GCRunner

---
+++ engine/pkg/externalresource/manager/gc_coordinator.go
@@ -0,0 +1,239 @@
+// Copyright 2022 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package manager
+
+import (
+	"context"
+	gerrors "errors"
+	"time"
+
+	"github.com/pingcap/errors"
+	"github.com/pingcap/tiflow/dm/pkg/log"
+	"go.uber.org/ratelimit"
+	"go.uber.org/zap"
+
+	libModel "github.com/pingcap/tiflow/engine/lib/model"
+	"github.com/pingcap/tiflow/engine/model"
+	resModel "github.com/pingcap/tiflow/engine/pkg/externalresource/resourcemeta/model"
+	"github.com/pingcap/tiflow/engine/pkg/notifier"
+	pkgOrm "github.com/pingcap/tiflow/engine/pkg/orm"
+)
+
+// DefaultGCCoordinator implements interface GCCoordinator.
+// It is responsible for triggering file resource garbage collection.
+type DefaultGCCoordinator struct {
+	executorInfos ExecutorInfoProvider
+	jobInfos      JobStatusProvider
+	metaClient    pkgOrm.ResourceClient
+	notifier      GCNotifier
+}
+
+// GCNotifier exposes a GCNotify method which is called
+// when a new object is marked as needing GC.
+type GCNotifier interface {
+	GCNotify()
+}
+
+// NewGCCoordinator creates a new DefaultGCCoordinator.
+func NewGCCoordinator(
+	executorInfos ExecutorInfoProvider,
+	jobInfos JobStatusProvider,
+	metaClient pkgOrm.ResourceClient,
+	gcNotifier GCNotifier,
+) *DefaultGCCoordinator {
+	return &DefaultGCCoordinator{
+		executorInfos: executorInfos,
+		jobInfos:      jobInfos,
+		metaClient:    metaClient,
+		notifier:      gcNotifier,
+	}
+}
+
+// Run runs the DefaultGCCoordinator.
+func (c *DefaultGCCoordinator) Run(ctx context.Context) error {
+	// We run a retry loop at the max frequency of once per second.
+	rl := ratelimit.New(1 /* once per second */)
+	for {
+		select {
+		case <-ctx.Done():
+			return errors.Trace(ctx.Err())
+		default:
+		}
+
+		jobReceiver, executorReceiver, err := c.initializeGC(ctx)
+		if err != nil {
+			log.L().Warn("GC error", zap.Error(err))
+			rl.Take()
+			continue
+		}
+
+		err = c.runGCEventLoop(ctx, jobReceiver.C, executorReceiver.C)
+		jobReceiver.Close()
+		executorReceiver.Close()
+
+		if gerrors.Is(err, context.Canceled) || gerrors.Is(err, context.DeadlineExceeded) {
+			return errors.Trace(err)
+		}
+		rl.Take()
+	}
+}
+
+// OnKeepAlive is not implemented for now.
+func (c *DefaultGCCoordinator) OnKeepAlive(resourceID resModel.ResourceID, workerID libModel.WorkerID) {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (c *DefaultGCCoordinator) runGCEventLoop(
+	ctx context.Context,
+	jobWatchCh <-chan JobStatusChangeEvent,
+	executorWatchCh <-chan model.ExecutorStatusChange,
+) error {
+	for {
+		select {
+		case <-ctx.Done():
+			return errors.Trace(ctx.Err())
+		case jobStatusChange := <-jobWatchCh:
+			if jobStatusChange.EventType != JobRemovedEvent {
+				continue
+			}
+			err := c.gcByOfflineJobID(ctx, jobStatusChange.JobID)
+			if err != nil {
+				return err
+			}
+		case executorEvent := <-executorWatchCh:
+			if executorEvent.Tp != model.EventExecutorOffline {
+				continue
+			}
+			err := c.gcByOfflineExecutorID(ctx, executorEvent.ID)
+			if err != nil {
+				return err
+			}
+		}
+	}
+}
+
+func (c *DefaultGCCoordinator) initializeGC(
+	ctx context.Context,
+) (*notifier.Receiver[JobStatusChangeEvent], *notifier.Receiver[model.ExecutorStatusChange], error) {
+	jobSnapshot, jobWatchCh, err := c.jobInfos.WatchJobStatuses(ctx)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	executorSnapshot, executorReceiver, err := c.executorInfos.WatchExecutors(ctx)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	if err := c.gcByStatusSnapshots(ctx, jobSnapshot, executorSnapshot); err != nil {
+		return nil, nil, err
+	}
+
+	return jobWatchCh, executorReceiver, nil
+}
+
+func (c *DefaultGCCoordinator) gcByStatusSnapshots(
+	ctx context.Context,
+	jobSnapshot JobStatusesSnapshot,
+	executorSnapshot []model.ExecutorID,
+) error {
+	startTime := time.Now()
+	defer func() {
+		duration := time.Since(startTime)
+		log.L().Info("gcByStatusSnapshots finished",
+			zap.Duration("duration", duration))
+	}()
+
+	resources, err := c.metaClient.QueryResources(ctx)
+	if err != nil {
+		return err
+	}
+
+	executorSet := make(map[model.ExecutorID]struct{}, len(executorSnapshot))
+	for _, id := range executorSnapshot {
+		executorSet[id] = struct{}{}
+	}
+
+	var (
+		toGC     []resModel.ResourceID
+		toRemove []resModel.ResourceID
+	)
+	for _, resMeta := range resources {
+		if _, exists := jobSnapshot[resMeta.Job]; !exists {
+			// The resource belongs to a deleted job.
+			toGC = append(toGC, resMeta.ID)
+			continue
+		}
+
+		if _, exists := executorSet[resMeta.Executor]; !exists {
+			// The resource belongs to an offlined executor.
+			toRemove = append(toGC, resMeta.ID)
+			continue
+		}
+	}
+
+	if len(toGC) > 0 {
+		log.L().Info("Adding resources to GC queue",
+			zap.Any("resource-ids", toGC))
+		if err := c.metaClient.SetGCPending(ctx, toGC); err != nil {
+			return err
+		}
+		c.notifier.GCNotify()
+	}
+
+	if len(toRemove) > 0 {
+		log.L().Info("Removing stale resources for offlined executors",
+			zap.Any("resource-ids", toRemove))
+		if _, err := c.metaClient.DeleteResources(ctx, toRemove); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (c *DefaultGCCoordinator) gcByOfflineJobID(ctx context.Context, jobID string) error {
+	resources, err := c.metaClient.QueryResourcesByJobID(ctx, jobID)
+	if err != nil {
+		return err
+	}
+
+	if len(resources) == 0 {
+		// If there is no resource associated to the job,
+		// we return early.
+		return nil
+	}
+
+	toGC := make([]resModel.ResourceID, 0, len(resources))
+	for _, resMeta := range resources {
+		toGC = append(toGC, resMeta.ID)
+	}
+
+	log.L().Info("Added resources to GC queue",
+		zap.Any("resource-ids", toGC))
+
+	if err := c.metaClient.SetGCPending(ctx, toGC); err != nil {
+		return err
+	}
+
+	c.notifier.GCNotify()
+	return nil
+}
+
+func (c *DefaultGCCoordinator) gcByOfflineExecutorID(ctx context.Context, executorID model.ExecutorID) error {
+	log.L().Info("Cleaning up resources meta for offlined executor",
+		zap.String("executor-id", string(executorID)))
+	return c.metaClient.DeleteResourcesByExecutorID(ctx, string(executorID))
+}
---
+++ engine/pkg/externalresource/manager/gc_runner.go
@@ -0,0 +1,137 @@
+// Copyright 2022 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package manager
+
+import (
+	"context"
+	"time"
+
+	"github.com/pingcap/errors"
+	"github.com/pingcap/log"
+	"go.uber.org/zap"
+
+	"github.com/pingcap/tiflow/engine/pkg/clock"
+	resModel "github.com/pingcap/tiflow/engine/pkg/externalresource/resourcemeta/model"
+	pkgOrm "github.com/pingcap/tiflow/engine/pkg/orm"
+)
+
+const (
+	gcCheckInterval = 10 * time.Second
+	gcTimeout       = 10 * time.Second
+)
+
+type gcHandlerFunc = func(ctx context.Context, meta *resModel.ResourceMeta) error
+
+// DefaultGCRunner implements GCRunner.
+type DefaultGCRunner struct {
+	client     pkgOrm.ResourceClient
+	gcHandlers map[resModel.ResourceType]gcHandlerFunc
+	notifyCh   chan struct{}
+
+	clock clock.Clock
+}
+
+// NewGCRunner returns a new GCRunner.
+func NewGCRunner(
+	client pkgOrm.ResourceClient,
+	gcHandlers map[resModel.ResourceType]gcHandlerFunc,
+) *DefaultGCRunner {
+	return &DefaultGCRunner{
+		client:     client,
+		gcHandlers: gcHandlers,
+		notifyCh:   make(chan struct{}, 1),
+		clock:      clock.New(),
+	}
+}
+
+// Run runs the GCRunner. It blocks until ctx is canceled.
+func (r *DefaultGCRunner) Run(ctx context.Context) error {
+	// TODO this will result in DB queries every 10 seconds.
+	// This is a very naive strategy, we will modify the
+	// algorithm after doing enough system testing.
+	ticker := r.clock.Ticker(gcCheckInterval)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return errors.Trace(ctx.Err())
+		case <-ticker.C:
+		case <-r.notifyCh:
+		}
+
+		timeoutCtx, cancel := context.WithTimeout(ctx, gcTimeout)
+		err := r.gcOnce(timeoutCtx)
+		cancel()
+
+		if err != nil {
+			log.L().Warn("resource GC encountered error", zap.Error(err))
+		}
+	}
+}
+
+// Notify is used to ask GCRunner to GC the next resource immediately.
+// It is used when we have just marked a resource as gc_pending.
+func (r *DefaultGCRunner) Notify() {
+	select {
+	case r.notifyCh <- struct{}{}:
+	default:
+	}
+}
+
+func (r *DefaultGCRunner) gcOnce(
+	ctx context.Context,
+) error {
+	res, err := r.client.GetOneResourceForGC(ctx)
+	if pkgOrm.IsNotFoundError(err) {
+		// It is expected that sometimes we have
+		// nothing to GC.
+		return nil
+	}
+	if err != nil {
+		return err
+	}
+
+	log.Info("start gc'ing resource", zap.Any("resource", res))
+	if !res.GCPending {
+		log.L().Panic("unexpected gc_pending = false")
+	}
+
+	tp, _, err := resModel.ParseResourcePath(res.ID)
+	if err != nil {
+		return err
+	}
+
+	handler, exists := r.gcHandlers[tp]
+	if !exists {
+		log.L().Warn("no gc handler is found for given resource type",
+			zap.Any("resource-id", res.ID))
+	}
+
+	if err := handler(ctx, res); err != nil {
+		return err
+	}
+
+	result, err := r.client.DeleteResource(ctx, res.ID)
+	if err != nil {
+		// If deletion fails, we do not need to retry for now.
+		log.L().Warn("Failed to delete resource after GC", zap.Any("resource", res))
+		return nil
+	}
+	if result.RowsAffected() == 0 {
+		log.L().Warn("Resource is deleted unexpectedly", zap.Any("resource", res))
+	}
+
+	return nil
+}
