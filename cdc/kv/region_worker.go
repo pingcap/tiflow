b02fb3f4701ce0aa1897fbba2aeb612dc2a5ddea
Neil Shen <overvenus@gmail.com>
2022-05-24 20:58:46 +0800

    kv(ticdc): set debug level for resolved ts fallback log (#5558)
    ref pingcap/tiflow#4146

--- cdc/kv/region_worker.go
+++ cdc/kv/region_worker.go
@@ -755,7 +755,7 @@ func (w *regionWorker) handleResolvedTs(
 	}
 
 	if resolvedTs < state.lastResolvedTs {
-		log.Warn("The resolvedTs is fallen back in kvclient",
+		log.Debug("The resolvedTs is fallen back in kvclient",
 			zap.String("changefeed", w.session.client.changefeed),
 			zap.String("EventType", "RESOLVED"),
 			zap.Uint64("resolvedTs", resolvedTs),
