12317f4139996245d238fcaff7c15d18308607da
Zixiong Liu <liuzixiong@pingcap.com>
2022-05-24 11:48:46 +0800

    externalresources(engine): Miscellaneous changes required for implementing resource GC. (#5514)
    ref pingcap/tiflow#5513

--- engine/pkg/externalresource/manager/interfaces.go
+++ engine/pkg/externalresource/manager/interfaces.go
@@ -17,6 +17,9 @@ import (
 	"context"
 
 	libModel "github.com/pingcap/tiflow/engine/lib/model"
+	"github.com/pingcap/tiflow/engine/model"
+	resModel "github.com/pingcap/tiflow/engine/pkg/externalresource/resourcemeta/model"
+	"github.com/pingcap/tiflow/engine/pkg/notifier"
 )
 
 // ExecutorInfoProvider describes an object that maintains a list
@@ -24,15 +27,61 @@ import (
 type ExecutorInfoProvider interface {
 	HasExecutor(executorID string) bool
 	ListExecutors() []string
+
+	// WatchExecutors returns a snapshot of all online executors plus
+	// a stream of events describing changes that happen to the executors
+	// after the snapshot is taken.
+	WatchExecutors(ctx context.Context) (
+		snap []model.ExecutorID, stream *notifier.Receiver[model.ExecutorStatusChange], err error,
+	)
 }
 
 // JobStatus describes the a Job's status.
 type JobStatus = libModel.MasterStatusCode
+
+// JobStatusesSnapshot describes the statuses of all jobs
+// at some time point.
+type JobStatusesSnapshot = map[libModel.MasterID]JobStatus
+
+// JobStatusChangeType describes the type of job status changes.
+type JobStatusChangeType int32
+
+const (
+	// JobRemovedEvent means that a job has been removed.
+	JobRemovedEvent = JobStatusChangeType(iota + 1)
+	// TODO add more event types to support more features
+)
+
+// JobStatusChangeEvent is an event denoting a job status
+// has changed.
+type JobStatusChangeEvent struct {
+	EventType JobStatusChangeType
+	JobID     libModel.MasterID
+}
 
 // JobStatusProvider describes an object that can be queried
 // on the status of jobs.
 type JobStatusProvider interface {
 	// GetJobStatuses returns the status of all jobs that are
 	// not deleted.
-	GetJobStatuses(ctx context.Context) (map[libModel.MasterID]JobStatus, error)
+	GetJobStatuses(ctx context.Context) (JobStatusesSnapshot, error)
+
+	// WatchJobStatuses listens on all job status changes followed by
+	// a snapshot.
+	WatchJobStatuses(
+		ctx context.Context,
+	) (JobStatusesSnapshot, *notifier.Receiver[JobStatusChangeEvent], error)
+}
+
+// GCCoordinator describes an object responsible for triggering
+// file resource garbage collection.
+type GCCoordinator interface {
+	Run(ctx context.Context) error
+	OnKeepAlive(resourceID resModel.ResourceID, workerID libModel.WorkerID)
+}
+
+// GCRunner perform the actual GC operations.
+type GCRunner interface {
+	Run(ctx context.Context) error
+	Notify()
 }
