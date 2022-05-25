097d4ff0be716ac24b5ba5c21469a2fac9903879
buchuitoudegou <756541536@qq.com>
2022-05-25 14:59:57 +0800

    fix comment

--- dm/dm/worker/status.go
+++ dm/dm/worker/status.go
@@ -103,16 +103,7 @@ func (w *SourceWorker) Status(stName string, sourceStatus *binlog.SourceStatus) 
 				case pb.UnitType_Sync:
 					stStatus.Status = &pb.SubTaskStatus_Sync{Sync: us.(*pb.SyncStatus)}
 					if v := st.getValidator(); v != nil {
-						vStatus := v.GetValidatorStatus()
-						stStatus.Validation = &pb.ValidationBriefStatus{
-							Mode:   st.getCfg().ValidatorCfg.Mode,
-							Status: vStatus.Stage,
-							Stats: &pb.ValidationStats{
-								ProcessedRows: vStatus.ProcessedRowsStatus,
-								PendingRows:   vStatus.PendingRowsStatus,
-								ErrorRows:     vStatus.AllErrorRowsStatus,
-							},
-						}
+						stStatus.Validation = v.GetValidatorStatus()
 					}
 				}
 			}
--- dm/tests/dmctl_command/run.sh
+++ dm/tests/dmctl_command/run.sh
@@ -188,13 +188,13 @@ function run_validator_cmd {
 		"new\/ignored\/resolved: 4\/0\/0" 1 \
 		"\"stage\": \"Running\"" 4 \
 		"\"stage\": \"Stopped\"" 1
-	run_sql_source1 "create table dmctl_command.t_trigger_flush101(id int primary key)" # trigger flush
 	run_dm_ctl_with_retry $WORK_DIR "127.0.0.1:$MASTER_PORT" \
 		"query-status test" \
 		"insert\/update\/delete: 4\/1\/1\"" 1 \
 		"insert\/update\/delete: 0\/0\/1\"" 1 \
 		"new\/ignored\/resolved: 0\/0\/0" 1 \
-		"new\/ignored\/resolved: 4\/0\/0" 1
+		"new\/ignored\/resolved: 4\/0\/0" 1 
+	run_sql_source1 "create table dmctl_command.t_trigger_flush101(id int primary key)" # trigger flush
 
 	run_dm_ctl_with_retry $WORK_DIR "127.0.0.1:$MASTER_PORT" \
 		"validation status --table-stage running test" \
