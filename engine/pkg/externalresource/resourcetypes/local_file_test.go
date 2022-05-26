e944eda4ce831958e6d20f70c6c9e555aece4776
Zixiong Liu <liuzixiong@pingcap.com>
2022-05-26 17:15:18 +0800

    add TestRemoveFileOnExecutor

--- engine/pkg/externalresource/resourcetypes/local_file_test.go
+++ engine/pkg/externalresource/resourcetypes/local_file_test.go
@@ -12,3 +12,39 @@
 // limitations under the License.
 
 package resourcetypes
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+
+	"github.com/pingcap/tiflow/engine/client"
+	"github.com/pingcap/tiflow/engine/pb"
+	resModel "github.com/pingcap/tiflow/engine/pkg/externalresource/resourcemeta/model"
+)
+
+func TestRemoveFileOnExecutor(t *testing.T) {
+	clientManager := client.NewClientManager()
+	mockCli := &client.MockExecutorClient{}
+	err := clientManager.AddExecutorClient("executor-1", mockCli)
+	require.NoError(t, err)
+
+	resourceTp := NewLocalFileResourceType(clientManager)
+	gcHandler := resourceTp.GCHandler()
+
+	resMeta := &resModel.ResourceMeta{
+		ID:       "resource-1",
+		Job:      "job-1",
+		Worker:   "worker-1",
+		Executor: "executor-1",
+	}
+
+	mockCli.On("RemoveLocalResource", mock.Anything, &pb.RemoveLocalResourceRequest{
+		ResourceId: "resource-1",
+		CreatorId:  "worker-1",
+	}).Return(nil)
+	err = gcHandler(context.Background(), resMeta)
+	require.NoError(t, err)
+}
