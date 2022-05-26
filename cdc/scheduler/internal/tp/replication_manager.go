c7c6354e2e48676d07965106ef4f71f9f1b0b42f
Neil Shen <overvenus@gmail.com>
2022-05-26 18:33:59 +0800

    tp: implement replicationManager (#5606)
    Signed-off-by: Neil Shen <overvenus@gmail.com>

--- cdc/scheduler/internal/tp/replication_manager.go
+++ cdc/scheduler/internal/tp/replication_manager.go
@@ -14,128 +14,301 @@
 package tp
 
 import (
-	"context"
-
 	"github.com/pingcap/errors"
+	"github.com/pingcap/log"
 	"github.com/pingcap/tiflow/cdc/model"
 	"github.com/pingcap/tiflow/cdc/scheduler/internal/tp/schedulepb"
+	"go.uber.org/zap"
 )
 
-type callback func(model.TableID)
+type callback func()
 
 // burstBalance for changefeed set up or unplaned TiCDC node failure.
 // TiCDC needs to balance intrrupted tables as soon as possible.
-//nolint:deadcode
 type burstBalance struct {
-	tables map[model.TableID]model.CaptureID
-
-	done callback
+	// Add tables to captures
+	Tables map[model.TableID]model.CaptureID
 }
 
 type moveTable struct {
-	tableID       model.TableID
-	sourceCapture model.CaptureID
-	destCapture   model.CaptureID
-
-	done callback
+	TableID     model.TableID
+	DestCapture model.CaptureID
 }
 
 type addTable struct {
-	tableID   model.TableID
-	captureID model.CaptureID
-
-	done callback
+	TableID   model.TableID
+	CaptureID model.CaptureID
 }
 
-type deleteTable struct {
-	tableID   model.TableID
-	captureID model.CaptureID
-
-	done callback
+type removeTable struct {
+	TableID   model.TableID
+	CaptureID model.CaptureID
 }
 
 type scheduleTask struct {
-	moveTable   *moveTable
-	addTable    *addTable
-	deleteTable *deleteTable
+	moveTable    *moveTable
+	addTable     *addTable
+	removeTable  *removeTable
+	burstBalance *burstBalance
+
+	accept callback
 }
 
 type replicationManager struct {
-	version      string
-	tables       map[model.TableID]*ReplicationSet
-	runningTasks map[model.TableID]*scheduleTask
+	tables map[model.TableID]*ReplicationSet
+
+	runningTasks       map[model.TableID]*scheduleTask
+	maxTaskConcurrency int
 }
 
-func (r *replicationManager) poll(
-	ctx context.Context,
-	// Latest global checkpoint of the changefeed
-	checkpointTs model.Ts,
-	// All tables that SHOULD be replicated (or started) at the current checkpoint.
-	currentTables []model.TableID,
-	// All captures that are alive according to the latest Etcd states.
-	aliveCaptures map[model.CaptureID]*model.CaptureInfo,
+func newReplicationManager(maxTaskConcurrency int) *replicationManager {
+	return &replicationManager{
+		tables:             make(map[int64]*ReplicationSet),
+		runningTasks:       make(map[int64]*scheduleTask),
+		maxTaskConcurrency: maxTaskConcurrency,
+	}
+}
+
+func (r *replicationManager) HandleMessage(
 	msgs []*schedulepb.Message,
-	tasks []*scheduleTask,
 ) ([]*schedulepb.Message, error) {
-	msgBuf := make([]*schedulepb.Message, 0)
-
-	sendMsgs, err := r.handleMessage(msgs)
-	if err != nil {
-		return nil, errors.Trace(err)
+	sentMegs := make([]*schedulepb.Message, 0)
+	for i := range msgs {
+		msg := msgs[i]
+		switch msg.MsgType {
+		case schedulepb.MsgCheckpoint:
+			msgs, err := r.handleMessageCheckpoint(msg.Checkpoints)
+			if err != nil {
+				return nil, errors.Trace(err)
+			}
+			sentMegs = append(sentMegs, msgs...)
+		case schedulepb.MsgDispatchTableResponse:
+			msgs, err := r.handleMessageDispatchTableResponse(msg.From, msg.DispatchTableResponse)
+			if err != nil {
+				return nil, errors.Trace(err)
+			}
+			sentMegs = append(sentMegs, msgs...)
+		case schedulepb.MsgHeartbeatResponse:
+			msgs, err := r.handleMessageHeartbeatResponse(msg.From, msg.HeartbeatResponse)
+			if err != nil {
+				return nil, errors.Trace(err)
+			}
+			sentMegs = append(sentMegs, msgs...)
+		}
 	}
-	msgBuf = append(msgBuf, sendMsgs...)
-
-	sendMsgs, err = r.handleMessage(msgs)
-	if err != nil {
-		return nil, errors.Trace(err)
-	}
-	msgBuf = append(msgBuf, sendMsgs...)
-
-	return msgBuf, nil
+	return sentMegs, nil
 }
 
-func (r *replicationManager) handleMessage(
-	msg []*schedulepb.Message,
+func (r *replicationManager) handleMessageHeartbeatResponse(
+	from model.CaptureID, msg *schedulepb.HeartbeatResponse,
 ) ([]*schedulepb.Message, error) {
-	// s.handleMessageHeartbeat()
-	// s.handleMessageCheckpoint()
-	// s.handleMessageDispatchTableResponse()
-	return nil, nil
-}
-
-func (r *replicationManager) handleMessageHeartbeat(msg *schedulepb.Heartbeat) {
-	// TODO: build s.tables from Heartbeat message.
+	sentMsgs := make([]*schedulepb.Message, 0)
+	for i := range msg.Tables {
+		status := msg.Tables[i]
+		table, ok := r.tables[status.TableID]
+		if !ok {
+			log.Info("tpscheduler: ignore table status no table found",
+				zap.Any("message", status))
+			continue
+		}
+		msgs, err := table.handleTableStatus(from, &status)
+		if err != nil {
+			return nil, errors.Trace(err)
+		}
+		if table.hasRemoved() {
+			log.Info("tpscheduler: table has removed", zap.Int64("tableID", status.TableID))
+			delete(r.tables, status.TableID)
+		}
+		sentMsgs = append(sentMsgs, msgs...)
+	}
+	return sentMsgs, nil
 }
 
 func (r *replicationManager) handleMessageDispatchTableResponse(
-	msg *schedulepb.DispatchTableResponse,
-) {
-	// TODO: update s.tables from DispatchTableResponse message.
+	from model.CaptureID, msg *schedulepb.DispatchTableResponse,
+) ([]*schedulepb.Message, error) {
+	var status *schedulepb.TableStatus
+	switch resp := msg.Response.(type) {
+	case *schedulepb.DispatchTableResponse_AddTable:
+		status = resp.AddTable.Status
+	case *schedulepb.DispatchTableResponse_RemoveTable:
+		status = resp.RemoveTable.Status
+	default:
+		log.Warn("tpscheduler: ignore unknown dispatch table response",
+			zap.Any("message", msg))
+		return nil, nil
+	}
+
+	table, ok := r.tables[status.TableID]
+	if !ok {
+		log.Info("tpscheduler: ignore table status no table found",
+			zap.Any("message", status))
+		return nil, nil
+	}
+	msgs, err := table.handleTableStatus(from, status)
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+	if table.hasRemoved() {
+		log.Info("tpscheduler: table has removed", zap.Int64("tableID", status.TableID))
+		delete(r.tables, status.TableID)
+	}
+	return msgs, nil
 }
 
-func (r *replicationManager) handleMessageCheckpoint(msg *schedulepb.Checkpoint) {
-	// TODO: update s.tables from Checkpoint message.
+func (r *replicationManager) handleMessageCheckpoint(
+	checkpoints map[model.TableID]schedulepb.Checkpoint,
+) ([]*schedulepb.Message, error) {
+	return nil, nil
 }
 
-// ========
+func (r *replicationManager) HandleTasks(
+	tasks []*scheduleTask,
+) ([]*schedulepb.Message, error) {
+	// Check if a running task is finished.
+	for tableID := range r.runningTasks {
+		if table, ok := r.tables[tableID]; ok {
+			// If table is back to Replicating or Removed,
+			// the running task is finished.
+			if table.State == ReplicationSetStateReplicating || table.hasRemoved() {
+				delete(r.runningTasks, tableID)
+			}
+		} else {
+			// No table found, remove the task
+			delete(r.runningTasks, tableID)
+		}
+	}
 
-func (r *replicationManager) handleTasks(tasks []*scheduleTask) {
-	// s.handleTaskAddTable(nil)
-	// s.handleTaskMoveTable(nil)
-	// s.handleTaskDeleteTable(nil)
+	sentMsgs := make([]*schedulepb.Message, 0)
+	for _, task := range tasks {
+		// Burst balance does not affect by maxTaskConcurrency.
+		if task.burstBalance != nil {
+			msgs, err := r.handleBurstBalanceTasks(task.burstBalance)
+			if err != nil {
+				return nil, errors.Trace(err)
+			}
+			sentMsgs = append(sentMsgs, msgs...)
+			if task.accept != nil {
+				task.accept()
+			}
+			continue
+		}
+
+		// Check if accpeting one more task exceeds maxTaskConcurrency.
+		if len(r.runningTasks)+1 > r.maxTaskConcurrency {
+			log.Debug("tpcheduler: too many running task")
+			// Does not use break, in case there is burst balance task
+			// in the remaining tasks.
+			continue
+		}
+
+		var tableID model.TableID
+		if task.addTable != nil {
+			tableID = task.addTable.TableID
+		} else if task.removeTable != nil {
+			tableID = task.removeTable.TableID
+		} else if task.moveTable != nil {
+			tableID = task.moveTable.TableID
+		}
+
+		// Skip task if the table is already running a task,
+		// or the table has removed.
+		if _, ok := r.runningTasks[tableID]; ok {
+			log.Info("tpscheduler: ignore task, already exists",
+				zap.Any("task", task))
+			continue
+		}
+		if _, ok := r.tables[tableID]; !ok && task.addTable == nil {
+			log.Info("tpscheduler: ignore task, table not found",
+				zap.Any("task", task))
+			continue
+		}
+
+		var msgs []*schedulepb.Message
+		var err error
+		if task.addTable != nil {
+			msgs, err = r.handleAddTableTask(task.addTable)
+		} else if task.removeTable != nil {
+			msgs, err = r.handleRemoveTableTask(task.removeTable)
+		} else if task.moveTable != nil {
+			msgs, err = r.handleMoveTableTask(task.moveTable)
+		}
+		if err != nil {
+			return nil, errors.Trace(err)
+		}
+		sentMsgs = append(sentMsgs, msgs...)
+		r.runningTasks[tableID] = task
+		if task.accept != nil {
+			task.accept()
+		}
+	}
+	return sentMsgs, nil
 }
 
-func (r *replicationManager) handleTaskMoveTable(task *moveTable) error {
-	// TODO: update s.runingTasks and s.tables.
-	return nil
+func (r *replicationManager) handleAddTableTask(
+	task *addTable,
+) ([]*schedulepb.Message, error) {
+	var err error
+	table := r.tables[task.TableID]
+	if table == nil {
+		table, err = newReplicationSet(task.TableID, nil)
+		if err != nil {
+			return nil, errors.Trace(err)
+		}
+		r.tables[task.TableID] = table
+	}
+	return table.handleAddTable(task.CaptureID)
 }
 
-func (r *replicationManager) handleTaskAddTable(task *addTable) error {
-	// TODO: update s.runingTasks and s.tables.
-	return nil
+func (r *replicationManager) handleRemoveTableTask(
+	task *removeTable,
+) ([]*schedulepb.Message, error) {
+	table := r.tables[task.TableID]
+	if table.hasRemoved() {
+		log.Info("tpscheduler: table has removed", zap.Int64("tableID", task.TableID))
+		delete(r.tables, task.TableID)
+		return nil, nil
+	}
+	return table.handleRemoveTable()
 }
 
-func (r *replicationManager) handleTaskDeleteTable(task *deleteTable) error {
-	// TODO: update s.runingTasks and s.tables.
-	return nil
+func (r *replicationManager) handleMoveTableTask(
+	task *moveTable,
+) ([]*schedulepb.Message, error) {
+	table := r.tables[task.TableID]
+	return table.handleMoveTable(task.DestCapture)
+}
+
+func (r *replicationManager) handleBurstBalanceTasks(
+	task *burstBalance,
+) ([]*schedulepb.Message, error) {
+	perCapture := make(map[model.CaptureID]int)
+	for _, captureID := range task.Tables {
+		perCapture[captureID]++
+	}
+	fields := make([]zap.Field, 0, len(perCapture))
+	for captureID, count := range perCapture {
+		fields = append(fields, zap.Int(captureID, count))
+	}
+	fields = append(fields, zap.Int("total", len(task.Tables)))
+	log.Info("tpscheduler: handle burst balance task", fields...)
+
+	sentMsgs := make([]*schedulepb.Message, 0, len(task.Tables))
+	for tableID := range task.Tables {
+		if r.runningTasks[tableID] != nil {
+			// Skip add table if the table is already running a task.
+			continue
+		}
+		captureID := task.Tables[tableID]
+		msgs, err := r.handleAddTableTask(&addTable{
+			TableID: tableID, CaptureID: captureID,
+		})
+		if err != nil {
+			return nil, errors.Trace(err)
+		}
+		sentMsgs = append(sentMsgs, msgs...)
+		// Just for place holding.
+		r.runningTasks[tableID] = &scheduleTask{}
+	}
+	return sentMsgs, nil
 }
