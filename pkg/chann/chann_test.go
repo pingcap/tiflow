96f3182f82844b38cd40648c8c5a909f36dd696b
hi-rustin <rustin.liu@gmail.com>
2022-05-31 21:36:14 +0800

    sink(ticdc): add LICENSE

--- pkg/chann/chann_test.go
+++ pkg/chann/chann_test.go
@@ -13,6 +13,10 @@
 //
 // ============================================================
 // Forked from https://github.com/golang-design/chann.
+// Copyright 2021 The golang.design Initiative Authors.
+// All rights reserved. Use of this source code is governed
+// by a MIT license that can be found in the LICENSE file.
+//
 // Written by Changkun Ou <changkun.de>
 
 package chann
