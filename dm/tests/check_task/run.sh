8b0615259ba6c19fcb0b0375ce1113a84dd33250
buchuitoudegou <756541536@qq.com>
2022-06-07 16:13:13 +0800

    fix lint

--- dm/tests/check_task/run.sh
+++ dm/tests/check_task/run.sh
@@ -7,21 +7,21 @@ WORK_DIR=$TEST_DIR/$TEST_NAME
 TABLE_NUM=20
 
 function prepare_incompatible_tables() {
-    run_sql_both_source "drop database if exists checktask"
-    run_sql_both_source "create database if not exists checktask"
-    for i in $(seq $TABLE_NUM); do
-        run_sql_both_source "create table checktask.test${i}(id int, b varchar(10))" # no primary key
-    done
+	run_sql_both_source "drop database if exists checktask"
+	run_sql_both_source "create database if not exists checktask"
+	for i in $(seq $TABLE_NUM); do
+		run_sql_both_source "create table checktask.test${i}(id int, b varchar(10))" # no primary key
+	done
 }
 
 function prepare_many_tables() {
-    run_sql_both_source "drop database if exists checktask"
-    run_sql_both_source "create database if not exists checktask"
-    run_sql_file $cur/data/db1.prepare.sql $MYSQL_HOST1 $MYSQL_PORT1 $MYSQL_PASSWORD1
+	run_sql_both_source "drop database if exists checktask"
+	run_sql_both_source "create database if not exists checktask"
+	run_sql_file $cur/data/db1.prepare.sql $MYSQL_HOST1 $MYSQL_PORT1 $MYSQL_PASSWORD1
 }
 
 function prepare() {
-    run_dm_master $WORK_DIR/master $MASTER_PORT $cur/conf/dm-master.toml
+	run_dm_master $WORK_DIR/master $MASTER_PORT $cur/conf/dm-master.toml
 	check_rpc_alive $cur/../bin/check_master_online 127.0.0.1:$MASTER_PORT
 	run_dm_worker $WORK_DIR/worker1 $WORKER1_PORT $cur/conf/dm-worker1.toml
 	check_rpc_alive $cur/../bin/check_worker_online 127.0.0.1:$WORKER1_PORT
@@ -30,23 +30,23 @@ function prepare() {
 }
 
 function test_check_task_fail_no_block() {
-    prepare_incompatible_tables
-    run_dm_ctl $WORK_DIR "127.0.0.1:$MASTER_PORT" \
-        "check-task $cur/conf/task-noshard.yaml" \
-        "\"state\": \"fail\"" 1
+	prepare_incompatible_tables
+	run_dm_ctl $WORK_DIR "127.0.0.1:$MASTER_PORT" \
+		"check-task $cur/conf/task-noshard.yaml" \
+		"\"state\": \"fail\"" 1
 }
 
 function test_check_task_fail_no_block_forsharding() {
-    prepare_many_tables
-    run_dm_ctl $WORK_DIR "127.0.0.1:$MASTER_PORT" \
-        "check-task $cur/conf/task-sharding.yaml" \
-        "\"state\": \"fail\"" 1
+	prepare_many_tables
+	run_dm_ctl $WORK_DIR "127.0.0.1:$MASTER_PORT" \
+		"check-task $cur/conf/task-sharding.yaml" \
+		"\"state\": \"fail\"" 1
 }
 
 function run() {
-    prepare
-    test_check_task_fail_no_block
-    test_check_task_fail_no_block_forsharding
+	prepare
+	test_check_task_fail_no_block
+	test_check_task_fail_no_block_forsharding
 }
 
 cleanup_data checktask
