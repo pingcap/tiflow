97ecd4b4b93a03d565ad3a6b339190bfefce69f3
lance6716 <lance6716@gmail.com>
2022-06-14 14:21:34 +0800

    fix CI again

--- dm/dumpling/dumpling.go
+++ dm/dumpling/dumpling.go
@@ -99,7 +99,7 @@ func (m *Dumpling) Init(ctx context.Context) error {
 				"task": m.cfg.Name, "source_id": m.cfg.SourceID,
 			},
 		)
-		m.dumpConfig.PromRegistry = promutil2.NewDefaultRegistry()
+		m.dumpConfig.PromRegistry = prometheus.DefaultGatherer.(prometheus.Registerer)
 	}
 
 	m.logger.Info("create dumpling", zap.Stringer("config", m.dumpConfig))
