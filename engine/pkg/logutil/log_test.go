39828bf2029986d4cd4b4c36b070379a9c7535b5
maxshuang <huangguohao@pingcap.com>
2022-06-14 11:00:50 +0800

    fix(log): fix unstable TestNewLogger ut

--- engine/pkg/logutil/log_test.go
+++ engine/pkg/logutil/log_test.go
@@ -14,9 +14,13 @@
 package logutil
 
 import (
+	"context"
 	"os"
+	"path/filepath"
 	"regexp"
+	"sync"
 	"testing"
+	"time"
 
 	"github.com/hpcloud/tail"
 	"github.com/pingcap/log"
@@ -25,42 +29,63 @@ import (
 	"go.uber.org/zap"
 )
 
-var testFile = "test.log"
-
-func init() {
-	logger, prop, err := log.InitLogger(&log.Config{
+func TestNewLogger(t *testing.T) {
+	testFile := filepath.Join(t.TempDir(), "test.log")
+	os.Remove(testFile)
+	lg, prop, err := log.InitLogger(&log.Config{
 		Level: "warn",
 		File: log.FileLogConfig{
 			Filename: testFile,
 		},
 	})
-	if err != nil {
-		panic(err)
-	}
-	log.ReplaceGlobals(logger, prop)
-}
-
-func TestNewLogger(t *testing.T) {
-	defer os.Remove(testFile)
-	tail, err := tail.TailFile(testFile, tail.Config{Follow: true})
 	require.NoError(t, err)
-	defer tail.Stop()
+	log.ReplaceGlobals(lg, prop)
+
+	defer os.Remove(testFile)
+	var wg sync.WaitGroup
+	defer wg.Wait()
+
+	tailHandler, err := tail.TailFile(testFile, tail.Config{Follow: true, Poll: true})
+	require.NoError(t, err)
+	defer tailHandler.Stop()
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	defer cancel()
+
+	lineCh := make(chan string, 10)
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		defer close(lineCh)
+		for {
+			select {
+			case line, ok := <-tailHandler.Lines:
+				if !ok {
+					return
+				}
+				require.NoError(t, line.Err)
+				lineCh <- line.Text
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
 
 	logger := NewLogger4Framework()
 	logger.Warn("framework test", zap.String("type", "framework"))
 	logger.Sync()
-	line := <-tail.Lines
-	require.Regexp(t, regexp.QuoteMeta("[\"framework test\"] [framework=true] [type=framework]"), line.Text)
+	line := <-lineCh
+	require.Regexp(t, regexp.QuoteMeta("[\"framework test\"] [framework=true] [type=framework]"), line)
 
 	logger = NewLogger4Master(tenant.NewProjectInfo("tenant1", "proj1"), "job1")
 	logger.Warn("master test", zap.String("type", "master"))
 	logger.Sync()
-	line = <-tail.Lines
-	require.Regexp(t, regexp.QuoteMeta("[\"master test\"] [tenant=tenant1] [project_id=proj1] [job_id=job1] [type=master]"), line.Text)
+	line = <-lineCh
+	require.Regexp(t, regexp.QuoteMeta("[\"master test\"] [tenant=tenant1] [project_id=proj1] [job_id=job1] [type=master]"), line)
 
 	logger = NewLogger4Worker(tenant.NewProjectInfo("tenant1", "proj1"), "job1", "worker1")
 	logger.Warn("worker test", zap.String("type", "worker"))
 	logger.Sync()
-	line = <-tail.Lines
-	require.Regexp(t, regexp.QuoteMeta("[\"worker test\"] [tenant=tenant1] [project_id=proj1] [job_id=job1] [worker_id=worker1] [type=worker]"), line.Text)
+	line = <-lineCh
+	require.Regexp(t, regexp.QuoteMeta("[\"worker test\"] [tenant=tenant1] [project_id=proj1] [job_id=job1] [worker_id=worker1] [type=worker]"), line)
 }
