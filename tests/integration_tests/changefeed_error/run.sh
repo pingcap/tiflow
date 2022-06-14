8c700c05fba258456bf3542a08fe9d048f55eda0
dongmen <414110582@qq.com>
2022-06-14 16:10:56 +0800

    fix changefeed error it error

--- tests/integration_tests/changefeed_error/run.sh
+++ tests/integration_tests/changefeed_error/run.sh
@@ -64,7 +64,10 @@ function run() {
 
 	export GO_FAILPOINTS='github.com/pingcap/tiflow/cdc/owner/NewChangefeedRetryError=return(true)'
 	kill $capture_pid
+	# make sure old cpature key and old owner key expire in etcd
 	ensure $MAX_RETRIES "ETCDCTL_API=3 etcdctl get /tidb/cdc/default/__cdc_meta__/capture --prefix | grep -v 'capture'"
+	ensure $MAX_RETRIES "ETCDCTL_API=3 etcdctl get /tidb/cdc/default/__cdc_meta__/owner --prefix | grep -v 'owner'"
+
 	run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY
 	ensure $MAX_RETRIES check_changefeed_state http://${UP_PD_HOST_1}:${UP_PD_PORT_1} ${changefeedid} "error" "failpoint injected retriable error" ""
 
