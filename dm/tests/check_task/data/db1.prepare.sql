cbe70fa55a73f3cca8da93425bc315233800ca6c
ehco1996 <zh19960202@gmail.com>
2022-06-16 18:01:50 +0800

    add more

--- dm/tests/check_task/data/db1.prepare.sql
+++ dm/tests/check_task/data/db1.prepare.sql
@@ -10,3 +10,7 @@ create table t4(c int primary key, c4 int);
 create table t5(c int primary key, c5 int);
 
 create table t6(c int primary key, c6 int);
+
+-- test the table name that must be quoted are working properly
+create table `t-7`(c int primary key, c7 int);
+
