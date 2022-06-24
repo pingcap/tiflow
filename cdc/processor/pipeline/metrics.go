9fdc32bca43485a557442e8387eedd74650d8d99
qupeng <qupeng@pingcap.com>
2022-06-24 19:18:42 +0800

    add duration log in mysqlSink and some metrics
    Signed-off-by: qupeng <qupeng@pingcap.com>

--- cdc/processor/pipeline/metrics.go
+++ cdc/processor/pipeline/metrics.go
@@ -33,10 +33,27 @@ var (
 			Help:      "estimated memory consumption for a table after the sorter",
 			Buckets:   prometheus.ExponentialBuckets(1*1024*1024 /* mb */, 2, 10),
 		}, []string{"changefeed", "capture"})
+	flushSinkHistogram = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Namespace: "ticdc",
+			Subsystem: "processor",
+			Name:      "sink_node_flush_sink",
+			Help:      "sinkNode.flushSink duration",
+			Buckets:   prometheus.ExponentialBuckets(0.001 /* 1ms*/, 2, 20),
+		}, []string{"table"})
+	sinkNodeResolvedTs = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Namespace: "ticdc",
+			Subsystem: "processor",
+			Name:      "sink_node_resolved_ts",
+			Help:      "xxx",
+		}, []string{"table"})
 )
 
 // InitMetrics registers all metrics used in processor
 func InitMetrics(registry *prometheus.Registry) {
 	registry.MustRegister(txnCounter)
 	registry.MustRegister(tableMemoryHistogram)
+	registry.MustRegister(flushSinkHistogram)
+	registry.MustRegister(sinkNodeResolvedTs)
 }
