65181922fdc808891ea679452c67978fd65b34ee
lyzx2001 <rep_laureline@hotmail.com>
2022-06-27 17:48:42 +0800

    save changes

--- dm/tests/many_tables/run.sh
+++ dm/tests/many_tables/run.sh
@@ -26,6 +26,16 @@ function incremental_data() {
 		done
 	done
 	run_sql "ALTER TABLE many_tables_db.t1 ADD x datetime DEFAULT current_timestamp;" $MYSQL_PORT1 $MYSQL_PASSWORD1
+
+	run_dm_ctl_with_retry $WORK_DIR "127.0.0.1:$MASTER_PORT" \
+		"pause-task test" \
+		"\"result\": true" 2
+
+  sleep 1
+
+	run_dm_ctl_with_retry $WORK_DIR "127.0.0.1:$MASTER_PORT" \
+		"resume-task test" \
+		"\"result\": true" 2
 }
 
 function incremental_data_2() {
