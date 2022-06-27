3d2e543c9fe9705e833acb3a13ab264a372749b5
hi-rustin <rustin.liu@gmail.com>
2022-06-27 18:06:47 +0800

    sinkv2(ticdc): address comment

--- pkg/db/pebble_test.go
+++ pkg/db/pebble_test.go
@@ -58,6 +58,18 @@ func TestIteratorWithTableFilter(t *testing.T) {
 		t.Errorf("Flush failed: %v", err)
 	}
 
+	// There should be no any compactions but 2 tables at L0.
+	pebbleInstance := db.(*pebbleDB).db
+	stats := pebbleInstance.Metrics()
+	require.Equal(t, int64(0), stats.Compact.Count)
+	require.Equal(t, int64(2), stats.Levels[0].NumFiles)
+	// 7 is a pebble internal constant.
+	// See: https://github.com/cockroachdb/pebble/blob/
+	// 71d17c2a007bfad5111a229ba325d30251b88a41/internal/manifest/version.go#L579
+	for level := 1; level < 7; level++ {
+		require.Equal(t, int64(0), stats.Levels[level].NumFiles)
+	}
+
 	for _, x := range []struct {
 		lowerTs, upperTs uint64
 		expectedCount    int
