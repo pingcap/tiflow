f34d264a358598704cd55a309cef2a35a1e07de9
qupeng <qupeng@pingcap.com>
2022-06-29 16:53:07 +0800

    cdc: upgrade pebble and stabilize a case
    Signed-off-by: qupeng <qupeng@pingcap.com>

--- pkg/db/pebble_test.go
+++ pkg/db/pebble_test.go
@@ -18,7 +18,9 @@ import (
 	"fmt"
 	"path/filepath"
 	"testing"
+	"time"
 
+	"github.com/cockroachdb/pebble"
 	"github.com/pingcap/tiflow/cdc/sorter/encoding"
 	"github.com/pingcap/tiflow/pkg/config"
 	"github.com/stretchr/testify/require"
@@ -27,7 +29,10 @@ import (
 func TestIteratorWithTableFilter(t *testing.T) {
 	dbPath := filepath.Join(t.TempDir(), t.Name())
 	db, err := OpenPebble(context.Background(), 1, dbPath, &config.DBConfig{Count: 1},
-		WithCache(16<<20), WithTableCRTsCollectors())
+		WithCache(16<<20), WithTableCRTsCollectors(),
+		// Disable auto compactions to make the case more stable.
+		func(opts *pebble.Options) { opts.DisableAutomaticCompactions = true },
+	)
 	if err != nil {
 		errmsg := fmt.Sprintf("OpenPebble fail: %v", err)
 		panic(errmsg)
@@ -56,6 +61,21 @@ func TestIteratorWithTableFilter(t *testing.T) {
 	if err = db.(*pebbleDB).db.Flush(); err != nil {
 		errmsg := fmt.Sprintf("Flush fail: %v", err)
 		panic(errmsg)
+	}
+
+	// Sleep a while. Automatic compactions shouldn't be triggered.
+	time.Sleep(time.Second)
+
+	// There should be no any compactions but 2 tables at L0.
+	pebbleInstance := db.(*pebbleDB).db
+	stats := pebbleInstance.Metrics()
+	require.Equal(t, int64(0), stats.Compact.Count)
+	require.Equal(t, int64(2), stats.Levels[0].NumFiles)
+	// 7 is a pebble internal constant.
+	// See: https://github.com/cockroachdb/pebble/blob/
+	// 71d17c2a007bfad5111a229ba325d30251b88a41/internal/manifest/version.go#L579
+	for level := 1; level < 7; level++ {
+		require.Equal(t, int64(0), stats.Levels[level].NumFiles)
 	}
 
 	for _, x := range []struct {
