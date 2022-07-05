62e9036546c46761bfa62c25298b2a117800db6d
hi-rustin <rustin.liu@gmail.com>
2022-07-05 16:48:04 +0800

    sink(ticdc): precheck the sink URI and give some hits

---
+++ pkg/util/uri.go
@@ -0,0 +1,62 @@
+// Copyright 2022 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package util
+
+import "strings"
+
+// IsValidIPv6AddressInURI reports whether hostPort is a valid IPv6 address in URI.
+// See: https://www.ietf.org/rfc/rfc2732.txt.
+func IsValidIPv6AddressInURI(hostPort string) bool {
+	hostname := hostPort
+
+	colon := strings.LastIndexByte(hostname, ':')
+	if colon != -1 && validOptionalPort(hostname[colon:]) {
+		hostname = hostname[:colon]
+	}
+
+	isIPv6 := IsIPv6Address(hostname)
+	if !isIPv6 {
+		// IPv6 address must have `:`.
+		return false
+	}
+
+	if !strings.HasPrefix(hostname, "[") || !strings.HasSuffix(hostname, "]") {
+		return false
+	}
+
+	return true
+}
+
+// IsIPv6Address reports whether hostname is a IPv6 address.
+// Notice: There is hostname not host(host+port).
+func IsIPv6Address(hostname string) bool {
+	return strings.Contains(hostname, ":")
+}
+
+// validOptionalPort reports whether port is either an empty string
+// or matches /^:\d*$/
+func validOptionalPort(port string) bool {
+	if port == "" {
+		return true
+	}
+	if port[0] != ':' {
+		return false
+	}
+	for _, b := range port[1:] {
+		if b < '0' || b > '9' {
+			return false
+		}
+	}
+	return true
+}
---
+++ pkg/util/uri_test.go
@@ -0,0 +1,73 @@
+// Copyright 2022 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package util
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestIsValidIPv6AddressInURI(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name string
+		host string
+		want bool
+	}{
+		{"valid ipv6 address", "[::1]", true},
+		{"valid ipv6 address1 with port", "[::1]:8080", true},
+		{"valid ipv6 address2 with port", "[1080:0:0:0:8:800:200C:417A]:8080", true},
+		{"valid ipv6 address3 with port", "[::FFFF:129.144.52.38]:8080", true},
+		{"invalid ipv6 address", "::1", false},
+		{"invalid ipv6 address with port", "::1:8000", false},
+		{"ipv4 address", "127.0.0.1", false},
+		{"empty address", "", false},
+		{"not ip address", "emmmmmmmm", false},
+	}
+	for _, tt := range tests {
+		test := tt
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+			require.Equal(t, test.want, IsValidIPv6AddressInURI(test.host))
+		})
+	}
+}
+
+func TestIsIPv6Address(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name string
+		host string
+		want bool
+	}{
+		{"valid ipv6 address1", "::1", true},
+		{"valid ipv6 address2", "[::1]", true},
+		{"valid ipv6 address3", "1080:0:0:0:8:800:200C:417A", true},
+		{"valid ipv6 address4", "[1080:0:0:0:8:800:200C:417A]", true},
+		{"ipv4 address", "127.0.0.1", false},
+		{"empty address", "", false},
+		{"not ip address", "emmmmmmmm", false},
+	}
+
+	for _, tt := range tests {
+		test := tt
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+			require.Equal(t, test.want, IsIPv6Address(test.host))
+		})
+	}
+}
