4d929cb85329450c6adccb26f1f4fdb26b04465e
sdojjy <sdojjy@qq.com>
2022-07-08 11:26:16 +0800

    update comment

--- cdc/capture/capture.go
+++ cdc/capture/capture.go
@@ -65,6 +65,7 @@ type Capture interface {
 	GetUpstreamManager() (*upstream.Manager, error)
 	GetEtcdClient() etcd.CDCEtcdClientForAPI
 	// IsReady returns if the cdc server is ready
+	// currently only check is ettcd data migration is done
 	IsReady() bool
 }
 
