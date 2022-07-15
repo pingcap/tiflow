a03725bb358f98a3a66d54719f77a86087117f24
Zixiong Liu <liuzixiong@pingcap.com>
2022-07-15 15:43:06 +0800

    client(engine): Refactor the executor client (#6209)
    ref pingcap/tiflow#6208

---
+++ engine/pkg/client/executor_client_test.go
@@ -0,0 +1,43 @@
+// Copyright 2022 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package client
+
+import (
+	"testing"
+	"time"
+
+	"github.com/pingcap/tiflow/pkg/security"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewExecutorClientNotBlocking(t *testing.T) {
+	t.Parallel()
+
+	factory := newExecutorClientFactory(nil, nil)
+
+	startTime := time.Now()
+	_, _ = factory.NewExecutorClient("invalid:1234")
+	require.Less(t, time.Since(startTime), 1*time.Second)
+}
+
+func TestExecutorClientFactoryIllegalCredentials(t *testing.T) {
+	t.Parallel()
+
+	credentials := &security.Credential{
+		CAPath: "/dev/null", // illegal CA path to trigger an error
+	}
+	factory := newExecutorClientFactory(credentials, nil)
+	_, err := factory.NewExecutorClient("127.0.0.1:1234")
+	require.Error(t, err)
+}
