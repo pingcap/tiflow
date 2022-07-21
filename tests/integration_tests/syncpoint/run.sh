1f3c2980146278e2ec411f1deea7d9159076f467
dongmen <20351731+asddongmen@users.noreply.github.com>
2022-07-21 12:51:10 +0800

    This is an automated cherry-pick of #6390
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- tests/integration_tests/syncpoint/run.sh
+++ tests/integration_tests/syncpoint/run.sh
@@ -127,6 +127,8 @@ function run() {
 	run_sql "CREATE DATABASE testSync;"
 	run_sql "CREATE DATABASE testSync;" ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT}
 
+	# make suer no panic happen when the syncpoint enable and the ddl sink initializing slowly
+	export GO_FAILPOINTS='github.com/pingcap/tiflow/cdc/owner/DDLSinkInitializeSlowly=return(true)'
 	start_ts=$(run_cdc_cli_tso_query ${UP_PD_HOST_1} ${UP_PD_PORT_1})
 	run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY
 
