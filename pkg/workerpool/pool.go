455ba52edfb7918d51b6627beb9948c37b756bfa
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2022-07-25 14:49:10 +0800

    cdc: reduce context switch for stateful events (#5658) (#6375)
    close pingcap/tiflow#5610

--- pkg/workerpool/pool.go
+++ pkg/workerpool/pool.go
@@ -41,6 +41,9 @@ type EventHandle interface {
 	// Cancelling `ctx` here will cancel the on-going or next execution of the event.
 	AddEvent(ctx context.Context, event interface{}) error
 
+	// AddEvents is like AddEvent but retrieves a slice instead of an object.
+	AddEvents(ctx context.Context, events []interface{}) error
+
 	// SetTimer is used to provide a function that is periodic called, as long as the EventHandle has not been unregistered.
 	// The current implementation uses as the base clock source a ticker whose interval is the const workerPoolDefaultClockSourceInterval.
 	// DO NOT set an interval less than workerPoolDefaultClockSourceInterval.
