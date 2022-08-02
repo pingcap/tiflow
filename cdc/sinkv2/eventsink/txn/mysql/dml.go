f03f306054e76bb9b313f01d0b1cd11ed7f06069
qupeng <qupeng@pingcap.com>
2022-08-02 14:07:54 +0800

    init
    Signed-off-by: qupeng <qupeng@pingcap.com>

---
+++ cdc/sinkv2/eventsink/txn/mysql/dml.go
@@ -0,0 +1,390 @@
+// Copyright 2022 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package mysql
+
+import (
+	"context"
+	"database/sql"
+	"database/sql/driver"
+	"fmt"
+	"net"
+	"net/url"
+	"strconv"
+	"strings"
+	"time"
+
+	dmysql "github.com/go-sql-driver/mysql"
+	"github.com/pingcap/errors"
+	"github.com/pingcap/failpoint"
+	"github.com/pingcap/log"
+	"github.com/pingcap/tidb/parser/charset"
+	"github.com/pingcap/tidb/parser/mysql"
+	"go.uber.org/zap"
+
+	"github.com/pingcap/tiflow/cdc/model"
+	"github.com/pingcap/tiflow/cdc/sinkv2/eventsink"
+	"github.com/pingcap/tiflow/cdc/sinkv2/metrics"
+	dmutils "github.com/pingcap/tiflow/dm/pkg/utils"
+	cerror "github.com/pingcap/tiflow/pkg/errors"
+	"github.com/pingcap/tiflow/pkg/quotes"
+	"github.com/pingcap/tiflow/pkg/retry"
+)
+
+func prepareUpdate(quoteTable string, preCols, cols []*model.Column, forceReplicate bool) (string, []interface{}) {
+	var builder strings.Builder
+	builder.WriteString("UPDATE " + quoteTable + " SET ")
+
+	columnNames := make([]string, 0, len(cols))
+	args := make([]interface{}, 0, len(cols)+len(preCols))
+	for _, col := range cols {
+		if col == nil || col.Flag.IsGeneratedColumn() {
+			continue
+		}
+		columnNames = append(columnNames, col.Name)
+		args = appendQueryArgs(args, col)
+	}
+	if len(args) == 0 {
+		return "", nil
+	}
+	for i, column := range columnNames {
+		if i == len(columnNames)-1 {
+			builder.WriteString("`" + quotes.EscapeName(column) + "`=?")
+		} else {
+			builder.WriteString("`" + quotes.EscapeName(column) + "`=?,")
+		}
+	}
+
+	builder.WriteString(" WHERE ")
+	colNames, wargs := whereSlice(preCols, forceReplicate)
+	if len(wargs) == 0 {
+		return "", nil
+	}
+	for i := 0; i < len(colNames); i++ {
+		if i > 0 {
+			builder.WriteString(" AND ")
+		}
+		if wargs[i] == nil {
+			builder.WriteString(quotes.QuoteName(colNames[i]) + " IS NULL")
+		} else {
+			builder.WriteString(quotes.QuoteName(colNames[i]) + "=?")
+			args = append(args, wargs[i])
+		}
+	}
+	builder.WriteString(" LIMIT 1;")
+	sql := builder.String()
+	return sql, args
+}
+
+func prepareReplace(
+	quoteTable string,
+	cols []*model.Column,
+	appendPlaceHolder bool,
+	translateToInsert bool,
+) (string, []interface{}) {
+	var builder strings.Builder
+	columnNames := make([]string, 0, len(cols))
+	args := make([]interface{}, 0, len(cols))
+	for _, col := range cols {
+		if col == nil || col.Flag.IsGeneratedColumn() {
+			continue
+		}
+		columnNames = append(columnNames, col.Name)
+		args = appendQueryArgs(args, col)
+	}
+	if len(args) == 0 {
+		return "", nil
+	}
+
+	colList := "(" + buildColumnList(columnNames) + ")"
+	if translateToInsert {
+		builder.WriteString("INSERT INTO " + quoteTable + colList + " VALUES ")
+	} else {
+		builder.WriteString("REPLACE INTO " + quoteTable + colList + " VALUES ")
+	}
+	if appendPlaceHolder {
+		builder.WriteString("(" + placeHolder(len(columnNames)) + ");")
+	}
+
+	return builder.String(), args
+}
+
+func logDMLTxnErr(
+	err error, start time.Time, changefeed model.ChangeFeedID,
+	query string, count int, startTs []model.Ts,
+) error {
+	if isRetryableDMLError(err) {
+		log.Warn("execute DMLs with error, retry later",
+			zap.Error(err), zap.Duration("duration", time.Since(start)),
+			zap.String("query", query), zap.Int("count", count),
+			zap.Uint64s("startTs", startTs),
+			zap.String("namespace", changefeed.Namespace),
+			zap.String("changefeed", changefeed.ID))
+	} else {
+		log.Error("execute DMLs with error, can not retry",
+			zap.Error(err), zap.Duration("duration", time.Since(start)),
+			zap.String("query", query), zap.Int("count", count),
+			zap.String("namespace", changefeed.Namespace),
+			zap.String("changefeed", changefeed.ID))
+	}
+	return err
+}
+
+func isRetryableDMLError(err error) bool {
+	if !cerror.IsRetryableError(err) {
+		return false
+	}
+
+	errCode, ok := getSQLErrCode(err)
+	if !ok {
+		return true
+	}
+
+	switch errCode {
+	case mysql.ErrNoSuchTable, mysql.ErrBadDB:
+		return false
+	}
+	return true
+}
+
+func (s *mysqlSink) execDMLWithMaxRetries(ctx context.Context, dmls *preparedDMLs) error {
+	if len(dmls.sqls) != len(dmls.values) {
+		log.Panic("unexpected number of sqls and values",
+			zap.Strings("sqls", dmls.sqls),
+			zap.Any("values", dmls.values))
+	}
+
+	start := time.Now()
+	return retry.Do(ctx, func() error {
+		failpoint.Inject("MySQLSinkTxnRandomError", func() {
+			failpoint.Return(
+				logDMLTxnErr(
+					errors.Trace(driver.ErrBadConn),
+					start, s.changefeedID, "failpoint", 0, nil))
+		})
+		failpoint.Inject("MySQLSinkHangLongTime", func() {
+			time.Sleep(time.Hour)
+		})
+
+		err := s.statistics.RecordBatchExecution(func() (int, error) {
+			tx, err := s.db.BeginTx(ctx, nil)
+			if err != nil {
+				return 0, logDMLTxnErr(
+					cerror.WrapError(cerror.ErrMySQLTxnError, err),
+					start, s.changefeedID, "BEGIN", dmls.rowCount, dmls.startTs)
+			}
+
+			for i, query := range dmls.sqls {
+				args := dmls.values[i]
+				log.Debug("exec row", zap.String("sql", query), zap.Any("args", args))
+				if _, err := tx.ExecContext(ctx, query, args...); err != nil {
+					err := logDMLTxnErr(
+						cerror.WrapError(cerror.ErrMySQLTxnError, err),
+						start, s.changefeedID, query, dmls.rowCount, dmls.startTs)
+					if rbErr := tx.Rollback(); rbErr != nil {
+						log.Warn("failed to rollback txn", zap.Error(rbErr))
+					}
+					return 0, err
+				}
+			}
+
+			if err = tx.Commit(); err != nil {
+				return 0, logDMLTxnErr(
+					cerror.WrapError(cerror.ErrMySQLTxnError, err),
+					start, s.changefeedID, "COMMIT", dmls.rowCount, dmls.startTs)
+			}
+
+			for _, callback := range dmls.callbacks {
+				callback()
+			}
+			return dmls.rowCount, nil
+		})
+		if err != nil {
+			return errors.Trace(err)
+		}
+		log.Debug("Exec Rows succeeded",
+			zap.String("namespace", s.changefeedID.Namespace),
+			zap.String("changefeed", s.changefeedID.ID),
+			zap.Int("numOfRows", dmls.rowCount))
+		return nil
+	}, retry.WithBackoffBaseDelay(backoffBaseDelayInMs),
+		retry.WithBackoffMaxDelay(backoffMaxDelayInMs),
+		retry.WithMaxTries(defaultDMLMaxRetry),
+		retry.WithIsRetryableErr(isRetryableDMLError))
+}
+
+type preparedDMLs struct {
+	startTs   []model.Ts
+	sqls      []string
+	values    [][]interface{}
+	callbacks []eventsink.CallbackFunc
+	rowCount  int
+}
+
+// if the column value type is []byte and charset is not binary, we get its string
+// representation. Because if we use the byte array respresentation, the go-sql-driver
+// will automatically set `_binary` charset for that column, which is not expected.
+// See https://github.com/go-sql-driver/mysql/blob/ce134bfc/connection.go#L267
+func appendQueryArgs(args []interface{}, col *model.Column) []interface{} {
+	if col.Charset != "" && col.Charset != charset.CharsetBin {
+		colValBytes, ok := col.Value.([]byte)
+		if ok {
+			args = append(args, string(colValBytes))
+		} else {
+			args = append(args, col.Value)
+		}
+	} else {
+		args = append(args, col.Value)
+	}
+
+	return args
+}
+
+// reduceReplace groups SQLs with the same replace statement format, as following
+// sql: `REPLACE INTO `test`.`t` (`a`,`b`) VALUES (?,?,?,?,?,?)`
+// args: (1,"",2,"2",3,"")
+func reduceReplace(replaces map[string][][]interface{}, batchSize int) ([]string, [][]interface{}) {
+	nextHolderString := func(query string, valueNum int, last bool) string {
+		query += "(" + placeHolder(valueNum) + ")"
+		if !last {
+			query += ","
+		}
+		return query
+	}
+	sqls := make([]string, 0)
+	args := make([][]interface{}, 0)
+	for replace, vals := range replaces {
+		query := replace
+		cacheCount := 0
+		cacheArgs := make([]interface{}, 0)
+		last := false
+		for i, val := range vals {
+			cacheCount++
+			if i == len(vals)-1 || cacheCount >= batchSize {
+				last = true
+			}
+			query = nextHolderString(query, len(val), last)
+			cacheArgs = append(cacheArgs, val...)
+			if last {
+				sqls = append(sqls, query)
+				args = append(args, cacheArgs)
+				query = replace
+				cacheCount = 0
+				cacheArgs = make([]interface{}, 0, len(cacheArgs))
+				last = false
+			}
+		}
+	}
+	return sqls, args
+}
+
+func prepareDelete(quoteTable string, cols []*model.Column, forceReplicate bool) (string, []interface{}) {
+	var builder strings.Builder
+	builder.WriteString("DELETE FROM " + quoteTable + " WHERE ")
+
+	colNames, wargs := whereSlice(cols, forceReplicate)
+	if len(wargs) == 0 {
+		return "", nil
+	}
+	args := make([]interface{}, 0, len(wargs))
+	for i := 0; i < len(colNames); i++ {
+		if i > 0 {
+			builder.WriteString(" AND ")
+		}
+		if wargs[i] == nil {
+			builder.WriteString(quotes.QuoteName(colNames[i]) + " IS NULL")
+		} else {
+			builder.WriteString(quotes.QuoteName(colNames[i]) + " = ?")
+			args = append(args, wargs[i])
+		}
+	}
+	builder.WriteString(" LIMIT 1;")
+	sql := builder.String()
+	return sql, args
+}
+
+func whereSlice(cols []*model.Column, forceReplicate bool) (colNames []string, args []interface{}) {
+	// Try to use unique key values when available
+	for _, col := range cols {
+		if col == nil || !col.Flag.IsHandleKey() {
+			continue
+		}
+		colNames = append(colNames, col.Name)
+		args = appendQueryArgs(args, col)
+	}
+	// if no explicit row id but force replicate, use all key-values in where condition
+	if len(colNames) == 0 && forceReplicate {
+		colNames = make([]string, 0, len(cols))
+		args = make([]interface{}, 0, len(cols))
+		for _, col := range cols {
+			colNames = append(colNames, col.Name)
+			args = appendQueryArgs(args, col)
+		}
+	}
+	return
+}
+
+func getSQLErrCode(err error) (errors.ErrCode, bool) {
+	mysqlErr, ok := errors.Cause(err).(*dmysql.MySQLError)
+	if !ok {
+		return -1, false
+	}
+
+	return errors.ErrCode(mysqlErr.Number), true
+}
+
+func buildColumnList(names []string) string {
+	var b strings.Builder
+	for i, name := range names {
+		if i > 0 {
+			b.WriteString(",")
+		}
+		b.WriteString(quotes.QuoteName(name))
+
+	}
+
+	return b.String()
+}
+
+// placeHolder returns a string separated by comma
+// n must be greater or equal than 1, or the function will panic
+func placeHolder(n int) string {
+	var builder strings.Builder
+	builder.Grow((n-1)*2 + 1)
+	for i := 0; i < n; i++ {
+		if i > 0 {
+			builder.WriteString(",")
+		}
+		builder.WriteString("?")
+	}
+	return builder.String()
+}
+
+// GetDBConnImpl is the implement holder to get db connection. Export it for tests
+var GetDBConnImpl = getDBConn
+
+func getDBConn(ctx context.Context, dsnStr string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsnStr)
+	if err != nil {
+		return nil, cerror.ErrMySQLConnectionError.Wrap(err).GenWithStack("fail to open MySQL connection")
+	}
+	err = db.PingContext(ctx)
+	if err != nil {
+		// close db to recycle resources
+		if closeErr := db.Close(); closeErr != nil {
+			log.Warn("close db failed", zap.Error(err))
+		}
+		return nil, cerror.ErrMySQLConnectionError.Wrap(err).GenWithStack("fail to open MySQL connection")
+	}
+	return db, nil
+}
