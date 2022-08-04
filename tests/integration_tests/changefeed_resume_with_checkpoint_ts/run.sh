c3adacdeb745ce9da9eee80e3b843416c28dc83b
zhaoxinyu <zhaoxinyu512@gmail.com>
2022-08-04 11:49:02 +0800

    fix a failed integration test

--- tests/integration_tests/changefeed_resume_with_checkpoint_ts/run.sh
+++ tests/integration_tests/changefeed_resume_with_checkpoint_ts/run.sh
@@ -118,7 +118,7 @@ function resume_changefeed_in_failed_state() {
 		exit 1
 	fi
 
-	gc_safepoint=$(pd-ctl -u=$pd_addr service-gc-safepoint | jq '.gc_safe_point')
+	gc_safepoint=$(pd-ctl -u=$pd_addr service-gc-safepoint | grep -oE "\"safe_point\": [0-9]+" | grep -oE "[0-9]+" | sort | head -n1)
 	result=$(cdc cli changefeed resume --changefeed-id=$changefeed_id --pd=$pd_addr --overwrite-checkpoint-ts=$gc_safepoint --no-confirm=true 2>&1 || true)
 	if [[ $result != *"ErrStartTsBeforeGC"* ]]; then
 		echo "changefeeed resume result is expected to contain 'ErrStartTsBeforeGC', \
