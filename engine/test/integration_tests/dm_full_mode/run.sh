2ea67bab5a1ebc3073476ff4942a6a49d0962e6d
lance6716 <lance6716@gmail.com>
2022-08-04 17:01:22 +0800

    fix fmt
    Signed-off-by: lance6716 <lance6716@gmail.com>

--- engine/test/integration_tests/dm_full_mode/run.sh
+++ engine/test/integration_tests/dm_full_mode/run.sh
@@ -17,7 +17,7 @@ function run() {
 	# prepare data
 
 	run_sql 'DROP DATABASE IF EXISTS dm_full_mode'
-  run_sql 'CREATE DATABASE dm_full_mode;'
+	run_sql 'CREATE DATABASE dm_full_mode;'
 	for i in $(seq $TABLE_NUM); do
 		run_sql "CREATE TABLE dm_full_mode.t$i(i TINYINT, j INT UNIQUE KEY);"
 		for j in $(seq 2); do
@@ -27,20 +27,20 @@ function run() {
 		run_sql "INSERT INTO dm_full_mode.t$i VALUES (9, 90009);"
 	done
 
-  # create job
+	# create job
 
-	create_job_json=`jq -Rs '{ job_type: 3, tenant_id: "dm_full_mode", project_id: "dm_full_mode", job_config: . }' $CUR_DIR/conf/job.yaml`
+	create_job_json=$(jq -Rs '{ job_type: 3, tenant_id: "dm_full_mode", project_id: "dm_full_mode", job_config: . }' $CUR_DIR/conf/job.yaml)
 	echo "create_job_json: $create_job_json"
-  job_id=`curl -X POST -H "Content-Type: application/json" -d "$create_job_json" http://127.0.0.1:10245/api/v1/jobs`
-  echo "job_id: $job_id"
+	job_id=$(curl -X POST -H "Content-Type: application/json" -d "$create_job_json" http://127.0.0.1:10245/api/v1/jobs)
+	echo "job_id: $job_id"
 
-  # wait for job finished
+	# wait for job finished
 
-  # remove quotes
-  job_id=${job_id:1:-1}
-  exec_with_retry --count 200 "curl \"http://127.0.0.1:10245/api/v1/jobs/$job_id/status\" | jq -e '.TaskStatus.\"mysql-01\".Status.Stage == 4'"
+	# remove quotes
+	job_id=${job_id:1:-1}
+	exec_with_retry --count 200 "curl \"http://127.0.0.1:10245/api/v1/jobs/$job_id/status\" | jq -e '.TaskStatus.\"mysql-01\".Status.Stage == 4'"
 
-  # check data
+	# check data
 
 	check_sync_diff $WORK_DIR $CUR_DIR/conf/diff_config.toml 1
 }
