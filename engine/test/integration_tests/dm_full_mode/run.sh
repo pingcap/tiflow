ea1f18d3d56995079b7955e66b0191cd72427c6a
lance6716 <lance6716@gmail.com>
2022-08-04 16:36:06 +0800

    remove unused
    Signed-off-by: lance6716 <lance6716@gmail.com>

--- engine/test/integration_tests/dm_full_mode/run.sh
+++ engine/test/integration_tests/dm_full_mode/run.sh
@@ -5,7 +5,7 @@ set -eu
 CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 WORK_DIR=$OUT_DIR/$TEST_NAME
 CONFIG="$DOCKER_COMPOSE_DIR/3m3e.yaml $DOCKER_COMPOSE_DIR/dm_databases.yaml"
-TABLE_NUM=5
+TABLE_NUM=500
 
 function run() {
 	rm -rf $WORK_DIR && mkdir -p $WORK_DIR
@@ -38,7 +38,7 @@ function run() {
 
   # remove quotes
   job_id=${job_id:1:-1}
-  exec_with_retry --count 20 "curl \"http://127.0.0.1:10245/api/v1/jobs/$job_id/status\" | jq -e '.TaskStatus.\"mysql-01\".Status.Stage == 4'"
+  exec_with_retry --count 200 "curl \"http://127.0.0.1:10245/api/v1/jobs/$job_id/status\" | jq -e '.TaskStatus.\"mysql-01\".Status.Stage == 4'"
 
   # check data
 
