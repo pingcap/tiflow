0ea981476d7a26c9883cefeec5fc7c1b96192cfc
3AceShowHand <jinl1037@hotmail.com>
2022-08-11 16:08:34 +0800

    add version check file.

--- pkg/version/check_test.go
+++ pkg/version/check_test.go
@@ -400,6 +400,13 @@ func TestCheckTiCDCVersion(t *testing.T) {
 	require.Regexp(t, "TiCDC .* not supported, only support version less than.*", err)
 
 	versions = map[string]struct{}{
+		"v6.3.0":        {},
+		"v8.0.0-master": {},
+	}
+	err = CheckTiCDCVersion(versions)
+	require.Regexp(t, "TiCDC .* not supported, only support version less than.*", err)
+
+	versions = map[string]struct{}{
 		"v6.3.0": {},
 		"v8.0.0": {},
 	}
