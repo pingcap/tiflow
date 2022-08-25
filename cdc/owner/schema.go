81df5148f0ac629cbade5e231a56b3816e6b9d9d
3AceShowHand <jinl1037@hotmail.com>
2022-08-25 14:36:13 +0800

    schema log update.

--- cdc/owner/schema.go
+++ cdc/owner/schema.go
@@ -64,7 +64,7 @@ func newSchemaWrap4Owner(
 	if err != nil {
 		return nil, errors.Trace(err)
 	}
-	// It is no matter to use a empty as timezone here because schemaWrap4Owner
+	// It is no matter to use an empty as timezone here because schemaWrap4Owner
 	// doesn't use expression filter's method.
 	f, err := filter.NewFilter(config, "")
 	if err != nil {
@@ -133,7 +133,7 @@ func (s *schemaWrap4Owner) HandleDDL(job *timodel.Job) error {
 	s.allPhysicalTablesCache = nil
 	err := s.schemaSnapshot.HandleDDL(job)
 	if err != nil {
-		log.Error("handle DDL failed",
+		log.Error("owner update schema failed",
 			zap.String("namespace", s.id.Namespace),
 			zap.String("changefeed", s.id.ID),
 			zap.String("DDL", job.Query),
@@ -142,7 +142,7 @@ func (s *schemaWrap4Owner) HandleDDL(job *timodel.Job) error {
 			zap.Any("role", util.RoleOwner))
 		return errors.Trace(err)
 	}
-	log.Info("owner handle DDL success",
+	log.Info("owner update schema snapshot",
 		zap.String("namespace", s.id.Namespace),
 		zap.String("changefeed", s.id.ID),
 		zap.String("DDL", job.Query),
@@ -226,7 +226,7 @@ func (s *schemaWrap4Owner) BuildDDLEvents(
 		preTableInfo, err = s.schemaSnapshot.PreTableInfo(job)
 		if err != nil {
 			log.Error("build DDL event fail",
-				zap.Reflect("job", job), zap.Error(err))
+				zap.Any("job", job), zap.Error(err))
 			return nil, errors.Trace(err)
 		}
 		err = s.schemaSnapshot.FillSchemaName(job)
@@ -247,11 +247,11 @@ func (s *schemaWrap4Owner) BuildDDLEvents(
 			s.metricIgnoreDDLEventCounter.Inc()
 			log.Info(
 				"DDL event ignored",
+				zap.String("namespace", s.id.Namespace),
+				zap.String("changefeed", s.id.ID),
 				zap.String("query", event.Query),
 				zap.Uint64("startTs", event.StartTs),
 				zap.Uint64("commitTs", event.CommitTs),
-				zap.String("namespace", s.id.Namespace),
-				zap.String("changefeed", s.id.ID),
 			)
 			continue
 		}
@@ -271,10 +271,12 @@ func (s *schemaWrap4Owner) shouldIgnoreTable(t *model.TableInfo) bool {
 		// Skip Warn to avoid confusion.
 		// See https://github.com/pingcap/tiflow/issues/4559
 		if !t.IsSequence() {
-			log.Warn("skip ineligible table", zap.Int64("tableID", t.ID),
-				zap.Stringer("tableName", t.TableName),
+			log.Warn("skip ineligible table",
 				zap.String("namespace", s.id.Namespace),
-				zap.String("changefeed", s.id.ID))
+				zap.String("changefeed", s.id.ID),
+				zap.Int64("tableID", t.ID),
+				zap.Stringer("tableName", t.TableName),
+			)
 		}
 		return true
 	}
