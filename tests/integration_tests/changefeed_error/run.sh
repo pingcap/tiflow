c15093f312f2943f71b52dbba77141855a806224
dongmen <414110582@qq.com>
2022-08-25 15:40:59 +0800

    fix it error

--- tests/integration_tests/changefeed_error/run.sh
+++ tests/integration_tests/changefeed_error/run.sh
@@ -109,11 +109,11 @@ function run() {
 
 	changefeedid_3="changefeed-initialize-error"
 	run_cdc_cli changefeed create --start-ts=0 --sink-uri="$SINK_URI" -c $changefeedid_3
-	ensure $MAX_RETRIES check_changefeed_state http://${UP_PD_HOST_1}:${UP_PD_PORT_1} ${changefeedid_3} "normal" "" ""
+	ensure $MAX_RETRIES check_changefeed_state http://${UP_PD_HOST_1}:${UP_PD_PORT_1} ${changefeedid_3} "normal" "null" ""
 	run_cdc_cli changefeed pause -c $changefeedid_3
 	ensure $MAX_RETRIES check_changefeed_state http://${UP_PD_HOST_1}:${UP_PD_PORT_1} ${changefeedid_3} "stopped" "changefeed new redo manager injected error" ""
 	run_cdc_cli changefeed resume -c $changefeedid_3
-	ensure $MAX_RETRIES check_changefeed_state http://${UP_PD_HOST_1}:${UP_PD_PORT_1} ${changefeedid_3} "normal" "" ""
+	ensure $MAX_RETRIES check_changefeed_state http://${UP_PD_HOST_1}:${UP_PD_PORT_1} ${changefeedid_3} "normal" "null" ""
 	run_cdc_cli changefeed remove -c $changefeedid_3
 	export GO_FAILPOINTS=''
 	cleanup_process $CDC_BINARY
