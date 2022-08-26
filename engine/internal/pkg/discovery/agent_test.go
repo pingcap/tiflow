8b36577a868a7079b7de45fe4f242fceda8408e5
Yujie Xia <xiayjchn@gmail.com>
2022-08-26 18:28:21 +0800

    discovery(engine): refactor agent to use list api (#6891)
    ref pingcap/tiflow#6867

--- engine/internal/pkg/discovery/agent_test.go
+++ engine/internal/pkg/discovery/agent_test.go
@@ -15,100 +15,58 @@ package discovery
 
 import (
 	"context"
+	"reflect"
 	"sync"
 	"testing"
 	"time"
 
 	"github.com/golang/mock/gomock"
-	"github.com/pingcap/errors"
-	"github.com/pingcap/tiflow/engine/model"
-	"github.com/pingcap/tiflow/engine/pkg/srvdiscovery"
-	"github.com/pingcap/tiflow/engine/test/mock"
-	tmock "github.com/stretchr/testify/mock"
+	pb "github.com/pingcap/tiflow/engine/enginepb"
+	pkgClient "github.com/pingcap/tiflow/engine/pkg/client"
 	"github.com/stretchr/testify/require"
-	clientv3 "go.etcd.io/etcd/client/v3"
 )
-
-type agentTestHelper struct {
-	agent           *agentImpl
-	discoveryRunner *mock.MockDiscoveryRunner
-	session         *mock.MockSession
-	watchCh         chan srvdiscovery.WatchResp
-	sessionDoneCh   chan struct{}
-}
-
-type mockRunnerFactory struct {
-	tmock.Mock
-}
-
-func (f *mockRunnerFactory) Build(
-	etcdCli *clientv3.Client,
-	sessionTTL int,
-	watchDur time.Duration,
-	key string,
-	value string,
-) srvdiscovery.DiscoveryRunner {
-	args := f.Called(etcdCli, sessionTTL, watchDur, key, value)
-	return args.Get(0).(srvdiscovery.DiscoveryRunner)
-}
-
-func newAgentTestHelper(t *testing.T, selfInfo *model.NodeInfo) *agentTestHelper {
-	runner := mock.NewMockDiscoveryRunner(gomock.NewController(t))
-	session := mock.NewMockSession(gomock.NewController(t))
-	watchCh := make(chan srvdiscovery.WatchResp, 1)
-	sessionDoneCh := make(chan struct{})
-	session.EXPECT().Done().AnyTimes().Return(sessionDoneCh)
-	runner.
-		EXPECT().
-		ResetDiscovery(gomock.Any(), gomock.Eq(true)).
-		Times(1).
-		Return(session, nil)
-	runner.EXPECT().
-		GetWatcher().
-		AnyTimes().
-		Return(watchCh)
-
-	factory := &mockRunnerFactory{}
-
-	selfInfoStr, err := selfInfo.ToJSON()
-	require.NoError(t, err)
-
-	factory.On("Build", (*clientv3.Client)(nil), 5, 1*time.Second, selfInfo.EtcdKey(), selfInfoStr).
-		Return(runner).Times(1)
-	agent := NewAgent(selfInfo, nil, 5, 1*time.Second)
-	agent.(*agentImpl).discoveryRunnerFactory = factory
-	return &agentTestHelper{
-		agent:           agent.(*agentImpl),
-		discoveryRunner: runner,
-		session:         session,
-		watchCh:         watchCh,
-		sessionDoneCh:   sessionDoneCh,
-	}
-}
-
-func (h *agentTestHelper) ResetSession(t *testing.T) {
-	newSession := mock.NewMockSession(gomock.NewController(t))
-	newSessionDoneCh := make(chan struct{})
-	newSession.EXPECT().Done().AnyTimes().Return(newSessionDoneCh)
-	h.discoveryRunner.
-		EXPECT().
-		ResetDiscovery(gomock.Any(), gomock.Eq(true)).
-		Times(1).
-		Return(newSession, nil)
-	close(h.sessionDoneCh)
-	h.sessionDoneCh = newSessionDoneCh
-	h.session = newSession
-}
 
 func TestAgent(t *testing.T) {
 	t.Parallel()
 
-	selfInfo := &model.NodeInfo{
-		Type: model.NodeTypeExecutor,
-		ID:   "executor-1",
-		Addr: "dummy:2222",
-	}
-	helper := newAgentTestHelper(t, selfInfo)
+	discoveryClient := pkgClient.NewMockServerMasterClient(gomock.NewController(t))
+	agent := NewAgent(discoveryClient, time.Millisecond*100)
+
+	var nodes sync.Map
+
+	discoveryClient.EXPECT().
+		ListMasters(gomock.Any()).AnyTimes().
+		DoAndReturn(func(ctx context.Context) ([]*pb.Master, error) {
+			var ret []*pb.Master
+			nodes.Range(func(_, value any) bool {
+				node := value.(Node)
+				if node.Tp == NodeTypeMaster {
+					ret = append(ret, &pb.Master{
+						Id:      node.ID,
+						Address: node.Addr,
+					})
+				}
+				return true
+			})
+			return ret, nil
+		})
+
+	discoveryClient.EXPECT().
+		ListExecutors(gomock.Any()).AnyTimes().
+		DoAndReturn(func(ctx context.Context) ([]*pb.Executor, error) {
+			var ret []*pb.Executor
+			nodes.Range(func(_, value any) bool {
+				node := value.(Node)
+				if node.Tp == NodeTypeExecutor {
+					ret = append(ret, &pb.Executor{
+						Id:      node.ID,
+						Address: node.Addr,
+					})
+				}
+				return true
+			})
+			return ret, nil
+		})
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
@@ -118,140 +76,128 @@ func TestAgent(t *testing.T) {
 	go func() {
 		defer wg.Done()
 
-		err := helper.agent.Run(ctx)
+		err := agent.Run(ctx)
 		require.ErrorIs(t, err, context.Canceled)
 	}()
 
-	expectedSnap := srvdiscovery.Snapshot{
-		"executor-2": srvdiscovery.ServiceResource{
-			Type: model.NodeTypeExecutor,
+	initialSnap := Snapshot{
+		"master-1": Node{
+			Tp:   NodeTypeMaster,
+			ID:   "master-1",
+			Addr: "dummy-1:2222",
+		},
+		"executor-2": Node{
+			Tp:   NodeTypeExecutor,
 			ID:   "executor-2",
 			Addr: "dummy-2:2222",
 		},
-		"executor-3": srvdiscovery.ServiceResource{
-			Type: model.NodeTypeExecutor,
+		"executor-3": Node{
+			Tp:   NodeTypeExecutor,
 			ID:   "executor-3",
 			Addr: "dummy-3:2222",
 		},
 	}
-	helper.discoveryRunner.EXPECT().GetSnapshot().Times(1).Return(expectedSnap)
 
-	snap, receiver, err := helper.agent.Subscribe(ctx)
+	for _, node := range initialSnap {
+		nodes.Store(node.ID, node)
+	}
+
+	require.Eventually(t, func() bool {
+		snap, receiver, err := agent.Subscribe(ctx)
+		require.NoError(t, err)
+		receiver.Close()
+		return reflect.DeepEqual(snap, initialSnap)
+	}, time.Second, time.Millisecond*100, "should get initial snapshot")
+
+	_, receiver, err := agent.Subscribe(ctx)
 	require.NoError(t, err)
-	require.Equal(t, expectedSnap, snap)
 
-	select {
-	case <-receiver.C:
-		require.Fail(t, "unreachable")
-	default:
-	}
-	resp := srvdiscovery.WatchResp{
-		AddSet: map[srvdiscovery.UUID]srvdiscovery.ServiceResource{
-			"executor-4": {
-				Type: model.NodeTypeExecutor,
-				ID:   "executor-4",
-				Addr: "dummy-4:2222",
-			},
-		},
-		DelSet: map[srvdiscovery.UUID]srvdiscovery.ServiceResource{
-			"executor-3": {
-				Type: model.NodeTypeExecutor,
-				ID:   "executor-3",
-				Addr: "dummy-3:2222",
-			},
-		},
-	}
-	helper.discoveryRunner.EXPECT().ApplyWatchResult(resp).Times(1)
-	helper.watchCh <- resp
-
-	select {
-	case <-ctx.Done():
-		require.Failf(t, "unreachable", "%s", ctx.Err().Error())
-	case event := <-receiver.C:
-		require.Equal(t, Event{
-			Tp: EventTypeDel,
-			Info: model.NodeInfo{
-				Type: model.NodeTypeExecutor,
-				ID:   "executor-3",
-				Addr: "dummy-3:2222",
-			},
-		}, event)
-	}
-
-	select {
-	case <-ctx.Done():
-		require.Failf(t, "unreachable", "%s", ctx.Err().Error())
-	case event := <-receiver.C:
-		require.Equal(t, Event{
+	expectedEvents := []Event{
+		{
 			Tp: EventTypeAdd,
-			Info: model.NodeInfo{
-				Type: model.NodeTypeExecutor,
+			Node: Node{
+				Tp:   NodeTypeExecutor,
 				ID:   "executor-4",
 				Addr: "dummy-4:2222",
 			},
-		}, event)
+		},
+		{
+			Tp: EventTypeAdd,
+			Node: Node{
+				Tp:   NodeTypeExecutor,
+				ID:   "executor-5",
+				Addr: "dummy-5:2222",
+			},
+		},
+		{
+			Tp: EventTypeDel,
+			Node: Node{
+				Tp:   NodeTypeExecutor,
+				ID:   "executor-3",
+				Addr: "dummy-3:2222",
+			},
+		},
+		{
+			Tp: EventTypeAdd,
+			Node: Node{
+				Tp:   NodeTypeMaster,
+				ID:   "master-6",
+				Addr: "dummy-6:2222",
+			},
+		},
+		{
+			Tp: EventTypeDel,
+			Node: Node{
+				Tp:   NodeTypeMaster,
+				ID:   "master-1",
+				Addr: "dummy-1:2222",
+			},
+		},
 	}
 
-	helper.session.EXPECT().Close().Times(1)
+	for _, expectedEvent := range expectedEvents {
+		if expectedEvent.Tp == EventTypeAdd {
+			nodes.Store(expectedEvent.Node.ID, expectedEvent.Node)
+		} else {
+			nodes.Delete(expectedEvent.Node.ID)
+		}
+		select {
+		case <-ctx.Done():
+			require.Failf(t, "unreachable", "%s", ctx.Err().Error())
+		case event := <-receiver.C:
+			require.Equal(t, expectedEvent, event)
+		}
+	}
+	receiver.Close()
+
+	require.Eventually(t, func() bool {
+		snap, receiver, err := agent.Subscribe(ctx)
+		require.NoError(t, err)
+		receiver.Close()
+		return reflect.DeepEqual(snap, Snapshot{
+			"executor-2": Node{
+				Tp:   NodeTypeExecutor,
+				ID:   "executor-2",
+				Addr: "dummy-2:2222",
+			},
+			"executor-4": Node{
+				Tp:   NodeTypeExecutor,
+				ID:   "executor-4",
+				Addr: "dummy-4:2222",
+			},
+			"executor-5": Node{
+				Tp:   NodeTypeExecutor,
+				ID:   "executor-5",
+				Addr: "dummy-5:2222",
+			},
+			"master-6": Node{
+				Tp:   NodeTypeMaster,
+				ID:   "master-6",
+				Addr: "dummy-6:2222",
+			},
+		})
+	}, time.Second, time.Millisecond*100, "should get expected snapshot")
+
 	cancel()
-	wg.Wait()
-}
-
-func TestAgentSessionDone(t *testing.T) {
-	t.Parallel()
-
-	selfInfo := &model.NodeInfo{
-		Type: model.NodeTypeExecutor,
-		ID:   "executor-1",
-		Addr: "dummy:2222",
-	}
-	helper := newAgentTestHelper(t, selfInfo)
-
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-
-		err := helper.agent.Run(ctx)
-		require.ErrorIs(t, err, context.Canceled)
-	}()
-
-	for i := 0; i < 10; i++ {
-		helper.ResetSession(t)
-		time.Sleep(100 * time.Millisecond)
-	}
-
-	helper.session.EXPECT().Close().Times(1)
-	cancel()
-	wg.Wait()
-}
-
-func TestAgentRunnerError(t *testing.T) {
-	t.Parallel()
-
-	selfInfo := &model.NodeInfo{
-		Type: model.NodeTypeExecutor,
-		ID:   "executor-1",
-		Addr: "dummy:2222",
-	}
-	helper := newAgentTestHelper(t, selfInfo)
-
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-
-		err := helper.agent.Run(ctx)
-		require.ErrorContains(t, err, "runner error")
-	}()
-
-	helper.session.EXPECT().Close().Times(1)
-	helper.watchCh <- srvdiscovery.WatchResp{Err: errors.New("runner error")}
 	wg.Wait()
 }
