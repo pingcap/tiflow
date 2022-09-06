141a333c2a52c777d317ed3f3393905edad2a3b4
maxshuang <huangguohao@pingcap.com>
2022-09-06 14:46:39 +0800

    fix(test): fix fake job integration test fail when the worker is finished

--- engine/framework/fake/fake_worker.go
+++ engine/framework/fake/fake_worker.go
@@ -155,14 +155,19 @@ func (d *dummyWorker) Tick(ctx context.Context) error {
 		return nil
 	}
 
+	extMsg, err := d.status.Marshal()
+	if err != nil {
+		return err
+	}
+
 	if d.getState() == frameModel.WorkerStateStopped {
 		d.setState(frameModel.WorkerStateStopped)
-		return d.Exit(ctx, framework.ExitReasonCanceled, nil, []byte("worker has been canceled"))
+		return d.Exit(ctx, framework.ExitReasonCanceled, nil, extMsg)
 	}
 
 	if d.status.Tick >= d.config.TargetTick {
 		d.setState(frameModel.WorkerStateFinished)
-		return d.Exit(ctx, framework.ExitReasonFinished, nil, []byte("worker has reached target tick"))
+		return d.Exit(ctx, framework.ExitReasonFinished, nil, extMsg)
 	}
 
 	if d.config.InjectErrorInterval != 0 {
