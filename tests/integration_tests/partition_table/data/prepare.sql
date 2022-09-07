34e64b805ad37701a5c3c948ab19ae4b7f864331
dongmen <414110582@qq.com>
2022-09-07 11:44:41 +0800

    add integration test

--- tests/integration_tests/partition_table/data/prepare.sql
+++ tests/integration_tests/partition_table/data/prepare.sql
@@ -14,9 +14,12 @@ insert into t1 values (1),(2),(3),(4),(5),(6);
 insert into t1 values (7),(8),(9);
 insert into t1 values (11),(12),(20);
 alter table t1 add partition (partition p3 values less than (30), partition p4 values less than (40));
-insert into t1 values (25),(29),(35);
+insert into t1 values (25),(29),(35); /*these values in p3*/
 alter table t1 truncate partition p0;
 alter table t1 drop partition p1;
 insert into t1 values (7),(8),(9);
 update t1 set a=a+10 where a=9;
+
+create table t2 (a int primary key)
+ALTER TABLE t1 EXCHANGE PARTITION p3 WITH TABLE t2;
 
