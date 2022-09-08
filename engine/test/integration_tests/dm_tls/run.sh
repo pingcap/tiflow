4c8757fb8f03fe37925c0e65970b261f94b9f932
lance6716 <lance6716@gmail.com>
2022-09-08 11:40:55 +0800

    finish

--- engine/test/integration_tests/dm_tls/run.sh
+++ engine/test/integration_tests/dm_tls/run.sh
@@ -10,26 +10,26 @@ CONFIG=$(adjust_config $OUT_DIR $TEST_NAME $CONFIG)
 echo "using adjusted configs to deploy cluster: $CONFIG"
 
 function run() {
-  generate_cert /tmp/certs/downstream tidb
+	generate_cert /tmp/certs/downstream tidb
 	start_engine_cluster $CONFIG
 
 	# copy auto-generated certificates from MySQL to bypass permission
 	mkdir -p $WORK_DIR/mysql1
 	mkdir -p $WORK_DIR/mysql2
-	sudo cat /tmp/mysql1/client-key.pem > $WORK_DIR/mysql1/client-key.pem
-	sudo cat /tmp/mysql1/client-cert.pem > $WORK_DIR/mysql1/client-cert.pem
-	sudo cat /tmp/mysql2/client-key.pem > $WORK_DIR/mysql2/client-key.pem
-	sudo cat /tmp/mysql2/client-cert.pem > $WORK_DIR/mysql2/client-cert.pem
+	sudo cat /tmp/mysql1/client-key.pem >$WORK_DIR/mysql1/client-key.pem
+	sudo cat /tmp/mysql1/client-cert.pem >$WORK_DIR/mysql1/client-cert.pem
+	sudo cat /tmp/mysql2/client-key.pem >$WORK_DIR/mysql2/client-key.pem
+	sudo cat /tmp/mysql2/client-cert.pem >$WORK_DIR/mysql2/client-cert.pem
 
 	wait_mysql_online.sh --password 123456 --ssl-key $WORK_DIR/mysql1/client-key.pem --ssl-cert $WORK_DIR/mysql1/client-cert.pem
 	wait_mysql_online.sh --port 3307 --password 123456 --ssl-key $WORK_DIR/mysql2/client-key.pem --ssl-cert $WORK_DIR/mysql2/client-cert.pem
 
-  echo "verify can't connect to upstream without certificates"
-  mysql -P3306 -h127.0.0.1 -uroot -p123456 -e "show databases" && echo "failed" && exit 1 || true
+	echo "verify can't connect to upstream without certificates"
+	mysql -P3306 -h127.0.0.1 -uroot -p123456 -e "show databases" && echo "failed" && exit 1 || true
 
 	# prepare data
 
-  run_sql_file --password 123456 --ssl-key $WORK_DIR/mysql1/client-key.pem --ssl-cert $WORK_DIR/mysql1/client-cert.pem $CUR_DIR/data/db1.prepare.sql
+	run_sql_file --password 123456 --ssl-key $WORK_DIR/mysql1/client-key.pem --ssl-cert $WORK_DIR/mysql1/client-cert.pem $CUR_DIR/data/db1.prepare.sql
 	run_sql_file --port 3307 --password 123456 --ssl-key $WORK_DIR/mysql2/client-key.pem --ssl-cert $WORK_DIR/mysql2/client-cert.pem $CUR_DIR/data/db2.prepare.sql
 
 	# create downstream user
@@ -38,13 +38,13 @@ function run() {
 
 	# create job
 
-  cp $CUR_DIR/conf/job.yaml $WORK_DIR/job.yaml
-  sed -i "s,<downstream-key>,$(base64 -w0 /tmp/certs/downstream/client.key)," $WORK_DIR/job.yaml
-  sed -i "s,<downstream-cert>,$(base64 -w0 /tmp/certs/downstream/client.pem)," $WORK_DIR/job.yaml
-  sed -i "s,<mysql1-key>,$(base64 -w0 $WORK_DIR/mysql1/client-key.pem)," $WORK_DIR/job.yaml
-  sed -i "s,<mysql1-cert>,$(base64 -w0 $WORK_DIR/mysql1/client-cert.pem)," $WORK_DIR/job.yaml
-  sed -i "s,<mysql2-key>,$(base64 -w0 $WORK_DIR/mysql2/client-key.pem)," $WORK_DIR/job.yaml
-  sed -i "s,<mysql2-cert>,$(base64 -w0 $WORK_DIR/mysql2/client-cert.pem)," $WORK_DIR/job.yaml
+	cp $CUR_DIR/conf/job.yaml $WORK_DIR/job.yaml
+	sed -i "s,<downstream-key>,$(base64 -w0 /tmp/certs/downstream/client.key)," $WORK_DIR/job.yaml
+	sed -i "s,<downstream-cert>,$(base64 -w0 /tmp/certs/downstream/client.pem)," $WORK_DIR/job.yaml
+	sed -i "s,<mysql1-key>,$(base64 -w0 $WORK_DIR/mysql1/client-key.pem)," $WORK_DIR/job.yaml
+	sed -i "s,<mysql1-cert>,$(base64 -w0 $WORK_DIR/mysql1/client-cert.pem)," $WORK_DIR/job.yaml
+	sed -i "s,<mysql2-key>,$(base64 -w0 $WORK_DIR/mysql2/client-key.pem)," $WORK_DIR/job.yaml
+	sed -i "s,<mysql2-cert>,$(base64 -w0 $WORK_DIR/mysql2/client-cert.pem)," $WORK_DIR/job.yaml
 
 	create_job_json=$(base64 -w0 $WORK_DIR/job.yaml | jq -Rs '{ type: "DM", config: . }')
 	echo "create_job_json: $create_job_json"
@@ -55,17 +55,15 @@ function run() {
 
 	exec_with_retry --count 30 "curl \"http://127.0.0.1:10245/api/v1/jobs/$job_id/status\" | tee /dev/stderr | jq -e '.task_status.\"mysql-02\".status.unit == 12'"
 
-	# check data
-
-	check_sync_diff $WORK_DIR $CUR_DIR/conf/diff_config.toml
-
 	# insert increment data
 
-	run_sql_file --port 3307 $CUR_DIR/data/db2.increment.sql
+	run_sql_file --password 123456 --ssl-key $WORK_DIR/mysql1/client-key.pem --ssl-cert $WORK_DIR/mysql1/client-cert.pem $CUR_DIR/data/db1.increment.sql
+	run_sql_file --port 3307 --password 123456 --ssl-key $WORK_DIR/mysql2/client-key.pem --ssl-cert $WORK_DIR/mysql2/client-cert.pem $CUR_DIR/data/db2.increment.sql
 
 	# check data
 
-	check_sync_diff $WORK_DIR $CUR_DIR/conf/diff_config.toml
+	exec_with_retry 'run_sql --port 4000 --ssl-key /tmp/certs/downstream/client.key --ssl-cert /tmp/certs/downstream/client.pem "select count(1) from tls.t1\G" | grep -Fq "count(1): 2"'
+	exec_with_retry 'run_sql --port 4000 --ssl-key /tmp/certs/downstream/client.key --ssl-cert /tmp/certs/downstream/client.pem "select count(1) from tls.t2\G" | grep -Fq "count(1): 2"'
 }
 
 trap "stop_engine_cluster $WORK_DIR $CONFIG" EXIT
