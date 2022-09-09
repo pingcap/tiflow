4f9f97484d12118d62d5f7e960a257af79080c32
lance6716 <lance6716@gmail.com>
2022-09-09 10:22:03 +0800

    wip: add test to reproduce bug
    Signed-off-by: lance6716 <lance6716@gmail.com>

--- dm/tests/gbk/data/db1.prepare.sql
+++ dm/tests/gbk/data/db1.prepare.sql
@@ -9,3 +9,6 @@ insert into t1 (id, name) values (1, '你好1'), (2, '你好2');
 create table t3 (id int, name varchar(20) character set gbk, primary key(`id`)) character set utf8;
 insert into t3 (id, name) values (0, '你好0');
 insert into t3 (id, name) values (1, '你好1'), (2, '你好2');
+
+create table t5 (id int, name varchar(20), primary key(`id`)) character set latin1;
+insert into t5 (id, name) values (0, 'Müller');
