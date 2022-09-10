a085f975df870605e740dfce367c4b10de970f48
xiongjiwei <xiongjiwei1996@outlook.com>
2022-09-10 10:45:49 +0800

    add more test

--- dm/tests/all_mode/data/db3.prepare.sql
+++ dm/tests/all_mode/data/db3.prepare.sql
@@ -7,7 +7,22 @@ create table t_extract
     id int primary key auto_increment,
     j  json,
     a  int as (j ->> '$.a'),
-    b  char(10) as (j ->> '$.b')
+    b  char(10) as (j ->> '$.b'),
+    j1 json as (json_search(j, 'one', '1')),
+    j2 json as (json_array(j ->> '$.a', j ->> '$.b')),
+    j3 json as (json_object('a', j ->> '$.a', 'b', j ->> '$.b')),
+    j4 json as (json_merge_preserve(b, '{"k": "v"}')),
+    j5 json as (json_merge_patch(b, '{"k": "v"}')),
+    j6 json as (json_merge(b, '{"k": "v"}')),
+    j7 json as (json_set(b, '$.k', 'v')),
+    j8 json as (json_insert(b, '$.k', 'v')),
+    j9 json as (json_replace(b, '$.k', 'v')),
+    j10 json as (json_remove(b, '$.k')),
+    j11 json as (json_contains(b, '{"k": "v"}')),
+    j12 json as (json_contains_path(b, 'one', '$.k')),
+    j13 json as (json_array_append(b, '$.k', 'v')),
+
+    j14 json as (json_array_insert(j3, '$.k', 'v'))
 );
 
 insert into t_extract(j)values ('{"a":1, "b": {"a":1}}');
