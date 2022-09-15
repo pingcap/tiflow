cd4069b4a65edafc4a494bbebc8c94b38ede4550
zhaoxinyu <zhaoxinyu512@gmail.com>
2022-09-16 00:51:54 +0800

    further optimize some code

---
+++ cdc/sinkv2/eventsink/cloudstorage/encoding_worker_test.go
@@ -0,0 +1,137 @@
+package cloudstorage
+
+import (
+	"context"
+	"net/url"
+	"testing"
+
+	"github.com/pingcap/tidb/br/pkg/storage"
+	"github.com/pingcap/tiflow/cdc/model"
+	"github.com/pingcap/tiflow/cdc/sink/codec/builder"
+	"github.com/pingcap/tiflow/cdc/sinkv2/eventsink"
+	"github.com/pingcap/tiflow/cdc/sinkv2/util"
+	"github.com/pingcap/tiflow/pkg/chann"
+	"github.com/pingcap/tiflow/pkg/config"
+	"github.com/stretchr/testify/require"
+)
+
+func testEncodingWorker(ctx context.Context, t *testing.T) *encodingWorker {
+	uri := "local:///tmp/test"
+	sinkURI, err := url.Parse(uri)
+	require.Nil(t, err)
+	encoderConfig, err := util.GetEncoderConfig(sinkURI, config.ProtocolOpen,
+		config.GetDefaultReplicaConfig(), config.DefaultMaxMessageBytes)
+	require.Nil(t, err)
+	encoderBuilder, err := builder.NewEventBatchEncoderBuilder(context.TODO(), encoderConfig)
+	require.Nil(t, err)
+	encoder := encoderBuilder.Build()
+	errCh := make(chan error, 10)
+	changefeedID := model.DefaultChangeFeedID("test-encode")
+
+	bs, err := storage.ParseBackend(uri, &storage.BackendOptions{})
+	require.Nil(t, err)
+	storage, err := storage.New(ctx, bs, nil)
+	require.Nil(t, err)
+
+	dmlWriter := newDMLWriter(ctx, changefeedID, storage, 1, ".json", errCh)
+	worker := newEncodingWorker(1, changefeedID, encoder, dmlWriter, errCh)
+
+	return worker
+}
+
+func TestEncodeEvents(t *testing.T) {
+	// math.Inf(1)
+	ctx, cancel := context.WithCancel(context.Background())
+	worker := testEncodingWorker(ctx, t)
+	err := worker.encodeEvents(ctx, eventFragment{
+		tableName: &model.TableName{
+			Schema:  "test",
+			Table:   "table1",
+			TableID: 100,
+		},
+		seqNumber: 1,
+		event: &eventsink.TxnCallbackableEvent{
+			Event: &model.SingleTableTxn{
+				Table: &model.TableName{
+					Schema:  "test",
+					Table:   "table1",
+					TableID: 100,
+				},
+				Rows: []*model.RowChangedEvent{
+					{
+						Table: &model.TableName{
+							Schema:  "test",
+							Table:   "table1",
+							TableID: 100,
+						},
+						Columns: []*model.Column{
+							{Name: "c1", Value: 100},
+							{Name: "c2", Value: "hello world"},
+						},
+					},
+					{
+						Table: &model.TableName{
+							Schema:  "test",
+							Table:   "table1",
+							TableID: 100,
+						},
+						Columns: []*model.Column{
+							{Name: "c1", Value: 200},
+							{Name: "c2", Value: "你好，世界"},
+						},
+					},
+				},
+			},
+		},
+	})
+	require.Nil(t, err)
+	cancel()
+
+	worker.writer.stop()
+}
+
+func TestWorkerRun(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	worker := testEncodingWorker(ctx, t)
+	msgCh := chann.New[eventFragment]()
+	worker.run(ctx, msgCh)
+	table := &model.TableName{
+		Schema:  "test",
+		Table:   "table1",
+		TableID: 100,
+	}
+	event := &model.SingleTableTxn{
+		Table: table,
+		Rows: []*model.RowChangedEvent{
+			{
+				Table: &model.TableName{
+					Schema:  "test",
+					Table:   "table1",
+					TableID: 100,
+				},
+				Columns: []*model.Column{
+					{Name: "c1", Value: 100},
+					{Name: "c2", Value: "hello world"},
+				},
+			},
+		},
+	}
+
+	for i := 0; i < 3; i++ {
+		frag := eventFragment{
+			tableName: table,
+			seqNumber: uint64(i + 1),
+			event: &eventsink.TxnCallbackableEvent{
+				Event: event,
+			},
+		}
+		msgCh.In() <- frag
+	}
+	msgCh.In() <- eventFragment{
+		seqNumber: 3,
+		tableName: table,
+	}
+	cancel()
+	worker.stop()
+	worker.writer.stop()
+}
---
+++ pkg/sink/cloudstorage/config_test.go
@@ -0,0 +1,92 @@
+// Copyright 2022 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cloudstorage
+
+import (
+	"context"
+	"net/url"
+	"testing"
+
+	"github.com/pingcap/tiflow/pkg/config"
+	"github.com/stretchr/testify/require"
+)
+
+func TestConfigApply(t *testing.T) {
+	expected := NewConfig()
+	expected.WorkerCount = 32
+	uri := "s3://bucket/prefix?worker-count=32"
+	sinkURI, err := url.Parse(uri)
+	require.Nil(t, err)
+	cfg := NewConfig()
+	err = cfg.Apply(context.TODO(), sinkURI, config.GetDefaultReplicaConfig())
+	require.Nil(t, err)
+	require.Equal(t, expected, cfg)
+}
+
+func TestVerifySinkURIParams(t *testing.T) {
+	testCases := []struct {
+		name        string
+		uri         string
+		expectedErr string
+	}{
+		{
+			name:        "valid sink uri with local/nfs scheme",
+			uri:         "file://tmp/test",
+			expectedErr: "",
+		},
+		{
+			name:        "valid sink uri with s3 scheme",
+			uri:         "s3://bucket/prefix",
+			expectedErr: "",
+		},
+		{
+			name:        "valid sink uri with gcs scheme",
+			uri:         "gcs://bucket/prefix",
+			expectedErr: "",
+		},
+		{
+			name:        "valid sink uri with azblob scheme",
+			uri:         "azblob://bucket/prefix",
+			expectedErr: "",
+		},
+		{
+			name:        "invalid sink uri with unknown storage scheme",
+			uri:         "xxx://tmp/test",
+			expectedErr: "can't create cloud storage sink with unsupported scheme",
+		},
+		{
+			name:        "invalid sink uri with worker-count number less than lower limit",
+			uri:         "file://tmp/test?worker-count=-1",
+			expectedErr: "invalid worker-count -1, which must be greater than 0",
+		},
+		{
+			name:        "invalid sink uri with worker-count number greater than upper limit",
+			uri:         "s3://bucket/prefix?worker-count=10000",
+			expectedErr: "",
+		},
+	}
+
+	for _, tc := range testCases {
+		sinkURI, err := url.Parse(tc.uri)
+		require.Nil(t, err)
+		cfg := NewConfig()
+		err = cfg.Apply(context.TODO(), sinkURI, config.GetDefaultReplicaConfig())
+		if tc.expectedErr == "" {
+			require.Nil(t, err)
+			require.LessOrEqual(t, cfg.WorkerCount, maxWorkerCount)
+		} else {
+			require.Regexp(t, tc.expectedErr, err)
+		}
+	}
+}
