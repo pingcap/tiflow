c8067a06c6f15d8c3b3b1cf045b91b174c30aec0
lance6716 <lance6716@gmail.com>
2022-09-20 18:12:55 +0800

    save work
    Signed-off-by: lance6716 <lance6716@gmail.com>

--- engine/test/integration_tests/dm_full_mode/run.sh
+++ engine/test/integration_tests/dm_full_mode/run.sh
@@ -17,14 +17,14 @@ function run() {
 	# test a ill-formatted job should fail
 
 	cp $CUR_DIR/conf/job.yaml $WORK_DIR/job.yaml
-  sed -i "20,23d" $WORK_DIR/job.yaml
-  create_job_json=$(base64 -w0 $WORK_DIR/job.yaml | jq -Rs '{ type: "DM", config: . }')
-  echo "create_job_json: $create_job_json"
-  job_id=$(curl -X POST -H "Content-Type: application/json" -d "$create_job_json" "http://127.0.0.1:10245/api/v1/jobs?tenant_id=dm_full_mode&project_id=dm_full_mode" | jq -r .id)
-  echo "job_id: $job_id"
+	sed -i "20,23d" $WORK_DIR/job.yaml
+	job_id=$(create_job "DM" "$WORK_DIR/job.yaml" "dm_full_mode")
+	echo "job_id: $job_id"
 
-  read -p 123
+	exec_with_retry "curl \"http://127.0.0.1:10245/api/v1/jobs/$job_id\" | tee /dev/stderr | grep -q 'Access denied'"
 
+	curl -X POST "http://127.0.0.1:10245/api/v1/jobs/$job_id/cancel"
+	read -p 123
 
 	# prepare data
 
