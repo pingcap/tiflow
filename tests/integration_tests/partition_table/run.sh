4cca90866b604819a14e8b5ccf93ec364e002584
dongmen <414110582@qq.com>
2022-09-20 10:22:26 +0800

    fmt

--- tests/integration_tests/partition_table/run.sh
+++ tests/integration_tests/partition_table/run.sh
@@ -17,7 +17,7 @@ function run() {
 
 	# record tso before we create tables to skip the system table DDLs
 	start_ts=$(run_cdc_cli_tso_query ${UP_PD_HOST_1} ${UP_PD_PORT_1})
-  run_sql "set @@tidb_enable_exchange_partition=on;" ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT}
+	run_sql "set @@tidb_enable_exchange_partition=on;" ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT}
 
 	run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY
 
