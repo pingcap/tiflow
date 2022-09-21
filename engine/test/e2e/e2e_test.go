8a291427182e072b4032f907c9f3f313437997de
amyangfei <amyangfei@gmail.com>
2022-09-21 17:59:03 +0800

    test(engine): increase timeout in e2e test (#7134)
    close pingcap/tiflow#7102

--- engine/test/e2e/e2e_test.go
+++ engine/test/e2e/e2e_test.go
@@ -78,7 +78,7 @@ func TestSubmitTest(t *testing.T) {
 	require.NoError(t, err)
 
 	for _, demoAddr := range config.DemoAddrs {
-		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 		defer cancel()
 		democlient, err := NewDemoClient(ctx, demoAddr)
 		require.NoError(t, err)
