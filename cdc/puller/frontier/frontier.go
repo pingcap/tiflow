f1eef74b031d283896f795ed38a896b1c0c851af
sdojjy <sdojjy@qq.com>
2022-09-27 09:16:12 +0800

    remove span hack func call from frontier

--- cdc/puller/frontier/frontier.go
+++ cdc/puller/frontier/frontier.go
@@ -36,14 +36,10 @@ type spanFrontier struct {
 }
 
 // NewFrontier creates Frontier from the given spans.
+// spanFrontier don't support use Nil as the maximum key of End range
+// So we use set it as util.UpperBoundKey, the means the real use case *should not* have an
+// End key bigger than util.UpperBoundKey
 func NewFrontier(checkpointTs uint64, spans ...regionspan.ComparableSpan) Frontier {
-	// spanFrontier don't support use Nil as the maximum key of End range
-	// So we use set it as util.UpperBoundKey, the means the real use case *should not* have an
-	// End key bigger than util.UpperBoundKey
-	for i, span := range spans {
-		spans[i] = span.Hack()
-	}
-
 	s := &spanFrontier{
 		spanList: *newSpanList(),
 	}
