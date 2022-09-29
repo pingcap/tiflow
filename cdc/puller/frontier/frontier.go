0a0e268e7c6519d4f6d938092296f3542e1feb61
sdojjy <sdojjy@qq.com>
2022-09-29 17:17:36 +0800

    add ut

--- cdc/puller/frontier/frontier.go
+++ cdc/puller/frontier/frontier.go
@@ -93,9 +93,11 @@ func (s *spanFrontier) insert(regionID uint64, span regionspan.ComparableSpan, t
 	if next != nil {
 		if bytes.Equal(seekRes.Node().Key(), span.Start) && bytes.Equal(next.Key(), span.End) {
 			s.minTsHeap.UpdateKey(seekRes.Node().Value(), ts)
-			s.nodes[regionID] = seekRes.Node()
-			s.nodes[regionID].regionID = regionID
-			s.nodes[regionID].end = next.key
+			if regionID > 0 {
+				s.nodes[regionID] = seekRes.Node()
+				s.nodes[regionID].regionID = regionID
+				s.nodes[regionID].end = next.key
+			}
 			return
 		}
 	}
