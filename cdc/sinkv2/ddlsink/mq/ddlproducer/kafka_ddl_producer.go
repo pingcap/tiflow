4d31b616b33026d5b64dffb55f5fb8ef877bcff5
二手掉包工程师 <rustin.liu@gmail.com>
2022-09-30 16:15:45 +0800

    sinkv2(ticdc): add mq worker flush duration metric (#7263)
    ref pingcap/tiflow#5928

--- cdc/sinkv2/ddlsink/mq/ddlproducer/kafka_ddl_producer.go
+++ cdc/sinkv2/ddlsink/mq/ddlproducer/kafka_ddl_producer.go
@@ -24,7 +24,7 @@ import (
 	"github.com/pingcap/tiflow/cdc/contextutil"
 	"github.com/pingcap/tiflow/cdc/model"
 	"github.com/pingcap/tiflow/cdc/sink/codec/common"
-	collector "github.com/pingcap/tiflow/cdc/sinkv2/metrics/kafka"
+	collector "github.com/pingcap/tiflow/cdc/sinkv2/metrics/mq/kafka"
 	cerror "github.com/pingcap/tiflow/pkg/errors"
 	pkafka "github.com/pingcap/tiflow/pkg/sink/kafka"
 	"github.com/pingcap/tiflow/pkg/util"
