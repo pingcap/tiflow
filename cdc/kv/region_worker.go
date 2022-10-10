096b1c2639e7dd348c345e2531480390d7464899
sdojjy <sdojjy@qq.com>
2022-10-10 16:50:07 +0800

    add ut

--- cdc/kv/region_worker.go
+++ cdc/kv/region_worker.go
@@ -817,6 +817,9 @@ func (w *regionWorker) handleResolvedTs(
 	default:
 	}
 	for _, state := range revents.regions {
+		if !state.initialized {
+			continue
+		}
 		state.lock.Lock()
 		if resolvedTs > state.lastResolvedTs {
 			state.lastResolvedTs = resolvedTs
