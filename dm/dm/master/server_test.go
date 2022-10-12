8d6186c42b57f8d6a203f3fe716d2fd196034cb1
lance6716 <lance6716@gmail.com>
2022-10-12 17:47:13 +0800

    fix UT
    Signed-off-by: lance6716 <lance6716@gmail.com>

--- dm/dm/master/server_test.go
+++ dm/dm/master/server_test.go
@@ -1630,9 +1630,12 @@ func (t *testMaster) testNormalServerLifecycle(c *check.C, cfg *Config, checkLog
 
 func (t *testMaster) testHTTPInterface(c *check.C, url string, contain []byte) {
 	// we use HTTPS in some test cases.
-	tls, err := toolutils.NewTLS(pwd+"/tls_for_test/ca.pem", pwd+"/tls_for_test/dm.pem", pwd+"/tls_for_test/dm.key", url, []string{})
+	tlsConfig, err := toolutils.NewTLSConfig(
+		toolutils.WithCAPath(pwd+"/tls_for_test/ca.pem"),
+		toolutils.WithCertAndKeyPath(pwd+"/tls_for_test/dm.pem", pwd+"/tls_for_test/dm.key"),
+	)
 	c.Assert(err, check.IsNil)
-	cli := toolutils.ClientWithTLS(tls.TLSConfig())
+	cli := toolutils.ClientWithTLS(tlsConfig)
 
 	// nolint:noctx
 	resp, err := cli.Get(url)
