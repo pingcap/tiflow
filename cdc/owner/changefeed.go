94879317a5dc2edccfdaaf2a4328a5c6ffab022f
sdojjy <sdojjy@qq.com>
2022-10-20 10:40:42 +0800

    not wait the slowest resolved ts to forward the global resovled ts

--- cdc/owner/changefeed.go
+++ cdc/owner/changefeed.go
@@ -339,11 +339,15 @@ func (c *changefeed) tick(ctx cdcContext.Context, captures map[model.CaptureID]*
 		return nil
 	}
 
+	// if redo is not enabled, use the barrierTs as the `newResolvedTs`
+	// not need to wait the slowest table
+	if !c.redoManager.Enabled() {
+		newResolvedTs = barrierTs
+	}
+
 	// If the owner is just initialized, barrierTs can be `checkpoint-1`. To avoid
 	// global resolvedTs and checkpointTs regression, we need to handle the case.
-	if c.redoManager.Enabled() && newResolvedTs > barrierTs {
-		// if redo is not enabled, use the barrierTs as the `newResolvedTs`
-		// not need to wait the slowest table
+	if newResolvedTs > barrierTs {
 		newResolvedTs = barrierTs
 	}
 
