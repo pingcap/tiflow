5fcb99c53acc980e3e73f2d9b026234ee34c0ac2
lance6716 <lance6716@gmail.com>
2022-10-21 18:25:55 +0800

    dm(engine): Discard resource when it's no longer needed (#7403)
    close pingcap/tiflow#6784

---
+++ scripts/download-mc.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# Copyright 2022 PingCAP, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -eu
+
+echo "will download mc"
+curl https://dl.minio.io/client/mc/release/linux-amd64/mc -o bin/mc
+chmod a+x bin/mc
