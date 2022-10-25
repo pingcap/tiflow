55cab15a7a4e281b396ffe3117b5617770de3078
lance6716 <lance6716@gmail.com>
2022-10-25 16:14:13 +0800

    Update engine/executor/server.go
    Co-authored-by: CharlesCheung <61726649+CharlesCheung96@users.noreply.github.com>

--- engine/executor/server.go
+++ engine/executor/server.go
@@ -399,7 +399,7 @@ func (s *Server) Run(ctx context.Context) error {
 			limit = 0
 		}
 		threshold := limit * 7 / 10
-		log.L().Info("set memory threshold to GC tuner",
+		log.Info("set memory threshold to GC tuner",
 			zap.Uint64("memory limit", limit),
 			zap.Uint64("threshold", threshold))
 		gctuner.EnableGOGCTuner.Store(true)
