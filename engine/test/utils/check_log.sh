5a16ba177c0280ed601466bbdb5c1dfcf53f0832
amyangfei <amyangfei@gmail.com>
2022-10-27 19:22:22 +0800

    fix shfmt

--- engine/test/utils/check_log.sh
+++ engine/test/utils/check_log.sh
@@ -5,8 +5,8 @@ grep "Run all test success" /tmp/tiflow_engine_test/engine_it.log || exit 1
 grep -rE "\[PANIC\]|panic:" /tmp/tiflow_engine_test
 
 if [ $? -eq 0 ]; then
-    echo "found tiflow panic"
-    exit 1
+	echo "found tiflow panic"
+	exit 1
 else
-    exit 0
+	exit 0
 fi
