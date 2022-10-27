4fde7c326ce3e34cfa2e4c116454108edd6fa1df
3AceShowHand <jinl1037@hotmail.com>
2022-10-27 21:33:07 +0800

    rename mounterImpl to mounter.

--- cdc/entry/mounter.go
+++ cdc/entry/mounter.go
@@ -67,7 +67,7 @@ type Mounter interface {
 	DecodeEvent(ctx context.Context, event *model.PolymorphicEvent) error
 }
 
-type mounterImpl struct {
+type mounter struct {
 	schemaStorage                SchemaStorage
 	tz                           *time.Location
 	enableOldValue               bool
@@ -84,7 +84,7 @@ func NewMounter(schemaStorage SchemaStorage,
 	filter pfilter.Filter,
 	enableOldValue bool,
 ) Mounter {
-	return &mounterImpl{
+	return &mounter{
 		schemaStorage:  schemaStorage,
 		changefeedID:   changefeedID,
 		enableOldValue: enableOldValue,
@@ -100,7 +100,7 @@ func NewMounter(schemaStorage SchemaStorage,
 // DecodeEvent decode kv events using ddl puller's schemaStorage
 // this method could block indefinitely if the DDL puller is lagging.
 // Note: If pEvent.Row is nil after decode, it means this event should be ignored.
-func (m *mounterImpl) DecodeEvent(ctx context.Context, event *model.PolymorphicEvent) error {
+func (m *mounter) DecodeEvent(ctx context.Context, event *model.PolymorphicEvent) error {
 	m.metricTotalRows.Inc()
 	if event.IsResolved() {
 		return nil
@@ -117,7 +117,7 @@ func (m *mounterImpl) DecodeEvent(ctx context.Context, event *model.PolymorphicE
 	return nil
 }
 
-func (m *mounterImpl) unmarshalAndMountRowChanged(ctx context.Context, raw *model.RawKVEntry) (*model.RowChangedEvent, error) {
+func (m *mounter) unmarshalAndMountRowChanged(ctx context.Context, raw *model.RawKVEntry) (*model.RowChangedEvent, error) {
 	if !bytes.HasPrefix(raw.Key, tablePrefix) {
 		return nil, nil
 	}
@@ -190,7 +190,7 @@ func (m *mounterImpl) unmarshalAndMountRowChanged(ctx context.Context, raw *mode
 	return row, err
 }
 
-func (m *mounterImpl) unmarshalRowKVEntry(tableInfo *model.TableInfo, rawKey []byte, rawValue []byte, rawOldValue []byte, base baseKVEntry) (*rowKVEntry, error) {
+func (m *mounter) unmarshalRowKVEntry(tableInfo *model.TableInfo, rawKey []byte, rawValue []byte, rawOldValue []byte, base baseKVEntry) (*rowKVEntry, error) {
 	recordID, err := tablecodec.DecodeRowKey(rawKey)
 	if err != nil {
 		return nil, errors.Trace(err)
@@ -320,7 +320,7 @@ func datum2Column(tableInfo *model.TableInfo, datums map[int64]types.Datum, fill
 	return cols, rawCols, nil
 }
 
-func (m *mounterImpl) mountRowKVEntry(tableInfo *model.TableInfo, row *rowKVEntry, dataSize int64) (*model.RowChangedEvent, model.RowChangedDatums, error) {
+func (m *mounter) mountRowKVEntry(tableInfo *model.TableInfo, row *rowKVEntry, dataSize int64) (*model.RowChangedEvent, model.RowChangedDatums, error) {
 	var err error
 	// Decode previous columns.
 	var preCols []*model.Column
