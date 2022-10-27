c014b961bb700c3f51f1ae0d35a4af3223d284e3
amyangfei <amyangfei@gmail.com>
2022-10-27 18:21:55 +0800

    check master or executor panic

--- engine/test/utils/check_log.sh
+++ engine/test/utils/check_log.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-grep "Run all test success" /tmp/tiflow_engine_test/engine_it.log && exit 0 || exit 1
+grep "Run all test success" /tmp/tiflow_engine_test/engine_it.log || exit 1
+
+grep -rE "\[PANIC\]|panic:" /tmp/tiflow_engine_test
+
+if [ $? -eq 0 ]; then
+    echo "found tiflow panic"
+    exit 1
+else
+    exit 0
+fi
