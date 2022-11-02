a1882db9f46863dff2b0d167ff7b79d3cb538976
dongmen <414110582@qq.com>
2022-11-02 10:14:37 +0800

    fix kafka it error

--- tests/integration_tests/event_filter/run.sh
+++ tests/integration_tests/event_filter/run.sh
@@ -18,7 +18,7 @@ function run() {
 
 	TOPIC_NAME="ticdc-event-filter-$RANDOM"
 	case $SINK_TYPE in
-	kafka) SINK_URI="kafka://kafka01:9092/$TOPIC_NAME?protocol=open-protocol&partition-num=4&kafka-version=${KAFKA_VERSION}&max-message-bytes=10485760" ;;
+	kafka) SINK_URI="kafka://127.0.0.1:9092/$TOPIC_NAME?protocol=open-protocol&partition-num=4&kafka-version=${KAFKA_VERSION}&max-message-bytes=10485760" ;;
 	*) SINK_URI="mysql://normal:123456@127.0.0.1:3306/" ;;
 	esac
 
