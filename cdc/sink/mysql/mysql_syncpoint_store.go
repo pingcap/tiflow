d8c3c15af2369fe7b32180bacdab920ba5644f83
dongmen <414110582@qq.com>
2022-11-02 10:22:03 +0800

    add comments

--- cdc/sink/mysql/mysql_syncpoint_store.go
+++ cdc/sink/mysql/mysql_syncpoint_store.go
@@ -261,11 +261,12 @@ func (s *mysqlSyncPointStore) SinkSyncPoint(ctx context.Context,
 			s.syncPointRetention.Seconds())
 		_, err = tx.Exec(query)
 		if err != nil {
-			// It is ok to ignore the error, since clean sync point is not necessary.
-			log.Error("failed to clean syncpoint table", zap.Error(err))
+			// It is ok to ignore the error, since it will not affect the correctness of the system,
+			// and no any business logic depends on this behavior, so we just log the error.
+			log.Error("failed to clean syncpoint table", zap.Error(cerror.WrapError(cerror.ErrMySQLTxnError, err)))
+		} else {
+			s.lastCleanSyncPointTime = time.Now()
 		}
-		s.lastCleanSyncPointTime = time.Now()
-		log.Info("clean outdate syncpoint successfully", zap.String("query", query))
 	}
 
 	err = tx.Commit()
