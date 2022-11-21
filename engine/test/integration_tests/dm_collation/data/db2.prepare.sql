3d22bf46712e2df25963a95842f645ef6745f45e
lance6716 <lance6716@gmail.com>
2022-11-21 19:49:58 +0800

    dm(engine): declarative command should consider time (#7640)
    ref pingcap/tiflow#4287

--- engine/test/integration_tests/dm_collation/data/db2.prepare.sql
+++ engine/test/integration_tests/dm_collation/data/db2.prepare.sql
@@ -6,10 +6,10 @@ insert into t1 (id, name) values (1, 'Aa'), (2, 'aA');
 create table t2 (id int, name varchar(20) character set utf8mb4 collate utf8mb4_general_ci, primary key(`id`)) character set latin1 collate latin1_bin;
 insert into t2 (id, name) values (1, 'Aa'), (2, 'aA');
 
--- drop database if exists `test_panic`;
--- create database `test_panic`;
--- use `test_panic`;
--- create table t1 (
---     id int PRIMARY KEY,
---     name varchar(20) COLLATE utf8mb4_0900_as_cs
--- );
+drop database if exists `test_panic`;
+create database `test_panic`;
+use `test_panic`;
+create table t1 (
+    id int PRIMARY KEY,
+    name varchar(20) COLLATE utf8mb4_0900_as_cs
+);
