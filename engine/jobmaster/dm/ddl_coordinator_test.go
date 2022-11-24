7521c5107d8a5f551520a9b7d2afcbca0ded9606
gmhdbjd <gmhdbjd@gmail.com>
2022-11-24 17:30:12 +0800

    fix lint

--- engine/jobmaster/dm/ddl_coordinator_test.go
+++ engine/jobmaster/dm/ddl_coordinator_test.go
@@ -20,12 +20,12 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/pingcap/tiflow/pkg/errors"
 	"github.com/pingcap/log"
 	"github.com/pingcap/tiflow/dm/pkg/shardddl/optimism"
 	"github.com/pingcap/tiflow/engine/jobmaster/dm/config"
 	"github.com/pingcap/tiflow/engine/jobmaster/dm/metadata"
 	"github.com/pingcap/tiflow/engine/pkg/meta/mock"
+	"github.com/pingcap/tiflow/pkg/errors"
 	"github.com/stretchr/testify/require"
 )
 
