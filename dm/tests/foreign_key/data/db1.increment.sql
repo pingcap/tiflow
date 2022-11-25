11daedc7da80c0c20b2c5d391a856b220d6f7c2a
crazycs520 <crazycs520@gmail.com>
2022-11-25 20:32:43 +0800

    fix test
    Signed-off-by: crazycs520 <crazycs520@gmail.com>

--- dm/tests/foreign_key/data/db1.increment.sql
+++ dm/tests/foreign_key/data/db1.increment.sql
@@ -5,7 +5,7 @@ insert into t1 values (7),(8);
 insert into t2 values (7),(8);
 delete from t1 where id=7;
 
-insert into t3 values (1),(2);
-insert into t4 values (1, 1),(2, 2);
-delete from t3 where id in (1);
-update t3 set id=id+10 where id =2;
+insert into t3 values (7),(8);
+insert into t4 values (7, 7),(8, 8);
+delete from t3 where id in (7);
+update t3 set id=id+10 where id =8;
