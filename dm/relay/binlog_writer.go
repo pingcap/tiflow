570fdb658c065812e2f4fbbd9eb3f97935b86371
gmhdbjd <gmhdbjd@gmail.com>
2022-11-29 16:19:52 +0800

    Revert "add fileOffset"
    This reverts commit 4453b50173b235d269345229d054d9636a1135e1.

--- dm/relay/binlog_writer.go
+++ dm/relay/binlog_writer.go
@@ -37,15 +37,12 @@ var nilErr error
 // BinlogWriter is a binlog event writer which writes binlog events to a file.
 // Open/Write/Close cannot be called concurrently.
 type BinlogWriter struct {
-	// offset means the offset we add to binlog writer
-	offset atomic.Int64
-	// fileOffset means the offset we finish write to the file
-	fileOffset atomic.Int64
-	file       *os.File
-	relayDir   string
-	uuid       atomic.String
-	filename   atomic.String
-	err        atomic.Error
+	offset   atomic.Int64
+	file     *os.File
+	relayDir string
+	uuid     atomic.String
+	filename atomic.String
+	err      atomic.Error
 
 	logger log.Logger
 
@@ -102,7 +99,6 @@ func (w *BinlogWriter) run() {
 			errOccurs = true
 			return
 		}
-		w.fileOffset.Add(int64(n))
 		buf.Reset()
 	}
 
@@ -141,9 +137,7 @@ func (w *BinlogWriter) Open(uuid, filename string) error {
 		return terror.ErrBinlogWriterGetFileStat.Delegate(err, f.Name())
 	}
 
-	size := fs.Size()
-	w.offset.Store(size)
-	w.fileOffset.Store(size)
+	w.offset.Store(fs.Size())
 	w.file = f
 	w.uuid.Store(uuid)
 	w.filename.Store(filename)
@@ -176,7 +170,6 @@ func (w *BinlogWriter) Close() error {
 
 	w.file = nil
 	w.offset.Store(0)
-	w.fileOffset.Store(0)
 	w.uuid.Store("")
 	w.filename.Store("")
 	w.input = nil
@@ -213,5 +206,5 @@ func (w *BinlogWriter) Offset() int64 {
 }
 
 func (w *BinlogWriter) isActive(uuid, filename string) (bool, int64) {
-	return uuid == w.uuid.Load() && filename == w.filename.Load(), w.fileOffset.Load()
+	return uuid == w.uuid.Load() && filename == w.filename.Load(), w.offset.Load()
 }
