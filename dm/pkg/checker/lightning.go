b953b422f3acabc930ea2f6b941deaa839f35e6f
lance6716 <lance6716@gmail.com>
2022-12-02 18:12:01 +0800

    checker(dm): port free space lightning precheck (#7733)
    ref pingcap/tiflow#3510

--- dm/pkg/checker/lightning.go
+++ dm/pkg/checker/lightning.go
@@ -15,7 +15,10 @@ package checker
 
 import (
 	"context"
+	"fmt"
+	"strings"
 
+	"github.com/docker/go-units"
 	"github.com/pingcap/tidb/br/pkg/lightning/restore"
 )
 
@@ -133,5 +136,109 @@ func (c *LightningClusterVersionChecker) Check(ctx context.Context) *Result {
 		StateFailure,
 		`you can switch to logical import mode which has no requirements on downstream cluster version`,
 	)
+	return result
+}
+
+// LightningFreeSpaceChecker checks whether the cluster has enough free space.
+type LightningFreeSpaceChecker struct {
+	sourceDataSize int64
+	infoGetter     restore.TargetInfoGetter
+}
+
+// NewLightningFreeSpaceChecker creates a new LightningFreeSpaceChecker.
+func NewLightningFreeSpaceChecker(sourceDataSize int64, getter restore.TargetInfoGetter) RealChecker {
+	return &LightningFreeSpaceChecker{
+		sourceDataSize: sourceDataSize,
+		infoGetter:     getter,
+	}
+}
+
+// Name implements the RealChecker interface.
+func (c *LightningFreeSpaceChecker) Name() string {
+	return "lightning_free_space"
+}
+
+// Check implements the RealChecker interface.
+func (c *LightningFreeSpaceChecker) Check(ctx context.Context) *Result {
+	result := &Result{
+		Name:  c.Name(),
+		Desc:  "check whether the downstream has enough free space to store the data to be migrated",
+		State: StateFailure,
+	}
+	storeInfo, err := c.infoGetter.GetStorageInfo(ctx)
+	if err != nil {
+		markCheckError(result, err)
+		return result
+	}
+	clusterAvail := uint64(0)
+	for _, store := range storeInfo.Stores {
+		clusterAvail += uint64(store.Status.Available)
+	}
+	if clusterAvail < uint64(c.sourceDataSize) {
+		result.State = StateFailure
+		result.Errors = append(result.Errors, &Error{
+			Severity: StateFailure,
+			ShortErr: fmt.Sprintf("Downstream doesn't have enough space, available is %s, but we need %s",
+				units.BytesSize(float64(clusterAvail)), units.BytesSize(float64(c.sourceDataSize))),
+		})
+		result.Instruction = "you can try to scale-out TiKV storage or TiKV instance to gain more storage space"
+		return result
+	}
+
+	replConfig, err := c.infoGetter.GetReplicationConfig(ctx)
+	if err != nil {
+		markCheckError(result, err)
+		return result
+	}
+	safeSize := uint64(c.sourceDataSize) * replConfig.MaxReplicas * 2
+	if clusterAvail < safeSize {
+		result.State = StateWarning
+		result.Errors = append(result.Errors, &Error{
+			Severity: StateWarning,
+			ShortErr: fmt.Sprintf("Cluster may not have enough space, available is %s, but we need %s",
+				units.BytesSize(float64(clusterAvail)), units.BytesSize(float64(safeSize))),
+		})
+		result.Instruction = "you can try to scale-out TiKV storage or TiKV instance to gain more storage space"
+		return result
+	}
+	result.State = StateSuccess
+	return result
+}
+
+// LightningSortingSpaceChecker checks the local disk has enough space for physical
+// import mode sorting data.
+type LightningSortingSpaceChecker struct {
+	inner restore.PrecheckItem
+}
+
+// NewLightningSortingSpaceChecker creates a new LightningSortingSpaceChecker.
+func NewLightningSortingSpaceChecker(lightningChecker restore.PrecheckItem) RealChecker {
+	return &LightningSortingSpaceChecker{inner: lightningChecker}
+}
+
+// Name implements the RealChecker interface.
+func (c *LightningSortingSpaceChecker) Name() string {
+	return "lightning_enough_sorting_space"
+}
+
+// Check implements the RealChecker interface.
+func (c *LightningSortingSpaceChecker) Check(ctx context.Context) *Result {
+	result := &Result{
+		Name:  c.Name(),
+		Desc:  "check whether the free space of sorting-dir-physical is enough for Physical import mode",
+		State: StateFailure,
+	}
+	convertLightningPrecheck(
+		ctx,
+		result,
+		c.inner,
+		StateWarning,
+		`you can change sorting-dir-physical to another mounting disk or set disk-quota-physical`,
+	)
+	// don't expose lightning's config name in message
+	if len(result.Errors) > 0 {
+		result.Errors[0].ShortErr = strings.ReplaceAll(result.Errors[0].ShortErr, "tikv-importer.disk-quota", "disk-quota-physical")
+		result.Errors[0].ShortErr = strings.ReplaceAll(result.Errors[0].ShortErr, "mydumper.sorted-kv-dir", "sorting-dir-physical")
+	}
 	return result
 }
