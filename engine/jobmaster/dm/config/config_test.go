9391d8c388ad3e2259d87a64ba4eb7a413933d33
buchuitoudegou <756541536@qq.com>
2022-12-05 15:01:30 +0800

    fix ut

--- engine/jobmaster/dm/config/config_test.go
+++ engine/jobmaster/dm/config/config_test.go
@@ -99,6 +99,10 @@ func TestTaskCfg(t *testing.T) {
 		_, err := toml.DecodeFile(fmt.Sprintf("%s/dm_subtask_%d.toml", subtaskTemplateDir, taskCfg.Upstreams[0].DBCfg.Port), expectCfg)
 		require.NoError(t, err)
 		expectCfg.IOTotalBytes = atomic.NewUint64(0)
+		// require uuid is set
+		require.Greater(t, len(expectCfg.UUID), 0)
+		// don't check uuid's value
+		expectCfg.UUID = subTaskCfg.UUID
 		require.EqualValues(t, expectCfg, subTaskCfg)
 	}
 }
