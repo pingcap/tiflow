215bf26ac9fc0bfa0058a3c2aacd48c7ecf163d3
zhaoxinyu <zhaoxinyu512@gmail.com>
2022-12-08 11:36:04 +0800

    sink(ticdc): Add DDL query into `schema.json` file (#7834)
    ref pingcap/tiflow#6797

--- cdc/sinkv2/ddlsink/cloudstorage/cloud_storage_ddl_sink.go
+++ cdc/sinkv2/ddlsink/cloudstorage/cloud_storage_ddl_sink.go
@@ -68,24 +68,24 @@ func NewCloudStorageDDLSink(ctx context.Context, sinkURI *url.URL) (*ddlSink, er
 	return d, nil
 }
 
-func (d *ddlSink) generateSchemaPath(def cloudstorage.TableDetail) string {
-	return fmt.Sprintf("%s/%s/%d/schema.json", def.Schema, def.Table, def.Version)
+func generateSchemaPath(def cloudstorage.TableDefinition) string {
+	return fmt.Sprintf("%s/%s/%d/schema.json", def.Schema, def.Table, def.TableVersion)
 }
 
 func (d *ddlSink) WriteDDLEvent(ctx context.Context, ddl *model.DDLEvent) error {
-	var def cloudstorage.TableDetail
+	var def cloudstorage.TableDefinition
 
 	if ddl.TableInfo.TableInfo == nil {
 		return nil
 	}
 
-	def.FromTableInfo(ddl.TableInfo)
+	def.FromDDLEvent(ddl)
 	encodedDef, err := json.MarshalIndent(def, "", "    ")
 	if err != nil {
 		return errors.Trace(err)
 	}
 
-	path := d.generateSchemaPath(def)
+	path := generateSchemaPath(def)
 	err = d.statistics.RecordDDLExecution(func() error {
 		err1 := d.storage.WriteFile(ctx, path, encodedDef)
 		if err1 != nil {
