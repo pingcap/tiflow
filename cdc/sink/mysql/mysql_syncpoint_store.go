285b37dc7e76b4b653891eafc68c09e3e557ee5e
dongmen <414110582@qq.com>
2022-12-14 16:59:25 +0800

    fix error

--- cdc/sink/mysql/mysql_syncpoint_store.go
+++ cdc/sink/mysql/mysql_syncpoint_store.go
@@ -297,7 +297,7 @@ func (s *mysqlSyncPointStore) setTidbEnableExternalTsReadOff(
 ) error {
 	// We should always try to set this variable, and ignore the error if
 	// downstream does not support this variable, it is by design.
-	query := fmt.Sprintf("SET SESSION %s = %d", "tidb_enable_external_ts_read", "off")
+	query := fmt.Sprintf("SET SESSION %s = %s", "tidb_enable_external_ts_read", "off")
 	_, err := txn.ExecContext(ctx, query)
 	if err != nil {
 		if mysqlErr, ok := errors.Cause(err).(*gmysql.MySQLError); ok &&
