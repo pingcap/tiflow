434f99417a7a33ba54c292a4393766e29795513a
qupeng <qupeng@pingcap.com>
2023-01-17 15:41:41 +0800

    change default memory quota from 256M to 2GB.
    Signed-off-by: qupeng <qupeng@pingcap.com>

--- cdc/model/changefeed_test.go
+++ cdc/model/changefeed_test.go
@@ -145,7 +145,7 @@ func TestVerifyAndComplete(t *testing.T) {
 		SinkURI: "blackhole://",
 		StartTs: 417257993615179777,
 		Config: &config.ReplicaConfig{
-			MemoryQuota:        268435456,
+			MemoryQuota:        2147483648,
 			CaseSensitive:      true,
 			EnableOldValue:     true,
 			CheckGCSafePoint:   true,
