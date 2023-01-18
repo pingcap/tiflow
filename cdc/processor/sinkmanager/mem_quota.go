f01be6f5bcece171d58607fd8e197befecf00607
qupeng <qupeng@pingcap.com>
2023-01-18 09:01:48 +0800

    sinkv2(cdc): seperate redo and sink memory quota (#8068)
    close pingcap/tiflow#8080

--- cdc/processor/sinkmanager/mem_quota.go
+++ cdc/processor/sinkmanager/mem_quota.go
@@ -53,14 +53,14 @@ type memQuota struct {
 	metricUsed  prometheus.Gauge
 }
 
-func newMemQuota(changefeedID model.ChangeFeedID, totalBytes uint64) *memQuota {
+func newMemQuota(changefeedID model.ChangeFeedID, totalBytes uint64, comp string) *memQuota {
 	m := &memQuota{
 		changefeedID: changefeedID,
 		totalBytes:   totalBytes,
 		usedBytes:    0,
 		tableMemory:  spanz.NewHashMap[[]*memConsumeRecord](),
-		metricTotal:  MemoryQuota.WithLabelValues(changefeedID.Namespace, changefeedID.ID, "total"),
-		metricUsed:   MemoryQuota.WithLabelValues(changefeedID.Namespace, changefeedID.ID, "used"),
+		metricTotal:  MemoryQuota.WithLabelValues(changefeedID.Namespace, changefeedID.ID, "total", comp),
+		metricUsed:   MemoryQuota.WithLabelValues(changefeedID.Namespace, changefeedID.ID, "used", comp),
 	}
 	m.blockAcquireCond = sync.NewCond(&m.mu)
 	m.metricTotal.Set(float64(totalBytes))
--- pkg/config/server_config.go
+++ pkg/config/server_config.go
@@ -48,7 +48,7 @@ const (
 	DefaultTableMemoryQuota = 10 * 1024 * 1024 // 10 MB
 
 	// DefaultChangefeedMemoryQuota is the default memory quota for each changefeed.
-	DefaultChangefeedMemoryQuota = 256 * 1024 * 1024 // 256MB.
+	DefaultChangefeedMemoryQuota = 1024 * 1024 * 1024 // 1GB.
 )
 
 var (
