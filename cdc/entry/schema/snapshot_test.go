5a38ed9a89e2a8b0d65dc5d04bbd6a10a807f8b3
dongmen <20351731+asddongmen@users.noreply.github.com>
2023-02-08 10:35:58 +0800

    *(all): update dependencies on tidb and pd. (#8126)
    close pingcap/tiflow#8110, ref pingcap/tiflow#8115

--- cdc/entry/schema/snapshot_test.go
+++ cdc/entry/schema/snapshot_test.go
@@ -199,9 +199,10 @@ func TestTable(t *testing.T) {
 			require.False(t, snap.IsIneligibleTableID(12))
 			require.False(t, snap.IsIneligibleTableID(12+65536))
 		}
-
 		// IterTables should get no available tables.
-		require.Equal(t, snap.TableCount(true), 0)
+		require.Equal(t, snap.TableCount(true, func(table, schema string) bool {
+			return true
+		}), 0)
 	}
 }
 
