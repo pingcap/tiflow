ae12f82ade7d28d0d5cf63efcf7163c8724b0f3e
qupeng <qupeng@pingcap.com>
2023-02-08 16:27:58 +0800

    *(ticdc): fix some major problems about pull-based-sink (#8179)
    close pingcap/tiflow#8103

--- cdc/processor/memquota/mem_quota.go
+++ cdc/processor/memquota/mem_quota.go
@@ -11,12 +11,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package sinkmanager
+package memquota
 
 import (
 	"sort"
 	"sync"
 	"sync/atomic"
+	"time"
 
 	"github.com/pingcap/log"
 	"github.com/pingcap/tiflow/cdc/model"
@@ -32,37 +33,48 @@ type memConsumeRecord struct {
 	size       uint64
 }
 
-type memQuota struct {
+// MemQuota is used to trace memory usage.
+type MemQuota struct {
 	changefeedID model.ChangeFeedID
 	// totalBytes is the total memory quota for one changefeed.
 	totalBytes uint64
 
-	// blockAcquireCond is used to notify the blocked acquire.
-	blockAcquireCond *sync.Cond
-
-	// mu protects the following fields.
-	mu sync.Mutex
 	// usedBytes is the memory usage of one changefeed.
-	usedBytes uint64
-	// tableMemory is the memory usage of each table.
-	tableMemory *spanz.HashMap[[]*memConsumeRecord]
+	usedBytes atomic.Uint64
+
 	// isClosed is used to indicate whether the mem quota is closed.
 	isClosed atomic.Bool
 
+	// To close the background metrics goroutine.
+	wg      sync.WaitGroup
+	closeBg chan struct{}
+
+	// blockAcquireCond is used to notify the blocked acquire.
+	blockAcquireCond *sync.Cond
+	// condMu protects nothing, but sync.Cond needs a mutex.
+	condMu sync.Mutex
+
 	metricTotal prometheus.Gauge
 	metricUsed  prometheus.Gauge
+
+	// mu protects the following fields.
+	mu sync.Mutex
+	// tableMemory is the memory usage of each table.
+	tableMemory *spanz.HashMap[[]*memConsumeRecord]
 }
 
-func newMemQuota(changefeedID model.ChangeFeedID, totalBytes uint64, comp string) *memQuota {
-	m := &memQuota{
+// NewMemQuota creates a MemQuota instance.
+func NewMemQuota(changefeedID model.ChangeFeedID, totalBytes uint64, comp string) *MemQuota {
+	m := &MemQuota{
 		changefeedID: changefeedID,
 		totalBytes:   totalBytes,
-		usedBytes:    0,
-		tableMemory:  spanz.NewHashMap[[]*memConsumeRecord](),
 		metricTotal:  MemoryQuota.WithLabelValues(changefeedID.Namespace, changefeedID.ID, "total", comp),
 		metricUsed:   MemoryQuota.WithLabelValues(changefeedID.Namespace, changefeedID.ID, "used", comp),
+		closeBg:      make(chan struct{}, 1),
+
+		tableMemory: spanz.NewHashMap[[]*memConsumeRecord](),
 	}
-	m.blockAcquireCond = sync.NewCond(&m.mu)
+	m.blockAcquireCond = sync.NewCond(&m.condMu)
 	m.metricTotal.Set(float64(totalBytes))
 	m.metricUsed.Set(float64(0))
 
@@ -70,73 +82,86 @@ func newMemQuota(changefeedID model.ChangeFeedID, totalBytes uint64, comp string
 		zap.String("namespace", changefeedID.Namespace),
 		zap.String("changefeed", changefeedID.ID),
 		zap.Uint64("total", totalBytes))
+
+	m.wg.Add(1)
+	go func() {
+		timer := time.NewTicker(3 * time.Second)
+		defer timer.Stop()
+		for {
+			select {
+			case <-timer.C:
+				m.metricUsed.Set(float64(m.usedBytes.Load()))
+			case <-m.closeBg:
+				m.metricUsed.Set(0.0)
+				m.wg.Done()
+				return
+			}
+		}
+	}()
 	return m
 }
 
-// tryAcquire returns true if the memory quota is available, otherwise returns false.
-func (m *memQuota) tryAcquire(nBytes uint64) bool {
-	m.mu.Lock()
-	defer m.mu.Unlock()
-	if m.usedBytes+nBytes > m.totalBytes {
-		return false
+// TryAcquire returns true if the memory quota is available, otherwise returns false.
+func (m *MemQuota) TryAcquire(nBytes uint64) bool {
+	for {
+		usedBytes := m.usedBytes.Load()
+		if usedBytes+nBytes > m.totalBytes {
+			return false
+		}
+		if m.usedBytes.CompareAndSwap(usedBytes, usedBytes+nBytes) {
+			return true
+		}
 	}
-	m.usedBytes += nBytes
-	m.metricUsed.Set(float64(m.usedBytes))
-	return true
 }
 
-// forceAcquire is used to force acquire the memory quota.
-func (m *memQuota) forceAcquire(nBytes uint64) {
-	m.mu.Lock()
-	defer m.mu.Unlock()
-	m.usedBytes += nBytes
-	m.metricUsed.Set(float64(m.usedBytes))
+// ForceAcquire is used to force acquire the memory quota.
+func (m *MemQuota) ForceAcquire(nBytes uint64) {
+	m.usedBytes.Add(nBytes)
 }
 
-// blockAcquire is used to block the request when the memory quota is not available.
-func (m *memQuota) blockAcquire(nBytes uint64) error {
-	m.mu.Lock()
-	defer m.mu.Unlock()
+// BlockAcquire is used to block the request when the memory quota is not available.
+func (m *MemQuota) BlockAcquire(nBytes uint64) error {
 	for {
 		if m.isClosed.Load() {
 			return cerrors.ErrFlowControllerAborted.GenWithStackByArgs()
 		}
-
-		if m.usedBytes+nBytes <= m.totalBytes {
-			m.usedBytes += nBytes
-			m.metricUsed.Set(float64(m.usedBytes))
+		usedBytes := m.usedBytes.Load()
+		if usedBytes+nBytes > m.totalBytes {
+			m.condMu.Lock()
+			m.blockAcquireCond.Wait()
+			m.condMu.Unlock()
+			continue
+		}
+		if m.usedBytes.CompareAndSwap(usedBytes, usedBytes+nBytes) {
 			return nil
 		}
-		m.blockAcquireCond.Wait()
 	}
 }
 
-// refund directly release the memory quota.
-func (m *memQuota) refund(nBytes uint64) {
+// Refund directly release the memory quota.
+func (m *MemQuota) Refund(nBytes uint64) {
 	if nBytes == 0 {
 		return
 	}
-	m.mu.Lock()
-	defer m.mu.Unlock()
-	if m.usedBytes < nBytes {
-		log.Panic("memQuota.refund fail",
-			zap.Uint64("used", m.usedBytes), zap.Uint64("refund", nBytes))
+	usedBytes := m.usedBytes.Load()
+	if usedBytes < nBytes {
+		log.Panic("MemQuota.refund fail",
+			zap.Uint64("used", usedBytes), zap.Uint64("refund", nBytes))
 	}
-	m.usedBytes -= nBytes
-	m.metricUsed.Set(float64(m.usedBytes))
-	if m.usedBytes < m.totalBytes {
+	if m.usedBytes.Add(^(nBytes - 1)) < m.totalBytes {
 		m.blockAcquireCond.Broadcast()
 	}
 }
 
-func (m *memQuota) addTable(span tablepb.Span) {
+// AddTable adds a table into the quota.
+func (m *MemQuota) AddTable(span tablepb.Span) {
 	m.mu.Lock()
 	defer m.mu.Unlock()
 	m.tableMemory.ReplaceOrInsert(span, make([]*memConsumeRecord, 0, 2))
 }
 
-// record records the memory usage of a table.
-func (m *memQuota) record(span tablepb.Span, resolved model.ResolvedTs, nBytes uint64) {
+// Record records the memory usage of a table.
+func (m *MemQuota) Record(span tablepb.Span, resolved model.ResolvedTs, nBytes uint64) {
 	if nBytes == 0 {
 		return
 	}
@@ -144,13 +169,12 @@ func (m *memQuota) record(span tablepb.Span, resolved model.ResolvedTs, nBytes u
 	defer m.mu.Unlock()
 	if _, ok := m.tableMemory.Get(span); !ok {
 		// Can't find the table record, the table must be removed.
-		if m.usedBytes < nBytes {
-			log.Panic("memQuota.refund fail",
-				zap.Uint64("used", m.usedBytes), zap.Uint64("refund", nBytes))
+		usedBytes := m.usedBytes.Load()
+		if usedBytes < nBytes {
+			log.Panic("MemQuota.refund fail",
+				zap.Uint64("used", usedBytes), zap.Uint64("refund", nBytes))
 		}
-		m.usedBytes -= nBytes
-		m.metricUsed.Set(float64(m.usedBytes))
-		if m.usedBytes < m.totalBytes {
+		if m.usedBytes.Add(^(nBytes - 1)) < m.totalBytes {
 			m.blockAcquireCond.Broadcast()
 		}
 		return
@@ -161,10 +185,10 @@ func (m *memQuota) record(span tablepb.Span, resolved model.ResolvedTs, nBytes u
 	}))
 }
 
-// release try to use resolvedTs to release the memory quota.
+// Release try to use resolvedTs to release the memory quota.
 // Because we append records in order, we can use binary search to find the first record
 // that is greater than resolvedTs, and release the memory quota of the records before it.
-func (m *memQuota) release(span tablepb.Span, resolved model.ResolvedTs) {
+func (m *MemQuota) Release(span tablepb.Span, resolved model.ResolvedTs) {
 	m.mu.Lock()
 	defer m.mu.Unlock()
 	if _, ok := m.tableMemory.Get(span); !ok {
@@ -185,20 +209,20 @@ func (m *memQuota) release(span tablepb.Span, resolved model.ResolvedTs) {
 	if toRelease == 0 {
 		return
 	}
-	if m.usedBytes < toRelease {
-		log.Panic("memQuota.release fail",
-			zap.Uint64("used", m.usedBytes), zap.Uint64("release", toRelease))
+
+	usedBytes := m.usedBytes.Load()
+	if usedBytes < toRelease {
+		log.Panic("MemQuota.release fail",
+			zap.Uint64("used", usedBytes), zap.Uint64("release", toRelease))
 	}
-	m.usedBytes -= toRelease
-	m.metricUsed.Set(float64(m.usedBytes))
-	if m.usedBytes < m.totalBytes {
+	if m.usedBytes.Add(^(toRelease - 1)) < m.totalBytes {
 		m.blockAcquireCond.Broadcast()
 	}
 }
 
-// clean all records of the table.
+// Clean all records of the table.
 // Return the cleaned memory quota.
-func (m *memQuota) clean(span tablepb.Span) uint64 {
+func (m *MemQuota) Clean(span tablepb.Span) uint64 {
 	m.mu.Lock()
 	defer m.mu.Unlock()
 
@@ -210,41 +234,35 @@ func (m *memQuota) clean(span tablepb.Span) uint64 {
 			zap.Stringer("span", &span))
 		return 0
 	}
+
 	cleaned := uint64(0)
 	records := m.tableMemory.GetV(span)
 	for _, record := range records {
 		cleaned += record.size
 	}
-	m.usedBytes -= cleaned
-	m.metricUsed.Set(float64(m.usedBytes))
 	m.tableMemory.Delete(span)
-	if m.usedBytes < m.totalBytes {
+
+	if m.usedBytes.Add(^(cleaned - 1)) < m.totalBytes {
 		m.blockAcquireCond.Broadcast()
 	}
 	return cleaned
 }
 
-// close the mem quota and notify the blocked acquire.
-func (m *memQuota) close() {
-	m.mu.Lock()
-	defer m.mu.Unlock()
-	// NOTE: m.usedBytes is not reset, because refund can still be called after closed.
-	m.tableMemory = spanz.NewHashMap[[]*memConsumeRecord]()
-	m.metricUsed.Set(float64(0))
-	m.isClosed.Store(true)
-	m.blockAcquireCond.Broadcast()
+// Close the mem quota and notify the blocked acquire.
+func (m *MemQuota) Close() {
+	if m.isClosed.CompareAndSwap(false, true) {
+		m.blockAcquireCond.Broadcast()
+		close(m.closeBg)
+		m.wg.Wait()
+	}
 }
 
-// getUsedBytes returns the used memory quota.
-func (m *memQuota) getUsedBytes() uint64 {
-	m.mu.Lock()
-	defer m.mu.Unlock()
-	return m.usedBytes
+// GetUsedBytes returns the used memory quota.
+func (m *MemQuota) GetUsedBytes() uint64 {
+	return m.usedBytes.Load()
 }
 
 // hasAvailable returns true if the memory quota is available, otherwise returns false.
-func (m *memQuota) hasAvailable(nBytes uint64) bool {
-	m.mu.Lock()
-	defer m.mu.Unlock()
-	return m.usedBytes+nBytes <= m.totalBytes
+func (m *MemQuota) hasAvailable(nBytes uint64) bool {
+	return m.usedBytes.Load()+nBytes <= m.totalBytes
 }
