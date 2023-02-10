e01b378f60f426f41e0a315acb25f98c479493a0
sdojjy <sdojjy@qq.com>
2023-02-10 15:31:26 +0800

    changefeed cli using open api v2

--- pkg/cmd/cli/cli_capture_list_test.go
+++ pkg/cmd/cli/cli_capture_list_test.go
@@ -20,28 +20,11 @@ import (
 	"github.com/golang/mock/gomock"
 	"github.com/pingcap/errors"
 	"github.com/pingcap/tiflow/cdc/model"
-	apiv1client "github.com/pingcap/tiflow/pkg/api/v1"
-	"github.com/pingcap/tiflow/pkg/api/v1/mock"
 	apiv2client "github.com/pingcap/tiflow/pkg/api/v2"
-	v2mock "github.com/pingcap/tiflow/pkg/api/v2/mock"
+	"github.com/pingcap/tiflow/pkg/api/v2/mock"
 	"github.com/pingcap/tiflow/pkg/cmd/factory"
 	"github.com/stretchr/testify/require"
 )
-
-type mockAPIV1Client struct {
-	apiv1client.APIV1Interface
-	changefeeds apiv1client.ChangefeedInterface
-	processor   apiv1client.ProcessorInterface
-	status      apiv2client.StatusInterface
-}
-
-func (f *mockAPIV1Client) Processors() apiv1client.ProcessorInterface {
-	return f.processor
-}
-
-func (f *mockAPIV1Client) Changefeeds() apiv1client.ChangefeedInterface {
-	return f.changefeeds
-}
 
 type mockAPIV2Client struct {
 	apiv2client.APIV2Interface
@@ -74,60 +57,46 @@ func (f *mockAPIV2Client) Processors() apiv2client.ProcessorInterface {
 
 type mockFactory struct {
 	factory.Factory
-	captures    *v2mock.MockCaptureInterface
+	captures    *mock.MockCaptureInterface
 	changefeeds *mock.MockChangefeedInterface
 	processor   *mock.MockProcessorInterface
-	status      *v2mock.MockStatusInterface
-
-	changefeedsv2 *v2mock.MockChangefeedInterface
-	tso           *v2mock.MockTsoInterface
-	unsafes       *v2mock.MockUnsafeInterface
-	processorsv2  *v2mock.MockProcessorInterface
+	status      *mock.MockStatusInterface
+	tso         *mock.MockTsoInterface
+	unsafes     *mock.MockUnsafeInterface
+	processors  *mock.MockProcessorInterface
 }
 
 func newMockFactory(ctrl *gomock.Controller) *mockFactory {
-	cps := v2mock.NewMockCaptureInterface(ctrl)
+	cps := mock.NewMockCaptureInterface(ctrl)
 	processor := mock.NewMockProcessorInterface(ctrl)
 	cf := mock.NewMockChangefeedInterface(ctrl)
-	status := v2mock.NewMockStatusInterface(ctrl)
-	unsafes := v2mock.NewMockUnsafeInterface(ctrl)
-	tso := v2mock.NewMockTsoInterface(ctrl)
-	cfv2 := v2mock.NewMockChangefeedInterface(ctrl)
-	processorsv2 := v2mock.NewMockProcessorInterface(ctrl)
+	statuses := mock.NewMockStatusInterface(ctrl)
+	unsafes := mock.NewMockUnsafeInterface(ctrl)
+	tso := mock.NewMockTsoInterface(ctrl)
 	return &mockFactory{
-		captures:      cps,
-		changefeeds:   cf,
-		processor:     processor,
-		status:        status,
-		changefeedsv2: cfv2,
-		tso:           tso,
-		unsafes:       unsafes,
-		processorsv2:  processorsv2,
+		captures:    cps,
+		changefeeds: cf,
+		processor:   processor,
+		status:      statuses,
+		tso:         tso,
+		unsafes:     unsafes,
 	}
-}
-
-func (f *mockFactory) APIV1Client() (apiv1client.APIV1Interface, error) {
-	return &mockAPIV1Client{
-		changefeeds: f.changefeeds,
-		status:      f.status,
-		processor:   f.processor,
-	}, nil
 }
 
 func (f *mockFactory) APIV2Client() (apiv2client.APIV2Interface, error) {
 	return &mockAPIV2Client{
 		captures:    f.captures,
-		changefeeds: f.changefeedsv2,
+		changefeeds: f.changefeeds,
 		tso:         f.tso,
 		unsafes:     f.unsafes,
-		processors:  f.processorsv2,
+		processors:  f.processors,
 	}, nil
 }
 
 func TestCaptureListCli(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
-	cf := v2mock.NewMockCaptureInterface(ctrl)
+	cf := mock.NewMockCaptureInterface(ctrl)
 	f := &mockFactory{captures: cf}
 	cmd := newCmdListCapture(f)
 	cf.EXPECT().List(gomock.Any()).Return([]model.Capture{
