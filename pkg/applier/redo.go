531a8eb6ed2ab863033c8c99c9f06a8e10b7c774
hi-rustin <rustin.liu@gmail.com>
2023-02-14 17:25:32 +0800

    *(ticdc): fix broken tests

--- pkg/applier/redo.go
+++ pkg/applier/redo.go
@@ -179,9 +179,7 @@ func (ra *RedoApplier) consumeLogs(ctx context.Context) error {
 			return err
 		}
 		// Make sure all events are flushed to downstream.
-		for ra.tableSinks[tableID].GetCheckpointTs().EqualOrGreater(resolvedTs) {
-			log.Info("resolved ts flushed to downstream", zap.Any("resolvedTs", resolvedTs))
-			break
+		for !ra.tableSinks[tableID].GetCheckpointTs().EqualOrGreater(resolvedTs) {
 		}
 		ra.tableSinks[tableID].Close(ctx)
 	}
