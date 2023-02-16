c8d52dd189cc805c7805e9361c0c9dd6270857d8
Chao Wang <cclcwangchao@hotmail.com>
2023-02-16 19:10:01 +0800

    update

--- tests/integration_tests/ddl_attributes/data/prepare.sql
+++ tests/integration_tests/ddl_attributes/data/prepare.sql
@@ -25,7 +25,6 @@ DROP PLACEMENT POLICY IF EXISTS placement1;
 CREATE PLACEMENT POLICY placement1 followers=2;
 CREATE TABLE placement_t1 (id BIGINT NOT NULL PRIMARY KEY auto_increment, b varchar(255)) PLACEMENT POLICY=placement1;
 CREATE TABLE `placement_t2` (id BIGINT NOT NULL PRIMARY KEY auto_increment) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin /*T![placement] PLACEMENT POLICY=`placement1` */;
-CREATE TABLE finish_mark(a INT PRIMARY KEY);
 
 DROP TABLE IF EXISTS ttl_t1, ttl_t2, ttl_t3, ttl_t4;
 CREATE TABLE ttl_t1(t datetime) TTL=`t` + INTERVAL 1 DAY;
@@ -36,3 +35,4 @@ ALTER TABLE ttl_t2 TTL=`t` + INTERVAL 1 DAY;
 ALTER TABLE ttl_t3 TTL_ENABLE='ON';
 ALTER TABLE ttl_t4 REMOVE TTL;
 
+CREATE TABLE finish_mark(a INT PRIMARY KEY);
