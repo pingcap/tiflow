3a2803d61206eb8a5416a87619b3a2fd4ca8a2d8
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2023-02-21 18:09:06 +0800

    redo(cdc): fix the bug that sink emits events post the barrier (#8314) (#8325)
    close pingcap/tiflow#8289

--- cdc/processor/sinkmanager/redo_cache_test.go
+++ cdc/processor/sinkmanager/redo_cache_test.go
@@ -27,44 +27,56 @@ func TestRedoEventCache(t *testing.T) {
 	var broken uint64
 	var popRes popResult
 
-	appender := cache.maybeCreateAppender(3, engine.Position{StartTs: 1, CommitTs: 2})
+	appender := cache.maybeCreateAppender(3, engine.Position{StartTs: 3, CommitTs: 4})
 
-	appender.push(&model.RowChangedEvent{StartTs: 1, CommitTs: 2}, 100, engine.Position{})
-	appender.push(&model.RowChangedEvent{StartTs: 1, CommitTs: 2}, 200, engine.Position{})
-	appender.push(&model.RowChangedEvent{StartTs: 3, CommitTs: 4}, 300, engine.Position{StartTs: 3, CommitTs: 4})
-	appender.push(&model.RowChangedEvent{StartTs: 5, CommitTs: 6}, 400, engine.Position{})
+	appender.push(&model.RowChangedEvent{StartTs: 3, CommitTs: 4}, 100, engine.Position{})
+	appender.push(&model.RowChangedEvent{StartTs: 3, CommitTs: 4}, 200, engine.Position{})
+	appender.push(&model.RowChangedEvent{StartTs: 5, CommitTs: 6}, 300, engine.Position{StartTs: 5, CommitTs: 6})
+	appender.push(&model.RowChangedEvent{StartTs: 7, CommitTs: 8}, 400, engine.Position{})
 	require.Equal(t, uint64(1000), cache.allocated)
 
-	ok, broken = appender.push(&model.RowChangedEvent{StartTs: 5, CommitTs: 6}, 400, engine.Position{StartTs: 3, CommitTs: 4})
+	ok, broken = appender.push(&model.RowChangedEvent{StartTs: 7, CommitTs: 8}, 400, engine.Position{StartTs: 7, CommitTs: 8})
 	require.False(t, ok)
 	require.Equal(t, uint64(400), broken)
 	require.True(t, appender.broken)
-	require.Equal(t, uint64(3), appender.upperBound.StartTs)
-	require.Equal(t, uint64(4), appender.upperBound.CommitTs)
+	require.Equal(t, uint64(5), appender.upperBound.StartTs)
+	require.Equal(t, uint64(6), appender.upperBound.CommitTs)
+	// Until here, cache contains events in range [{3,4}, {5,6}].
 
-	popRes = appender.pop(engine.Position{StartTs: 0, CommitTs: 1}, engine.Position{StartTs: 5, CommitTs: 6})
+	// Try to pop [{0,1}, {0,4}], shoud fail. And the returned boundary should be {1,4}.
+	popRes = appender.pop(engine.Position{StartTs: 0, CommitTs: 1}, engine.Position{StartTs: 0, CommitTs: 4})
 	require.False(t, popRes.success)
 	require.Equal(t, uint64(1), popRes.boundary.StartTs)
-	require.Equal(t, uint64(2), popRes.boundary.CommitTs)
+	require.Equal(t, uint64(4), popRes.boundary.CommitTs)
 
-	popRes = appender.pop(engine.Position{StartTs: 1, CommitTs: 2}, engine.Position{StartTs: 1, CommitTs: 2})
+	// Try to pop [{0,2}, {0,4}], shoud fail. And the returned boundary should be {3,4}.
+	popRes = appender.pop(engine.Position{StartTs: 0, CommitTs: 1}, engine.Position{StartTs: 5, CommitTs: 6})
+	require.False(t, popRes.success)
+	require.Equal(t, uint64(3), popRes.boundary.StartTs)
+	require.Equal(t, uint64(4), popRes.boundary.CommitTs)
+
+	// Try to pop [{3,4}, {3,4}], should success.
+	popRes = appender.pop(engine.Position{StartTs: 3, CommitTs: 4}, engine.Position{StartTs: 3, CommitTs: 4})
 	require.True(t, popRes.success)
 	require.Equal(t, 2, len(popRes.events))
 	require.Equal(t, uint64(300), popRes.size)
 	require.Equal(t, 2, popRes.pushCount)
-	require.Equal(t, uint64(1), popRes.boundary.StartTs)
-	require.Equal(t, uint64(2), popRes.boundary.CommitTs)
+	require.Equal(t, uint64(3), popRes.boundary.StartTs)
+	require.Equal(t, uint64(4), popRes.boundary.CommitTs)
 
-	popRes = appender.pop(engine.Position{StartTs: 1, CommitTs: 2}, engine.Position{StartTs: 1, CommitTs: 2})
+	// Try to pop [{3,4}, {3,4}] again, shoud fail. And the returned boundary should be {4,4}.
+	popRes = appender.pop(engine.Position{StartTs: 3, CommitTs: 4}, engine.Position{StartTs: 3, CommitTs: 4})
 	require.False(t, popRes.success)
+	require.Equal(t, uint64(4), popRes.boundary.StartTs)
+	require.Equal(t, uint64(4), popRes.boundary.CommitTs)
 
-	popRes = appender.pop(engine.Position{StartTs: 2, CommitTs: 2}, engine.Position{StartTs: 7, CommitTs: 8})
+	popRes = appender.pop(engine.Position{StartTs: 4, CommitTs: 4}, engine.Position{StartTs: 9, CommitTs: 10})
 	require.True(t, popRes.success)
 	require.Equal(t, 1, len(popRes.events))
 	require.Equal(t, uint64(300), popRes.size)
 	require.Equal(t, 1, popRes.pushCount)
-	require.Equal(t, uint64(3), popRes.boundary.StartTs)
-	require.Equal(t, uint64(4), popRes.boundary.CommitTs)
+	require.Equal(t, uint64(5), popRes.boundary.StartTs)
+	require.Equal(t, uint64(6), popRes.boundary.CommitTs)
 	require.Equal(t, 0, len(appender.events))
 	require.True(t, appender.broken)
 
