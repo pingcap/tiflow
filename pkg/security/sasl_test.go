aab7bdd855d7df241d262feb62a6c39a1f7b3d6a
D3Hunter <jujj603@gmail.com>
2023-02-23 17:37:19 +0800

    fix lint

--- pkg/security/sasl_test.go
+++ pkg/security/sasl_test.go
@@ -129,6 +129,7 @@ func TestAuthTypeFromString(t *testing.T) {
 	}
 
 	for _, test := range tests {
+		test := test
 		t.Run(test.name, func(t *testing.T) {
 			t.Parallel()
 
--- pkg/sink/kafka/sarama_test.go
+++ pkg/sink/kafka/sarama_test.go
@@ -201,6 +201,7 @@ func TestApplyTLS(t *testing.T) {
 	}
 
 	for _, test := range tests {
+		test := test
 		t.Run(test.name, func(t *testing.T) {
 			t.Parallel()
 			options := NewOptions()
