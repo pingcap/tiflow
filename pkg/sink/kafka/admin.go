71e9db498ee09fcbf9b62ce1394a54bac089cced
3AceShowHand <jinl1037@hotmail.com>
2023-02-28 15:21:00 +0800

    fix topic already exist.

--- pkg/sink/kafka/admin.go
+++ pkg/sink/kafka/admin.go
@@ -19,6 +19,7 @@ import (
 	"io"
 	"net"
 	"strconv"
+	"strings"
 	"sync"
 	"syscall"
 
@@ -293,7 +294,11 @@ func (a *saramaAdminClient) CreateTopic(
 		ReplicationFactor: detail.ReplicationFactor,
 	}
 	query := func() error {
-		return a.admin.CreateTopic(detail.Name, request, validateOnly)
+		err := a.admin.CreateTopic(detail.Name, request, validateOnly)
+		if err != nil && !strings.Contains(err.Error(), sarama.ErrTopicAlreadyExists.Error()) {
+			return err
+		}
+		return nil
 	}
 	return a.queryClusterWithRetry(ctx, query)
 }
