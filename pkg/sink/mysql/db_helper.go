287e25949936004c472ef28d8fdec80cdb09d73a
amyangfei <amyangfei@gmail.com>
2023-03-01 10:53:10 +0800

    Update pkg/sink/mysql/db_helper.go

--- pkg/sink/mysql/db_helper.go
+++ pkg/sink/mysql/db_helper.go
@@ -168,7 +168,6 @@ func generateDSNByConfig(
 		// set the `tidb_enable_external_ts_read` to `OFF`, so cdc could write to the sink
 		dsnCfg.Params["tidb_enable_external_ts_read"] = fmt.Sprintf(`"%s"`, tidbEnableExternalTSRead)
 	}
-
 	dsnClone := dsnCfg.Clone()
 	dsnClone.Passwd = "******"
 	log.Info("sink uri is configured", zap.String("dsn", dsnClone.FormatDSN()))
