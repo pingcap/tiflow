c56b786f4ecd77c976fb2167a4502d559c85ec5f
Jianyuan Jiang <sdojjy@qq.com>
2023-03-01 23:29:10 +0800

    apiv2(ticdc): remove engine field from changefeed config (#8397)
    close pingcap/tiflow#8398

--- cdc/api/v2/api_helpers.go
+++ cdc/api/v2/api_helpers.go
@@ -249,7 +249,6 @@ func (APIV2HelpersImpl) verifyCreateChangefeedConfig(
 		CreateTime:     time.Now(),
 		StartTs:        cfg.StartTs,
 		TargetTs:       cfg.TargetTs,
-		Engine:         cfg.Engine,
 		Config:         replicaCfg,
 		State:          model.StateNormal,
 		CreatorVersion: version.ReleaseVersion,
@@ -309,9 +308,6 @@ func (APIV2HelpersImpl) verifyUpdateChangefeedConfig(
 				cfg.TargetTs, newInfo.StartTs)
 		}
 		newInfo.TargetTs = cfg.TargetTs
-	}
-	if cfg.Engine != "" {
-		newInfo.Engine = cfg.Engine
 	}
 	if cfg.ReplicaConfig != nil {
 		configUpdated = true
