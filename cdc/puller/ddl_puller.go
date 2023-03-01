c1920ae10766abc36dec70a94ca5de1af285dfd7
dongmen <414110582@qq.com>
2023-03-01 11:15:08 +0800

    changefeed (ticdc): add ddl manager (#8343)
    ref pingcap/tiflow#8106
    
    (cherry picked from commit 34ad010f8311d90071646bccb1dfa29486c19aa4)

--- cdc/puller/ddl_puller.go
+++ cdc/puller/ddl_puller.go
@@ -503,6 +503,7 @@ type DDLPuller interface {
 	FrontDDL() (uint64, *timodel.Job)
 	// PopFrontDDL returns and pops the first DDL job in the internal queue
 	PopFrontDDL() (uint64, *timodel.Job)
+	ResolvedTs() uint64
 	// Close closes the DDLPuller
 	Close()
 }
@@ -676,4 +677,9 @@ func (h *ddlPullerImpl) Close() {
 		zap.String("namespace", h.changefeedID.Namespace),
 		zap.String("changefeed", h.changefeedID.ID))
 	h.cancel()
+}
+
+func (h *ddlPullerImpl) ResolvedTs() uint64 {
+	ts, _ := h.FrontDDL()
+	return ts
 }
