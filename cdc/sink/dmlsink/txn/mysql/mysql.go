4f3aaefa85e0b4aac849e5cc7aaab5fde66770c2
zhaoxinyu <zhaoxinyu512@gmail.com>
2023-03-02 23:19:21 +0800

    initial commit

--- cdc/sink/dmlsink/txn/mysql/mysql.go
+++ cdc/sink/dmlsink/txn/mysql/mysql.go
@@ -287,15 +287,15 @@ func convert2RowChanges(
 	return res
 }
 
-func convertBinaryToString(row *model.RowChangedEvent) {
-	for i, col := range row.Columns {
+func convertBinaryToString(cols []*model.Column) {
+	for i, col := range cols {
 		if col == nil {
 			continue
 		}
 		if col.Charset != "" && col.Charset != charset.CharsetBin {
 			colValBytes, ok := col.Value.([]byte)
 			if ok {
-				row.Columns[i].Value = string(colValBytes)
+				cols[i].Value = string(colValBytes)
 			}
 		}
 	}
@@ -316,7 +316,8 @@ func (s *mysqlBackend) groupRowsByType(
 	deleteRow := make([]*sqlmodel.RowChange, 0, preAllocateSize)
 
 	for _, row := range event.Event.Rows {
-		convertBinaryToString(row)
+		convertBinaryToString(row.Columns)
+		convertBinaryToString(row.PreColumns)
 
 		if row.IsInsert() {
 			insertRow = append(
