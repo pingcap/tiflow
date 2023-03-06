dc1d368d580c68feb6960411b7cb1dd714f4e605
3AceShowHand <jinl1037@hotmail.com>
2023-03-06 15:26:09 +0800

    add kafka-go metrics collector.

---
+++ pkg/sink/kafka/v2/metrics.go
@@ -0,0 +1,75 @@
+package v2
+
+import "github.com/prometheus/client_golang/prometheus"
+
+var (
+	batchDurationHistogram = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Namespace: "ticdc",
+			Subsystem: "sink",
+			Name:      "kafka_producer_batch_duration",
+			Help:      "Kafka client internal batch message time cost in milliseconds",
+			Buckets:   prometheus.ExponentialBuckets(0.004, 2.0, 10),
+		}, []string{"namespace", "changefeed"})
+
+	batchMessageCountHistogram = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Namespace: "ticdc",
+			Subsystem: "sink",
+			Name:      "kafka_producer_batch_message_count",
+			Help:      "Kafka client internal batch message count",
+			Buckets:   prometheus.ExponentialBuckets(8, 2.0, 11),
+		}, []string{"namespace", "changefeed"})
+
+	batchSizeHistogram = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Namespace: "ticdc",
+			Subsystem: "sink",
+			Name:      "kafka_producer_batch_size",
+			Help:      "Kafka client internal batch size in bytes",
+			Buckets:   prometheus.ExponentialBuckets(1024, 2.0, 18),
+		}, []string{"namespace", "changefeed"})
+
+	requestRateGauge = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Namespace: "ticdc",
+			Subsystem: "sink",
+			Name:      "kafka_producer_request_rate",
+			Help:      "Kafka Client Requests/second sent to all brokers.",
+		}, []string{"namespace", "changefeed"})
+	requestLatencyInMsGauge = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Namespace: "ticdc",
+			Subsystem: "sink",
+			Name:      "kafka_producer_request_latency",
+			Help:      "The request latency in ms",
+		}, []string{"namespace", "changefeed"})
+
+	retryCount = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Namespace: "ticdc",
+			Subsystem: "sink",
+			Name:      "kafka_producer_retry_count",
+			Help:      "Kafka Client send request retry count",
+		}, []string{"namespace", "changefeed"})
+
+	errCount = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Namespace: "ticdc",
+			Subsystem: "sink",
+			Name:      "kafka_producer_err_count",
+			Help:      "Kafka Client send request retry count",
+		}, []string{"namespace", "changefeed"})
+)
+
+func InitMetrics(registry *prometheus.Registry) {
+	registry.MustRegister(batchDurationHistogram)
+	registry.MustRegister(batchMessageCountHistogram)
+	registry.MustRegister(batchSizeHistogram)
+	registry.MustRegister(requestRateGauge)
+
+	registry.MustRegister(requestLatencyInMsGauge)
+
+	registry.MustRegister(retryCount)
+	registry.MustRegister(errCount)
+}
--- pkg/sink/kafka/v2/metrics_collector.go
+++ pkg/sink/kafka/v2/metrics_collector.go
@@ -15,32 +15,89 @@ package v2
 
 import (
 	"context"
+	"time"
 
 	"github.com/pingcap/tiflow/cdc/model"
-	"github.com/pingcap/tiflow/pkg/sink/kafka"
+	pkafka "github.com/pingcap/tiflow/pkg/sink/kafka"
 	"github.com/pingcap/tiflow/pkg/util"
+	"github.com/segmentio/kafka-go"
 )
 
 // MetricsCollector is the kafka metrics collector based on kafka-go library.
 type MetricsCollector struct {
 	changefeedID model.ChangeFeedID
 	role         util.Role
-	admin        kafka.ClusterAdminClient
+	admin        pkafka.ClusterAdminClient
+	writer       *kafka.Writer
 }
 
 // NewMetricsCollector return a kafka metrics collector
 func NewMetricsCollector(
 	changefeedID model.ChangeFeedID,
 	role util.Role,
-	admin kafka.ClusterAdminClient,
+	admin pkafka.ClusterAdminClient,
+	writer *kafka.Writer,
 ) *MetricsCollector {
 	return &MetricsCollector{
 		changefeedID: changefeedID,
 		role:         role,
 		admin:        admin,
+		writer:       writer,
 	}
 }
 
+// flushMetricsInterval specifies the interval of refresh sarama metrics.
+const flushMetricsInterval = 5 * time.Second
+
 // Run implement the MetricsCollector interface
 func (m *MetricsCollector) Run(ctx context.Context) {
+	ticker := time.NewTicker(flushMetricsInterval)
+	defer func() {
+		ticker.Stop()
+		m.cleanupMetrics()
+	}()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-ticker.C:
+			m.collectMetrics()
+		}
+	}
+}
+
+func (m *MetricsCollector) collectMetrics() {
+	statistics := m.writer.Stats()
+
+	// batch related metrics
+	batchDurationHistogram.WithLabelValues(m.changefeedID.Namespace, m.changefeedID.ID).
+		Observe(float64(statistics.BatchTime.Avg.Milliseconds()))
+	batchMessageCountHistogram.WithLabelValues(m.changefeedID.Namespace, m.changefeedID.ID).
+		Observe(float64(statistics.BatchSize.Avg))
+	batchSizeHistogram.WithLabelValues(m.changefeedID.Namespace, m.changefeedID.ID).
+		Observe(float64(statistics.BatchBytes.Avg))
+
+	// send batch related metrics
+	requestRateGauge.WithLabelValues(m.changefeedID.Namespace, m.changefeedID.ID).
+		Set(float64(statistics.Writes))
+	requestLatencyInMsGauge.WithLabelValues(m.changefeedID.Namespace, m.changefeedID.ID).
+		Set(float64(statistics.WriteTime.Avg.Milliseconds()))
+
+	retryCount.WithLabelValues(m.changefeedID.Namespace, m.changefeedID.ID).
+		Set(float64(statistics.Retries))
+	errCount.WithLabelValues(m.changefeedID.Namespace, m.changefeedID.ID).
+		Set(float64(statistics.Errors))
+}
+
+func (m *MetricsCollector) cleanupMetrics() {
+	batchDurationHistogram.DeleteLabelValues(m.changefeedID.Namespace, m.changefeedID.ID)
+	batchMessageCountHistogram.DeleteLabelValues(m.changefeedID.Namespace, m.changefeedID.ID)
+	batchSizeHistogram.DeleteLabelValues(m.changefeedID.Namespace, m.changefeedID.ID)
+
+	requestRateGauge.DeleteLabelValues(m.changefeedID.Namespace, m.changefeedID.ID)
+	requestLatencyInMsGauge.DeleteLabelValues(m.changefeedID.Namespace, m.changefeedID.ID)
+
+	retryCount.DeleteLabelValues(m.changefeedID.Namespace, m.changefeedID.ID)
+	errCount.DeleteLabelValues(m.changefeedID.Namespace, m.changefeedID.ID)
 }
