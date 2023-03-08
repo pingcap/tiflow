d20a289b17413889089b6b65f4ab79012dc73cf9
zhaoxinyu <zhaoxinyu512@gmail.com>
2023-03-08 23:35:05 +0800

    tune a script

--- tests/integration_tests/multi_tables_ddl/run.sh
+++ tests/integration_tests/multi_tables_ddl/run.sh
@@ -114,7 +114,7 @@ function run() {
 	check_changefeed_state "http://${UP_PD_HOST_1}:${UP_PD_PORT_1}" $cf_err1 "normal" "null" ""
 	check_changefeed_state "http://${UP_PD_HOST_1}:${UP_PD_PORT_1}" $cf_err2 "error" "ErrSyncRenameTableFailed" ""
 
-	check_sync_diff $WORK_DIR $CUR/conf/diff_config.toml 60 || sleep 3600
+	check_sync_diff $WORK_DIR $CUR/conf/diff_config.toml 60
 
 	cleanup_process $CDC_BINARY
 }
