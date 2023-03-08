f3575bad5770b911d90e3dce7350d44656821122
sdojjy <sdojjy@qq.com>
2023-03-08 20:39:50 +0800

    fix ut test

--- cdc/api/v2/status_test.go
+++ cdc/api/v2/status_test.go
@@ -39,6 +39,7 @@ func TestGetStatus(t *testing.T) {
 		GetClusterID().Return("1234").AnyTimes()
 	cp.EXPECT().GetEtcdClient().Return(etcdClient, nil).AnyTimes()
 	cp.EXPECT().IsReady().Return(true).AnyTimes()
+	cp.EXPECT().IsOwner().Return(true).AnyTimes()
 	cp.EXPECT().Liveness().Return(model.LivenessCaptureStopping).AnyTimes()
 	cp.EXPECT().Info().Return(model.CaptureInfo{
 		ID: "capture-id",
