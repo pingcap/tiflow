747c10f05e1131de620b23caeffca02855d2d40c
zhaoxinyu <zhaoxinyu512@gmail.com>
2023-03-09 23:21:33 +0800

    address comments

--- cmd/storage-consumer/main.go
+++ cmd/storage-consumer/main.go
@@ -371,7 +371,21 @@ func (c *consumer) getNewFiles(ctx context.Context) (map[dmlPathKey]fileIndexRan
 				// skip handling this file
 				return nil
 			}
-			// fake a dml key for schema.json file
+			// fake a dml key for schema.json file, which is useful for putting DDL
+			// in front of the DML files when sorting.
+			// e.g, for the partitioned table:
+			//
+			// test/test1/439972354120482843/schema.json					(partitionNum = -1)
+			// test/test1/439972354120482843/55/2023-03-09/CDC000001.csv	(partitionNum = 55)
+			// test/test1/439972354120482843/66/2023-03-09/CDC000001.csv	(partitionNum = 66)
+			//
+			// and for the non-partitioned table:
+			// test/test2/439972354120482843/schema.json				(partitionNum = -1)
+			// test/test2/439972354120482843/2023-03-09/CDC000001.csv	(partitionNum = 0)
+			// test/test2/439972354120482843/2023-03-09/CDC000002.csv	(partitionNum = 0)
+			//
+			// the DDL event recorded in schema.json should be executed first, then the DML events
+			// in csv files can be executed.
 			dmlkey.schemaPathKey = schemaKey
 			dmlkey.partitionNum = fakePartitionNumForSchemaFile
 			dmlkey.date = ""
