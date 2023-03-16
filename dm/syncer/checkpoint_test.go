1733a0e2587dfed800e602d90573b82f4acb4024
Yujie Xia <xiayjchn@gmail.com>
2023-03-17 07:24:39 +0800

    This is an automated cherry-pick of #8378
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- dm/syncer/checkpoint_test.go
+++ dm/syncer/checkpoint_test.go
@@ -22,6 +22,13 @@ import (
 	"testing"
 	"time"
 
+<<<<<<< HEAD
+=======
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/go-mysql-org/go-mysql/mysql"
+	. "github.com/pingcap/check"
+	"github.com/pingcap/log"
+>>>>>>> cd6f4e4248 (syncer(dm): discard checkpoint snapshot when syncer reset (#8378))
 	tidbddl "github.com/pingcap/tidb/ddl"
 	"github.com/pingcap/tidb/parser/ast"
 	"github.com/stretchr/testify/require"
@@ -178,8 +185,11 @@ func (s *testCheckpointSuite) testGlobalCheckPoint(c *C, cp CheckPoint) {
 	s.mock.ExpectBegin()
 	s.mock.ExpectExec("(162)?"+flushCheckPointSQL).WithArgs(cpid, "", "", pos1.Name, pos1.Pos, "", "", 0, "", "null", true).WillReturnResult(sqlmock.NewResult(0, 1))
 	s.mock.ExpectCommit()
-	err = cp.FlushPointsExcept(tctx, cp.Snapshot(true).id, nil, nil, nil)
-	c.Log(errors.ErrorStack(err))
+	// Create a new snapshot, and discard it, then create a new snapshot again.
+	cp.Snapshot(true)
+	cp.DiscardPendingSnapshots()
+	snap := cp.Snapshot(true)
+	err = cp.FlushPointsExcept(tctx, snap.id, nil, nil, nil)
 	c.Assert(err, IsNil)
 	c.Assert(cp.GlobalPoint().Position, Equals, pos1)
 	c.Assert(cp.FlushedGlobalPoint().Position, Equals, pos1)
@@ -278,7 +288,7 @@ func (s *testCheckpointSuite) testGlobalCheckPoint(c *C, cp CheckPoint) {
 
 	// should flush because checkpoint hasn't been updated before (cp.globalPointCheckOrSaveTime.IsZero() == true).
 	snapshot := cp.Snapshot(true)
-	c.Assert(snapshot.id, Equals, 3)
+	c.Assert(snapshot.id, Equals, 4)
 
 	s.mock.ExpectQuery(loadCheckPointSQL).WillReturnRows(sqlmock.NewRows(nil))
 	err = cp.Load(tctx)
