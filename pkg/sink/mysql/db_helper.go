9a718628406e8e1afd921cef931b5bccfc27f2a9
amyangfei <amyangfei@gmail.com>
2023-03-22 14:20:02 +0800

    revert

--- pkg/sink/mysql/db_helper.go
+++ pkg/sink/mysql/db_helper.go
@@ -341,7 +341,7 @@ func QueryMaxPreparedStmtCount(ctx context.Context, db *sql.DB) (int, error) {
 
 // QueryMaxAllowedPacket gets the value of max_allowed_packet
 func QueryMaxAllowedPacket(ctx context.Context, db *sql.DB) (int64, error) {
-	row := db.QueryRowContext(ctx, "select @@session.max_allowed_packet;")
+	row := db.QueryRowContext(ctx, "select @@global.max_allowed_packet;")
 	var maxAllowedPacket sql.NullInt64
 	if err := row.Scan(&maxAllowedPacket); err != nil {
 		return 0, cerror.WrapError(cerror.ErrMySQLQueryError, err)
