680de1d65f0ffd19884d3142c324d5a628f0dbfb
lance6716 <lance6716@gmail.com>
2023-03-31 16:58:55 +0800

    This is an automated cherry-pick of #8705
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- dm/tests/expression_filter/data/db1.prepare2.sql
+++ dm/tests/expression_filter/data/db1.prepare2.sql
@@ -9,4 +9,10 @@ create table t2 (id int primary key,
 );
 
 create table t6 (id int, name varchar(20), msg text, primary key(`id`)) character set latin1;
+<<<<<<< HEAD
 insert into t6 (id, name, msg) values (0, 'Müller', 'Müller');
+=======
+insert into t6 (id, name, msg) values (0, 'Müller…', 'Müller…');
+CREATE TABLE t7 (a BIGINT PRIMARY KEY, r VARCHAR(10), s INT);
+INSERT INTO t7 VALUES (1, 'a', 2);
+>>>>>>> fe2f347c52 (syncer(dm): fix wrong MySQL latin1 decoder (#8705))
