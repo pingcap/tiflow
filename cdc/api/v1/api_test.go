0787fab0bcb8ce9607af0957a9d1cc5a36300bc9
jiangjianyuan <sdojjy@qq.com>
2023-04-03 18:12:15 +0800

    return creator_version

--- cdc/api/v1/api_test.go
+++ cdc/api/v1/api_test.go
@@ -132,7 +132,9 @@ func newStatusProvider() *mockStatusProvider {
 		Return(map[model.CaptureID]*model.TaskStatus{captureID: {}}, nil)
 
 	statusProvider.On("GetChangeFeedInfo", mock.Anything).
-		Return(&model.ChangeFeedInfo{State: model.StateNormal}, nil)
+		Return(&model.ChangeFeedInfo{State: model.StateNormal,
+			CreatorVersion: "v6.5.1",
+		}, nil)
 
 	statusProvider.On("GetProcessors", mock.Anything).
 		Return([]*model.ProcInfoSnap{{CfID: changeFeedID, CaptureID: captureID}}, nil)
@@ -206,6 +208,7 @@ func TestGetChangefeed(t *testing.T) {
 	err := json.NewDecoder(w.Body).Decode(&resp)
 	require.Nil(t, err)
 	require.Equal(t, model.StateNormal, resp.FeedState)
+	require.Equal(t, "v6.5.1", resp.CreatorVersion)
 
 	// test get changefeed failed
 	api = testCase{
