465c560e70758bc5eff31ec2eba36177208da2b6
3AceShowHand <jinl1037@hotmail.com>
2023-04-06 18:35:32 +0800

    update tidb

--- cdc/owner/schema.go
+++ cdc/owner/schema.go
@@ -198,8 +198,6 @@ func (s *schemaWrap4Owner) parseRenameTables(
 			newSchemaName, preTableInfo,
 			model.WrapTableInfo(newSchemaIDs[i], newSchemaName,
 				job.BinlogInfo.FinishedTS, tableInfo))
-		event.Charset = job.Charset
-		event.Collate = job.Collate
 		ddlEvents = append(ddlEvents, event)
 	}
 
@@ -246,8 +244,6 @@ func (s *schemaWrap4Owner) BuildDDLEvents(
 			}
 		}
 		event.FromJob(job, preTableInfo, tableInfo)
-		event.Charset = job.Charset
-		event.Collate = job.Collate
 		ddlEvents = append(ddlEvents, event)
 	}
 	// filter out ddl here
