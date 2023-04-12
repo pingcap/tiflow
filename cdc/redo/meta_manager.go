f8167a6f0cbb11b48d5ebbaefec9698b14b92640
qupeng <qupeng@pingcap.com>
2023-04-12 21:35:02 +0800

    processor(cdc): seperate errors of sub components (#8754)
    ref pingcap/tiflow#8657

--- cdc/redo/meta_manager.go
+++ cdc/redo/meta_manager.go
@@ -156,6 +156,10 @@ func (m *metaManager) Run(ctx context.Context) error {
 	return eg.Wait()
 }
 
+func (m *metaManager) WaitForReady(_ context.Context) {}
+
+func (m *metaManager) Close() {}
+
 // UpdateMeta updates meta.
 func (m *metaManager) UpdateMeta(checkpointTs, resolvedTs model.Ts) {
 	if ok := m.metaResolvedTs.checkAndSetUnflushed(resolvedTs); !ok {
