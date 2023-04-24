917deb43160ea635926cf4bf0279400cbd020ed9
CharlesCheung <xiaoshuaizx@gmail.com>
2023-04-24 23:53:53 +0800

    fix tests

--- dm/tests/run_group.sh
+++ dm/tests/run_group.sh
@@ -22,9 +22,10 @@ groups=(
 	["G06"]="relay_interrupt safe_mode sequence_safe_mode"
 	["G07"]="shardddl1 shardddl1_1 shardddl2 shardddl2_1"
 	["G08"]="shardddl3 shardddl3_1 shardddl4 shardddl4_1 sharding sequence_sharding"
-	["G09"]="others others_2 others_3"
+	["G09"]="import_v10x sharding2 ha"
 	["G10"]="start_task print_status http_apis new_relay all_mode"
-	["G11"]="import_v10x sharding2 ha"
+	# `others others_2 others_3` tests of old pipeline
+	["G11"]="adjust_gtid async_checkpoint_flush binlog_parse case_sensitive checkpoint_transaction check_task dm_syncer downstream_diff_index downstream_more_column drop_column_with_index duplicate_event expression_filter extend_column fake_rotate_event foreign_key full_mode gbk gtid ha_cases http_proxies lightning_load_task lightning_mode metrics new_collation_off only_dml openapi s3_dumpling_lightning sequence_sharding_optimistic sequence_sharding_removemeta shardddl_optimistic slow_relay_writer sql_mode sync_collation tracker_ignored_ddl validator_basic validator_checkpoint"
 	["TLS_GROUP"]="tls"
 )
 
@@ -38,20 +39,21 @@ for script in "$CUR"/*/run.sh; do
 	fi
 done
 
-# Get test names
-test_names=""
-# shellcheck disable=SC2076
-if [[ "$group" == "others" ]]; then
-	test_names="${others[*]}"
+if [[ "$group" == "check others" ]]; then
+	if [[ -z $others ]]; then
+		echo "All engine integration test cases are added to groups"
+		exit 0
+	fi
+	echo "Error: "$others" is not added to any group in dm/tests/run_group.sh"
+	exit 1
 elif [[ " ${!groups[*]} " =~ " ${group} " ]]; then
 	test_names="${groups[${group}]}"
+	# Run test cases
+	if [[ -n $test_names ]]; then
+		echo "Run cases: ${test_names}"
+		"${CUR}"/run.sh "${test_names}"
+	fi
 else
 	echo "Error: invalid group name: ${group}"
 	exit 1
-fi
-
-# Run test cases
-if [[ -n $test_names ]]; then
-	echo "Run cases: ${test_names}"
-	"${CUR}"/run.sh "${test_names}"
 fi
