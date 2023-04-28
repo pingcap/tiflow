f4615b2377493e6f1696245ec498161dcdaeee39
hi-rustin <rustin.liu@gmail.com>
2023-04-28 16:03:14 +0800

    pkg/sink(ticdc): fix typo

--- pkg/sink/mysql/db_helper.go
+++ pkg/sink/mysql/db_helper.go
@@ -216,7 +216,7 @@ func checkTiDBVariable(ctx context.Context, db *sql.DB, variableName, defaultVal
 	var name string
 	var value string
 	// Some database may only support `select @@variable_name` to get the value of session variable.
-	// So we try to use `select @@variable_name` first, if it fails,
+	// So we try to use `select @variable_name` first, if it fails,
 	// we try to use `show session variables like '%s'` instead.
 	// SAFETY: variableName is a constant string, so it is safe to use it.
 	//nolint:gosec
