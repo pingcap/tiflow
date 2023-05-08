6620fd034b8e1a64c7d9791df41404ac62676a6e
CharlesCheung <xiaoshuaizx@gmail.com>
2023-05-08 15:09:15 +0800

    refactor storage sink path protocol

--- cdc/sink/ddlsink/cloudstorage/cloud_storage_ddl_sink_test.go
+++ cdc/sink/ddlsink/cloudstorage/cloud_storage_ddl_sink_test.go
@@ -63,12 +63,12 @@ func TestWriteDDLEvent(t *testing.T) {
 			},
 		},
 	}
-	tableDir := path.Join(parentDir, "test/table1/100")
-	os.MkdirAll(tableDir, 0o755)
+	tableDir := path.Join(parentDir, "test/table1/meta/")
+	// os.MkdirAll(tableDir, 0o755)
 	err = sink.WriteDDLEvent(ctx, ddlEvent)
 	require.Nil(t, err)
 
-	tableSchema, err := os.ReadFile(path.Join(tableDir, "schema.json"))
+	tableSchema, err := os.ReadFile(path.Join(tableDir, "schema_100_4192708364.json"))
 	require.Nil(t, err)
 	require.JSONEq(t, `{
 		"Table": "table1",
