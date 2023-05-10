689c4fb79f902bf40d89f6f8a05540e1aaaa618b
3AceShowHand <jinl1037@hotmail.com>
2023-05-10 17:07:15 +0800

    add more to the avro data sql

--- tests/integration_tests/avro_basic/data/data.sql
+++ tests/integration_tests/avro_basic/data/data.sql
@@ -118,6 +118,54 @@ insert into t values (
      '部署', "美国", "纽约", "世界,你好", 0xCAC0BDE7C4E3BAC3
 );
 
+update t set c_float = 3.1415, c_double = 2.7182, c_decimal = 8000, c_numeric = 179394.233 where id = 3;
+
+
+alter table t add column a double null;
+
+insert into t values (
+     5,
+     -128, -32768, -8388608, -2147483648, -9223372036854775808,
+     0, 0, 0, 0, 0,
+     -2.7182818284, -3.1415926, -8000, -179394.233,
+     3.1415, 2.7182, 8000, 179394.233,
+     '2020-02-20', '2020-02-20 02:20:20', '2020-02-20 02:20:20', '02:20:20', '2020',
+     '89504E470D0A1A0B', '89504E470D0A1A0B', '89504E470D0A1A0B', '89504E470D0A1A0B',
+     x'89504E470D0A1A0A', x'89504E470D0A1A0A', x'89504E470D0A1A0A', x'89504E470D0A1A0A',
+     '89504E470D0A1A0B', '89504E470D0A1A0B', x'89504E470D0A1A0B', x'89504E470D0A1A0B',
+     'b', 'b,c', b'1000001', '{
+"key1": "value1",
+"key2": "value2",
+"key3": "123"
+}',
+     '部署', "美国", "纽约", "世界,你好", 0xCAC0BDE7C4E3BAC3, 3.1415
+);
+
+alter table t modify c_float double null;
+insert into t values (
+     6,
+     -128, -32768, -8388608, -2147483648, -9223372036854775808,
+     0, 0, 0, 0, 0,
+     -2.7182818284, -3.1415926, -8000, -179394.233,
+     3.1415, 2.7182, 8000, 179394.233,
+     '2020-02-20', '2020-02-20 02:20:20', '2020-02-20 02:20:20', '02:20:20', '2020',
+     '89504E470D0A1A0B', '89504E470D0A1A0B', '89504E470D0A1A0B', '89504E470D0A1A0B',
+     x'89504E470D0A1A0A', x'89504E470D0A1A0A', x'89504E470D0A1A0A', x'89504E470D0A1A0A',
+     '89504E470D0A1A0B', '89504E470D0A1A0B', x'89504E470D0A1A0B', x'89504E470D0A1A0B',
+     'b', 'b,c', b'1000001', '{
+"key1": "value1",
+"key2": "value2",
+"key3": "123"
+}',
+     '部署', "美国", "纽约", "世界,你好", 0xCAC0BDE7C4E3BAC3, 3.1415
+ );
+
+alter table t drop column c_smallint;
+insert into tp_int(c_tinyint, c_mediumint, c_int, c_bigint, a) values (4, 5, 6, 7, 8);
+
+alter table t modify c_mediumint varchar(10) null;
+insert into t(c_tinyint, c_mediumint, c_int, c_bigint, a) values (5, "234", 6, 7, 8);
+
 create table finish_mark
 (
     id int PRIMARY KEY
