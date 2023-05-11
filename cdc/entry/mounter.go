c813bfeaa849c4d76eba65591dc32f4db16f52dd
3AceShowHand <jinl1037@hotmail.com>
2023-05-11 19:28:47 +0800

    set log level to debug.

--- cdc/entry/mounter.go
+++ cdc/entry/mounter.go
@@ -446,7 +446,7 @@ func (m *mounter) verifyChecksum(
 
 	// the first checksum matched, it hits in the most case.
 	if checksum == first {
-		log.Debug("checksum matched",
+		log.Info("checksum matched",
 			zap.Uint32("checksum", checksum), zap.Uint32("first", first))
 		return checksum, version, true, nil
 	}
