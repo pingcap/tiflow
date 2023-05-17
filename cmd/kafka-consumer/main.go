b6558501494a934a9b900d5613adb08c973bf813
CharlesCheung <xiaoshuaizx@gmail.com>
2023-05-17 11:04:17 +0800

    fix version

--- cmd/kafka-consumer/main.go
+++ cmd/kafka-consumer/main.go
@@ -51,6 +51,7 @@ import (
 	"github.com/pingcap/tiflow/pkg/sink/codec/open"
 	"github.com/pingcap/tiflow/pkg/spanz"
 	"github.com/pingcap/tiflow/pkg/util"
+	"github.com/pingcap/tiflow/pkg/version"
 	"github.com/prometheus/client_golang/prometheus"
 	"go.uber.org/zap"
 )
@@ -81,6 +82,7 @@ var (
 )
 
 func init() {
+	version.LogVersionInfo("kafka consumer")
 	var (
 		upstreamURIStr string
 		configFile     string
