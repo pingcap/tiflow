11202f207ce45479f45f8af2785abc068ea04eb4
CharlesCheung <61726649+CharlesCheung96@users.noreply.github.com>
2023-05-18 19:25:35 +0800

    owner, redo(ticdc): decouple globalResolvedTs and globalBarrierTs (#8964)
    close pingcap/tiflow#8963

--- cdc/model/owner.go
+++ cdc/model/owner.go
@@ -262,9 +262,9 @@ func (p ProcessorsInfos) String() string {
 type ChangeFeedStatus struct {
 	ResolvedTs   uint64 `json:"resolved-ts"`
 	CheckpointTs uint64 `json:"checkpoint-ts"`
-	// MinTableBarrierTs is the minimum table barrier timestamp of all tables.
-	// It is only used when a changefeed is started to check whether there was
-	// a table's DDL job that had not finished when the changefeed was stopped.
+	// minTableBarrierTs is the minimum commitTs of all DDL events and is only
+	// used to check whether there is a pending DDL job at the checkpointTs when
+	// initializing the changefeed.
 	MinTableBarrierTs uint64       `json:"min-table-barrier-ts"`
 	AdminJobType      AdminJobType `json:"admin-job-type"`
 }
