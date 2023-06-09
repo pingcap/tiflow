4037240743b65a540669f52778cb0a3dda5c6b87
CharlesCheung <xiaoshuaizx@gmail.com>
2023-06-09 09:21:27 +0800

    fix tests

--- tests/integration_tests/processor_err_chan/run.sh
+++ tests/integration_tests/processor_err_chan/run.sh
@@ -17,6 +17,7 @@ function run() {
 	TOPIC_NAME="ticdc-processor-err-chan-$RANDOM"
 	case $SINK_TYPE in
 	kafka) SINK_URI="kafka://127.0.0.1:9092/$TOPIC_NAME?protocol=open-protocol&partition-num=4&kafka-version=${KAFKA_VERSION}&max-message-bytes=10485760" ;;
+	storage) SINK_URI="file://$WORK_DIR/storage_test/$TOPIC_NAME?protocol=canal-json&enable-tidb-extension=true" ;;
 	*) SINK_URI="mysql://normal:123456@127.0.0.1:3306/?max-txn-row=1" ;;
 	esac
 
@@ -31,9 +32,10 @@ function run() {
 	run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY --addr "127.0.0.1:8300" --pd $pd_addr
 
 	changefeed_id=$(cdc cli changefeed create --pd=$pd_addr --sink-uri="$SINK_URI" 2>&1 | tail -n2 | head -n1 | awk '{print $2}')
-	if [ "$SINK_TYPE" == "kafka" ]; then
-		run_kafka_consumer $WORK_DIR "kafka://127.0.0.1:9092/$TOPIC_NAME?protocol=open-protocol&partition-num=4&version=${KAFKA_VERSION}&max-message-bytes=10485760"
-	fi
+	case $SINK_TYPE in
+	kafka) run_kafka_consumer $WORK_DIR "kafka://127.0.0.1:9092/$TOPIC_NAME?protocol=open-protocol&partition-num=4&version=${KAFKA_VERSION}&max-message-bytes=10485760" ;;
+	storage) run_storage_consumer $WORK_DIR $SINK_URI "" "" ;;
+	esac
 
 	retry_time=10
 	ensure $retry_time check_changefeed_state $pd_addr $changefeed_id "normal" "null" ""
--- tests/integration_tests/run_group.sh
+++ tests/integration_tests/run_group.sh
@@ -14,9 +14,6 @@ mysql_only="bdr_mode capture_suicide_while_balance_table syncpoint"
 mysql_only_http="http_api http_api_tls api_v2"
 mysql_only_consistent_replicate="consistent_replicate_ddl consistent_replicate_gbk consistent_replicate_nfs consistent_replicate_storage_file consistent_replicate_storage_file_large_value consistent_replicate_storage_s3"
 
-# Tests that need to support kafka: bank kill_owner_with_ddl owner_remove_table_error
-# owner_resign processor_etcd_worker_delay processor_resolved_ts_fallback
-# multi_changefeed clustered_index sink_hang
 kafka_only="kafka_big_messages kafka_compression kafka_messages kafka_sink_error_resume"
 kafka_only_protocol="canal_json_adapter_compatibility canal_json_basic multi_topics avro_basic"
 kafka_only_v2="kafka_big_txn_v2 kafka_big_messages_v2 multi_tables_ddl_v2 multi_topics_v2"
@@ -35,30 +32,29 @@ declare -A groups
 groups=(
 	# Note: only the tests in the first three groups are running in storage sink pipeline.
 	["G00"]="$mysql_only $kafka_only $storage_only"
-	["G01"]="$mysql_only_http $kafka_only_protocol $storage_only_canal_json"
+	["G01"]="$mysql_only_http $kafka_only_protocol $storage_only_canal_json multi_tables_ddl"
 	["G02"]="$mysql_only_consistent_replicate $kafka_only_v2 $storage_only_csv"
-	["G03"]='row_format drop_many_tables processor_stop_delay'
-	["G04"]='foreign_key ddl_puller_lag ddl_only_block_related_table'
-	["G05"]='partition_table changefeed_auto_stop'
-	["G06"]='charset_gbk owner_remove_table_error ddl_manager'
-	["G07"]='clustered_index multi_tables_ddl'
-	["G08"]='bank multi_source multi_capture'
-	["G09"]='ddl_reentrant multi_cdc_cluster'
-	["G10"]='sink_retry changefeed_error ddl_sequence'
-	["G11"]='kv_client_stream_reconnect cdc default_value'
-	["G12"]='changefeed_fast_fail tidb_mysql_test server_config_compatibility'
-	["G13"]='resourcecontrol processor_etcd_worker_delay'
-	["G14"]='batch_update_to_no_batch gc_safepoint changefeed_pause_resume'
-	["G15"]='cli simple cdc_server_tips changefeed_resume_with_checkpoint_ts'
-	["G16"]='processor_err_chan resolve_lock move_table autorandom'
-	["G17"]='ddl_attributes many_pk_or_uk capture_session_done_during_task'
-	["G18"]='tiflash new_ci_collation_without_old_value region_merge common_1'
-	["G19"]='split_region availability kill_owner_with_ddl'
-	["G20"]='changefeed_reconstruct http_proxies savepoint'
-	["G21"]='event_filter generate_column sequence processor_resolved_ts_fallback'
-	["G22"]='big_txn changefeed_finish sink_hang'
-	["G23"]='new_ci_collation_with_old_value batch_add_table multi_rocks'
-	["G24"]='owner_resign force_replicate_table multi_changefeed'
+	["G03"]='row_format drop_many_tables processor_stop_delay partition_table'
+	["G04"]='foreign_key ddl_puller_lag ddl_only_block_related_table changefeed_auto_stop'
+	["G05"]='charset_gbk ddl_manager multi_source multi_capture'
+	["G06"]='sink_retry changefeed_error ddl_sequence resourcecontrol'
+	["G07"]='kv_client_stream_reconnect cdc default_value split_region'
+	["G08"]='tidb_mysql_test server_config_compatibility changefeed_reconstruct'
+	["G09"]='gc_safepoint changefeed_pause_resume cli savepoint'
+	["G10"]='simple cdc_server_tips processor_err_chan event_filter'
+	["G11"]='resolve_lock move_table autorandom generate_column'
+	["G12"]='many_pk_or_uk capture_session_done_during_task ddl_attributes'
+	["G13"]='tiflash new_ci_collation_without_old_value region_merge common_1'
+	["G14"]='big_txn changefeed_finish force_replicate_table'
+	["G15"]='new_ci_collation_with_old_value batch_add_table multi_rocks'
+	# currently G16 is not running in kafka pipeline
+	["G16"]='owner_resign processor_etcd_worker_delay sink_hang'
+	["G17"]='clustered_index processor_resolved_ts_fallback'
+	# only run the following tests in mysql pipeline
+	["G18"]='availability http_proxies sequence'
+	["G19"]='changefeed_fast_fail batch_update_to_no_batch changefeed_resume_with_checkpoint_ts'
+	["G20"]='ddl_reentrant multi_cdc_cluster multi_changefeed'
+	["G21"]='bank kill_owner_with_ddl owner_remove_table_error'
 )
 
 # Get other cases not in groups, to avoid missing any case
