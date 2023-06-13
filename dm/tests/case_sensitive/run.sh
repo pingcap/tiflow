79b857312b71daf20400ba670b9240b825f802da
lance6716 <lance6716@gmail.com>
2023-06-13 21:11:22 +0800

    Update run.sh

--- dm/tests/case_sensitive/run.sh
+++ dm/tests/case_sensitive/run.sh
@@ -146,4 +146,5 @@ run_with_prepared
 
 cleanup_process $*
 
+run_sql_both_source "SET @@GLOBAL.SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION'"
 echo "[$(date)] <<<<<< test case $TEST_NAME success! >>>>>>"
