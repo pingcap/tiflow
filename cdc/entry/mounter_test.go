b80ad6e963029f0473b2da316d6b481ff52bf392
3AceShowHand <jinl1037@hotmail.com>
2023-06-19 18:26:18 +0800

    fix unit test.

--- cdc/entry/mounter_test.go
+++ cdc/entry/mounter_test.go
@@ -941,7 +941,7 @@ func TestGetDefaultZeroValue(t *testing.T) {
 			ColInfo: timodel.ColumnInfo{FieldType: *ftTypeEnumNotNull},
 			// TypeEnum value will be a string and then translate to []byte
 			// NotNull && no default will choose first element
-			Res:     uint64(0),
+			Res:     uint64(1),
 			Default: nil,
 		},
 		// mysql.TypeEnum + notnull + default
@@ -983,10 +983,7 @@ func TestGetDefaultZeroValue(t *testing.T) {
 	}
 
 	for _, tc := range testCases {
-		datum, val, _, _, _ := getDefaultOrZeroValue(&tc.ColInfo)
-		if datum.Kind() == types.KindMysqlEnum {
-			require.Equal(t, uint64(1), datum.GetMysqlEnum().Value)
-		}
+		_, val, _, _, _ := getDefaultOrZeroValue(&tc.ColInfo)
 		require.Equal(t, tc.Res, val, tc.Name)
 		val = getDDLDefaultDefinition(&tc.ColInfo)
 		require.Equal(t, tc.Default, val, tc.Name)
