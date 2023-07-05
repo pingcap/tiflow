8d392cbf7ff718f980d0e782ad3a9561be20eb59
dongmen <414110582@qq.com>
2023-07-05 14:22:54 +0800

    fix ut error

--- cdc/puller/puller_test.go
+++ cdc/puller/puller_test.go
@@ -134,7 +134,7 @@ func newPullerForTest(
 		ctx, pdCli, grpcPool, regionCache, store, pdutil.NewClock4Test(),
 		checkpointTs, spans, config.GetDefaultServerConfig().KVClient,
 		model.DefaultChangeFeedID("changefeed-id-test"), 0,
-		"table-test", false)
+		"table-test", false, false)
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
