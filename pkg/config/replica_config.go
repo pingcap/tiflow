3a80fe3daea675f32065ccb297766ca226838eac
CharlesCheung <xiaoshuaizx@gmail.com>
2023-07-10 10:22:14 +0800

    set default to hex for testing

--- pkg/config/replica_config.go
+++ pkg/config/replica_config.go
@@ -56,7 +56,7 @@ var defaultReplicaConfig = &ReplicaConfig{
 			Quote:                string(DoubleQuoteChar),
 			Delimiter:            Comma,
 			NullString:           NULL,
-			BinaryEncodingMethod: "base64",
+			BinaryEncodingMethod: "hex",
 		},
 		EncoderConcurrency:       16,
 		Terminator:               CRLF,
