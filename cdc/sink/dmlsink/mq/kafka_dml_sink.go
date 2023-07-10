9f2bff9090a5d10f7388607512ae6312682c929d
3AceShowHand <jinl1037@hotmail.com>
2023-07-10 18:01:01 +0800

    adjust kafka ddl sink

--- cdc/sink/dmlsink/mq/kafka_dml_sink.go
+++ cdc/sink/dmlsink/mq/kafka_dml_sink.go
@@ -112,6 +112,11 @@ func NewKafkaDMLSink(
 	if err != nil {
 		return nil, cerror.WrapError(cerror.ErrKafkaNewProducer, err)
 	}
+	defer func() {
+		if err != nil && asyncProducer != nil {
+			asyncProducer.Close()
+		}
+	}()
 
 	metricsCollector := factory.MetricsCollector(tiflowutil.RoleProcessor, adminClient)
 	dmlProducer := producerCreator(ctx, changefeedID, asyncProducer, metricsCollector, errCh, failpointCh)
