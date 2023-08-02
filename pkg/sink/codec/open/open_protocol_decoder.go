e67605130f634ac5f25cfe780673256aad3cc954
3AceShowHand <jinl1037@hotmail.com>
2023-08-02 15:30:14 +0800

    refactor open protocol decoder.

--- pkg/sink/codec/open/open_protocol_decoder.go
+++ pkg/sink/codec/open/open_protocol_decoder.go
@@ -19,6 +19,7 @@ import (
 	"encoding/binary"
 
 	"github.com/pingcap/errors"
+	"github.com/pingcap/log"
 	"github.com/pingcap/tidb/br/pkg/storage"
 	"github.com/pingcap/tiflow/cdc/model"
 	cerror "github.com/pingcap/tiflow/pkg/errors"
@@ -26,6 +27,7 @@ import (
 	"github.com/pingcap/tiflow/pkg/sink/codec/common"
 	"github.com/pingcap/tiflow/pkg/sink/codec/internal"
 	"github.com/pingcap/tiflow/pkg/util"
+	"go.uber.org/zap"
 	"golang.org/x/text/encoding"
 	"golang.org/x/text/encoding/charmap"
 )
@@ -35,12 +37,83 @@ type BatchDecoder struct {
 	keyBytes   []byte
 	valueBytes []byte
 	nextKey    *internal.MessageKey
-	nextKeyLen uint64
 
 	storage storage.ExternalStorage
 
 	upstreamTiDB *sql.DB
 	bytesDecoder *encoding.Decoder
+}
+
+// NewBatchDecoder creates a new BatchDecoder.
+func NewBatchDecoder(ctx context.Context, config *common.Config, db *sql.DB) (codec.RowEventDecoder, error) {
+	var (
+		storage storage.ExternalStorage
+		err     error
+	)
+	if config.LargeMessageHandle.EnableClaimCheck() {
+		storageURI := config.LargeMessageHandle.ClaimCheckStorageURI
+		storage, err = util.GetExternalStorageFromURI(ctx, storageURI)
+		if err != nil {
+			return nil, cerror.WrapError(cerror.ErrKafkaInvalidConfig, err)
+		}
+	}
+
+	if config.LargeMessageHandle.HandleKeyOnly() && db == nil {
+		return nil, cerror.ErrCodecDecode.
+			GenWithStack("handle-key-only is enabled, but upstream TiDB is not provided")
+	}
+
+	return &BatchDecoder{
+		storage:      storage,
+		upstreamTiDB: db,
+		bytesDecoder: charmap.ISO8859_1.NewDecoder(),
+	}, nil
+}
+
+// AddKeyValue implements the RowEventDecoder interface
+func (b *BatchDecoder) AddKeyValue(key, value []byte) error {
+	if len(b.keyBytes) != 0 || len(b.valueBytes) != 0 {
+		return cerror.ErrOpenProtocolCodecInvalidData.
+			GenWithStack("decoder key and value not nil")
+	}
+	version := binary.BigEndian.Uint64(key[:8])
+	key = key[8:]
+	if version != codec.BatchVersion1 {
+		return cerror.ErrOpenProtocolCodecInvalidData.
+			GenWithStack("unexpected key format version")
+	}
+
+	b.keyBytes = key
+	b.valueBytes = value
+
+	return nil
+}
+
+func (b *BatchDecoder) hasNext() bool {
+	keyLen := len(b.keyBytes)
+	valueLen := len(b.valueBytes)
+
+	if keyLen > 0 && valueLen > 0 {
+		return true
+	}
+
+	log.Warn("open-protocol meet invalid data",
+		zap.Int("keyLen", keyLen), zap.Int("valueLen", valueLen))
+	return false
+}
+
+func (b *BatchDecoder) decodeNextKey() error {
+	keyLen := binary.BigEndian.Uint64(b.keyBytes[:8])
+	key := b.keyBytes[8 : keyLen+8]
+	msgKey := new(internal.MessageKey)
+	err := msgKey.Decode(key)
+	if err != nil {
+		return errors.Trace(err)
+	}
+	b.nextKey = msgKey
+
+	b.keyBytes = b.keyBytes[keyLen+8:]
+	return nil
 }
 
 // HasNext implements the RowEventDecoder interface
@@ -56,19 +129,13 @@ func (b *BatchDecoder) HasNext() (model.MessageType, bool, error) {
 
 // NextResolvedEvent implements the RowEventDecoder interface
 func (b *BatchDecoder) NextResolvedEvent() (uint64, error) {
-	if b.nextKey == nil {
-		if err := b.decodeNextKey(); err != nil {
-			return 0, err
-		}
-	}
-	b.keyBytes = b.keyBytes[b.nextKeyLen+8:]
 	if b.nextKey.Type != model.MessageTypeResolved {
 		return 0, cerror.ErrOpenProtocolCodecInvalidData.GenWithStack("not found resolved event message")
 	}
-	valueLen := binary.BigEndian.Uint64(b.valueBytes[:8])
-	b.valueBytes = b.valueBytes[valueLen+8:]
 	resolvedTs := b.nextKey.Ts
 	b.nextKey = nil
+	// resolved ts event's value part is empty, can be ignored.
+	b.valueBytes = nil
 	return resolvedTs, nil
 }
 
@@ -240,67 +307,4 @@ func (b *BatchDecoder) NextDDLEvent() (*model.DDLEvent, error) {
 	ddlEvent := msgToDDLEvent(b.nextKey, ddlMsg)
 	b.nextKey = nil
 	return ddlEvent, nil
-}
-
-func (b *BatchDecoder) hasNext() bool {
-	return len(b.keyBytes) > 0 && len(b.valueBytes) > 0
-}
-
-func (b *BatchDecoder) decodeNextKey() error {
-	keyLen := binary.BigEndian.Uint64(b.keyBytes[:8])
-	key := b.keyBytes[8 : keyLen+8]
-	msgKey := new(internal.MessageKey)
-	err := msgKey.Decode(key)
-	if err != nil {
-		return errors.Trace(err)
-	}
-	b.nextKey = msgKey
-	b.nextKeyLen = keyLen
-	return nil
-}
-
-// NewBatchDecoder creates a new BatchDecoder.
-func NewBatchDecoder(ctx context.Context, config *common.Config, db *sql.DB) (codec.RowEventDecoder, error) {
-	var (
-		storage storage.ExternalStorage
-		err     error
-	)
-	if config.LargeMessageHandle.EnableClaimCheck() {
-		storageURI := config.LargeMessageHandle.ClaimCheckStorageURI
-		storage, err = util.GetExternalStorageFromURI(ctx, storageURI)
-		if err != nil {
-			return nil, cerror.WrapError(cerror.ErrKafkaInvalidConfig, err)
-		}
-	}
-
-	if config.LargeMessageHandle.HandleKeyOnly() && db == nil {
-		return nil, cerror.ErrCodecDecode.
-			GenWithStack("handle-key-only is enabled, but upstream TiDB is not provided")
-	}
-
-	return &BatchDecoder{
-		storage:      storage,
-		upstreamTiDB: db,
-		bytesDecoder: charmap.ISO8859_1.NewDecoder(),
-	}, nil
-
-}
-
-// AddKeyValue implements the RowEventDecoder interface
-func (b *BatchDecoder) AddKeyValue(key, value []byte) error {
-	if len(b.keyBytes) != 0 || len(b.valueBytes) != 0 {
-		return cerror.ErrOpenProtocolCodecInvalidData.
-			GenWithStack("decoder key and value not nil")
-	}
-	version := binary.BigEndian.Uint64(key[:8])
-	key = key[8:]
-	if version != codec.BatchVersion1 {
-		return cerror.ErrOpenProtocolCodecInvalidData.
-			GenWithStack("unexpected key format version")
-	}
-
-	b.keyBytes = key
-	b.valueBytes = value
-
-	return nil
 }
