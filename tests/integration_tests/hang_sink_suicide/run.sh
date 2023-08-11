520c799ccb8d87a71c5c6542f43c524c7b5d3269
qupeng <qupeng@pingcap.com>
2023-08-11 12:33:08 +0800

    improve the new test case
    Signed-off-by: qupeng <qupeng@pingcap.com>

--- tests/integration_tests/hang_sink_suicide/run.sh
+++ tests/integration_tests/hang_sink_suicide/run.sh
@@ -19,7 +19,7 @@ function run() {
 	cd $WORK_DIR
 
 	pd_addr="http://$UP_PD_HOST_1:$UP_PD_PORT_1"
-	export GO_FAILPOINTS='github.com/pingcap/tiflow/cdc/sink/dmlsink/txn/mysql/MySQLSinkHangLongTime=1*return(true)'
+	export GO_FAILPOINTS='github.com/pingcap/tiflow/cdc/sink/dmlsink/txn/mysql/MySQLSinkHangLongTime=2*return(true)'
 	run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY --pd $pd_addr --logsuffix 1 --addr "127.0.0.1:8300"
 
 	SINK_URI="mysql://normal:123456@127.0.0.1:3306/?max-txn-row=1"
