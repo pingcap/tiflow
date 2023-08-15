09cba78b343accdfdb9c9190c1cd5e2caecc248d
qupeng <qupeng@pingcap.com>
2023-08-15 16:46:38 +0800

    fix test
    Signed-off-by: qupeng <qupeng@pingcap.com>

--- tests/integration_tests/processor_err_chan/run.sh
+++ tests/integration_tests/processor_err_chan/run.sh
@@ -29,7 +29,7 @@ function run() {
 	done
 
 	export GO_FAILPOINTS='github.com/pingcap/tiflow/cdc/processor/sourcemanager/puller/ProcessorAddTableError=1*return(true)'
-	run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY --addr "127.0.0.1:8300" --pd $pd_addr
+	run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY --addr "127.0.0.1:8300" --pd $pd_addr --config $CUR/conf/server.toml
 
 	changefeed_id=$(cdc cli changefeed create --pd=$pd_addr --sink-uri="$SINK_URI" 2>&1 | tail -n2 | head -n1 | awk '{print $2}')
 	case $SINK_TYPE in
