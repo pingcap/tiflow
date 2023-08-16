cfb333c5ed7c9137393e4f544e60cd5c449db50c
dongmen <414110582@qq.com>
2023-08-16 13:35:13 +0800

    address comments

--- cdc/processor/sinkmanager/manager.go
+++ cdc/processor/sinkmanager/manager.go
@@ -338,8 +338,6 @@ func (m *SinkManager) initSinkFactory() (chan error, uint64) {
 		log.Info("failpoint SinkManagerRunError injected", zap.String("changefeed", m.changefeedID.ID))
 		err = errors.New("SinkManagerRunError")
 	})
-	time.Sleep(5 * time.Minute)
-	err = errors.New("SinkManagerRunError, for test only")
 	if err != nil {
 		emitError(err)
 		return m.sinkFactory.errors, m.sinkFactory.version
