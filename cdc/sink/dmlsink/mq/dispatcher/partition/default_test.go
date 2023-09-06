254cc2b18dab97be67c01e67ea92af3defa36c40
dongmen <20351731+asddongmen@users.noreply.github.com>
2023-09-06 15:51:43 +0800

    pulsar (ticdc): add partition key for pulsar meesage (#9677)
    ref pingcap/tiflow#9413

--- cdc/sink/dmlsink/mq/dispatcher/partition/default_test.go
+++ cdc/sink/dmlsink/mq/dispatcher/partition/default_test.go
@@ -38,6 +38,6 @@ func TestDefaultDispatcher(t *testing.T) {
 		IndexColumns: [][]int{{0}},
 	}
 
-	targetPartition := NewDefaultDispatcher().DispatchRowChangedEvent(row, 3)
+	targetPartition, _ := NewDefaultDispatcher().DispatchRowChangedEvent(row, 3)
 	require.Equal(t, int32(0), targetPartition)
 }
