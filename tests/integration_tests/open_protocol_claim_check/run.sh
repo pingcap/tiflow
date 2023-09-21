96bd66ef320dacdd4e1d645fdd5d049d60d3cdc3
3AceShowHand <jinl1037@hotmail.com>
2023-09-21 12:51:52 +0800

    add more to integration test.

--- tests/integration_tests/open_protocol_claim_check/run.sh
+++ tests/integration_tests/open_protocol_claim_check/run.sh
@@ -25,15 +25,19 @@ function run() {
 	# record tso before we create tables to skip the system table DDLs
 	start_ts=$(run_cdc_cli_tso_query ${UP_PD_HOST_1} ${UP_PD_PORT_1})
 
+	run_sql_file $CUR/data/data.sql ${UP_TIDB_HOST} ${UP_TIDB_PORT}
+
+  sleep 3
+
+  target_ts = $(run_cdc_cli_tso_query ${UP_PD_HOST_1} ${UP_PD_PORT_1})
+
 	run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY
 
 	SINK_URI="kafka://127.0.0.1:9092/$TOPIC_NAME?protocol=open-protocol&max-message-bytes=800&kafka-version=${KAFKA_VERSION}"
 
-	run_cdc_cli changefeed create --start-ts=$start_ts --sink-uri="$SINK_URI" --config="$CUR/conf/changefeed.toml"
+	run_cdc_cli changefeed create --start-ts=$start_ts --target-ts=$target_ts --sink-uri="$SINK_URI" --config="$CUR/conf/changefeed.toml"
 
 	cdc_kafka_consumer --upstream-uri $SINK_URI --downstream-uri="mysql://root@127.0.0.1:3306/?safe-mode=true&batch-dml-enable=false" --upstream-tidb-dsn="root@tcp(${UP_TIDB_HOST}:${UP_TIDB_PORT})/?" --config="$CUR/conf/changefeed.toml" 2>&1 &
-
-	run_sql_file $CUR/data/data.sql ${UP_TIDB_HOST} ${UP_TIDB_PORT}
 
 	# sync_diff can't check non-exist table, so we check expected tables are created in downstream first
 	check_table_exists test.finish_mark ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT} 200
