7ac086375bf72e83385753f14315ce0f0e990d6e
CharlesCheung <xiaoshuaizx@gmail.com>
2023-09-22 15:01:33 +0800

    add migrate test

--- cdcv2/metadata/sql/client_test.go
+++ cdcv2/metadata/sql/client_test.go
@@ -19,6 +19,7 @@ import (
 
 func TestUpstreamClient(t *testing.T) {
 	t.Parallel()
+
 }
 
 func TestChangefeedInfoClient(t *testing.T) {
--- cdcv2/metadata/sql/tables_test.go
+++ cdcv2/metadata/sql/tables_test.go
@@ -16,202 +16,67 @@ package sql
 import (
 	"database/sql"
 	"testing"
-	"time"
 
-	"github.com/pingcap/tiflow/cdcv2/metadata/sql/internal"
-	"github.com/pingcap/tiflow/engine/pkg/dbutil"
+	"github.com/DATA-DOG/go-sqlmock"
 	ormUtil "github.com/pingcap/tiflow/engine/pkg/orm"
-	"github.com/pingcap/tiflow/pkg/config"
-	"github.com/pingcap/tiflow/pkg/errors"
 	"github.com/stretchr/testify/require"
 	"gorm.io/gorm"
 )
 
-// only for test
-const TableNameElection = internal.TableNameElection
-
-type Election = internal.Election
-
-func initAndCleanupTablesForTest(t *testing.T, dsn string) (*sql.DB, *gorm.DB) {
-	backendDB, err := dbutil.NewSQLDB("mysql", dsn, nil)
+func newMockDB(t *testing.T) (*sql.DB, *gorm.DB, sqlmock.Sqlmock) {
+	backendDB, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
 	require.NoError(t, err)
 
+	mock.ExpectQuery("SELECT VERSION()").
+		WillReturnRows(sqlmock.NewRows([]string{"VERSION()"}).AddRow("5.7.35-log"))
 	db, err := ormUtil.NewGormDB(backendDB, "mysql")
 	require.NoError(t, err)
-
-	db.AutoMigrate(&Election{})
-	db.Exec("TRUNCATE " + TableNameElection)
-
-	AutoMigrate(db)
-	db.Exec("TRUNCATE " + TableNameUpstream)
-	db.Exec("TRUNCATE " + TableNameChangefeedInfo)
-	db.Exec("TRUNCATE " + TableNameChangefeedState)
-	db.Exec("TRUNCATE " + TableNameSchedule)
-	db.Exec("TRUNCATE " + TableNameProgress)
-
-	return backendDB, db
+	return backendDB, db, mock
 }
 
-func genMockData(t *testing.T, db *gorm.DB, captureNum int) {
-	election := internal.NewTestElectionRow(captureNum)
-	ret := db.Create(&election)
-	require.NoError(t, ret.Error)
-	require.Equal(t, int64(1), ret.RowsAffected)
-
-	ret = db.Create(&UpstreamDO{
-		ID:        1,
-		Endpoints: "localhost:2379",
-		Version:   1,
-		// UpdateAt:  time.Now(),
-	})
-	require.NoError(t, ret.Error)
-	require.Equal(t, int64(1), ret.RowsAffected)
-
-	c := ReplicaConfig(*config.GetDefaultReplicaConfig())
-	ret = db.Create(&ChangefeedInfoDO{
-		UUID:       1,
-		ID:         "changefeed-1",
-		Namespace:  "default",
-		UpstreamID: 1,
-		Config:     c,
-		Version:    1,
-		UpdateAt:   time.Now(),
-	})
-	require.NoError(t, ret.Error)
-	require.Equal(t, int64(1), ret.RowsAffected)
-
-	ret = db.Create(&ChangefeedStateDO{
-		ChangefeedUUID: 1,
-		Version:        1,
-		UpdateAt:       time.Now(),
-	})
-	require.NoError(t, ret.Error)
-	require.Equal(t, int64(1), ret.RowsAffected)
-
-	o := "capture-0"
-	ret = db.Create(&ScheduleDO{
-		ChangefeedUUID: 1,
-		Owner:          &o,
-		OwnerState:     "removed",
-		Processors:     &o,
-		TaskPosition:   &ChangefeedProgress{},
-		Version:        1,
-		UpdateAt:       time.Now(),
-	})
-	require.NoError(t, ret.Error)
-	require.Equal(t, int64(1), ret.RowsAffected)
-
-	ret = db.Create(&ProgressDO{
-		CaptureID: "capture-0",
-		Version:   1,
-		UpdateAt:  time.Now(),
-	})
-	require.NoError(t, ret.Error)
-	require.Equal(t, int64(1), ret.RowsAffected)
-}
-
-func TestMigrateTableBasicInsert(t *testing.T) {
+func TestAutoMigrateTables(t *testing.T) {
 	t.Parallel()
 
-	dsn := "root:123456@tcp(10.2.6.9:3306)/test_local?charset=utf8mb4&parseTime=true&loc=Local&multiStatements=true"
-	backendDB, db := initAndCleanupTablesForTest(t, dsn)
+	backendDB, db, mock := newMockDB(t)
 	defer backendDB.Close()
 
-	genMockData(t, db, 3)
-
-	ret := db.Model(&UpstreamDO{}).Where("id = ?", 1).Update("version", 2)
-	require.NoError(t, ret.Error)
-}
-
-func TestXxx(t *testing.T) {
-	t.Parallel()
-
-	dsn := "root:123456@tcp(10.2.6.9:3306)/test_local?charset=utf8mb4&parseTime=true&loc=Local&multiStatements=true"
-	db, err := dbutil.NewSQLDB("mysql", dsn, nil)
-	require.NoError(t, err)
-	defer db.Close()
-
-	ormDB, err := ormUtil.NewGormDB(db, "mysql")
-	if err != nil {
-		panic("failed to connect database")
+	mockSchmaFn := func(tableName string) {
+		mock.ExpectQuery("SELECT SCHEMA_NAME from Information_schema.SCHEMATA " +
+			"where SCHEMA_NAME LIKE ? ORDER BY SCHEMA_NAME=? DESC limit 1").WillReturnRows(
+			sqlmock.NewRows([]string{"SCHEMA_NAME"}))
 	}
 
-	_, err = ormDB.DB()
+	mockSchmaFn("upstream")
+	mock.ExpectExec("CREATE TABLE `upstream` (`id` bigint(20) unsigned,`endpoints` text NOT NULL," +
+		"`config` text,`version` bigint(20) unsigned NOT NULL,`update_at` datetime(6) NOT NULL," +
+		"PRIMARY KEY (`id`))").
+		WillReturnResult(sqlmock.NewResult(0, 0))
+
+	mockSchmaFn("changefeed_info")
+	mock.ExpectExec("CREATE TABLE `changefeed_info` (`uuid` bigint(20) unsigned,`namespace` varchar(128) NOT NULL," +
+		"`id` varchar(128) NOT NULL,`removed_at` datetime(6),`upstream_id` bigint(20) unsigned NOT NULL," +
+		"`sink_uri` text NOT NULL,`start_ts` bigint(20) unsigned NOT NULL,`target_ts` bigint(20) unsigned NOT NULL," +
+		"`config` longtext NOT NULL,`version` bigint(20) unsigned NOT NULL,`update_at` datetime(6) NOT NULL," +
+		"PRIMARY KEY (`uuid`),UNIQUE INDEX `namespace` (`namespace`,`id`),INDEX `upstream_id` (`upstream_id`))").
+		WillReturnResult(sqlmock.NewResult(0, 0))
+
+	mockSchmaFn("changefeed_state")
+	mock.ExpectExec("CREATE TABLE `changefeed_state` (`changefeed_uuid` bigint(20) unsigned,`state` text NOT NULL," +
+		"`warning` text,`error` text,`version` bigint(20) unsigned NOT NULL,`update_at` datetime(6) NOT NULL," +
+		"PRIMARY KEY (`changefeed_uuid`))").
+		WillReturnResult(sqlmock.NewResult(0, 0))
+
+	mockSchmaFn("changefeed_state")
+	mock.ExpectExec("CREATE TABLE `schedule` (`changefeed_uuid` bigint(20) unsigned,`owner` varchar(128)," +
+		"`owner_state` text NOT NULL,`processors` text,`task_position` text,`version` bigint(20) unsigned NOT NULL," +
+		"`update_at` datetime(6) NOT NULL,PRIMARY KEY (`changefeed_uuid`))").
+		WillReturnResult(sqlmock.NewResult(0, 0))
+
+	mockSchmaFn("progress")
+	mock.ExpectExec("CREATE TABLE `progress` (`capture_id` varchar(128),`progress` longtext," +
+		"`version` bigint(20) unsigned NOT NULL,`update_at` datetime(6) NOT NULL,PRIMARY KEY (`capture_id`))").
+		WillReturnResult(sqlmock.NewResult(0, 0))
+
+	err := AutoMigrate(db)
 	require.NoError(t, err)
-
-	ormDB.AutoMigrate(&Election{}, &ScheduleDO{})
-	ormDB.Exec("TRUNCATE " + TableNameElection)
-	ormDB.Exec("TRUNCATE " + TableNameSchedule)
-
-	ormDB.Create(&Election{ID: 1, Version: 1, LeaderID: "leader1"})
-	o := "owner1"
-	ormDB.Create(&ScheduleDO{
-		ChangefeedUUID: 1,
-		Version:        1,
-		Owner:          &o,
-		OwnerState:     "",
-		Processors:     &o,
-		TaskPosition:   &ChangefeedProgress{},
-	})
-
-	// ret, err := db.Exec("BEGIN;UPDATE schedule SET owner='owner11', owner_state='launched', processors='owner11' WHERE changefeed_uuid=1;COMMIT;")
-	// require.NoError(t, err)
-	// aft, err := ret.RowsAffected()
-	// require.NoError(t, err)
-	// require.Equal(t, int64(1), aft)
-
-	// var leaderID string
-	// ret := ormDB.Raw("SELECT leader_id FROM election WHERE leader_id = 'leader1' LOCK IN SHARE MODE")
-	// ret.Scan(&leaderID)
-	// require.Equal(t, "leader1", leaderID)
-
-	// seSTR := `BEGIN;
-	// SELECT 1 FROM election LOCK IN SHARE MODE;
-	// UPDATE schedule SET owner=?, owner_state='launched', processors=?
-	// 	WHERE changefeed_uuid=? AND EXISTS (
-	// 	  SELECT 1 FROM election WHERE election.leader_id = ?
-	// );
-	// COMMIT;`
-	// seSQL := ormDB.ToSQL(func(tx *gorm.DB) *gorm.DB {
-	// 	return tx.Raw(seSTR, "owner222", "owner232", 1, "leader1")
-	// })
-
-	// t.Error(seSQL)
-	// res := ormDB.Exec(seSQL)
-	// require.NoError(t, res.Error)
-	// require.Equal(t, int64(1), res.RowsAffected)
-
-	err = ormDB.Transaction(func(tx *gorm.DB) error {
-		var leaderID string
-		ret := tx.Raw("SELECT leader_id FROM election WHERE leader_id = 'leader1' LOCK IN SHARE MODE").Scan(&leaderID)
-		if ret.Error != nil {
-			return ret.Error
-		} else if ret.RowsAffected == 0 {
-			return errors.ErrNotController
-		}
-		if leaderID != "leader1" {
-			return errors.ErrNotController
-		}
-		time.Sleep(time.Hour)
-
-		/* orm */
-		tx.Table(TableNameSchedule).Where("changefeed_uuid = ?", 1).Updates(map[string]interface{}{
-			"owner":       "owner2",
-			"owner_state": "launched",
-			"processors":  "owner2",
-		})
-
-		// 	/* raw sql */
-		// 	ret = tx.Exec("UPDATE schedule SET owner=?, owner_state='launched', processors=? WHERE changefeed_uuid=?", "owner3", "owner3", 1)
-		require.Equal(t, int64(1), ret.RowsAffected)
-		return ret.Error
-	})
-	require.NoError(t, err)
-
-	// res, err := db.Exec(seSTR, "owner2", "owner2", 1, "leader1")
-	// require.NoError(t, err)
-
-	// aft, err := res.RowsAffected()
-	// require.NoError(t, err)
-	// require.Equal(t, int64(1), aft)
 }
