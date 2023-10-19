0f3d08670ef312c70633125bc80a1e7db48d50cc
Feran Morgan <feran.morgan@pingcap.com>
2023-10-19 10:12:08 -0700

    Removed String() call in favor of variable substitution.
    Signed-off-by: Feran Morgan <feran.morgan@pingcap.com>

--- dm/syncer/checkpoint.go
+++ dm/syncer/checkpoint.go
@@ -675,7 +675,7 @@ func (cp *RemoteCheckPoint) IsOlderThanTablePoint(table *filter.Table, location 
 	}
 	oldLocation := point.MySQLLocation()
 	// if we update enable-gtid = false to true, we need to compare binlog position instead of GTID before we save table point
-	cmpGTID := cp.cfg.EnableGTID && !(oldLocation.GTIDSetStr() == "" && binlog.ComparePosition(oldLocation.Position, binlog.MinPosition) > 0)
+	cmpGTID := cp.cfg.EnableGTID && !(oldLocation.GetGTID() == emptyMySQLGTIDSet && binlog.ComparePosition(oldLocation.Position, binlog.MinPosition) > 0)
 	cp.logCtx.L().Debug("compare table location whether is newer", zap.Stringer("location", location), zap.Stringer("old location", oldLocation), zap.Bool("cmpGTID", cmpGTID))
 
 	return binlog.CompareLocation(location, oldLocation, cmpGTID) <= 0
