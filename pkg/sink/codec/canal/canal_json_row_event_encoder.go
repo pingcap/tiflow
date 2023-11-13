74a9cb1c54eb896e1949f5d32d505aa31560da3f
3AceShowHand <jinl1037@hotmail.com>
2023-11-13 17:54:17 +0800

    tiny adjust.

--- pkg/sink/codec/canal/canal_json_row_event_encoder.go
+++ pkg/sink/codec/canal/canal_json_row_event_encoder.go
@@ -170,7 +170,7 @@ func newJSONMessageForDML(
 				} else {
 					out.RawByte(',')
 				}
-				mysqlType := getMySQLType(col.Type, col.Flag)
+				g
 				javaType, err := getJavaSQLType(col.Value, col.Type, col.Flag)
 				if err != nil {
 					return nil, cerror.WrapError(cerror.ErrCanalEncodeFailed, err)
@@ -178,7 +178,7 @@ func newJSONMessageForDML(
 				out.String(col.Name)
 				out.RawByte(':')
 				out.Int32(int32(javaType))
-				mysqlTypeMap[col.Name] = mysqlType
+				mysqlTypeMap[col.Name] = getMySQLType(col.Type, col.Flag)
 			}
 		}
 		if emptyColumn {
