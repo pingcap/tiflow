6540e98f36679c10613fb2ec50c8be7e3d90bfd9
dongmen <414110582@qq.com>
2023-11-20 15:11:40 +0800

    fix error

--- cdc/entry/mounter_test.go
+++ cdc/entry/mounter_test.go
@@ -1231,7 +1231,7 @@ func TestBuildTableInfo(t *testing.T) {
 		},
 	}
 	p := parser.New()
-	for _, c := range cases {
+	for i, c := range cases {
 		stmt, err := p.ParseOneStmt(c.origin, "", "")
 		require.NoError(t, err)
 		originTI, err := ddl.BuildTableInfoFromAST(stmt.(*ast.CreateTableStmt))
@@ -1296,7 +1296,7 @@ func TestNewDMRowChange(t *testing.T) {
 		},
 	}
 	p := parser.New()
-	for i, c := range cases {
+	for _, c := range cases {
 		stmt, err := p.ParseOneStmt(c.origin, "", "")
 		require.NoError(t, err)
 		originTI, err := ddl.BuildTableInfoFromAST(stmt.(*ast.CreateTableStmt))
