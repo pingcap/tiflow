1a6b4b78b022031f76cd9924bfa132976df44dce
qupeng <qupeng@pingcap.com>
2023-11-23 13:55:08 +0800

    add an unit test
    Signed-off-by: qupeng <qupeng@pingcap.com>

--- cdc/processor/sinkmanager/table_sink_wrapper.go
+++ cdc/processor/sinkmanager/table_sink_wrapper.go
@@ -166,6 +166,11 @@ func (t *tableSinkWrapper) start(ctx context.Context, startTs model.Ts) (err err
 			break
 		}
 	}
+	if model.NewResolvedTs(startTs).Greater(t.tableSink.checkpointTs) {
+		t.tableSink.checkpointTs = model.NewResolvedTs(startTs)
+		t.tableSink.resolvedTs = model.NewResolvedTs(startTs)
+		t.tableSink.advanced = time.Now()
+	}
 	t.state.Store(tablepb.TableStateReplicating)
 	return nil
 }
