fbca859420d5a12c03d73bb2adc947189fcaa0ba
Daniël van Eeden <git@myname.nl>
2023-11-29 12:40:40 +0200

    Improve logging of ignored TableMap/FormatDesc

--- dm/syncer/syncer.go
+++ dm/syncer/syncer.go
@@ -2441,6 +2441,13 @@ func (s *Syncer) Run(ctx context.Context) (err error) {
 			if needContinue {
 				continue
 			}
+		case *replication.TableMapEvent:
+			s.tctx.L().Info("unhandled TableMap event",
+				zap.String("schema", string(ev.Schema)),
+				zap.String("table", string(ev.Table)),
+			)
+		case *replication.FormatDescriptionEvent:
+			s.tctx.L().Info("unhandled FormatDescription event")
 		default:
 			s.tctx.L().Warn("unhandled event", zap.String("type", fmt.Sprintf("%T", ev)))
 		}
