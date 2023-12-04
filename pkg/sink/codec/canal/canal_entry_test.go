a66333402c623e3783134934375bbc3389015cf7
3AceShowHand <jinl1037@hotmail.com>
2023-12-04 15:14:54 +0800

    fix check

--- pkg/sink/codec/canal/canal_entry_test.go
+++ pkg/sink/codec/canal/canal_entry_test.go
@@ -34,12 +34,12 @@ func TestGetMySQLTypeAndJavaSQLType(t *testing.T) {
 		require.Equal(t, item.expectedMySQLType, obtainedMySQLType)
 
 		obtainedJavaSQLType, err := getJavaSQLType(item.column.Value, item.column.Type, item.column.Flag)
-		require.Nil(t, err)
+		require.NoError(t, err)
 		require.Equal(t, item.expectedJavaSQLType, obtainedJavaSQLType)
 
+		obtainedFinalValue, err := canalEntryBuilder.formatValue(item.column.Value, item.column.Flag.IsBinary())
+		require.NoError(t, err)
 		if !item.column.Flag.IsBinary() {
-			obtainedFinalValue, err := canalEntryBuilder.formatValue(item.column.Value, obtainedJavaSQLType)
-			require.Nil(t, err)
 			require.Equal(t, item.expectedEncodedValue, obtainedFinalValue)
 		}
 	}
