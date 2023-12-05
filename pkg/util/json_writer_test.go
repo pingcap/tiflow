5440a237ec80a7aca86abda1c0b251b691b00392
Wish <breezewish@outlook.com>
2023-12-05 21:24:34 +0800

    Support internal buffers
    Signed-off-by: Wish <breezewish@outlook.com>

--- pkg/util/json_writer_test.go
+++ pkg/util/json_writer_test.go
@@ -17,56 +17,70 @@ import (
 	"bytes"
 	"testing"
 
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+	"github.com/thanhpk/randstr"
 )
 
-func writeJSON(fn func(*JSONWriter)) string {
-	out := &bytes.Buffer{}
-	w := BorrowJSONWriter(out)
-	fn(w)
-	ReturnJSONWriter(w)
-	return out.String()
+type JSONWriterTestSuite struct {
+	suite.Suite
+	useInternalBuffer bool
 }
 
-func TestObject(t *testing.T) {
+func (s *JSONWriterTestSuite) writeJSON(fn func(*JSONWriter)) string {
+	if s.useInternalBuffer {
+		w := BorrowJSONWriter(nil)
+		fn(w)
+		ret := string(w.Buffer())
+		ReturnJSONWriter(w)
+		return ret
+	} else {
+		out := &bytes.Buffer{}
+		w := BorrowJSONWriter(out)
+		fn(w)
+		ReturnJSONWriter(w)
+		return out.String()
+	}
+}
+
+func (suite *JSONWriterTestSuite) TestObject() {
 	var s string
 
-	s = writeJSON(func(w *JSONWriter) {
+	s = suite.writeJSON(func(w *JSONWriter) {
 		w.WriteObject(func() {})
 	})
-	require.Equal(t, `{}`, s)
+	suite.Require().Equal(`{}`, s)
 
-	s = writeJSON(func(w *JSONWriter) {
+	s = suite.writeJSON(func(w *JSONWriter) {
 		w.WriteObject(func() {
 			w.WriteAnyField("foo", 1)
 		})
 	})
-	require.Equal(t, `{"foo":1}`, s)
+	suite.Require().Equal(`{"foo":1}`, s)
 
-	s = writeJSON(func(w *JSONWriter) {
+	s = suite.writeJSON(func(w *JSONWriter) {
 		w.WriteObject(func() {
 			w.WriteAnyField("foo", 1)
 			w.WriteAnyField("bar", 2)
 		})
 	})
-	require.Equal(t, `{"foo":1,"bar":2}`, s)
+	suite.Require().Equal(`{"foo":1,"bar":2}`, s)
 
-	s = writeJSON(func(w *JSONWriter) {
+	s = suite.writeJSON(func(w *JSONWriter) {
 		w.WriteObject(func() {
 			w.WriteObjectField("foo", func() {})
 		})
 	})
-	require.Equal(t, `{"foo":{}}`, s)
+	suite.Require().Equal(`{"foo":{}}`, s)
 
-	s = writeJSON(func(w *JSONWriter) {
+	s = suite.writeJSON(func(w *JSONWriter) {
 		w.WriteObject(func() {
 			w.WriteObjectField("foo", func() {})
 			w.WriteObjectField("bar", func() {})
 		})
 	})
-	require.Equal(t, `{"foo":{},"bar":{}}`, s)
+	suite.Require().Equal(`{"foo":{},"bar":{}}`, s)
 
-	s = writeJSON(func(w *JSONWriter) {
+	s = suite.writeJSON(func(w *JSONWriter) {
 		w.WriteObject(func() {
 			w.WriteObjectField("foo", func() {
 				w.WriteObjectField("foo1", func() {})
@@ -76,9 +90,9 @@ func TestObject(t *testing.T) {
 			})
 		})
 	})
-	require.Equal(t, `{"foo":{"foo1":{}},"bar":{"foo2":{}}}`, s)
+	suite.Require().Equal(`{"foo":{"foo1":{}},"bar":{"foo2":{}}}`, s)
 
-	s = writeJSON(func(w *JSONWriter) {
+	s = suite.writeJSON(func(w *JSONWriter) {
 		w.WriteObject(func() {
 			w.WriteObjectField("foo", func() {
 				w.WriteObjectField("foo1", func() {})
@@ -89,9 +103,9 @@ func TestObject(t *testing.T) {
 			})
 		})
 	})
-	require.Equal(t, `{"foo":{"foo1":{},"bar1":{}},"bar":{"foo2":{}}}`, s)
+	suite.Require().Equal(`{"foo":{"foo1":{},"bar1":{}},"bar":{"foo2":{}}}`, s)
 
-	s = writeJSON(func(w *JSONWriter) {
+	s = suite.writeJSON(func(w *JSONWriter) {
 		w.WriteObject(func() {
 			w.WriteObjectField("foo", func() {
 				w.WriteObjectField("foo1", func() {})
@@ -103,9 +117,9 @@ func TestObject(t *testing.T) {
 			})
 		})
 	})
-	require.Equal(t, `{"foo":{"foo1":{},"bar1":{}},"bar":{"foo2":{},"bar2":{}}}`, s)
+	suite.Require().Equal(`{"foo":{"foo1":{},"bar1":{}},"bar":{"foo2":{},"bar2":{}}}`, s)
 
-	s = writeJSON(func(w *JSONWriter) {
+	s = suite.writeJSON(func(w *JSONWriter) {
 		w.WriteObject(func() {
 			w.WriteObjectField("foo", func() {
 				w.WriteObjectField("foo1", func() {
@@ -119,101 +133,101 @@ func TestObject(t *testing.T) {
 			})
 		})
 	})
-	require.Equal(t, `{"foo":{"foo1":{"abc":null},"bar1":{}},"bar":{"foo2":{},"bar2":{}}}`, s)
+	suite.Require().Equal(`{"foo":{"foo1":{"abc":null},"bar1":{}},"bar":{"foo2":{},"bar2":{}}}`, s)
 }
 
-func TestBase64(t *testing.T) {
+func (suite *JSONWriterTestSuite) TestBase64() {
 	var s string
 
-	s = writeJSON(func(w *JSONWriter) {
+	s = suite.writeJSON(func(w *JSONWriter) {
 		w.WriteBase64String([]byte("foo"))
 	})
-	require.Equal(t, `"Zm9v"`, s)
+	suite.Require().Equal(`"Zm9v"`, s)
 
-	s = writeJSON(func(w *JSONWriter) {
+	s = suite.writeJSON(func(w *JSONWriter) {
 		w.WriteObject(func() {
 			w.WriteBase64StringField("foo", []byte("bar"))
 		})
 	})
-	require.Equal(t, `{"foo":"YmFy"}`, s)
+	suite.Require().Equal(`{"foo":"YmFy"}`, s)
 }
 
-func TestField(t *testing.T) {
+func (suite *JSONWriterTestSuite) TestField() {
 	var s string
 
-	s = writeJSON(func(w *JSONWriter) {
+	s = suite.writeJSON(func(w *JSONWriter) {
 		w.WriteObject(func() {
 			w.WriteStringField("foo", "bar")
 		})
 	})
-	require.Equal(t, `{"foo":"bar"}`, s)
+	suite.Require().Equal(`{"foo":"bar"}`, s)
 
-	s = writeJSON(func(w *JSONWriter) {
+	s = suite.writeJSON(func(w *JSONWriter) {
 		w.WriteObject(func() {
 			w.WriteUint64Field("foo", 1)
 		})
 	})
-	require.Equal(t, `{"foo":1}`, s)
+	suite.Require().Equal(`{"foo":1}`, s)
 
-	s = writeJSON(func(w *JSONWriter) {
+	s = suite.writeJSON(func(w *JSONWriter) {
 		w.WriteObject(func() {
 			w.WriteFloat64Field("foo", 1.1)
 		})
 	})
-	require.Equal(t, `{"foo":1.1}`, s)
+	suite.Require().Equal(`{"foo":1.1}`, s)
 
-	s = writeJSON(func(w *JSONWriter) {
+	s = suite.writeJSON(func(w *JSONWriter) {
 		w.WriteObject(func() {
 			w.WriteNullField("foo")
 		})
 	})
-	require.Equal(t, `{"foo":null}`, s)
+	suite.Require().Equal(`{"foo":null}`, s)
 
-	s = writeJSON(func(w *JSONWriter) {
+	s = suite.writeJSON(func(w *JSONWriter) {
 		w.WriteObject(func() {
 			w.WriteAnyField("foo", nil)
 		})
 	})
-	require.Equal(t, `{"foo":null}`, s)
+	suite.Require().Equal(`{"foo":null}`, s)
 
-	s = writeJSON(func(w *JSONWriter) {
+	s = suite.writeJSON(func(w *JSONWriter) {
 		w.WriteObject(func() {
 			w.WriteAnyField("foo", 1)
 		})
 	})
-	require.Equal(t, `{"foo":1}`, s)
+	suite.Require().Equal(`{"foo":1}`, s)
 
-	s = writeJSON(func(w *JSONWriter) {
+	s = suite.writeJSON(func(w *JSONWriter) {
 		w.WriteObject(func() {
 			w.WriteBoolField("foo", true)
 		})
 	})
-	require.Equal(t, `{"foo":true}`, s)
+	suite.Require().Equal(`{"foo":true}`, s)
 
-	s = writeJSON(func(w *JSONWriter) {
+	s = suite.writeJSON(func(w *JSONWriter) {
 		w.WriteObject(func() {
 			w.WriteBoolField("foo", false)
 		})
 	})
-	require.Equal(t, `{"foo":false}`, s)
+	suite.Require().Equal(`{"foo":false}`, s)
 
-	s = writeJSON(func(w *JSONWriter) {
+	s = suite.writeJSON(func(w *JSONWriter) {
 		w.WriteObject(func() {
 			w.WriteObjectField("foo", func() {})
 		})
 	})
-	require.Equal(t, `{"foo":{}}`, s)
+	suite.Require().Equal(`{"foo":{}}`, s)
 
-	s = writeJSON(func(w *JSONWriter) {
+	s = suite.writeJSON(func(w *JSONWriter) {
 		w.WriteObject(func() {
 			w.WriteObjectField("foo", func() {
 				w.WriteUint64Field("bar", 1)
 			})
 		})
 	})
-	require.Equal(t, `{"foo":{"bar":1}}`, s)
+	suite.Require().Equal(`{"foo":{"bar":1}}`, s)
 
-	s = writeJSON(func(w *JSONWriter) {
+	s = suite.writeJSON(func(w *JSONWriter) {
 		w.WriteObject(func() {
 			w.WriteObjectField("foo", func() {
 				w.WriteUint64Field("bar", 1)
@@ -221,18 +235,18 @@ func TestField(t *testing.T) {
 			})
 		})
 	})
-	require.Equal(t, `{"foo":{"bar":1,"abc":"def"}}`, s)
+	suite.Require().Equal(`{"foo":{"bar":1,"abc":"def"}}`, s)
 
-	s = writeJSON(func(w *JSONWriter) {
+	s = suite.writeJSON(func(w *JSONWriter) {
 		w.WriteObject(func() {
 			w.WriteUint64Field("bar", 1)
 			w.WriteStringField("abc", "def")
 			w.WriteNullField("xyz")
 		})
 	})
-	require.Equal(t, `{"bar":1,"abc":"def","xyz":null}`, s)
+	suite.Require().Equal(`{"bar":1,"abc":"def","xyz":null}`, s)
 
-	s = writeJSON(func(w *JSONWriter) {
+	s = suite.writeJSON(func(w *JSONWriter) {
 		w.WriteObject(func() {
 			w.WriteUint64Field("bar", 1)
 			w.WriteStringField("abc", "def")
@@ -240,5 +254,65 @@ func TestField(t *testing.T) {
 			w.WriteObjectField("foo", func() {})
 		})
 	})
-	require.Equal(t, `{"bar":1,"abc":"def","xyz":null,"foo":{}}`, s)
+	suite.Require().Equal(`{"bar":1,"abc":"def","xyz":null,"foo":{}}`, s)
+}
+
+func TestExternalBuffer(t *testing.T) {
+	suite.Run(t, &JSONWriterTestSuite{useInternalBuffer: false})
+}
+
+func TestInternalBuffer(t *testing.T) {
+	suite.Run(t, &JSONWriterTestSuite{useInternalBuffer: true})
+}
+
+func BenchmarkExternalBufferWriteBase64(b *testing.B) {
+	out := &bytes.Buffer{}
+	str := randstr.Bytes(1024)
+	for i := 0; i < b.N; i++ {
+		out.Reset()
+		w := BorrowJSONWriter(out)
+		w.WriteObject(func() {
+			w.WriteBase64StringField("foo", str)
+		})
+		_ = out.Bytes()
+		ReturnJSONWriter(w)
+	}
+}
+
+func BenchmarkInternalBufferWriteBase64(b *testing.B) {
+	str := randstr.Bytes(1024)
+	for i := 0; i < b.N; i++ {
+		w := BorrowJSONWriter(nil)
+		w.WriteObject(func() {
+			w.WriteBase64StringField("foo", str)
+		})
+		_ = w.Buffer()
+		ReturnJSONWriter(w)
+	}
+}
+
+func BenchmarkExternalBufferWriteString(b *testing.B) {
+	out := &bytes.Buffer{}
+	str := randstr.String(1024)
+	for i := 0; i < b.N; i++ {
+		out.Reset()
+		w := BorrowJSONWriter(out)
+		w.WriteObject(func() {
+			w.WriteStringField("foo", str)
+		})
+		_ = out.Bytes()
+		ReturnJSONWriter(w)
+	}
+}
+
+func BenchmarkInternalBufferWriteString(b *testing.B) {
+	str := randstr.String(1024)
+	for i := 0; i < b.N; i++ {
+		w := BorrowJSONWriter(nil)
+		w.WriteObject(func() {
+			w.WriteStringField("foo", str)
+		})
+		_ = w.Buffer()
+		ReturnJSONWriter(w)
+	}
 }
