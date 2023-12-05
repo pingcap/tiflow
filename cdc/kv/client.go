d5bf65147535f77aab650ab761387864f2bdc6ac
qupeng <qupeng@pingcap.com>
2023-12-05 17:53:28 +0800

    kv-client(cdc): debug frontier (#10253)
    * cdc: fixes minor bugs #10168 and #10169 (#10170)
    * puller(cdc): debug frontier (#10202)
    * kv-client(cdc): correct conditions of canceling grpc streams  (#10237)
    
    Signed-off-by: qupeng <qupeng@pingcap.com>

--- cdc/kv/client.go
+++ cdc/kv/client.go
@@ -1012,7 +1012,7 @@ func (s *eventFeedSession) receiveFromStream(
 
 	// always create a new region worker, because `receiveFromStream` is ensured
 	// to call exactly once from outer code logic
-	worker := newRegionWorker(parentCtx, s.changefeed, s, addr)
+	worker := newRegionWorker(parentCtx, s.changefeed, s, addr, pendingRegions)
 	defer worker.evictAllRegions()
 
 	ctx, cancel := context.WithCancel(parentCtx)
@@ -1058,7 +1058,7 @@ func (s *eventFeedSession) receiveFromStream(
 			})
 			if err != nil {
 				if status.Code(errors.Cause(err)) == codes.Canceled {
-					log.Debug(
+					log.Info(
 						"receive from stream canceled",
 						zap.String("namespace", s.changefeed.Namespace),
 						zap.String("changefeed", s.changefeed.ID),
@@ -1327,7 +1327,7 @@ func (s *eventFeedSession) getStreamCancel(storeAddr string) (cancel context.Can
 }
 
 func (s *eventFeedSession) logSlowRegions(ctx context.Context) error {
-	ticker := time.NewTicker(10 * time.Second)
+	ticker := time.NewTicker(30 * time.Second)
 	defer ticker.Stop()
 	for {
 		select {
@@ -1341,10 +1341,16 @@ func (s *eventFeedSession) logSlowRegions(ctx context.Context) error {
 			continue
 		}
 		attr := s.rangeLock.CollectLockedRangeAttrs(nil)
+		ckptTime := oracle.GetTimeFromTS(attr.SlowestRegion.CheckpointTs)
+		log.Info("event feed starts to check locked regions",
+			zap.String("namespace", s.changefeed.Namespace),
+			zap.String("changefeed", s.changefeed.ID),
+			zap.Int64("tableID", s.tableID),
+			zap.String("tableName", s.tableName))
+
 		if attr.SlowestRegion.Initialized {
-			ckptTime := oracle.GetTimeFromTS(attr.SlowestRegion.CheckpointTs)
 			if currTime.Sub(ckptTime) > 20*time.Second {
-				log.Info("event feed finds a slow region",
+				log.Info("event feed finds a initialized slow region",
 					zap.String("namespace", s.changefeed.Namespace),
 					zap.String("changefeed", s.changefeed.ID),
 					zap.Int64("tableID", s.tableID),
@@ -1353,6 +1359,13 @@ func (s *eventFeedSession) logSlowRegions(ctx context.Context) error {
 			}
 		} else if currTime.Sub(attr.SlowestRegion.Created) > 10*time.Minute {
 			log.Info("event feed initializes a region too slow",
+				zap.String("namespace", s.changefeed.Namespace),
+				zap.String("changefeed", s.changefeed.ID),
+				zap.Int64("tableID", s.tableID),
+				zap.String("tableName", s.tableName),
+				zap.Any("slowRegion", attr.SlowestRegion))
+		} else if currTime.Sub(ckptTime) > 10*time.Minute {
+			log.Info("event feed finds a uninitialized slow region",
 				zap.String("namespace", s.changefeed.Namespace),
 				zap.String("changefeed", s.changefeed.ID),
 				zap.Int64("tableID", s.tableID),
