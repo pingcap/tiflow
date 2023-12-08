db372d2a9f0a15c601b537cdfe978a460afc1c3e
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2023-12-08 15:26:50 +0800

    puller(cdc): debug frontier (#10202) (#10269)
    ref pingcap/tiflow#10181

--- cdc/kv/client.go
+++ cdc/kv/client.go
@@ -1326,7 +1326,7 @@ func (s *eventFeedSession) getStreamCancel(storeAddr string) (cancel context.Can
 }
 
 func (s *eventFeedSession) logSlowRegions(ctx context.Context) error {
-	ticker := time.NewTicker(10 * time.Second)
+	ticker := time.NewTicker(30 * time.Second)
 	defer ticker.Stop()
 	for {
 		select {
@@ -1335,12 +1335,18 @@ func (s *eventFeedSession) logSlowRegions(ctx context.Context) error {
 		case <-ticker.C:
 		}
 
-		currTime := s.client.pdClock.CurrentTime()
 		attr := s.rangeLock.CollectLockedRangeAttrs(nil)
+		ckptTime := oracle.GetTimeFromTS(attr.SlowestRegion.CheckpointTs)
+		currTime := s.client.pdClock.CurrentTime()
+		log.Info("event feed starts to check locked regions",
+			zap.String("namespace", s.changefeed.Namespace),
+			zap.String("changefeed", s.changefeed.ID),
+			zap.Int64("tableID", s.tableID),
+			zap.String("tableName", s.tableName))
+
 		if attr.SlowestRegion.Initialized {
-			ckptTime := oracle.GetTimeFromTS(attr.SlowestRegion.CheckpointTs)
 			if currTime.Sub(ckptTime) > 2*resolveLockMinInterval {
-				log.Info("event feed finds a slow region",
+				log.Info("event feed finds a initialized slow region",
 					zap.String("namespace", s.changefeed.Namespace),
 					zap.String("changefeed", s.changefeed.ID),
 					zap.Int64("tableID", s.tableID),
@@ -1349,6 +1355,13 @@ func (s *eventFeedSession) logSlowRegions(ctx context.Context) error {
 			}
 		} else if currTime.Sub(attr.SlowestRegion.Created) > 10*time.Minute {
 			log.Info("event feed initializes a region too slow",
+				zap.String("namespace", s.changefeed.Namespace),
+				zap.String("changefeed", s.changefeed.ID),
+				zap.Int64("tableID", s.tableID),
+				zap.String("tableName", s.tableName),
+				zap.Any("slowRegion", attr.SlowestRegion))
+		} else if currTime.Sub(ckptTime) > 10*time.Minute {
+			log.Info("event feed finds a uninitialized slow region",
 				zap.String("namespace", s.changefeed.Namespace),
 				zap.String("changefeed", s.changefeed.ID),
 				zap.Int64("tableID", s.tableID),
