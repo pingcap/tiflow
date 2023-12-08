bb8c2db771a95b6938d3e19fec4a0f3224ef9749
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2023-12-08 16:42:47 +0800

    scheduler(ticdc): Updating ReplicationSet.stats only when stats is not empty (#10225) (#10228)
    close pingcap/tiflow#10224

--- cdc/scheduler/internal/v3/replication/replication_set.go
+++ cdc/scheduler/internal/v3/replication/replication_set.go
@@ -1014,7 +1014,11 @@ func (r *ReplicationSet) updateCheckpointAndStats(
 			zap.Any("checkpointTs", r.Checkpoint.CheckpointTs),
 			zap.Any("resolvedTs", r.Checkpoint.ResolvedTs))
 	}
-	r.Stats = stats
+
+	// we only update stats when stats is not empty, because we only collect stats every 10s.
+	if stats.Size() > 0 {
+		r.Stats = stats
+	}
 }
 
 // SetHeap is a max-heap, it implements heap.Interface.
