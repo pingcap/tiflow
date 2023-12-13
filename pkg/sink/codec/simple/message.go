335ab0a158d74813922fcb574f357b780f9f132e
3AceShowHand <jinl1037@hotmail.com>
2023-12-13 11:06:53 +0800

    debug read data from tidb.

--- pkg/sink/codec/simple/message.go
+++ pkg/sink/codec/simple/message.go
@@ -483,7 +483,7 @@ func encodeValue(value interface{}, ft *types.FieldType) (interface{}, error) {
 		case []uint8:
 			number, err = strconv.ParseUint(string(v), 10, 64)
 			if err != nil {
-				log.Error("parse number for enum failed", zap.Any("number", number), zap.Error(err))
+				log.Error("parse number for enum failed", zap.Any("number", string(v)), zap.Error(err))
 				return "", cerror.WrapError(cerror.ErrEncodeFailed, err)
 			}
 		default:
