fcc36413b04654229bcdadce33e8f24666405847
hongyunyan <649330952@qq.com>
2024-01-02 15:42:05 +0800

    update

--- cdc/api/v2/changefeed.go
+++ cdc/api/v2/changefeed.go
@@ -855,12 +855,12 @@ func (h *OpenAPIV2) synced(c *gin.Context) {
 		var message string
 		if (oracle.ExtractPhysical(status.PullerResolvedTs) - oracle.ExtractPhysical(status.CheckpointTs)) <
 			cfg.ReplicaConfig.SyncedStatus.CheckpointInterval*1000 {
-			message = fmt.Sprintf("Please check whether pd is healthy and tikv region is all available. " +
-				"If pd is not healthy or tikv region is not available, the data syncing is finished. " +
-				"When pd is offline means that pd is not healthy. For tikv region, you can check the grafana info " +
-				"in 'TiKV-Details-Resolved-Ts-Max Leader Resolved TS gap'. If the gap is a large value, such as a few minutes, " +
-				"it means some regions in tikv are unavailable. " +
-				" Otherwise the data syncing is not finished, please wait")
+			message = fmt.Sprintf("Please check whether PD is online and TiKV Regions are all available. " +
+				"If PD is offline or some TiKV regions are not available, it means that the data syncing process is complete. " +
+				"To check whether TiKV regions are all available, " +
+				"you can view 'TiKV-Details' > 'Resolved-Ts' > 'Max Leader Resolved TS gap' on Grafana. " +
+				"If the gap is large, such as a few minutes, it means that some regions in TiKV are unavailable. " +
+				"Otherwise, if the gap is small and PD is online, it means the data syncing is incomplete, so please wait")
 		} else {
 			message = "The data syncing is not finished, please wait"
 		}
