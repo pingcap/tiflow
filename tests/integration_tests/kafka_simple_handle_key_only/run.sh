3beb7aea187e091422ec00d8f8590edbe68f2f14
3AceShowHand <jinl1037@hotmail.com>
2024-01-02 17:31:15 +0800

    some simple integration test support avro.

--- tests/integration_tests/kafka_simple_handle_key_only/run.sh
+++ tests/integration_tests/kafka_simple_handle_key_only/run.sh
@@ -21,11 +21,10 @@ function run() {
 	cd $WORK_DIR
 	run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY
 
-	TOPIC_NAME="simple-handle-key-only"
-
 	# record tso before we create tables to skip the system table DDLs
 	start_ts=$(run_cdc_cli_tso_query ${UP_PD_HOST_1} ${UP_PD_PORT_1})
 
+	TOPIC_NAME="simple-handle-key-only"
 	changefeed_id="simple-handle-key-only"
 	SINK_URI="kafka://127.0.0.1:9092/$TOPIC_NAME?protocol=simple"
 	run_cdc_cli changefeed create --start-ts=$start_ts --sink-uri="$SINK_URI" -c ${changefeed_id} --config="$CUR/conf/changefeed.toml"
@@ -41,6 +40,28 @@ function run() {
 	cdc_kafka_consumer --upstream-uri $SINK_URI --downstream-uri="mysql://root@127.0.0.1:3306/?safe-mode=true&batch-dml-enable=false" --upstream-tidb-dsn="root@tcp(${UP_TIDB_HOST}:${UP_TIDB_PORT})/?" --config="$CUR/conf/changefeed.toml" 2>&1 &
 
 	run_sql_file $CUR/data/data.sql ${UP_TIDB_HOST} ${UP_TIDB_PORT}
+
+	# sync_diff can't check non-exist table, so we check expected tables are created in downstream first
+	check_table_exists test.finish_mark ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT} 200
+	check_sync_diff $WORK_DIR $CUR/conf/diff_config.toml
+
+  run_sql "drop database test;" ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT}
+
+  # create a new changefeed with avro encoding, set the start_ts before run the sql file.
+  TOPIC_NAME = "simple-handle-key-only-avro"
+  changefeed_id="simple-handle-key-only-avro"
+  SINK_URI="kafka://127.0.0.1:9092/$TOPIC_NAME?protocol=simple&encoding-format=avro"
+  run_cdc_cli changefeed create --start-ts=$start_ts --sink-uri="$SINK_URI" -c ${changefeed_id} --config="$CUR/conf/changefeed.toml"
+  run_sql_file $CUR/data/ddl.sql ${UP_TIDB_HOST} ${UP_TIDB_PORT}
+
+  sleep 5
+
+  run_cdc_cli changefeed pause -c ${changefeed_id}
+
+  SINK_URI="kafka://127.0.0.1:9092/$TOPIC_NAME?protocol=simple&encoding-format=avro&max-message-bytes=500"
+  run_cdc_cli changefeed update -c ${changefeed_id} --sink-uri="$SINK_URI" --config="$CUR/conf/changefeed.toml" --no-confirm
+  run_cdc_cli changefeed resume -c ${changefeed_id}
+  cdc_kafka_consumer --upstream-uri $SINK_URI --downstream-uri="mysql://root@127.0.0.1:3306/?safe-mode=true&batch-dml-enable=false" --upstream-tidb-dsn="root@tcp(${UP_TIDB_HOST}:${UP_TIDB_PORT})/?" --config="$CUR/conf/changefeed.toml" 2>&1 &
 
 	# sync_diff can't check non-exist table, so we check expected tables are created in downstream first
 	check_table_exists test.finish_mark ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT} 200
