14e41a15741319c472baf258b63b007c8c645367
CharlesCheung <61726649+CharlesCheung96@users.noreply.github.com>
2024-01-03 10:02:02 +0800

    This is an automated cherry-pick of #10339
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- cdc/puller/frontier/frontier.go
+++ cdc/puller/frontier/frontier.go
@@ -32,6 +32,7 @@ type Frontier interface {
 	Forward(regionID uint64, span regionspan.ComparableSpan, ts uint64)
 	Frontier() uint64
 	String() string
+	SpanString(span tablepb.Span) string
 	Entries(fn func(key []byte, ts uint64))
 }
 
@@ -69,7 +70,7 @@ func NewFrontier(checkpointTs uint64,
 			firstSpan = false
 			continue
 		}
-		s.insert(0, span, checkpointTs)
+		s.insert(fakeRegionID, span, checkpointTs)
 	}
 
 	return s
@@ -94,8 +95,13 @@ func (s *spanFrontier) Forward(regionID uint64, span regionspan.ComparableSpan, 
 	s.insert(regionID, span, ts)
 }
 
+<<<<<<< HEAD
 func (s *spanFrontier) insert(regionID uint64, span regionspan.ComparableSpan, ts uint64) {
 	// clear the  seek result
+=======
+func (s *spanFrontier) insert(regionID uint64, span tablepb.Span, ts uint64) {
+	// clear the seek result
+>>>>>>> 7f8f280b7a (puller(ticdc): add more log for puller (#10339))
 	for i := 0; i < len(s.seekTempResult); i++ {
 		s.seekTempResult[i] = nil
 	}
@@ -103,9 +109,11 @@ func (s *spanFrontier) insert(regionID uint64, span regionspan.ComparableSpan, t
 	// if there is no change in the region span
 	// We just need to update the ts corresponding to the span in list
 	next := seekRes.Node().Next()
+	// next is nil means the span.StartKey is larger than all the spans in list
 	if next != nil {
 		if bytes.Equal(seekRes.Node().Key(), span.Start) && bytes.Equal(next.Key(), span.End) {
 			s.minTsHeap.UpdateKey(seekRes.Node().Value(), ts)
+
 			if regionID != fakeRegionID {
 				s.cachedRegions[regionID] = seekRes.Node()
 				s.cachedRegions[regionID].regionID = regionID
@@ -119,9 +127,11 @@ func (s *spanFrontier) insert(regionID uint64, span regionspan.ComparableSpan, t
 	node := seekRes.Node()
 	delete(s.cachedRegions, node.regionID)
 	lastNodeTs := uint64(math.MaxUint64)
+	lastRegionID := uint64(fakeRegionID)
 	shouldInsertStartNode := true
 	if node.Value() != nil {
 		lastNodeTs = node.Value().key
+		lastRegionID = node.regionID
 	}
 	for ; node != nil; node = node.Next() {
 		delete(s.cachedRegions, node.regionID)
@@ -133,8 +143,10 @@ func (s *spanFrontier) insert(regionID uint64, span regionspan.ComparableSpan, t
 			break
 		}
 		lastNodeTs = node.Value().key
+		lastRegionID = node.regionID
 		if cmpStart == 0 {
 			s.minTsHeap.UpdateKey(node.Value(), ts)
+			node.regionID = regionID
 			shouldInsertStartNode = false
 		} else {
 			s.spanList.Remove(seekRes, node)
@@ -142,10 +154,17 @@ func (s *spanFrontier) insert(regionID uint64, span regionspan.ComparableSpan, t
 		}
 	}
 	if shouldInsertStartNode {
+<<<<<<< HEAD
 		s.spanList.InsertNextToNode(seekRes, span.Start, s.minTsHeap.Insert(ts))
 		seekRes.Next()
 	}
 	s.spanList.InsertNextToNode(seekRes, span.End, s.minTsHeap.Insert(lastNodeTs))
+=======
+		s.spanList.InsertNextToNode(seekRes, span.StartKey, s.minTsHeap.Insert(ts), regionID)
+		seekRes.Next()
+	}
+	s.spanList.InsertNextToNode(seekRes, span.EndKey, s.minTsHeap.Insert(lastNodeTs), lastRegionID)
+>>>>>>> 7f8f280b7a (puller(ticdc): add more log for puller (#10339))
 }
 
 // Entries visit all traced spans.
@@ -164,6 +183,43 @@ func (s *spanFrontier) String() string {
 		} else {
 			buf.WriteString(fmt.Sprintf("[%s @ %d] ", key, ts))
 		}
+	})
+	return buf.String()
+}
+
+func (s *spanFrontier) stringWtihRegionID() string {
+	var buf strings.Builder
+	s.spanList.Entries(func(n *skipListNode) bool {
+		if n.Value().key == math.MaxUint64 {
+			buf.WriteString(fmt.Sprintf("[%d:%s @ Max] ", n.regionID, n.Key()))
+		} else { // the next span
+			buf.WriteString(fmt.Sprintf("[%d:%s @ %d] ", n.regionID, n.Key(), n.Value().key))
+		}
+		return true
+	})
+	return buf.String()
+}
+
+// SpanString returns the string of the span's frontier.
+func (s *spanFrontier) SpanString(span tablepb.Span) string {
+	var buf strings.Builder
+	idx := 0
+	s.spanList.Entries(func(n *skipListNode) bool {
+		key := n.Key()
+		nextKey := []byte{}
+		if n.Next() != nil {
+			nextKey = n.Next().Key()
+		}
+		if n.Value().key == math.MaxUint64 {
+			buf.WriteString(fmt.Sprintf("[%d:%s @ Max] ", n.regionID, n.Key()))
+		} else if idx == 0 || // head
+			bytes.Equal(key, span.StartKey) || // start key sapn
+			bytes.Equal(nextKey, span.StartKey) || // the previous sapn of start key
+			bytes.Equal(key, span.EndKey) { // the end key span
+			buf.WriteString(fmt.Sprintf("[%d:%s @ %d] ", n.regionID, n.Key(), n.Value().key))
+		}
+		idx++
+		return true
 	})
 	return buf.String()
 }
--- cdc/puller/frontier/frontier_test.go
+++ cdc/puller/frontier/frontier_test.go
@@ -200,6 +200,39 @@ func TestSpanFrontierFallback(t *testing.T) {
 	// f.Forward(spAC, 10)
 }
 
+func TestSpanString(t *testing.T) {
+	t.Parallel()
+
+	spAB := tablepb.Span{StartKey: []byte("a"), EndKey: []byte("b")}
+	spBC := tablepb.Span{StartKey: []byte("b"), EndKey: []byte("c")}
+	spCD := tablepb.Span{StartKey: []byte("c"), EndKey: []byte("d")}
+	spDE := tablepb.Span{StartKey: []byte("d"), EndKey: []byte("e")}
+	spEF := tablepb.Span{StartKey: []byte("e"), EndKey: []byte("f")}
+	spFG := tablepb.Span{StartKey: []byte("f"), EndKey: []byte("g")}
+	spGH := tablepb.Span{StartKey: []byte("g"), EndKey: []byte("h")}
+
+	spAH := tablepb.Span{StartKey: []byte("a"), EndKey: []byte("h")}
+	f := NewFrontier(1, spAH).(*spanFrontier)
+	require.Equal(t, `[0:a @ 1] [0:h @ Max] `, f.SpanString(spAH))
+
+	f.Forward(1, spAB, 2)
+	f.Forward(2, spBC, 5)
+	f.Forward(3, spCD, 10)
+	f.Forward(4, spDE, 20)
+	f.Forward(5, spEF, 30)
+	f.Forward(6, spFG, 25)
+	f.Forward(7, spGH, 35)
+	require.Equal(t, uint64(2), f.Frontier())
+	require.Equal(t, `[1:a @ 2] [2:b @ 5] [3:c @ 10] [4:d @ 20] [5:e @ 30] [6:f @ 25] [7:g @ 35] [0:h @ Max] `, f.stringWtihRegionID())
+	// Print 5 span: start, before, target span, next, end
+	require.Equal(t, `[1:a @ 2] [3:c @ 10] [4:d @ 20] [5:e @ 30] [0:h @ Max] `, f.SpanString(spDE))
+
+	spBH := tablepb.Span{StartKey: []byte("b"), EndKey: []byte("h")}
+	f.Forward(8, spBH, 18)
+	require.Equal(t, uint64(2), f.Frontier())
+	require.Equal(t, `[1:a @ 2] [8:b @ 18] [0:h @ Max] `, f.stringWtihRegionID())
+}
+
 func TestMinMax(t *testing.T) {
 	t.Parallel()
 	var keyMin []byte
