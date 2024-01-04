abf950eeb7692631bf1004b9baa5b6651d53623e
lance6716 <lance6716@gmail.com>
2024-01-04 16:45:12 +0800

    make fmt
    Signed-off-by: lance6716 <lance6716@gmail.com>

--- dm/tests/tls/run.sh
+++ dm/tests/tls/run.sh
@@ -292,7 +292,7 @@ function test_master_ha_when_enable_tidb_and_only_ca_source_tls() {
 	check_rpc_alive $cur/../bin/check_master_online_http 127.0.0.1:$MASTER_PORT1 "$cur/conf/ca.pem" "$cur/conf/dm.pem" "$cur/conf/dm.key"
 	check_rpc_alive $cur/../bin/check_master_http_apis 127.0.0.1:$MASTER_PORT1 "$cur/conf/ca.pem" "$cur/conf/dm.pem" "$cur/conf/dm.key"
 
-  # https://github.com/pingcap/dm/issues/1458
+	# https://github.com/pingcap/dm/issues/1458
 	# check the log is not repeatedly printed
 	check_log_contains $WORK_DIR/master1/log/dm-master.log "remote error: tls: bad certificate" 0
 	check_log_contains $WORK_DIR/master1/log/dm-master.log "client certificate authentication failed" 0
