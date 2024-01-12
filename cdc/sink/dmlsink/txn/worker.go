baf9031fdbcbf171fcd5617d2dc002c5be1f9073
hongyunyan <649330952@qq.com>
2024-01-12 16:25:56 +0800

    cdc: remove unused code and add some logs (#10458)
    ref pingcap/tiflow#10457

--- cdc/sink/dmlsink/txn/worker.go
+++ cdc/sink/dmlsink/txn/worker.go
@@ -92,7 +92,7 @@ func (w *worker) close() {
 	w.txnCh.CloseAndDrain()
 }
 
-// Run a loop.
+// Continuously get events from txnCh and call backend flush based on conditions.
 func (w *worker) runLoop() error {
 	defer func() {
 		if err := w.backend.Close(); err != nil {
@@ -148,7 +148,7 @@ func (w *worker) runLoop() error {
 }
 
 // onEvent is called when a new event is received.
-// It returns true if the event is sent to backend.
+// It returns true if it needs flush immediately.
 func (w *worker) onEvent(txn txnWithNotifier) bool {
 	w.hasPending = true
 
@@ -169,7 +169,6 @@ func (w *worker) onEvent(txn txnWithNotifier) bool {
 }
 
 // doFlush flushes the backend.
-// It returns true only if it can no longer be flushed.
 func (w *worker) doFlush(flushTimeSlice *time.Duration) error {
 	if w.hasPending {
 		start := time.Now()
