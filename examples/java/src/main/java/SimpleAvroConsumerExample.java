0d215956895e045cff36b7893cb1de6ded3efe26
3AceShowHand <jinl1037@hotmail.com>
2024-01-12 17:48:13 +0800

    add simple avro consumer example.

--- examples/java/src/main/java/SimpleAvroConsumerExample.java
+++ examples/java/src/main/java/SimpleAvroConsumerExample.java
@@ -15,12 +15,14 @@
 
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericDatumReader;
-import org.apache.avro.io.DatumReader;
 import org.apache.avro.io.Decoder;
 import org.apache.avro.io.DecoderFactory;
 import org.apache.avro.generic.GenericRecord;
-import org.apache.kafka.clients.consumer.*;
-import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.ByteArrayDeserializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -30,39 +32,61 @@ import java.io.File;
 import java.io.IOException;
 import java.time.Duration;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Properties;
 
 public class SimpleAvroConsumerExample {
 
     private static final Logger log = LoggerFactory.getLogger(SimpleAvroConsumerExample.class.getName());
-
     public static void main(String[] args) throws IOException {
         Schema schema = new Schema.Parser().parse(new File("src/main/resources/schema.avsc"));
 
         Properties props = new Properties();
         props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "simple-avro-consumer-example");
-        props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ByteArrayDeserializer.class.getName());
+        props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ByteArrayDeserializer.class.getName());
 
-        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        KafkaConsumer<byte[], byte[]> consumer = new KafkaConsumer<>(props);
 
-        String topic = "simple-test";
+        String topic = "simple-avro-test";
         consumer.subscribe(Arrays.asList(topic));
 
         try {
             while (true) {
-                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
-                for (ConsumerRecord<String, String> record : records) {
-                    ByteArrayInputStream inputStream = new ByteArrayInputStream(record.value().getBytes());
+                ConsumerRecords<byte[], byte[]> records = consumer.poll(Duration.ofMillis(100));
+                for (ConsumerRecord<byte[], byte[]> record : records) {
+                    ByteArrayInputStream inputStream = new ByteArrayInputStream(record.value());
                     Decoder decoder = DecoderFactory.get().binaryDecoder(inputStream, null);
 
-                    DatumReader<GenericRecord> reader = new GenericDatumReader<>(schema);
-                    GenericRecord result = reader.read(null, decoder);
+                    GenericDatumReader<GenericRecord> datumReader = new GenericDatumReader<>(schema);
+                    GenericRecord result = datumReader.read(null, decoder);
 
-                    System.out.println(result);
-//                    System.out.printf("offset = %d, key = %s, value = %s \n", record.offset(), record.key(), record.value());
+                    String eventType = result.get("type").toString();
+                    switch (eventType) {
+                        case "WATERMARK":
+                            log.info("watermark: {}", result.get("commitTs"));
+                            break;
+                        case "DDL":
+                            log.info("ddl, ddl = {}", result.get("ddlQuery"));
+                            break;
+                        case "BOOTSTRAP":
+                            log.info("bootstrap, tableSchema = {}", result.get("tableSchema"));
+                            break;
+                        default:
+                            log.info("row changed event, type = {}, schema = {}, table = {}, commitTs = {}, buildTs = {}, schemaVersion = {}, claimCheckLocation = {}, handleKeyOnly = {}, ",
+                                    eventType, result.get("schema"), result.get("table"), result.get("commitTs"), result.get("buildTs"), result.get("schemaVersion"), result.get("claimCheckLocation"), result.get("handleKeyOnly"));
+                            if (result.hasField("checksum")) {
+                                log.info("checksum = {}", result.get("checksum"));
+                            }
+                            if (result.hasField("data")) {
+                                log.info("data = {}", result.get("data"));
+                            }
+                            if (result.hasField("old")) {
+                                log.info("old = {}", result.get("old"));
+                            }
+
+                            break;
+                    }
                 }
             }
         } finally {
