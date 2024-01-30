01d37f16fa97fc14e5e0e76ff338765e097a2e12
3AceShowHand <jinl1037@hotmail.com>
2024-01-30 16:09:03 +0800

    remove tests.

--- cdc/entry/mounter_test.go
+++ cdc/entry/mounter_test.go
@@ -1192,51 +1192,6 @@ func TestE2ERowLevelChecksum(t *testing.T) {
 	require.NoError(t, err)
 }
 
-func TestRowLevelChecksum(t *testing.T) {
-	replicaConfig := config.GetDefaultReplicaConfig()
-	replicaConfig.Integrity.IntegrityCheckLevel = integrity.CheckLevelCorrectness
-	helper := NewSchemaTestHelperWithReplicaConfig(t, replicaConfig)
-	defer helper.Close()
-
-	helper.tk.MustExec("set global tidb_enable_row_level_checksum = 1")
-	helper.Tk().MustExec("use test")
-
-	_ = helper.DDL2Event(`CREATE TABLE TBL1 (id int PRIMARY KEY)`)
-	_ = helper.DML2Event(`INSERT INTO TBL1 (id) VALUES(1)`, "test", "TBL1")
-
-	_ = helper.DDL2Event(`ALTER TABLE TBL1 ADD COLUMN v1 INTEGER DEFAULT 0`)
-	_ = helper.DML2Event(`INSERT INTO TBL1 (id) VALUES(2)`, "test", "TBL1")
-
-	_ = helper.DML2Event(`UPDATE TBL1 SET id = 3, v1 = 10 WHERE id = 1`, "test", "TBL1")
-	_ = helper.DML2Event(`UPDATE TBL1 SET v1 = 11 WHERE id = 2`, "test", "TBL1")
-	_ = helper.DML2Event(`INSERT INTO TBL1 (id, v1) VALUES(1000, 1000)`, "test", "TBL1")
-	_ = helper.DML2Event(`UPDATE TBL1 SET v1 = 1001 WHERE id = 1000`, "test", "TBL1")
-}
-
-func TestVerifyChecksumTime(t *testing.T) {
-	replicaConfig := config.GetDefaultReplicaConfig()
-	replicaConfig.Integrity.IntegrityCheckLevel = integrity.CheckLevelCorrectness
-	replicaConfig.Integrity.CorruptionHandleLevel = integrity.CorruptionHandleLevelError
-
-	helper := NewSchemaTestHelperWithReplicaConfig(t, replicaConfig)
-	defer helper.Close()
-
-	helper.Tk().MustExec("set global tidb_enable_row_level_checksum = 1")
-	helper.Tk().MustExec("use test")
-
-	helper.Tk().MustExec("set global time_zone = '+00:00'")
-	helper.Tk().MustExec("set @@global.time_zone = 'system'")
-
-	_ = helper.DDL2Event(`CREATE table TBL1 (datetime_column DATETIME, timestamp_column TIMESTAMP, PRIMARY KEY (datetime_column))`)
-	event := helper.DML2Event(`INSERT INTO TBL1 VALUES ('2023-02-09 13:00:00', '2023-02-09 13:00:00')`, "test", "TBL1")
-	require.NotNil(t, event)
-
-	helper.Tk().MustExec("set global time_zone = '-5:00'")
-	_ = helper.DDL2Event(`CREATE table TBL2 (datetime_column DATETIME, timestamp_column TIMESTAMP, PRIMARY KEY (datetime_column))`)
-	event = helper.DML2Event(`INSERT INTO TBL2 VALUES ('2023-02-09 13:00:00', '2023-02-09 13:00:00')`, "test", "TBL2")
-	require.NotNil(t, event)
-}
-
 func TestDecodeRowEnableChecksum(t *testing.T) {
 	helper := NewSchemaTestHelper(t)
 	defer helper.Close()
