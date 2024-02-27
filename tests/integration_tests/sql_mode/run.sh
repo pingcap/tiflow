b990f5ba61c627b29ba7495556d04a555879fcc5
hongyunyan <649330952@qq.com>
2024-02-27 14:57:04 +0800

    update

--- tests/integration_tests/sql_mode/run.sh
+++ tests/integration_tests/sql_mode/run.sh
@@ -11,7 +11,7 @@
 # use test;
 # create table t20(id bigint primary key, a text, b text as ((regexp_replace(a, "^[1-9]\d{9,29}$", "aaaaa"))), c text);
 # insert into t20 (id, a, c) values(1,123456, 'ab\\\\c');
-# insert into t20 (id, a, c) values(1,1234567890123, 'ab\\c');
+# insert into t20 (id, a, c) values(2,1234567890123, 'ab\\c');
 
 # When the downstream is mysql or tidb, we need to check the result in downstream server.
 # Otherwise, we just need to check it works without error.
@@ -32,6 +32,9 @@ DB_COUNT=4
 rm -rf $WORK_DIR && mkdir -p $WORK_DIR
 start_tidb_cluster --workdir $WORK_DIR
 
+run_sql "set global sql_mode='NO_BACKSLASH_ESCAPES';" ${UP_TIDB_HOST} ${UP_TIDB_PORT}
+run_sql "set global sql_mode='NO_BACKSLASH_ESCAPES';" ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT}
+
 trap stop_tidb_cluster EXIT
 
 cd $WORK_DIR
@@ -42,18 +45,16 @@ SINK_URI="mysql://root@127.0.0.1:3306/?max-txn-row=1"
 run_cdc_cli changefeed create --start-ts=$start_ts --sink-uri="$SINK_URI" --changefeed-id="test-1"
 
 # test for case 1: upstream server is set to NO_BACKSLASH_ESCAPES mode. downstream server is set to NO_BACKSLASH_ESCAPES mode.
-run_sql "set global sql_mode='NO_BACKSLASH_ESCAPES';" ${UP_TIDB_HOST} ${UP_TIDB_PORT}
-run_sql "set global sql_mode='NO_BACKSLASH_ESCAPES';" ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT}
-run_sql "use test; \
-         create table t1(id bigint primary key, a text, b text as ((regexp_replace(a, "^[1-9]\d{9,29}$", "aaaaa"))), c text); \
-         insert into t1 (id, a, c) values(1,123456, 'ab\\\\c'); \
-         insert into t1 (id, a, c) values(1,1234567890123, 'ab\\c');" ${UP_TIDB_HOST} ${UP_TIDB_PORT}
+run_sql "use test; create table t1(id bigint primary key, a text, b text as ((regexp_replace(a, '^[1-9]\d{9,29}$', 'aaaaa'))), c text); insert into t1 (id, a, c) values(1,123456, 'ab\\\\\\\\c'); insert into t1 (id, a, c) values(2,1234567890123, 'ab\\\\c');" ${UP_TIDB_HOST} ${UP_TIDB_PORT}
 
 check_table_exists "test.t1" ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT}
+
+sleep 100
+
 run_sql "SELECT * from test.t1" ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT} &&
 		check_contains "b: 123456" &&
 		check_contains "b: aaaaa" &&
-		check_contains "c: ab\\\\c" &&
-		check_contains "c: ab\\c" 
+		check_contains "c: ab\\\\\\\\c" &&
+		check_contains "c: ab\\\\c" 
 
 
