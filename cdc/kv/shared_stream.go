6d978575bd8b8ccf4a1aab1a896ee69d7ad1de71
3AceShowHand <jinl1037@hotmail.com>
2024-03-28 16:25:41 +0800

    use gctx to control stream.

--- cdc/kv/shared_stream.go
+++ cdc/kv/shared_stream.go
@@ -171,7 +171,7 @@ func (s *requestedStream) run(ctx context.Context, c *SharedClient, rs *requeste
 	}()
 
 	g, gctx := errgroup.WithContext(ctx)
-	cc, err := c.grpcPool.Connect(ctx, rs.storeAddr)
+	cc, err := c.grpcPool.Connect(gctx, rs.storeAddr)
 	if err != nil {
 		log.Warn("event feed create grpc stream failed",
 			zap.String("namespace", c.changefeed.Namespace),
