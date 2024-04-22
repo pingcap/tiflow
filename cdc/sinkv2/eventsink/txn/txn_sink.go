c3f88235bc30ad0173bd585b87a683ca1cf9280f
CharlesCheung <61726649+CharlesCheung96@users.noreply.github.com>
2024-04-22 15:36:09 +0800

    sink(ticdc): limit the number of transactions cached in a mysql worker (#10892)
    close pingcap/tiflow#10896

--- cdc/sinkv2/eventsink/txn/txn_sink.go
+++ cdc/sinkv2/eventsink/txn/txn_sink.go
@@ -44,7 +44,7 @@ var _ eventsink.EventSink[*model.SingleTableTxn] = (*sink)(nil)
 type sink struct {
 	alive struct {
 		sync.RWMutex
-		conflictDetector *causality.ConflictDetector[*worker, *txnEvent]
+		conflictDetector *causality.ConflictDetector[*txnEvent]
 		isDead           bool
 	}
 	scheme string
@@ -103,14 +103,19 @@ func newSink(ctx context.Context, backends []backend,
 		dead:    make(chan struct{}),
 	}
 
+	sink.alive.conflictDetector = causality.NewConflictDetector[*txnEvent](conflictDetectorSlots, causality.TxnCacheOption{
+		Count:         len(backends),
+		Size:          1024,
+		BlockStrategy: causality.BlockStrategyWaitEmpty,
+	})
+
 	g, ctx1 := errgroup.WithContext(ctx)
 	for i, backend := range backends {
 		w := newWorker(ctx1, i, backend, len(backends))
-		g.Go(func() error { return w.runLoop() })
+		txnCh := sink.alive.conflictDetector.GetOutChByCacheID(int64(i))
+		g.Go(func() error { return w.runLoop(txnCh) })
 		sink.workers = append(sink.workers, w)
 	}
-
-	sink.alive.conflictDetector = causality.NewConflictDetector[*worker, *txnEvent](sink.workers, conflictDetectorSlots)
 
 	sink.wg.Add(1)
 	go func() {
@@ -166,9 +171,6 @@ func (s *sink) Close() {
 	}
 	s.wg.Wait()
 
-	for _, w := range s.workers {
-		w.close()
-	}
 	if s.statistics != nil {
 		s.statistics.Close()
 	}
