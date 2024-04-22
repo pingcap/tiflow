5ade819c3cf7901c7a632e4d2e7075a65696c7d4
dongmen <414110582@qq.com>
2024-04-22 20:14:53 +0800

    simple (ticdc): add comment for benchmark test
    Signed-off-by: dongmen <414110582@qq.com>

--- pkg/sink/codec/simple/marshaller_bench_test.go
+++ pkg/sink/codec/simple/marshaller_bench_test.go
@@ -1,7 +1,6 @@
 package simple
 
 import (
-	"sync"
 	"testing"
 
 	"github.com/pingcap/errors"
@@ -12,11 +11,8 @@ import (
 )
 
 var event *model.RowChangedEvent
-var lock = sync.Mutex{}
 
 func eventGenerator() *model.RowChangedEvent {
-	lock.Lock()
-	defer lock.Unlock()
 	if event == nil {
 		t := &testing.T{}
 		_, insertEvent, _, _ := utils.NewLargeEvent4Test(t, config.GetDefaultReplicaConfig())
@@ -25,24 +21,31 @@ func eventGenerator() *model.RowChangedEvent {
 	return event
 }
 
+// Note(dongmen): Below is the result of running the benchmark at 2024-4-22.
+// goos: linux
+// goarch: amd64
+// pkg: github.com/pingcap/tiflow/pkg/sink/codec/simple
+// cpu: Intel(R) Xeon(R) Gold 6240 CPU @ 2.60GHz
+// BenchmarkMarshalRowChangedEvent-16    	   47527	     29011 ns/op	    8161 B/op	     130 allocs/op
 func BenchmarkMarshalRowChangedEvent(b *testing.B) {
 	codecConfig := common.NewConfig(config.ProtocolSimple)
 	avroMarshaller, err := newAvroMarshaller(codecConfig, string(avroSchemaBytes))
 	if err != nil {
 		panic(err)
 	}
-	e := eventGenerator()
-	if e == nil {
+	rowChangeEvent := eventGenerator()
+	if rowChangeEvent == nil {
 		panic(errors.New("event is nil"))
 	}
-	// initialize your event here
-	handleKeyOnly := false   // set your handleKeyOnly value
-	claimCheckFileName := "" // set your claimCheckFileName
+	handleKeyOnly := false
+	claimCheckFileName := ""
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		//log.Info("marshal row changed event", zap.Any("event", e))
-		_, err := avroMarshaller.MarshalRowChangedEvent(e, handleKeyOnly, claimCheckFileName)
+		_, err := avroMarshaller.MarshalRowChangedEvent(
+			rowChangeEvent,
+			handleKeyOnly,
+			claimCheckFileName)
 		if err != nil {
 			panic(errors.Trace(err))
 		}
