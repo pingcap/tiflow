a0fb04ff9d286f0905b851c5ba98fc65fa335e39
hongyunyan <649330952@qq.com>
2024-04-23 14:41:56 +0800

    update

--- cdc/sink/dmlsink/txn/mysql/mysql.go
+++ cdc/sink/dmlsink/txn/mysql/mysql.go
@@ -842,7 +842,7 @@ func logDMLTxnErr(
 			zap.String("query", query), zap.Int("count", count),
 			zap.String("changefeed", changefeed))
 	}
-	return fmt.Errorf("%v; Query info: %s", err, query)
+	return errors.WithMessage(err, fmt.Sprintf("Failed query info: %s; ", query))
 }
 
 func isRetryableDMLError(err error) bool {
