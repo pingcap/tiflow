6a96bf34a97fa1a664b2ef218fed2cf193be024c
CharlesCheung <61726649+CharlesCheung96@users.noreply.github.com>
2024-04-29 18:52:49 +0800

    Update run.sh

--- tests/integration_tests/sequence/run.sh
+++ tests/integration_tests/sequence/run.sh
@@ -47,14 +47,8 @@ function run() {
 }
 
 trap stop_tidb_cluster EXIT
-<<<<<<< HEAD
 # FIXME: re-enable the case later.
 # run $*
 # check_logs $WORK_DIR
 # echo "[$(date)] <<<<<< run test case $TEST_NAME success! >>>>>>"
 echo "case $TEST_NAME is skipped"
-=======
-# run $*
-check_logs $WORK_DIR
-echo "[$(date)] <<<<<< run test case $TEST_NAME success! >>>>>>"
->>>>>>> 36e9e1bf6f (cli(ticdc): allow client authentication to be enabled without tls (#11005))
