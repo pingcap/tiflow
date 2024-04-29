ffee17a6556cd8014ee7ddaf7b1fb62352f7d19b
jiangjianyuan <sdojjy@qq.com>
2024-04-29 15:02:49 +0800

    add ut case for open protocol

--- pkg/sink/codec/open/open_protocol_message.go
+++ pkg/sink/codec/open/open_protocol_message.go
@@ -149,7 +149,7 @@ func rowChangeToMsg(
 			value.PreColumns = rowChangeColumns2CodecColumns(e.GetPreColumns(), largeMessageOnlyHandleKeyColumns)
 		}
 		if largeMessageOnlyHandleKeyColumns && (len(value.Update) == 0 ||
-			(len(value.PreColumns) == 0 && !config.OpenOutputOldValue)) {
+			(len(value.PreColumns) == 0 && config.OpenOutputOldValue)) {
 			return nil, nil, cerror.ErrOpenProtocolCodecInvalidData.GenWithStack("not found handle key columns for the update event")
 		}
 		if config.OnlyOutputUpdatedColumns {
