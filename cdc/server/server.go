33c7436a90b1b9ae4e53c00b5f6e8ab05e7c4878
dongmen <20351731+asddongmen@users.noreply.github.com>
2024-05-13 22:35:48 +0800

    pdClient  (ticdc): Enable the pdClient forwarding function to make cdc more stable during network isolation between the PD leader (#11076)
    close pingcap/tiflow#10849

--- cdc/server/server.go
+++ cdc/server/server.go
@@ -151,7 +151,8 @@ func (s *server) prepare(ctx context.Context) error {
 				},
 				MinConnectTimeout: 3 * time.Second,
 			}),
-		))
+		),
+		pd.WithForwardingOption(config.EnablePDForwarding))
 	if err != nil {
 		return errors.Trace(err)
 	}
