79be443cc219aacbe3dd7da0e21a9d3d69cbe5b5
dongmen <414110582@qq.com>
2024-05-13 14:25:40 +0800

    Revert "kv (ticdc): reduce some verbose log"
    This reverts commit 1a38104c053fabe00c98253f820e41be1daa7802.

--- cdc/kv/shared_client.go
+++ cdc/kv/shared_client.go
@@ -815,7 +815,7 @@ func (s *SharedClient) logSlowRegions(ctx context.Context) error {
 			return ctx.Err()
 		case <-ticker.C:
 		}
-		s.logRegionDetails("event feed starts to check locked regions",
+		log.Info("event feed starts to check locked regions",
 			zap.String("namespace", s.changefeed.Namespace),
 			zap.String("changefeed", s.changefeed.ID))
 
@@ -892,7 +892,7 @@ func (s *SharedClient) newSubscribedTable(
 func (r *subscribedTable) resolveStaleLocks(s *SharedClient, targetTs uint64) {
 	util.MustCompareAndMonotonicIncrease(&r.staleLocksTargetTs, targetTs)
 	res := r.rangeLock.IterAll(r.tryResolveLock)
-	s.logRegionDetails("event feed finds slow locked ranges",
+	log.Warn("event feed finds slow locked ranges",
 		zap.String("namespace", s.changefeed.Namespace),
 		zap.String("changefeed", s.changefeed.ID),
 		zap.Any("subscriptionID", r.subscriptionID),
