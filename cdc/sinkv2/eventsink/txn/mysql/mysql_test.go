3a0127bbd565679f3b684840b0817a343ad02d31
lidezhu <lidezhu@pingcap.com>
2024-05-17 12:53:55 +0800

    fix typo

--- cdc/sinkv2/eventsink/txn/mysql/mysql_test.go
+++ cdc/sinkv2/eventsink/txn/mysql/mysql_test.go
@@ -83,7 +83,7 @@ func newTestMockDB(t *testing.T) (db *sql.DB, mock sqlmock.Sqlmock) {
 		Number:  1305,
 		Message: "FUNCTION test.tidb_version does not exist",
 	})
-	// mock a differnt possible error for the second query
+	// mock a different possible error for the second query
 	mock.ExpectQuery("select tidb_version()").WillReturnError(&dmysql.MySQLError{
 		Number:  1044,
 		Message: "Access denied for user 'cdc'@'%' to database 'information_schema'",
