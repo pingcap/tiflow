4066cba2b6fc54461f8dc8e56d8f31d2b35e9633
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2024-05-20 16:28:15 +0800

    *(cdc): accept access denied error when query tidb_version (#11129) (#11142)
    close pingcap/tiflow#11128

--- cdc/sink/dmlsink/txn/mysql/mysql_test.go
+++ cdc/sink/dmlsink/txn/mysql/mysql_test.go
@@ -91,9 +91,10 @@ func newTestMockDB(t *testing.T) (db *sql.DB, mock sqlmock.Sqlmock) {
 		Number:  1305,
 		Message: "FUNCTION test.tidb_version does not exist",
 	})
+	// mock a different possible error for the second query
 	mock.ExpectQuery("select tidb_version()").WillReturnError(&dmysql.MySQLError{
-		Number:  1305,
-		Message: "FUNCTION test.tidb_version does not exist",
+		Number:  1044,
+		Message: "Access denied for user 'cdc'@'%' to database 'information_schema'",
 	})
 	require.Nil(t, err)
 	return
