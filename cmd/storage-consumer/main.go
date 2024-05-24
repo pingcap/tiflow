bbf14f891ecd8a991f41399f9c1148fc8808cdb6
lidezhu <lidezhu@pingcap.com>
2024-05-24 17:41:58 +0800

    small fix

--- cmd/storage-consumer/main.go
+++ cmd/storage-consumer/main.go
@@ -197,7 +197,7 @@ func newConsumer(ctx context.Context) (*consumer, error) {
 		return nil, err
 	}
 
-	ddlSink, err := ddlfactory.New(ctx, downstreamURIStr, config.GetDefaultReplicaConfig())
+	ddlSink, err := ddlfactory.New(ctx, downstreamURIStr, replicaConfig)
 	if err != nil {
 		log.Error("failed to create ddl sink", zap.Error(err))
 		return nil, err
