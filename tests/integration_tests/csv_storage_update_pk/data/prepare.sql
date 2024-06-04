cd87e9334fcf15d2f813baa9744818de545bf037
CharlesCheung96 <xiaoshuaizx@gmail.com>
2024-06-04 18:42:20 +0800

    fix it

--- tests/integration_tests/csv_storage_update_pk/data/prepare.sql
+++ tests/integration_tests/csv_storage_update_pk/data/prepare.sql
@@ -16,9 +16,10 @@ CREATE TABLE `update_uk` (
   `id` int NOT NULL,
   `uk` int NOT NULL,
   `pad` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
   UNIQUE KEY `uk` (`uk`)
 );
-INSERT INTO `update_pk` (`id`, `uk`, `pad`) VALUES (1, 1, 'example1'), (2, 2, 'example2');
-INSERT INTO `update_pk` (`id`, `uk`, `pad`) VALUES (100, 100, 'example100');
-INSERT INTO `update_pk` (`id`, `uk`, `pad`) VALUES (1000, 1000, 'example1000');
+INSERT INTO `update_uk` (`id`, `uk`, `pad`) VALUES (1, 1, 'example1'), (2, 2, 'example2');
+INSERT INTO `update_uk` (`id`, `uk`, `pad`) VALUES (10, 10, 'example10'), (20, 20, 'example20');
+INSERT INTO `update_uk` (`id`, `uk`, `pad`) VALUES (100, 100, 'example100');
+INSERT INTO `update_uk` (`id`, `uk`, `pad`) VALUES (1000, 1000, 'example1000');
