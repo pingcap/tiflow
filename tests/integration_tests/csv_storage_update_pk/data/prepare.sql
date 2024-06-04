b4a06d1cdf1b388e82ae5bbef6cf50a1180274ff
CharlesCheung96 <xiaoshuaizx@gmail.com>
2024-06-04 22:17:52 +0800

    fix apiv2

--- tests/integration_tests/csv_storage_update_pk/data/prepare.sql
+++ tests/integration_tests/csv_storage_update_pk/data/prepare.sql
@@ -3,23 +3,25 @@ create database `test`;
 use `test`;
 
 CREATE TABLE `update_pk` (
-  `id` int NOT NULL,
-  `pad` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`)
+  `id` int PRIMARY KEY NONCLUSTERED,
+  `pad` varchar(100) NOT NULL
 );
 INSERT INTO `update_pk` (`id`, `pad`) VALUES (1, 'example1'), (2, 'example2');
 INSERT INTO `update_pk` (`id`, `pad`) VALUES (10, 'example10'), (20, 'example20');
 INSERT INTO `update_pk` (`id`, `pad`) VALUES (100, 'example100');
 INSERT INTO `update_pk` (`id`, `pad`) VALUES (1000, 'example1000');
 
+SHOW INDEX FROM update_pk;
+
 CREATE TABLE `update_uk` (
-  `id` int NOT NULL,
+  `id` int PRIMARY KEY NONCLUSTERED,
   `uk` int NOT NULL,
   `pad` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`),
   UNIQUE KEY `uk` (`uk`)
 );
 INSERT INTO `update_uk` (`id`, `uk`, `pad`) VALUES (1, 1, 'example1'), (2, 2, 'example2');
 INSERT INTO `update_uk` (`id`, `uk`, `pad`) VALUES (10, 10, 'example10'), (20, 20, 'example20');
 INSERT INTO `update_uk` (`id`, `uk`, `pad`) VALUES (100, 100, 'example100');
 INSERT INTO `update_uk` (`id`, `uk`, `pad`) VALUES (1000, 1000, 'example1000');
+
+SHOW INDEX FROM update_uk;
