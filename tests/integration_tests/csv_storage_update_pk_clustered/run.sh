cc5e465ef7bfac928715759cab2abeef7af20bce
CharlesCheung96 <xiaoshuaizx@gmail.com>
2024-06-05 00:53:21 +0800

    fix tests

--- tests/integration_tests/csv_storage_update_pk_clustered/run.sh
+++ tests/integration_tests/csv_storage_update_pk_clustered/run.sh
@@ -8,6 +8,24 @@ WORK_DIR=$OUT_DIR/$TEST_NAME
 CDC_BINARY=cdc.test
 SINK_TYPE=$1
 
+function run_changefeed() {
+	local changefeed_id=$1
+	local start_ts=$2
+	local expected_split_count=$3
+	SINK_URI="file://$WORK_DIR/storage_test/$changefeed_id?flush-interval=5s"
+	run_cdc_cli changefeed create --start-ts=$start_ts --sink-uri="$SINK_URI" --config=$CUR/conf/$changefeed_id.toml -c "$changefeed_id"
+
+	run_storage_consumer $WORK_DIR $SINK_URI $CUR/conf/$changefeed_id.toml ""
+	sleep 8
+	check_sync_diff $WORK_DIR $CUR/conf/diff_config.toml 100
+
+	real_split_count=$(grep "split update event" $WORK_DIR/cdc.log | wc -l)
+	if [[ $real_split_count  -ne $expected_split_count ]]; then
+		echo "expected split count $expected_split_count, real split count $real_split_count"
+		exit 1
+	fi
+}
+
 function run() {
 	if [ "$SINK_TYPE" != "storage" ]; then
 		return
@@ -18,34 +36,15 @@ function run() {
 	cd $WORK_DIR
 	run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY
 
-	SINK_URI1="file://$WORK_DIR/storage_test/changefeed1?flush-interval=5s"
-	run_cdc_cli changefeed create --sink-uri="$SINK_URI1" --config=$CUR/conf/changefeed1.toml -c "changefeed1"
-	SINK_URI2="file://$WORK_DIR/storage_test/changefeed2?flush-interval=5s"
-	run_cdc_cli changefeed create --sink-uri="$SINK_URI2" --config=$CUR/conf/changefeed2.toml -c "changefeed2"
-	SINK_URI3="file://$WORK_DIR/storage_test/changefeed3?flush-interval=5s"
-	run_cdc_cli changefeed create --sink-uri="$SINK_URI3" --config=$CUR/conf/changefeed3.toml -c "changefeed3"
-	SINK_URI4="file://$WORK_DIR/storage_test/changefeed4?flush-interval=5s"
-	run_cdc_cli changefeed create --sink-uri="$SINK_URI4" --config=$CUR/conf/changefeed4.toml -c "changefeed4"
+	start_ts=$(run_cdc_cli_tso_query ${UP_PD_HOST_1} ${UP_PD_PORT_1})
 
 	run_sql_file $CUR/data/prepare.sql ${UP_TIDB_HOST} ${UP_TIDB_PORT}
-	sleep 10
 	run_sql_file $CUR/data/run.sql ${UP_TIDB_HOST} ${UP_TIDB_PORT}
 
-	run_storage_consumer $WORK_DIR $SINK_URI1 $CUR/conf/changefeed1.toml ""
-	sleep 8
-	check_sync_diff $WORK_DIR $CUR/conf/diff_config.toml 100
-
-	run_storage_consumer $WORK_DIR $SINK_URI2 $CUR/conf/changefeed2.toml ""
-	sleep 8
-	check_sync_diff $WORK_DIR $CUR/conf/diff_config.toml 100
-
-	run_storage_consumer $WORK_DIR $SINK_URI3 $CUR/conf/changefeed3.toml ""
-	sleep 8
-	check_sync_diff $WORK_DIR $CUR/conf/diff_config.toml 100
-
-	run_storage_consumer $WORK_DIR $SINK_URI4 $CUR/conf/changefeed4.toml ""
-	sleep 8
-	check_sync_diff $WORK_DIR $CUR/conf/diff_config.toml 100
+	run_changefeed "changefeed1" $start_ts 5
+	run_changefeed "changefeed2" $start_ts 5
+	run_changefeed "changefeed3" $start_ts 5
+	run_changefeed "changefeed4" $start_ts 10
 }
 
 trap stop_tidb_cluster EXIT
