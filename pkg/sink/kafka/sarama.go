531c9f3d4ddfe0f8300e1209ad40ad343d8c1995
3AceShowHand <jinl1037@hotmail.com>
2024-06-05 18:40:08 +0800

    Revert "enable Idempotent and set MaxOpenRequests to be 1"
    This reverts commit e8a61937d0a61bc6009df10d68e4e02aa32ec564.

--- pkg/sink/kafka/sarama.go
+++ pkg/sink/kafka/sarama.go
@@ -60,7 +60,6 @@ func NewSaramaConfig(ctx context.Context, o *Options) (*sarama.Config, error) {
 	// or fail as soon as possible is preferred.
 	config.Producer.Retry.Max = 3
 	config.Producer.Retry.Backoff = 100 * time.Millisecond
-	config.Producer.Idempotent = true
 
 	// make sure sarama producer flush messages as soon as possible.
 	config.Producer.Flush.Bytes = 0
@@ -71,7 +70,6 @@ func NewSaramaConfig(ctx context.Context, o *Options) (*sarama.Config, error) {
 	config.Net.DialTimeout = o.DialTimeout
 	config.Net.WriteTimeout = o.WriteTimeout
 	config.Net.ReadTimeout = o.ReadTimeout
-	config.Net.MaxOpenRequests = 1
 
 	config.Producer.Partitioner = sarama.NewManualPartitioner
 	config.Producer.MaxMessageBytes = o.MaxMessageBytes
