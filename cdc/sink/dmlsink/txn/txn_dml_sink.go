858808209cf9894e808655e6144a9af5f1d48e38
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2024-06-13 19:43:55 +0800

    mysql (ticdc): Improve the performance of the mysql sink by refining the transaction event batching logic (#10466) (#11248)
    close pingcap/tiflow#11241

--- cdc/sink/dmlsink/txn/txn_dml_sink.go
+++ cdc/sink/dmlsink/txn/txn_dml_sink.go
@@ -117,7 +117,7 @@ func newSink(ctx context.Context,
 	for i, backend := range backends {
 		w := newWorker(ctx1, changefeedID, i, backend, len(backends))
 		txnCh := sink.alive.conflictDetector.GetOutChByCacheID(int64(i))
-		g.Go(func() error { return w.runLoop(txnCh) })
+		g.Go(func() error { return w.run(txnCh) })
 		sink.workers = append(sink.workers, w)
 	}
 
