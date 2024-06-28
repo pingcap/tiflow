376db4c8d1f95e3746a1c7878e6ad3c044d5db5a
CharlesCheung96 <xiaoshuaizx@gmail.com>
2024-06-28 11:42:59 +0800

    fix it

--- tests/integration_tests/mq_split_by_partition_key_uk/data/data.sql
+++ tests/integration_tests/mq_split_by_partition_key_uk/data/data.sql
@@ -4,10 +4,12 @@ use test;
 
 CREATE TABLE t (a INT PRIMARY KEY NONCLUSTERED, b INT);
 
-INSERT INTO t VALUES (1, 2);
+-- b = 222, partition = 2
+INSERT INTO t VALUES (1, 222);
 UPDATE t SET a = 2 WHERE a = 1;
 
-INSERT INTO t VALUES (1, 3);
-UPDATE t SET a="3" WHERE a="1";
+-- b = 333333, partition = 1
+INSERT INTO t VALUES (1, 333333);
+UPDATE t SET a=3 WHERE a=1;
 
 CREATE TABLE test.finish_mark (a int primary key);
