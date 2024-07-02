c52f11455a78e3506402dd8c342f1d422b4e0985
lidezhu <lidezhu@pingcap.com>
2024-07-02 11:29:43 +0800

    small fix

--- tests/integration_tests/ci_collation_compatibility/data/test.sql
+++ tests/integration_tests/ci_collation_compatibility/data/test.sql
@@ -2,10 +2,10 @@ drop database if exists `ci_collation_compatibility`;
 create database `ci_collation_compatibility`;
 use `ci_collation_compatibility`;
 
-CREATE TABLE t1 (
+CREATE TABLE t (
     a varchar(20) not null,
     b int default 10,
     PRIMARY KEY (`a`) /*T![clustered_index] CLUSTERED */
 );
 
-insert into t1 values ('hello', 1);
+insert into t values ('hello', 1);
