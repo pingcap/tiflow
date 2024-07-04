d8d4d1ae44590eb71998987a79149b1379318bbe
3AceShowHand <jinl1037@hotmail.com>
2024-07-04 14:23:21 +0800

    disable clustered index

--- tests/integration_tests/kafka_simple_basic/data/data.sql
+++ tests/integration_tests/kafka_simple_basic/data/data.sql
@@ -1,7 +1,6 @@
 use test;
 
-insert into tp_int()
-values ();
+insert into tp_int() values ();
 
 insert into tp_int(c_tinyint, c_smallint, c_mediumint, c_int, c_bigint)
 values (1, 2, 3, 4, 5);
@@ -14,13 +13,18 @@ values (127, 32767, 8388607, 2147483647, 9223372036854775807);
 insert into tp_int(c_tinyint, c_smallint, c_mediumint, c_int, c_bigint)
 values (-128, -32768, -8388608, -2147483648, -9223372036854775808);
 
+-- not key changed update
 update tp_int set c_int = 0, c_tinyint = 0 where c_smallint = 2;
+
 delete from tp_int where c_int = 0;
 
+rename table tp_int to tp_int2;
+update tp_int2 set c_int = 5 where id = 2;
 
+-- key changed update
+update tp_int2 set id = 5 where id = 3;
 
-insert into tp_unsigned_int()
-values ();
+insert into tp_unsigned_int() values ();
 
 insert into tp_unsigned_int(c_unsigned_tinyint, c_unsigned_smallint, c_unsigned_mediumint,
                             c_unsigned_int, c_unsigned_bigint)
@@ -45,8 +49,7 @@ delete from tp_unsigned_int where c_unsigned_int = 0;
 
 
 
-insert into tp_real()
-values ();
+insert into tp_real() values ();
 
 insert into tp_real(c_float, c_double, c_decimal, c_decimal_2)
 values (2020.0202, 2020.0303, 2020.0404, 2021.1208);
@@ -56,9 +59,7 @@ values (-2.7182818284, -3.1415926, -8000, -179394.233);
 
 update tp_real set c_double = 2.333 where c_double = 2020.0303;
 
-
-insert into tp_unsigned_real()
-values ();
+insert into tp_unsigned_real() values ();
 
 insert into tp_unsigned_real(c_unsigned_float, c_unsigned_double, c_unsigned_decimal, c_unsigned_decimal_2)
 values (2020.0202, 2020.0303, 2020.0404, 2021.1208);
@@ -78,8 +79,7 @@ create table tp_time
         primary key (id)
 );
 
-insert into tp_time()
-values ();
+insert into tp_time() values ();
 
 insert into tp_time(c_date, c_datetime, c_timestamp, c_time, c_year)
 values ('2020-02-20', '2020-02-20 02:20:20', '2020-02-20 02:20:20', '02:20:20', '2020');
@@ -89,6 +89,23 @@ values ('2022-02-22', '2022-02-22 22:22:22', '2020-02-20 02:20:20', '02:20:20', 
 
 update tp_time set c_year = '2022' where c_year = '2020';
 update tp_time set c_date = '2022-02-22' where c_datetime = '2020-02-20 02:20:20';
+
+alter table tp_time add column c_timestamp2 timestamp default current_timestamp;
+
+insert into tp_time(c_date, c_datetime, c_timestamp, c_time, c_year)
+values ('2024-03-09', '2022-02-22 22:22:22', '2020-02-20 02:20:20', '02:20:20', '2021');
+
+alter table tp_time drop column c_timestamp2;
+
+alter table tp_time add column c_timestamp2 timestamp default now();
+
+insert into tp_time(c_date, c_datetime, c_timestamp, c_time, c_year)
+values ('2024-03-09', '2022-02-22 22:22:22', '2020-02-20 02:20:20', '02:20:20', '2021');
+
+alter table tp_time alter column c_timestamp2 drop default;
+
+insert into tp_time(c_date, c_datetime, c_timestamp, c_time, c_year, c_timestamp2)
+values ('2024-03-09', '2022-02-22 22:22:22', '2020-02-20 02:20:20', '02:20:20', '2021', "2024-03-09 18:00:00");
 
 -- text
 create table tp_text
@@ -102,8 +119,7 @@ create table tp_text
         primary key (id)
 );
 
-insert into tp_text()
-values ();
+insert into tp_text() values ();
 
 insert into tp_text(c_tinytext, c_text, c_mediumtext, c_longtext)
 values ('89504E470D0A1A0A', '89504E470D0A1A0A', '89504E470D0A1A0A', '89504E470D0A1A0A');
@@ -125,8 +141,7 @@ create table tp_blob
         primary key (id)
 );
 
-insert into tp_blob()
-values ();
+insert into tp_blob() values ();
 
 insert into tp_blob(c_tinyblob, c_blob, c_mediumblob, c_longblob)
 values (x'89504E470D0A1A0A', x'89504E470D0A1A0A', x'89504E470D0A1A0A', x'89504E470D0A1A0A');
@@ -148,8 +163,7 @@ create table tp_char_binary
         primary key (id)
 );
 
-insert into tp_char_binary()
-values ();
+insert into tp_char_binary() values ();
 
 insert into tp_char_binary(c_char, c_varchar, c_binary, c_varbinary)
 values ('89504E470D0A1A0A', '89504E470D0A1A0A', x'89504E470D0A1A0A', x'89504E470D0A1A0A');
@@ -165,7 +179,7 @@ create table tp_other
     id     int auto_increment,
     c_enum enum ('a','b','c') null,
     c_set  set ('a','b','c')  null,
-    c_bit  bit(64)            null,
+    c_bit  bit(10)            null,
     c_json json               null,
     constraint pk
         primary key (id)
@@ -188,6 +202,31 @@ values ('b', 'b,c', b'1000001', '{
 }');
 
 update tp_other set c_enum = 'c' where c_set = 'b,c';
+
+alter table tp_other modify column c_enum enum ('a', 'b', 'c','d') null;
+
+insert into tp_other(c_enum, c_set, c_bit, c_json)
+values ('d', 'b,c', b'1000001', '{
+  "key1": "value1",
+  "key2": "value2",
+  "key3": "123"
+}');
+
+alter table tp_other modify column c_enum enum ('d', 'c', 'b', 'a') null;
+
+update tp_other set c_enum = 'a' where c_enum = 'd';
+
+alter table tp_other modify column c_set set ('a', 'b', 'c', 'd') null;
+
+update tp_other set c_set = 'b,c' where c_set = 'a,b';
+
+alter table tp_other modify column c_bit bit(20) null;
+
+update tp_other set c_bit = b'1' where c_bit = b'1000001';
+
+alter table tp_other modify column c_bit bit(1) null;
+
+update tp_other set c_bit = b'0' where c_bit = b'1';
 
 -- gbk dmls
 CREATE TABLE cs_gbk (
