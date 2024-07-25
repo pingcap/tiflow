98901f88bf2b651caab407f17c178ec50429e026
3AceShowHand <jinl1037@hotmail.com>
2024-07-25 15:50:52 +0800

    fix import

--- pkg/sink/codec/canal/canal_json_message.go
+++ pkg/sink/codec/canal/canal_json_message.go
@@ -15,19 +15,14 @@ package canal
 
 import (
 	"sort"
-	"strconv"
 	"strings"
 
-	"github.com/pingcap/log"
 	timodel "github.com/pingcap/tidb/pkg/parser/model"
-	"github.com/pingcap/tidb/pkg/parser/mysql"
 	"github.com/pingcap/tidb/pkg/parser/types"
 	"github.com/pingcap/tiflow/cdc/model"
 	cerrors "github.com/pingcap/tiflow/pkg/errors"
 	"github.com/pingcap/tiflow/pkg/sink/codec/internal"
 	canal "github.com/pingcap/tiflow/proto/canal"
-	"go.uber.org/zap"
-	"golang.org/x/text/encoding/charmap"
 )
 
 const tidbWaterMarkType = "TIDB_WATERMARK"
@@ -225,47 +220,6 @@ func extractBasicMySQLType(mysqlType string) string {
 		}
 	}
 	return mysqlType
-}
-
-func canalJSONFormatColumn(value interface{}, name string, mysqlTypeStr string) *model.Column {
-	mysqlTypeStr = extractBasicMySQLType(mysqlTypeStr)
-	mysqlType := types.StrToType(mysqlTypeStr)
-
-	result := &model.Column{
-		Type:  mysqlType,
-		Name:  name,
-		Value: value,
-	}
-	if result.Value == nil {
-		return result
-	}
-
-	data, ok := value.(string)
-	if !ok {
-		log.Panic("canal-json encoded message should have type in `string`")
-	}
-
-	if mysqlType == mysql.TypeBit || mysqlType == mysql.TypeSet {
-		val, err := strconv.ParseUint(data, 10, 64)
-		if err != nil {
-			log.Panic("invalid column value for bit", zap.Any("col", result), zap.Error(err))
-		}
-		result.Value = val
-		return result
-	}
-
-	var err error
-	if isBinaryMySQLType(mysqlTypeStr) {
-		// when encoding the `JavaSQLTypeBLOB`, use `ISO8859_1` decoder, now reverse it back.
-		encoder := charmap.ISO8859_1.NewEncoder()
-		value, err = encoder.String(data)
-		if err != nil {
-			log.Panic("invalid column value, please report a bug", zap.Any("col", result), zap.Error(err))
-		}
-	}
-
-	result.Value = value
-	return result
 }
 
 func isBinaryMySQLType(mysqlType string) bool {
