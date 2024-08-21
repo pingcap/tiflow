1fea47162bc191a0831dc49af26ae6a65dbbcf28
wlwilliamx <wlwilliamx@gmail.com>
2024-08-21 17:58:21 +0800

    refactor(CheckIsTiDB): remove returned error and modify the log output from error to warning

--- cdc/sink/ddlsink/mysql/mysql_ddl_sink.go
+++ cdc/sink/ddlsink/mysql/mysql_ddl_sink.go
@@ -91,10 +91,7 @@ func NewDDLSink(
 		return nil, err
 	}
 
-	cfg.IsTiDB, err = pmysql.CheckIsTiDB(ctx, db)
-	if err != nil {
-		return nil, err
-	}
+	cfg.IsTiDB = pmysql.CheckIsTiDB(ctx, db)
 
 	cfg.IsWriteSourceExisted, err = pmysql.CheckIfBDRModeIsSupported(ctx, db)
 	if err != nil {
