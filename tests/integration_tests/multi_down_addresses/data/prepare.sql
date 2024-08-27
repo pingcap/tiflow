b8b6c49015f8da578273a728f3b84d5cbd48e369
wlwilliamx <wlwilliamx@gmail.com>
2024-08-27 18:29:56 +0800

    fix(multi_down_addresses): fix the wrong SQL in integration test multi_down_addresses

--- tests/integration_tests/multi_down_addresses/data/prepare.sql
+++ tests/integration_tests/multi_down_addresses/data/prepare.sql
@@ -1,3 +1,5 @@
-drop database if exists test;
-create database test;
-use test;
+drop database if exists multi_down_addresses;
+create database multi_down_addresses;
+use multi_down_addresses;
+
+CREATE TABLE round (round INT PRIMARY KEY, val INT);
