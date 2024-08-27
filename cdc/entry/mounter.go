af6f08e704dfcc433265beedcc8b47dec7976e49
3AceShowHand <jinl1037@hotmail.com>
2024-08-27 15:54:53 +0800

    ignore checksum v2

--- cdc/entry/mounter.go
+++ cdc/entry/mounter.go
@@ -440,6 +440,13 @@ func (m *mounter) verifyChecksum(
 		return 0, false, errors.New("cannot found the decoder to get the checksum")
 	}
 
+	version := decoder.ChecksumVersion()
+	if version != 0 {
+		log.Debug("checksum is not v1, this happens since upstream TiDB is in a higher version, ignore it",
+			zap.Int("version", version))
+		return 0, true, nil
+	}
+
 	// if the checksum cannot be found, which means the upstream TiDB checksum is not enabled,
 	// so return matched as true to skip check the event.
 	first, ok := decoder.GetChecksum()
