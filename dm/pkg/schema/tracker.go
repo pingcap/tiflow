91b5a316d34fa62316f1c9dbdc5571d0d273732a
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2024-09-02 16:05:16 +0800

    schematracker(dm): Increase max-index-length config for dm schema tracker (#11465) (#11546)
    close pingcap/tiflow#11459

--- dm/pkg/schema/tracker.go
+++ dm/pkg/schema/tracker.go
@@ -21,6 +21,7 @@ import (
 	"sync"
 
 	"github.com/pingcap/errors"
+	tidbConfig "github.com/pingcap/tidb/config"
 	"github.com/pingcap/tidb/ddl"
 	"github.com/pingcap/tidb/ddl/schematracker"
 	"github.com/pingcap/tidb/executor"
@@ -130,6 +131,11 @@ func (tr *Tracker) Init(
 	}()
 
 	logger = logger.WithFields(zap.String("component", "schema-tracker"), zap.String("task", task))
+
+	// set max-index-length to maximum allowable (3072*4)
+	tidbConfig.UpdateGlobal(func(conf *tidbConfig.Config) {
+		conf.MaxIndexLength = 12288
+	})
 
 	upTracker := schematracker.NewSchemaTracker(lowerCaseTableNames)
 	dsSession := mock.NewContext()
