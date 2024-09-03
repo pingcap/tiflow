68a04172d5ec0aa79e95348eab6930ef16118daa
wk989898 <wangkun989898@gmail.com>
2024-09-03 14:30:47 +0800

    update test

--- tests/integration_tests/vector/run.sh
+++ tests/integration_tests/vector/run.sh
@@ -32,8 +32,7 @@ function run() {
 	run_sql_file $CUR/data/data.sql ${UP_TIDB_HOST} ${UP_TIDB_PORT}
 	sleep 3
 	# sync_diff can't check non-exist table, so we check expected tables are created in downstream first
-	check_table_exists test.simple1 ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT}
-	check_table_exists test.simple2 ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT}
+	check_table_exists test.finish_mark ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT}
 	check_sync_diff $WORK_DIR $CUR/conf/diff_config.toml
 
 	cleanup_process $CDC_BINARY
