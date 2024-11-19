346e7a9d1d5280adbf2fbd4a8c1bc079afcc4582
Jianyuan Jiang <sdojjy@qq.com>
2024-11-19 16:26:53 +0800

    This is an automated cherry-pick of #11770
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- cdc/api/v2/api.go
+++ cdc/api/v2/api.go
@@ -86,7 +86,11 @@ func RegisterOpenAPIV2Routes(router *gin.Engine, api OpenAPIV2) {
 
 	// owner apis
 	ownerGroup := v2.Group("/owner")
+<<<<<<< HEAD
 	unsafeGroup.Use(middleware.ForwardToOwnerMiddleware(api.capture))
+=======
+	ownerGroup.Use(ownerMiddleware)
+>>>>>>> 8e3b63acec (fix resign owner api not processed by owner node bug (#11770))
 	ownerGroup.POST("/resign", api.resignOwner)
 
 	// common APIs
