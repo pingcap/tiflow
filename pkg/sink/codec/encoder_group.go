5afd54d8d51791050a4d0a07ab29f4edf88a2277
nhsmw <nhsmwk@gmail.com>
2024-11-26 14:52:20 +0800

    Update encoder_group.go

--- pkg/sink/codec/encoder_group.go
+++ pkg/sink/codec/encoder_group.go
@@ -15,13 +15,13 @@ package codec
 
 import (
 	"context"
+	"runtime"
 	"strconv"
 	"sync/atomic"
 	"time"
 
 	"github.com/pingcap/errors"
 	"github.com/pingcap/log"
-	"github.com/pingcap/tidb/pkg/util/cpu"
 	"github.com/pingcap/tiflow/cdc/model"
 	"github.com/pingcap/tiflow/cdc/sink/dmlsink"
 	"github.com/pingcap/tiflow/pkg/config"
@@ -72,7 +72,7 @@ func NewEncoderGroup(
 	if concurrency <= 0 {
 		concurrency = config.DefaultEncoderGroupConcurrency
 	}
-	limitConcurrency := cpu.GetCPUCount() * 10
+	limitConcurrency := runtime.GOMAXPROCS(0) * 10
 	if concurrency > limitConcurrency {
 		concurrency = limitConcurrency
 		log.Warn("limit concurrency to avoid crash", zap.Int("concurrency", concurrency), zap.Any("limitConcurrency", limitConcurrency))
