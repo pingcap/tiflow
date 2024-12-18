8e6a322e7e071bdf763930574c5c0c727848fc57
3AceShowHand <jinl1037@hotmail.com>
2024-12-18 17:34:24 +0800

    fix the make fmt

--- pkg/sink/codec/canal/canal_json_message.go
+++ pkg/sink/codec/canal/canal_json_message.go
@@ -14,12 +14,12 @@
 package canal
 
 import (
-	pmodel "github.com/pingcap/tidb/pkg/parser/model"
 	"strconv"
 	"strings"
 
 	"github.com/pingcap/log"
 	timodel "github.com/pingcap/tidb/pkg/meta/model"
+	pmodel "github.com/pingcap/tidb/pkg/parser/model"
 	"github.com/pingcap/tidb/pkg/parser/mysql"
 	"github.com/pingcap/tiflow/cdc/model"
 	cerrors "github.com/pingcap/tiflow/pkg/errors"
