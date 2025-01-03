f616313907a89b6dd6905b911a23449f18e5ecc7
nhsmw <nhsmwk@gmail.com>
2025-01-03 20:39:15 +0800

    Update main.go

--- cmd/pulsar-consumer/main.go
+++ cmd/pulsar-consumer/main.go
@@ -524,14 +524,14 @@ func (c *Consumer) HandleMsg(msg pulsar.Message) error {
 				// todo: mark the offset after the DDL is fully synced to the downstream mysql.
 				continue
 			}
-			var partitionID int64
-			if row.TableInfo.IsPartitionTable() {
-				partitionID = row.GetTableID()
+			tableID := row.GetTableID()
+			switch w.option.protocol {
+			case config.ProtocolSimple, config.ProtocolCanalJSON:
+			default:
+				tableID = w.fakeTableIDGenerator.
+					generateFakeTableID(row.TableInfo.GetSchemaName(), row.TableInfo.GetTableName(), tableID)
+				row.TableInfo.TableName.TableID = tableID
 			}
-			// use schema, table and tableID to identify a table
-			tableID := c.fakeTableIDGenerator.
-				generateFakeTableID(row.TableInfo.GetSchemaName(), row.TableInfo.GetTableName(), partitionID)
-			row.TableInfo.TableName.TableID = tableID
 
 			group, ok := c.eventGroups[tableID]
 			if !ok {
