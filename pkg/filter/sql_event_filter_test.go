2b1aedcca8d16f91bc0134afe6144c313f221d2b
nhsmw <nhsmwk@gmail.com>
2025-01-06 12:13:50 +0800

    Update sql_event_filter_test.go

--- pkg/filter/sql_event_filter_test.go
+++ pkg/filter/sql_event_filter_test.go
@@ -191,7 +191,7 @@ func TestShouldSkipDDL(t *testing.T) {
 				EventFilters: []*config.EventFilterRule{
 					{
 						Matcher:     []string{"*.t1"},
-						IgnoreEvent: []bf.EventType{bf.AllEvent},
+						IgnoreEvent: []bf.EventType{bf.AllDDL},
 					},
 				},
 			},
