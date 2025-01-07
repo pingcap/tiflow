8e3c48f42fa26beb710102a49d1e469f16fc3c22
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2025-01-08 00:03:58 +0800

    filter(ticdc): fix incorrect event filter with "rename" DDLs (#11956) (#11974)
    close pingcap/tiflow#11946

--- pkg/filter/sql_event_filter.go
+++ pkg/filter/sql_event_filter.go
@@ -168,6 +168,18 @@ func (f *sqlEventFilter) shouldSkipDDL(ddl *model.DDLEvent) (bool, error) {
 	if err != nil {
 		return false, err
 	}
+	if evenType == bf.RenameTable {
+		if ddl.PreTableInfo == nil {
+			log.Warn("sql event filter doesn't find old table info when the event type is `rename table`",
+				zap.String("schema", schema),
+				zap.String("table", table),
+				zap.Stringer("type", ddl.Type),
+				zap.String("query", ddl.Query))
+			return true, cerror.ErrTableIneligible.GenWithStackByArgs(ddl.TableInfo.TableName)
+		}
+		schema = ddl.PreTableInfo.TableName.Schema
+		table = ddl.PreTableInfo.TableName.Table
+	}
 	if evenType == bf.NullEvent {
 		log.Warn("sql event filter unsupported ddl type, do nothing",
 			zap.String("type", ddl.Type.String()),
