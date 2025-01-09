8a6c05e303e0b0d44541a66744b99bb5cf7712f0
wk989898 <wangkun989898@gmail.com>
2025-01-09 13:54:40 +0800

    update

--- pkg/sink/codec/debezium/helper_test.go
+++ pkg/sink/codec/debezium/helper_test.go
@@ -16,48 +16,9 @@ package debezium
 import (
 	"testing"
 
-	timodel "github.com/pingcap/tidb/pkg/meta/model"
-	pmodel "github.com/pingcap/tidb/pkg/parser/model"
-	"github.com/pingcap/tidb/pkg/parser/mysql"
-	"github.com/pingcap/tidb/pkg/types"
 	"github.com/pingcap/tiflow/cdc/model"
 	"github.com/stretchr/testify/require"
 )
-
-func TestGetColumns(t *testing.T) {
-	sql := "CREATE TABLE test (id INT PRIMARY KEY, val1 datetime default current_timestamp, val2 time(2) default 0,  val3 timestamp(3) default now(), val4 YEAR(4) default 1970 comment 'first');"
-	columnInfos := []*timodel.ColumnInfo{
-		{
-			Name:      pmodel.NewCIStr("id"),
-			FieldType: *types.NewFieldType(mysql.TypeLong),
-		},
-		{
-			Name:      pmodel.NewCIStr("val1"),
-			FieldType: *types.NewFieldType(mysql.TypeDatetime),
-		},
-		{
-			Name:      pmodel.NewCIStr("val2"),
-			FieldType: *types.NewFieldType(mysql.TypeDuration),
-		},
-		{
-			Name:      pmodel.NewCIStr("val3"),
-			FieldType: *types.NewFieldType(mysql.TypeTimestamp),
-		},
-		{
-			Name:      pmodel.NewCIStr("val4"),
-			FieldType: *types.NewFieldType(mysql.TypeYear),
-		},
-	}
-	parseColumns(sql, columnInfos)
-	require.Equal(t, columnInfos[1].GetDefaultValue(), "CURRENT_TIMESTAMP")
-	require.Equal(t, columnInfos[2].GetDecimal(), 2)
-	require.Equal(t, columnInfos[2].GetDefaultValue(), "0")
-	require.Equal(t, columnInfos[3].GetDecimal(), 3)
-	require.Equal(t, columnInfos[3].GetDefaultValue(), "CURRENT_TIMESTAMP")
-	require.Equal(t, columnInfos[4].GetFlen(), 4)
-	require.Equal(t, columnInfos[4].GetDefaultValue(), "1970")
-	require.Equal(t, columnInfos[4].Comment, "")
-}
 
 func TestGetValue(t *testing.T) {
 	column := &model.Column{
