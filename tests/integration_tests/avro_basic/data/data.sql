5fd9a2605e8d8899a3aa7aac75b40afe15adef1b
nhsmw <wangkun989898@gmail.com>
2025-01-15 19:33:34 +0800

    This is an automated cherry-pick of #11995
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- tests/integration_tests/avro_basic/data/data.sql
+++ tests/integration_tests/avro_basic/data/data.sql
@@ -164,6 +164,19 @@ insert into t(c_tinyint, c_mediumint, c_int, c_bigint, a) values (4, 5, 6, 7, 8)
 alter table t modify c_mediumint varchar(10) null;
 insert into t(c_tinyint, c_mediumint, c_int, c_bigint, a) values (5, "234", 6, 7, 8);
 
+create table t1(
+    id int primary key,
+    c1 varchar(255) default "null",
+    c2 varchar(255) default "NULL",
+    c3 varchar(255) default null
+);
+
+insert into t1(id) values(1);
+update t1 set c1 = "null", c2 = "NULL", c3 = null where id = 1;
+alter table t1 add column col json not null;
+alter table t1 modify column col json default null;
+insert into t1(id) values(2);
+
 create table finish_mark
 (
     id int PRIMARY KEY
