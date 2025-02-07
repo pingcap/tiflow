4328eb604a3455946693d19201da90a7c8a42bd7
nhsmw <nhsmwk@gmail.com>
2025-02-07 19:55:10 +0800

    test(ticdc): fix multi_source test (#12041)
    ref pingcap/tiflow#11879

--- tests/integration_tests/multi_source/main.go
+++ tests/integration_tests/multi_source/main.go
@@ -212,8 +212,9 @@ func dml(ctx context.Context, db *sql.DB, table string, id int) {
 	var deleteSuccess int
 	insertSQL := fmt.Sprintf("insert into test.`%s`(id1, id2) values(?,?)", table)
 	deleteSQL := fmt.Sprintf("delete from test.`%s` where id1 = ? or id2 = ?", table)
-	for i = 0; ; i++ {
-		_, err = db.Exec(insertSQL, i+id*100000000, i+id*100000000+1)
+	k := 100000000
+	for i = 0; i < k; i++ {
+		_, err = db.Exec(insertSQL, i+id*k, i+id*k+1)
 		if err == nil {
 			insertSuccess++
 			if insertSuccess%100 == 0 {
@@ -225,7 +226,7 @@ func dml(ctx context.Context, db *sql.DB, table string, id int) {
 		}
 
 		if i%2 == 0 {
-			result, err := db.Exec(deleteSQL, i+id*100000000, i+id*100000000+1)
+			result, err := db.Exec(deleteSQL, i+id*k, i+id*k+1)
 			if err == nil {
 				rows, _ := result.RowsAffected()
 				if rows != 0 {
