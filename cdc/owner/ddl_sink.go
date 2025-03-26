c4048f05ba6c825de314b9d206ae69ac4ff12c11
3AceShowHand <jinl1037@hotmail.com>
2025-03-26 14:43:44 +0800

    remove record duration at the ddl sink

--- cdc/owner/ddl_sink.go
+++ cdc/owner/ddl_sink.go
@@ -174,13 +174,11 @@ func (s *ddlSinkImpl) retrySinkAction(ctx context.Context, name string, action f
 			zap.Bool("retryable", isRetryable),
 			zap.Error(err))
 
-		start := time.Now()
 		if s.sink != nil {
 			s.sink.Close()
 			s.sink = nil
 			log.Info("close the ddl sink, rebuild it when trying again",
-				zap.String("namespace", s.changefeedID.Namespace), zap.String("changefeed", s.changefeedID.ID),
-				zap.Duration("duration", time.Since(start)))
+				zap.String("namespace", s.changefeedID.Namespace), zap.String("changefeed", s.changefeedID.ID))
 		}
 
 		if isRetryable {
