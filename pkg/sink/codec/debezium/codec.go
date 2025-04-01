96e40944fc75d4ecbc0b26062f2e9e5612c9ce69
nhsmw <nhsmwk@gmail.com>
2025-04-01 13:27:55 +0800

    Update codec.go

--- pkg/sink/codec/debezium/codec.go
+++ pkg/sink/codec/debezium/codec.go
@@ -1159,9 +1159,6 @@ func (c *dbzCodec) EncodeDDLEvent(
 		timodel.ActionRenameIndex,
 		timodel.ActionRenameTable,
 		timodel.ActionRenameTables,
-		// timodel.ActionRecoverTable,
-		// timodel.ActionAlterTTLInfo,
-		// timodel.ActionAlterTTLRemove,
 		timodel.ActionAddPrimaryKey,
 		timodel.ActionDropPrimaryKey:
 		changeType = "ALTER"
