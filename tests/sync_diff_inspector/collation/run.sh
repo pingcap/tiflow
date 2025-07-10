f9794bceeb3722af7e8f7314605d4dfc50fa5d0e
Ruihao Chen <ruihao.chen@pingcap.cn>
2025-07-10 13:34:48 +0800

    sync-diff-inspector: only enable new collation for data comparison (#862)
    close pingcap/tidb-tools#861

--- tests/sync_diff_inspector/collation/run.sh
+++ tests/sync_diff_inspector/collation/run.sh
@@ -12,6 +12,8 @@ mkdir -p $FIX_DIR
 
 mysql -uroot -h 127.0.0.1 -P 4000 -e "create database if not exists collation_test;"
 mysql -uroot -h 127.0.0.1 -P 4001 -e "create database if not exists collation_test;"
+mysql -uroot -h 127.0.0.1 -P 4002 -e "create database if not exists collation_test;"
+mysql -uroot -h ${MYSQL_HOST} -P ${MYSQL_PORT} -e "create database if not exists collation_test;"
 
 echo "Test1: collation with different ci"
 mysql -uroot -h 127.0.0.1 -P 4000 -e "create table collation_test.t (a varchar(16) collate utf8mb4_general_ci, b int, primary key(a, b));"
@@ -33,4 +35,14 @@ mysql -uroot -h 127.0.0.1 -P 4001 -e "insert into collation_test.t2 values ('a',
 echo "check should return three different rows"
 sync_diff_inspector --config=./config2.toml > $OUT_DIR/expression_diff.output || true
 check_contains "+3/-3" $OUT_DIR/summary.txt
+rm -rf $OUT_DIR/*
+
+echo "Test3: use collation not in new-collations"
+mysql -uroot -h ${MYSQL_HOST} -P ${MYSQL_PORT} -e "create table collation_test.t3 (id int PRIMARY KEY, name varchar(20) COLLATE utf8mb4_unicode_520_ci);"
+mysql -uroot -h ${MYSQL_HOST} -P ${MYSQL_PORT} -e "insert into collation_test.t3 values (1, 'a'), (2, 'b');"
+mysql -uroot -h 127.0.0.1 -P 4002 -e "create table collation_test.t3 (id int PRIMARY KEY, name varchar(20) COLLATE utf8mb4_unicode_520_ci);"
+mysql -uroot -h 127.0.0.1 -P 4002 -e "insert into collation_test.t3 values (1, 'a'), (2, 'b');"
+
+echo "check should pass"
+sync_diff_inspector --config=./config3.toml > $OUT_DIR/expression_diff.output
 rm -rf $OUT_DIR/*
