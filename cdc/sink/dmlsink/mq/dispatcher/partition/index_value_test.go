841924b89c143cc763e70ee3c044a52e1695106d
wk989898 <nhsmwk@gmail.com>
2025-07-11 08:35:08 +0000

    fix ut

--- cdc/sink/dmlsink/mq/dispatcher/partition/index_value_test.go
+++ cdc/sink/dmlsink/mq/dispatcher/partition/index_value_test.go
@@ -151,8 +151,8 @@ func TestIndexValueDispatcherWithIndexName(t *testing.T) {
 		Name:       pmodel.NewCIStr("t1"),
 		PKIsHandle: true,
 		Columns: []*timodel.ColumnInfo{
-			{ID: 1, Name: pmodel.NewCIStr("col2"), Offset: 1, FieldType: *types.NewFieldType(mysql.TypeLong)},
-			{ID: 2, Name: pmodel.NewCIStr("col1"), Offset: 0, FieldType: *types.NewFieldType(mysql.TypeLong)},
+			{ID: 1, Name: pmodel.NewCIStr("COL2"), Offset: 1, FieldType: *types.NewFieldType(mysql.TypeLong)},
+			{ID: 2, Name: pmodel.NewCIStr("Col1"), Offset: 0, FieldType: *types.NewFieldType(mysql.TypeLong)},
 			{ID: 3, Name: pmodel.NewCIStr("col3"), Offset: 2, FieldType: *types.NewFieldType(mysql.TypeLong)},
 		},
 		Indices: []*timodel.IndexInfo{
@@ -161,10 +161,10 @@ func TestIndexValueDispatcherWithIndexName(t *testing.T) {
 				Name:    pmodel.NewCIStr("index1"),
 				Columns: []*timodel.IndexColumn{
 					{
-						Name: pmodel.NewCIStr("col2"), Offset: 1,
+						Name: pmodel.NewCIStr("COL2"), Offset: 1,
 					},
 					{
-						Name: pmodel.NewCIStr("col1"), Offset: 0,
+						Name: pmodel.NewCIStr("Col1"), Offset: 0,
 					},
 				},
 			},
@@ -188,7 +188,7 @@ func TestIndexValueDispatcherWithIndexName(t *testing.T) {
 	p = NewIndexValueDispatcher("index1")
 	index, _, err := p.DispatchRowChangedEvent(event, 16)
 	require.NoError(t, err)
-	require.Equal(t, int32(15), index)
+	require.Equal(t, int32(5), index)
 
 	idx := index
 	p = NewIndexValueDispatcher("INDEX1")
