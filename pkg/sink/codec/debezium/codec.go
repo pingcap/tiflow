7d106de8da9901ce028fe3b62417a02043974526
wk989898 <nhsmwk@gmail.com>
2025-07-14 09:56:07 +0000

    chore

--- pkg/sink/codec/debezium/codec.go
+++ pkg/sink/codec/debezium/codec.go
@@ -195,7 +195,8 @@ func (c *dbzCodec) writeDebeziumFieldSchema(
 					writer.WriteInt64Field("default", 0)
 					return
 				}
-				t, err := types.StrToDateTime(types.DefaultStmtNoWarningContext.WithLocation(c.config.TimeZone), v, ft.GetDecimal())
+				// t, err := types.StrToDateTime(types.DefaultStmtNoWarningContext.WithLocation(c.config.TimeZone), v, ft.GetDecimal())
+				t, err := types.StrToDateTime(types.DefaultStmtNoWarningContext, v, ft.GetDecimal())
 				if err != nil {
 					writer.WriteInt64Field("default", 0)
 					return
@@ -238,7 +239,8 @@ func (c *dbzCodec) writeDebeziumFieldSchema(
 					}
 					return
 				}
-				t, err := types.StrToDateTime(types.DefaultStmtNoWarningContext.WithLocation(c.config.TimeZone), v, ft.GetDecimal())
+				// t, err := types.StrToDateTime(types.DefaultStmtNoWarningContext.WithLocation(c.config.TimeZone), v, ft.GetDecimal())
+				t, err := types.StrToDateTime(types.DefaultStmtNoWarningContext, v, ft.GetDecimal())
 				if err != nil {
 					writer.WriteInt64Field("default", 0)
 					return
@@ -614,7 +616,8 @@ func (c *dbzCodec) writeDebeziumFieldValue(
 			writer.WriteInt64Field(col.GetName(), 0)
 			return nil
 		}
-		t, err := types.StrToDateTime(types.DefaultStmtNoWarningContext.WithLocation(c.config.TimeZone), v, ft.GetDecimal())
+		// t, err := types.StrToDateTime(types.DefaultStmtNoWarningContext.WithLocation(c.config.TimeZone), v, ft.GetDecimal())
+		t, err := types.StrToDateTime(types.DefaultStmtNoWarningContext, v, ft.GetDecimal())
 		if err != nil {
 			return cerror.WrapError(
 				cerror.ErrDebeziumEncodeFailed,
