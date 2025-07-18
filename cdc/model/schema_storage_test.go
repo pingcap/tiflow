af05c5f30673b3eec09eb19dcda3e72dce98434f
nhsmw <nhsmwk@gmail.com>
2025-07-18 18:25:43 +0800

    This is an automated cherry-pick of #12254
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- cdc/model/schema_storage_test.go
+++ cdc/model/schema_storage_test.go
@@ -16,10 +16,19 @@ package model
 import (
 	"testing"
 
+<<<<<<< HEAD
 	pmodel "github.com/pingcap/tidb/parser/model"
 	timodel "github.com/pingcap/tidb/parser/model"
 	"github.com/pingcap/tidb/parser/mysql"
 	parser_types "github.com/pingcap/tidb/parser/types"
+=======
+	timodel "github.com/pingcap/tidb/pkg/meta/model"
+	pmodel "github.com/pingcap/tidb/pkg/parser/ast"
+	"github.com/pingcap/tidb/pkg/parser/charset"
+	"github.com/pingcap/tidb/pkg/parser/mysql"
+	parser_types "github.com/pingcap/tidb/pkg/parser/types"
+	"github.com/pingcap/tiflow/pkg/errors"
+>>>>>>> 35ff0a22ec (sink(ticdc): check virtual columns in column dispatcher (#12254))
 	"github.com/stretchr/testify/require"
 )
 
@@ -272,6 +281,252 @@ func TestTableInfoClone(t *testing.T) {
 	require.Equal(t, int64(10), info.SchemaID)
 }
 
+<<<<<<< HEAD
+=======
+func TestIndexByName(t *testing.T) {
+	tableInfo := &TableInfo{
+		TableInfo: &timodel.TableInfo{
+			Indices: nil,
+		},
+	}
+	names, offsets, ok := tableInfo.IndexByName("idx1")
+	require.False(t, ok)
+	require.Nil(t, names)
+	require.Nil(t, offsets)
+
+	tableInfo = &TableInfo{
+		TableInfo: &timodel.TableInfo{
+			Indices: []*timodel.IndexInfo{
+				{
+					Name: pmodel.NewCIStr("idx1"),
+					Columns: []*timodel.IndexColumn{
+						{
+							Name: pmodel.NewCIStr("col1"),
+						},
+					},
+				},
+			},
+		},
+	}
+
+	names, offsets, ok = tableInfo.IndexByName("idx2")
+	require.False(t, ok)
+	require.Nil(t, names)
+	require.Nil(t, offsets)
+
+	names, offsets, ok = tableInfo.IndexByName("idx1")
+	require.True(t, ok)
+	require.Equal(t, []string{"col1"}, names)
+	require.Equal(t, []int{0}, offsets)
+
+	names, offsets, ok = tableInfo.IndexByName("IDX1")
+	require.True(t, ok)
+	require.Equal(t, []string{"col1"}, names)
+	require.Equal(t, []int{0}, offsets)
+
+	names, offsets, ok = tableInfo.IndexByName("Idx1")
+	require.True(t, ok)
+	require.Equal(t, []string{"col1"}, names)
+	require.Equal(t, []int{0}, offsets)
+}
+
+func TestColumnsByNames(t *testing.T) {
+	tableInfo := WrapTableInfo(100, "test", 100, &timodel.TableInfo{
+		Columns: []*timodel.ColumnInfo{
+			{
+				Name: pmodel.NewCIStr("col2"),
+				ID:   1,
+			},
+			{
+				Name: pmodel.NewCIStr("col1"),
+				ID:   0,
+			},
+			{
+				Name: pmodel.NewCIStr("col3"),
+				ID:   2,
+			},
+			{
+				Name:                pmodel.NewCIStr("col4"),
+				ID:                  3,
+				GeneratedExprString: "generated",
+			},
+		},
+	})
+
+	names := []string{"col1", "col2", "col3"}
+	offsets, err := tableInfo.OffsetsByNames(names)
+	require.NoError(t, err)
+	require.Equal(t, []int{1, 0, 2}, offsets)
+
+	names = []string{"col2"}
+	offsets, err = tableInfo.OffsetsByNames(names)
+	require.NoError(t, err)
+	require.Equal(t, []int{0}, offsets)
+
+	names = []string{"col1", "col-not-found"}
+	offsets, err = tableInfo.OffsetsByNames(names)
+	require.ErrorIs(t, err, errors.ErrDispatcherFailed)
+	require.ErrorContains(t, err, "columns not found")
+	require.Nil(t, offsets)
+
+	names = []string{"Col1", "COL2", "CoL3"}
+	offsets, err = tableInfo.OffsetsByNames(names)
+	require.NoError(t, err)
+	require.Equal(t, []int{1, 0, 2}, offsets)
+
+	names = []string{"Col4"}
+	offsets, err = tableInfo.OffsetsByNames(names)
+	require.ErrorIs(t, err, errors.ErrDispatcherFailed)
+	require.ErrorContains(t, err, "found virtual generated columns")
+	require.Nil(t, offsets)
+}
+
+func TestBuildTiDBTableInfoWithIntPrimaryKey(t *testing.T) {
+	columns := []*Column{{
+		Name: "a1",
+		Type: mysql.TypeLong,
+		Flag: BinaryFlag | PrimaryKeyFlag | HandleKeyFlag,
+	}, {
+		Name:      "a2",
+		Type:      mysql.TypeVarchar,
+		Collation: charset.CollationUTF8,
+	}, {
+		Name:    "a4",
+		Type:    mysql.TypeTinyBlob,
+		Charset: charset.CharsetLatin1,
+	}}
+	tidbTableInfo := BuildTiDBTableInfo("t", columns, [][]int{{0}})
+	tableInfo := WrapTableInfo(100, "test", 1000, tidbTableInfo)
+	require.Equal(t, "test", tableInfo.TableName.Schema)
+	require.Equal(t, "t", tableInfo.TableName.Table)
+	require.Equal(t, 3, len(tableInfo.columnsOffset))
+	require.Equal(t, 3, len(tableInfo.Columns))
+
+	require.Equal(t, tableInfo.Columns[0].ID, tableInfo.ForceGetColumnIDByName("a1"))
+	require.Equal(t, columns[0].Name, tableInfo.ForceGetColumnName(tableInfo.Columns[0].ID))
+	require.Equal(t, columns[0].Type, tableInfo.ForceGetColumnInfo(tableInfo.Columns[0].ID).GetType())
+	require.Equal(t, "binary", tableInfo.ForceGetColumnInfo(tableInfo.Columns[0].ID).GetCharset())
+	require.Equal(t, mysql.UTF8MB4DefaultCollation, tableInfo.ForceGetColumnInfo(tableInfo.Columns[0].ID).GetCollate())
+	require.Equal(t, columns[0].Flag, *tableInfo.ForceGetColumnFlagType(tableInfo.Columns[0].ID))
+
+	require.Equal(t, columns[1].Name, tableInfo.ForceGetColumnName(tableInfo.Columns[1].ID))
+	require.Equal(t, columns[1].Type, tableInfo.ForceGetColumnInfo(tableInfo.Columns[1].ID).GetType())
+	require.Equal(t, mysql.UTF8MB4Charset, tableInfo.ForceGetColumnInfo(tableInfo.Columns[1].ID).GetCharset())
+	require.Equal(t, charset.CollationUTF8, tableInfo.ForceGetColumnInfo(tableInfo.Columns[1].ID).GetCollate())
+	require.Equal(t, columns[1].Flag, *tableInfo.ForceGetColumnFlagType(tableInfo.Columns[1].ID))
+
+	require.Equal(t, columns[2].Name, tableInfo.ForceGetColumnName(tableInfo.Columns[2].ID))
+	require.Equal(t, columns[2].Type, tableInfo.ForceGetColumnInfo(tableInfo.Columns[2].ID).GetType())
+	require.Equal(t, charset.CharsetLatin1, tableInfo.ForceGetColumnInfo(tableInfo.Columns[2].ID).GetCharset())
+	require.Equal(t, mysql.UTF8MB4DefaultCollation, tableInfo.ForceGetColumnInfo(tableInfo.Columns[2].ID).GetCollate())
+	require.Equal(t, columns[2].Flag, *tableInfo.ForceGetColumnFlagType(tableInfo.Columns[2].ID))
+}
+
+func TestBuildTiDBTableInfoWithCommonPrimaryKey(t *testing.T) {
+	columns := []*Column{{
+		Name: "a1",
+		Type: mysql.TypeLong,
+		Flag: BinaryFlag | PrimaryKeyFlag | UniqueKeyFlag | HandleKeyFlag | MultipleKeyFlag,
+	}, {
+		Name:    "a2",
+		Type:    mysql.TypeTinyBlob,
+		Charset: charset.CharsetLatin1,
+		Flag:    UniqueKeyFlag | UnsignedFlag | MultipleKeyFlag,
+	}, {
+		Name: "a4",
+		Type: mysql.TypeVarchar,
+		Flag: PrimaryKeyFlag | UniqueKeyFlag | HandleKeyFlag | MultipleKeyFlag,
+	}, {
+		Name: "a9",
+		Type: mysql.TypeLong,
+		Flag: NullableFlag | UnsignedFlag,
+	}}
+	tidbTableInfo := BuildTiDBTableInfo("t", columns, [][]int{{0, 2}, {0, 1}, {2}})
+	tableInfo := WrapTableInfo(100, "test", 1000, tidbTableInfo)
+	require.Equal(t, "test", tableInfo.TableName.Schema)
+	require.Equal(t, "t", tableInfo.TableName.Table)
+	require.Equal(t, 4, len(tableInfo.columnsOffset))
+	require.Equal(t, 4, len(tableInfo.Columns))
+
+	require.Equal(t, tableInfo.Columns[0].ID, tableInfo.ForceGetColumnIDByName("a1"))
+	require.Equal(t, columns[0].Name, tableInfo.ForceGetColumnName(tableInfo.Columns[0].ID))
+	require.Equal(t, columns[0].Type, tableInfo.ForceGetColumnInfo(tableInfo.Columns[0].ID).GetType())
+	require.Equal(t, "binary", tableInfo.ForceGetColumnInfo(tableInfo.Columns[0].ID).GetCharset())
+	require.Equal(t, mysql.UTF8MB4DefaultCollation, tableInfo.ForceGetColumnInfo(tableInfo.Columns[0].ID).GetCollate())
+	require.Equal(t, columns[0].Flag, *tableInfo.ForceGetColumnFlagType(tableInfo.Columns[0].ID))
+
+	require.Equal(t, columns[1].Name, tableInfo.ForceGetColumnName(tableInfo.Columns[1].ID))
+	require.Equal(t, columns[1].Type, tableInfo.ForceGetColumnInfo(tableInfo.Columns[1].ID).GetType())
+	require.Equal(t, charset.CharsetLatin1, tableInfo.ForceGetColumnInfo(tableInfo.Columns[1].ID).GetCharset())
+	require.Equal(t, mysql.UTF8MB4DefaultCollation, tableInfo.ForceGetColumnInfo(tableInfo.Columns[1].ID).GetCollate())
+	require.Equal(t, columns[1].Flag, *tableInfo.ForceGetColumnFlagType(tableInfo.Columns[1].ID))
+
+	require.Equal(t, columns[2].Name, tableInfo.ForceGetColumnName(tableInfo.Columns[2].ID))
+	require.Equal(t, columns[2].Type, tableInfo.ForceGetColumnInfo(tableInfo.Columns[2].ID).GetType())
+	require.Equal(t, mysql.UTF8MB4Charset, tableInfo.ForceGetColumnInfo(tableInfo.Columns[2].ID).GetCharset())
+	require.Equal(t, mysql.UTF8MB4DefaultCollation, tableInfo.ForceGetColumnInfo(tableInfo.Columns[2].ID).GetCollate())
+	require.Equal(t, columns[2].Flag, *tableInfo.ForceGetColumnFlagType(tableInfo.Columns[2].ID))
+
+	require.Equal(t, columns[3].Name, tableInfo.ForceGetColumnName(tableInfo.Columns[3].ID))
+	require.Equal(t, columns[3].Type, tableInfo.ForceGetColumnInfo(tableInfo.Columns[3].ID).GetType())
+	require.Equal(t, mysql.UTF8MB4Charset, tableInfo.ForceGetColumnInfo(tableInfo.Columns[3].ID).GetCharset())
+	require.Equal(t, mysql.UTF8MB4DefaultCollation, tableInfo.ForceGetColumnInfo(tableInfo.Columns[3].ID).GetCollate())
+	require.Equal(t, columns[3].Flag, *tableInfo.ForceGetColumnFlagType(tableInfo.Columns[3].ID))
+}
+
+func TestBuildTiDBTableInfoWithUniqueKey(t *testing.T) {
+	columns := []*Column{{
+		Name: "a1",
+		Type: mysql.TypeLong,
+		Flag: BinaryFlag | UniqueKeyFlag | HandleKeyFlag | MultipleKeyFlag,
+	}, {
+		Name:    "a2",
+		Type:    mysql.TypeTinyBlob,
+		Charset: charset.CharsetLatin1,
+		Flag:    UniqueKeyFlag | MultipleKeyFlag,
+	}, {
+		Name: "a4",
+		Type: mysql.TypeVarchar,
+		Flag: UniqueKeyFlag | HandleKeyFlag | MultipleKeyFlag,
+	}, {
+		Name: "a9",
+		Type: mysql.TypeLong,
+		Flag: UnsignedFlag | UniqueKeyFlag | MultipleKeyFlag,
+	}}
+	tidbTableInfo := BuildTiDBTableInfo("t", columns, [][]int{{0, 2}, {1, 3}})
+	tableInfo := WrapTableInfo(100, "test", 1000, tidbTableInfo)
+	require.Equal(t, "test", tableInfo.TableName.Schema)
+	require.Equal(t, "t", tableInfo.TableName.Table)
+	require.Equal(t, 4, len(tableInfo.columnsOffset))
+	require.Equal(t, 4, len(tableInfo.Columns))
+
+	require.Equal(t, tableInfo.Columns[0].ID, tableInfo.ForceGetColumnIDByName("a1"))
+	require.Equal(t, columns[0].Name, tableInfo.ForceGetColumnName(tableInfo.Columns[0].ID))
+	require.Equal(t, columns[0].Type, tableInfo.ForceGetColumnInfo(tableInfo.Columns[0].ID).GetType())
+	require.Equal(t, "binary", tableInfo.ForceGetColumnInfo(tableInfo.Columns[0].ID).GetCharset())
+	require.Equal(t, mysql.UTF8MB4DefaultCollation, tableInfo.ForceGetColumnInfo(tableInfo.Columns[0].ID).GetCollate())
+	require.Equal(t, columns[0].Flag, *tableInfo.ForceGetColumnFlagType(tableInfo.Columns[0].ID))
+
+	require.Equal(t, columns[1].Name, tableInfo.ForceGetColumnName(tableInfo.Columns[1].ID))
+	require.Equal(t, columns[1].Type, tableInfo.ForceGetColumnInfo(tableInfo.Columns[1].ID).GetType())
+	require.Equal(t, charset.CharsetLatin1, tableInfo.ForceGetColumnInfo(tableInfo.Columns[1].ID).GetCharset())
+	require.Equal(t, mysql.UTF8MB4DefaultCollation, tableInfo.ForceGetColumnInfo(tableInfo.Columns[1].ID).GetCollate())
+	require.Equal(t, columns[1].Flag, *tableInfo.ForceGetColumnFlagType(tableInfo.Columns[1].ID))
+
+	require.Equal(t, columns[2].Name, tableInfo.ForceGetColumnName(tableInfo.Columns[2].ID))
+	require.Equal(t, columns[2].Type, tableInfo.ForceGetColumnInfo(tableInfo.Columns[2].ID).GetType())
+	require.Equal(t, mysql.UTF8MB4Charset, tableInfo.ForceGetColumnInfo(tableInfo.Columns[2].ID).GetCharset())
+	require.Equal(t, mysql.UTF8MB4DefaultCollation, tableInfo.ForceGetColumnInfo(tableInfo.Columns[2].ID).GetCollate())
+	require.Equal(t, columns[2].Flag, *tableInfo.ForceGetColumnFlagType(tableInfo.Columns[2].ID))
+
+	require.Equal(t, columns[3].Name, tableInfo.ForceGetColumnName(tableInfo.Columns[3].ID))
+	require.Equal(t, columns[3].Type, tableInfo.ForceGetColumnInfo(tableInfo.Columns[3].ID).GetType())
+	require.Equal(t, mysql.UTF8MB4Charset, tableInfo.ForceGetColumnInfo(tableInfo.Columns[3].ID).GetCharset())
+	require.Equal(t, mysql.UTF8MB4DefaultCollation, tableInfo.ForceGetColumnInfo(tableInfo.Columns[3].ID).GetCollate())
+	require.Equal(t, columns[3].Flag, *tableInfo.ForceGetColumnFlagType(tableInfo.Columns[3].ID))
+}
+
+>>>>>>> 35ff0a22ec (sink(ticdc): check virtual columns in column dispatcher (#12254))
 func TestBuildTiDBTableInfoWithVirtualColumns(t *testing.T) {
 	t.Parallel()
 	ftNull := parser_types.NewFieldType(mysql.TypeUnspecified)
