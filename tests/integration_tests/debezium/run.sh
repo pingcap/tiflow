a03a9517401d83b7eda5eea1b33eafbe78308740
nhsmw <nhsmwk@gmail.com>
2025-12-10 13:58:51 +0800

    Update run.sh

--- tests/integration_tests/debezium/run.sh
+++ tests/integration_tests/debezium/run.sh
@@ -55,8 +55,7 @@ run_cdc_cli changefeed create -c test --sink-uri="kafka://127.0.0.1:9092/output_
 cd $CUR
 go run ./src
 
-if [ $? -ne 0 ]; then
-	exit 1
-fi
+
+exit 1
 
 echo "[$(date)] <<<<<< run test case $TEST_NAME success! >>>>>>"
