186c05e4da20a94045392fe81df537e7fd75a1c4
Ruihao Chen <joechenrh@gmail.com>
2026-02-28 04:14:05 -0500

    sync-diff-inspector: backport code

---
+++ sync_diff_inspector/source/common/table_diff.go
@@ -0,0 +1,110 @@
+// Copyright 2021 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package common
+
+import (
+	"database/sql"
+
+	"github.com/pingcap/tidb/pkg/meta/model"
+	"github.com/pingcap/tidb/pkg/util/collate"
+)
+
+// TableShardSource represents the origin schema and table and DB connection before router.
+// It used for MySQL Shard source.
+type TableShardSource struct {
+	TableSource
+	// DBConn represents the origin DB connection for this TableSource.
+	// This TableSource may exists in different MySQL shard.
+	DBConn *sql.DB
+}
+
+// TableSource represents the origin schema and table before router.
+// It used for TiDB/MySQL source.
+type TableSource struct {
+	OriginSchema string
+	OriginTable  string
+}
+
+// TableDiff saves config for diff table
+type TableDiff struct {
+	// Schema represents the database name.
+	Schema string `json:"schema"`
+
+	// Table represents the table name.
+	Table string `json:"table"`
+
+	// Info is the parser.TableInfo, include some meta infos for this table.
+	// It used for TiDB/MySQL/MySQL Shard sources.
+	Info *model.TableInfo `json:"info"`
+
+	// columns be ignored
+	IgnoreColumns []string `json:"-"`
+
+	// field should be the primary key, unique key or field with index
+	Fields string `json:"fields"`
+
+	// select range, for example: "age > 10 AND age < 20"
+	Range string `json:"range"`
+
+	// ignore check table's data
+	IgnoreDataCheck bool `json:"-"`
+
+	// the table has column timestamp, which need to reset time_zone.
+	NeedUnifiedTimeZone bool `json:"-"`
+
+	Collation string `json:"collation"`
+
+	ChunkSize int64 `json:"chunk-size"`
+
+	// TableLack = 1: the table only exists downstream,
+	// TableLack = -1: the table only exists upstream,
+	// TableLack = 0: the table exists both upstream and downstream.
+	TableLack int `json:"-"`
+}
+
+const (
+	// AllTableExistFlag means the table exists in both upstream and downstream
+	AllTableExistFlag = 0
+	// DownstreamTableLackFlag means the table only exists in upstream
+	DownstreamTableLackFlag = -1
+	// UpstreamTableLackFlag means the table only exists in downstream
+	UpstreamTableLackFlag = 1
+)
+
+// AllTableExist check the status
+func AllTableExist(tableLack int) bool {
+	return tableLack == AllTableExistFlag
+}
+
+// GetCollatorsForTable returns collators for the table based on its collation and order by columns.
+// Exported for test.
+func GetCollatorsForTable(table *TableDiff, orderByCols []*model.ColumnInfo) []collate.Collator {
+	collators := make([]collate.Collator, 0, len(orderByCols))
+
+	// If collation is set in the config, use it.
+	// Otherwise, use the default collator for each column.
+	for _, col := range orderByCols {
+		var collation string
+		if table.Collation != "" {
+			collation = table.Collation
+		} else if col.FieldType.GetCollate() != "" {
+			collation = col.FieldType.GetCollate()
+		} else {
+			collation = table.Info.Collate
+		}
+		collators = append(collators, collate.GetCollator(collation))
+	}
+
+	return collators
+}
