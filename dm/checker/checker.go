97d6225472312e8d7184c992f16d176c33d3e905
nhsmw <nhsmwk@gmail.com>
2026-06-10 17:57:16 +0800

    *: update tidb and parser (#12674)
    close pingcap/tiflow#12672

--- dm/checker/checker.go
+++ dm/checker/checker.go
@@ -25,11 +25,12 @@ import (
 
 	_ "github.com/go-sql-driver/mysql" // for mysql
 	"github.com/pingcap/tidb/dumpling/export"
+	"github.com/pingcap/tidb/lightning/pkg/checkpoints"
 	"github.com/pingcap/tidb/lightning/pkg/importer"
 	"github.com/pingcap/tidb/lightning/pkg/importer/opts"
 	"github.com/pingcap/tidb/lightning/pkg/precheck"
-	"github.com/pingcap/tidb/pkg/lightning/checkpoints"
 	"github.com/pingcap/tidb/pkg/lightning/common"
+	"github.com/pingcap/tidb/pkg/lightning/importdef"
 	"github.com/pingcap/tidb/pkg/lightning/mydump"
 	"github.com/pingcap/tidb/pkg/parser/mysql"
 	"github.com/pingcap/tidb/pkg/types"
@@ -878,7 +879,7 @@ func sameTableNameDetection(tables map[filter.Table][]filter.Table) error {
 // lightningPrecheckAdaptor implements the importer.PreRestoreInfoGetter interface.
 type lightningPrecheckAdaptor struct {
 	importer.TargetInfoGetter
-	allTables        map[string]*checkpoints.TidbDBInfo
+	allTables        map[string]*importdef.DBInfo
 	sourceDataResult importer.EstimateSourceDataSizeResult
 }
 
@@ -888,18 +889,18 @@ func newLightningPrecheckAdaptor(
 ) *lightningPrecheckAdaptor {
 	var (
 		sourceDataResult importer.EstimateSourceDataSizeResult
-		allTables        = make(map[string]*checkpoints.TidbDBInfo)
+		allTables        = make(map[string]*importdef.DBInfo)
 	)
 	if info != nil {
 		sourceDataResult.SizeWithIndex = info.totalDataSize.Load()
 	}
 	for db, tables := range info.db2TargetTables {
-		allTables[db] = &checkpoints.TidbDBInfo{
+		allTables[db] = &importdef.DBInfo{
 			Name:   db,
-			Tables: make(map[string]*checkpoints.TidbTableInfo),
+			Tables: make(map[string]*importdef.TableInfo),
 		}
 		for _, table := range tables {
-			allTables[db].Tables[table.Name] = &checkpoints.TidbTableInfo{
+			allTables[db].Tables[table.Name] = &importdef.TableInfo{
 				DB:   db,
 				Name: table.Name,
 			}
@@ -912,7 +913,7 @@ func newLightningPrecheckAdaptor(
 	}
 }
 
-func (l *lightningPrecheckAdaptor) GetAllTableStructures(ctx context.Context, opts ...opts.GetPreInfoOption) (map[string]*checkpoints.TidbDBInfo, error) {
+func (l *lightningPrecheckAdaptor) GetAllTableStructures(ctx context.Context, opts ...opts.GetPreInfoOption) (map[string]*importdef.DBInfo, error) {
 	// re-use with other checker? or in fact we only use other information than structure?
 	return l.allTables, nil
 }
